b593295c56f49d5ba72d1e915c96c53c76889844
dkoval <koval.dmitriy@gmail.com>
2020-05-04 23:33:05 +0200

    Solution to "Best Time to Buy and Sell Stock" problem (no limit on the number of transactions done)

---
+++ src/main/kotlin/com/github/dkoval/leetcode/interview/BestTimeToBuyAndSellStock2.kt
@@ -0,0 +1,21 @@
+package com.github.dkoval.leetcode.interview
+
+/**
+ * [Best Time to Buy and Sell Stock II](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/)
+ *
+ * Say you have an array prices for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+ */
+object BestTimeToBuyAndSellStock2 {
+
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        for (i in 1 until prices.size) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1]
+            }
+        }
+        return maxProfit
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/interview/BestTimeToBuyAndSellStock2Test.kt
@@ -0,0 +1,26 @@
+package com.github.dkoval.leetcode.interview
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class BestTimeToBuyAndSellStock2Test {
+
+    companion object {
+        @JvmStatic
+        fun input() = listOf<Arguments>(
+            Arguments.of(intArrayOf(7, 1, 5, 3, 6, 4), 7),
+            Arguments.of(intArrayOf(1, 2, 3, 4, 5), 4),
+            Arguments.of(intArrayOf(7, 6, 4, 3, 1), 0),
+            Arguments.of(intArrayOf(6, 1, 3, 2, 4, 7), 7)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    fun `should find the maximum profit`(prices: IntArray, expectedMaxProfit: Int) {
+        val actualMaxProfit = BestTimeToBuyAndSellStock2.maxProfit(prices)
+        assertEquals(expectedMaxProfit, actualMaxProfit)
+    }
+}
