72202fda46eb7e8085716aea2c090e1a86a421be
dkoval <koval.dmitriy@gmail.com>
2020-05-11 23:13:34 +0200

    Solution to "Plus One" problem

---
+++ src/main/kotlin/com/github/dkoval/leetcode/interview/PlusOne.kt
@@ -0,0 +1,37 @@
+package com.github.dkoval.leetcode.interview
+
+/**
+ * [Plus One](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/559/)
+ *
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ */
+object PlusOne {
+
+    fun plusOne(digits: IntArray): IntArray {
+        val result = IntArray(digits.size)
+        var index = digits.lastIndex
+        var overflow: Boolean
+        do {
+            val i = digits[index] + 1
+            result[index] = i % 10
+            overflow = i == 10
+            index--
+        } while (index >= 0 && overflow)
+
+        if (index >= 0) {
+            digits.copyInto(result, startIndex = 0, endIndex = index + 1)
+            return result
+        }
+
+        if (overflow) {
+            val extResult = IntArray(result.size + 1)
+            extResult[0] = 1
+            result.copyInto(extResult, destinationOffset = 1)
+            return extResult
+        }
+
+        return result
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/interview/PlusOneTest.kt
@@ -0,0 +1,27 @@
+package com.github.dkoval.leetcode.interview
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class PlusOneTest {
+
+    companion object {
+        @JvmStatic
+        fun input(): List<Arguments> = listOf(
+            Arguments.of(intArrayOf(1, 2, 3), intArrayOf(1, 2, 4)),
+            Arguments.of(intArrayOf(4, 3, 2, 1), intArrayOf(4, 3, 2, 2)),
+            Arguments.of(intArrayOf(9), intArrayOf(1, 0)),
+            Arguments.of(intArrayOf(9, 9), intArrayOf(1, 0, 0)),
+            Arguments.of(intArrayOf(9, 9, 9), intArrayOf(1, 0, 0, 0))
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    fun `should compute plus one to the integer`(digits: IntArray, expected: IntArray) {
+        val actual = PlusOne.plusOne(digits)
+        assertArrayEquals(expected, actual)
+    }
+}
