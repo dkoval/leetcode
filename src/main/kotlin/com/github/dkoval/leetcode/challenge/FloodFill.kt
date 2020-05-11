7ddb607b3496884c40a25a8052c112a782adbaf7
dkoval <koval.dmitriy@gmail.com>
2020-05-11 16:04:35 +0200

    Add description to "Flood Fill" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/FloodFill.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/FloodFill.kt
@@ -2,6 +2,12 @@ package com.github.dkoval.leetcode.challenge
 
 /**
  * [Flood Fill](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3326/)
+ *
+ * Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor,
+ * "flood fill" the image. To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally
+ * to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels
+ * (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+ * At the end, return the modified image.
  */
 object FloodFill {
 
