1b51521aa767128cddb417d5392de2922c0a726c
dkoval <koval.dmitriy@gmail.com>
2020-05-12 18:25:14 +0200

    Solution to "Move Zeroes" problem

---
+++ src/main/kotlin/com/github/dkoval/leetcode/interview/MoveZeros.kt
@@ -0,0 +1,31 @@
+package com.github.dkoval.leetcode.interview
+
+/**
+ * [Move Zeros](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/)
+ *
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Note:
+ * - You must do this in-place without making a copy of the array.
+ * - Minimize the total number of operations
+ */
+object MoveZeros {
+
+    fun moveZeroes(nums: IntArray) {
+        var i = 0
+        for (j in nums.indices) {
+            if (nums[j] != 0) {
+                nums.swap(i, j)
+                i++
+            }
+        }
+    }
+
+    private fun IntArray.swap(i: Int, j: Int) {
+        if (i != j) {
+            val tmp = this[i]
+            this[i] = this[j]
+            this[j] = tmp
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/interview/MoveZerosTest.kt
@@ -0,0 +1,26 @@
+package com.github.dkoval.leetcode.interview
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class MoveZerosTest {
+
+    companion object {
+        @JvmStatic
+        fun input(): List<Arguments> = listOf(
+            Arguments.of(intArrayOf(0, 1, 0, 3, 12), intArrayOf(1, 3, 12, 0, 0))
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    fun `should move all 0's to the end of it while maintaining the relative order of the non-zero elements`(
+        nums: IntArray,
+        expected: IntArray
+    ) {
+        MoveZeros.moveZeroes(nums)
+        assertArrayEquals(expected, nums)
+    }
+}
