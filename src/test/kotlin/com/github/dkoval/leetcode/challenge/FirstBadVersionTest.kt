bfacc818e1101137fe334b2a44316d9f60629afe
dkoval <koval.dmitriy@gmail.com>
2020-05-22 07:24:52 +0200

    Solution to "First Bad Version" problem

---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/FirstBadVersionTest.kt
@@ -0,0 +1,24 @@
+package com.github.dkoval.leetcode.challenge
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class FirstBadVersionTest {
+
+    companion object {
+        @JvmStatic
+        fun input(): List<Arguments> = listOf(
+            Arguments.of(5, 4),
+            Arguments.of(10, 4)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    fun `should find the first bad version`(n: Int, expected: Int) {
+        val actual = FirstBadVersion { version -> version >= expected }.firstBadVersion(n)
+        assertEquals(expected, actual)
+    }
+}
