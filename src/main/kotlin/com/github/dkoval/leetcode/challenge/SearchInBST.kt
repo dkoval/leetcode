b08f54cc8bba7ec4c2fe977f57fb16cceee548e5
dkoval <koval.dmitriy@gmail.com>
2020-06-15 10:08:53 +0200

    Document "Search in a Binary Search Tree" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/SearchInBST.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/SearchInBST.kt
@@ -2,6 +2,12 @@ package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
 
+/**
+ * [Search in a Binary Search Tree](https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3361/)
+ *
+ * Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value
+ * equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+ */
 object SearchInBST {
 
     fun searchBST(root: TreeNode?, `val`: Int): TreeNode? = when {
