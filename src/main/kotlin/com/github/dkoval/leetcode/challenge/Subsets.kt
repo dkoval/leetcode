a5ca6aa7323c1fa6cbc599157a7a64a9d87cda6a
dkoval <koval.dmitriy@gmail.com>
2020-07-12 01:51:01 +0200

    Add in iterative and recursive solutions to "Subsets" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/Subsets.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/Subsets.kt
@@ -6,9 +6,14 @@ package com.github.dkoval.leetcode.challenge
  * Given a set of distinct integers, nums, return all possible subsets (the power set).
  * Note: The solution set must not contain duplicate subsets.
  */
-object Subsets {
+interface Subsets {
 
-    fun subsets(nums: IntArray): List<List<Int>> {
+    fun subsets(nums: IntArray): List<List<Int>>
+}
+
+object SubsetsBitmask: Subsets {
+
+    override fun subsets(nums: IntArray): List<List<Int>> {
         val result = mutableListOf<List<Int>>()
         val numSubsets = 1 shl nums.size // 2 ^ nums.size
         for (i in 0 until numSubsets) {
@@ -25,6 +30,41 @@ object Subsets {
             }
             result.add(subset)
         }
+        return result
+    }
+}
+
+object SubsetsIter: Subsets {
+
+    override fun subsets(nums: IntArray): List<List<Int>> {
+        val result = mutableListOf(emptyList<Int>())
+        for (num in nums) {
+            val subsetsToAdd = mutableListOf<List<Int>>()
+            for (subset in result) {
+                val subsetToAdd = subset + num
+                subsetsToAdd.add(subsetToAdd)
+            }
+            result.addAll(subsetsToAdd)
+        }
+        return result
+    }
+}
+
+object SubsetsRecursive: Subsets {
+
+    override fun subsets(nums: IntArray): List<List<Int>> = doSubsets(nums, 0)
+
+    private fun doSubsets(nums: IntArray, startIndex: Int): MutableList<List<Int>> {
+        if (startIndex == nums.lastIndex) {
+            return mutableListOf(emptyList(), listOf(nums.last()))
+        }
+        val result = doSubsets(nums, startIndex + 1)
+        val subsetsToAdd = mutableListOf<List<Int>>()
+        for (subset in result) {
+            val subsetToAdd = mutableListOf(nums[startIndex]).apply { addAll(subset) }
+            subsetsToAdd.add(subsetToAdd)
+        }
+        result.addAll(subsetsToAdd)
         return result
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/SubsetsTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/SubsetsTest.kt
@@ -1,35 +1,68 @@
 package com.github.dkoval.leetcode.challenge
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class SubsetsTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
-            Arguments.of(
-                intArrayOf(1, 2, 3),
-                listOf(
-                    listOf(3),
-                    listOf(1),
-                    listOf(2),
-                    listOf(1, 2, 3),
-                    listOf(1, 3),
-                    listOf(2, 3),
-                    listOf(1, 2),
-                    emptyList()
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> =
+            Stream.of(
+                Arguments.of(
+                    intArrayOf(1, 2, 3),
+                    listOf(
+                        listOf(3),
+                        listOf(1),
+                        listOf(2),
+                        listOf(1, 2, 3),
+                        listOf(1, 3),
+                        listOf(2, 3),
+                        listOf(1, 2),
+                        emptyList()
+                    )
                 )
             )
-        )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return all possible subsets`(nums: IntArray, expected: List<List<Int>>) {
-        val actual = Subsets.subsets(nums)
+    @Nested
+    inner class SubsetsBitmaskTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return all possible subsets`(nums: IntArray, expected: List<List<Int>>) {
+            SubsetsBitmask.test(nums, expected)
+        }
+    }
+
+    @Nested
+    inner class SubsetsIterTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return all possible subsets`(nums: IntArray, expected: List<List<Int>>) {
+            SubsetsIter.test(nums, expected)
+        }
+    }
+
+    @Nested
+    inner class SubsetsRecursiveTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return all possible subsets`(nums: IntArray, expected: List<List<Int>>) {
+            SubsetsRecursive.test(nums, expected)
+        }
+    }
+
+    private fun Subsets.test(nums: IntArray, expected: List<List<Int>>) {
+        val actual = subsets(nums)
         assertThat(actual).containsExactlyInAnyOrderElementsOf(expected)
     }
 }
