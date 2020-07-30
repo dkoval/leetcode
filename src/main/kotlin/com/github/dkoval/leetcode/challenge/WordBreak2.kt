bc28dc0885ed056ee1b50dcaee947cf29170b1d9
dkoval <koval.dmitriy@gmail.com>
2020-07-30 23:48:37 +0200

    Refactor solution to "Word Break II" problem - make use of Kotlin nested functions

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/WordBreak2.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/WordBreak2.kt
@@ -14,27 +14,29 @@ package com.github.dkoval.leetcode.challenge
 object WordBreak2 {
 
     // Resource: https://www.youtube.com/watch?v=9-grHHGUVls
-    fun wordBreak(s: String, wordDict: List<String>): List<String> = doWordBreak(s, wordDict, mutableMapOf())
-
-    private fun doWordBreak(s: String, wordDict: List<String>, dp: MutableMap<String, List<String>>): List<String> {
-        val cachedResult = dp[s]
-        if (cachedResult != null) {
-            return cachedResult
-        }
-        val result = mutableListOf<String>()
-        for (word in wordDict) {
-            if (s.startsWith(word)) {
-                if (word.length == s.length) {
-                    result.add(word)
-                } else {
-                    val tmp = doWordBreak(s.substring(word.length), wordDict, dp)
-                    for (elem in tmp) {
-                        result.add("$word $elem")
+    fun wordBreak(s: String, wordDict: List<String>): List<String> {
+        val dp = mutableMapOf<String, List<String>>()
+        fun doWordBreak(s: String): List<String> {
+            val cachedResult = dp[s]
+            if (cachedResult != null) {
+                return cachedResult
+            }
+            val result = mutableListOf<String>()
+            for (word in wordDict) {
+                if (s.startsWith(word)) {
+                    if (word.length == s.length) {
+                        result.add(word)
+                    } else {
+                        val tmp = doWordBreak(s.substring(word.length))
+                        for (elem in tmp) {
+                            result.add("$word $elem")
+                        }
                     }
                 }
             }
+            dp[s] = result
+            return result
         }
-        dp[s] = result
-        return result
+        return doWordBreak(s)
     }
 }
