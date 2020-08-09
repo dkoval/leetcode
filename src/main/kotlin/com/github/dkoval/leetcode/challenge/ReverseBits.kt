d242d8a6f051e2947ec9b7e806ad71940bb53031
dkoval <koval.dmitriy@gmail.com>
2020-08-09 12:09:18 +0200

    Code cleanup

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/ReverseBits.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/ReverseBits.kt
@@ -32,8 +32,8 @@ object ReverseBitsMaskAndShift: ReverseBits {
         var result = (n ushr 16) or (n shl 16)
         result = ((result and 0xff00ff00.toInt()) ushr 8) or ((result and 0x00ff00ff) shl 8)
         result = ((result and 0xf0f0f0f0.toInt()) ushr 4) or ((result and 0x0f0f0f0f) shl 4)
-        result = ((result and 0xcccccccc.toInt()) ushr 2) or ((result and 0x33333333) shl 2);
-        result = ((result and 0xaaaaaaaa.toInt()) ushr 1) or ((result and 0x55555555) shl 1);
+        result = ((result and 0xcccccccc.toInt()) ushr 2) or ((result and 0x33333333) shl 2)
+        result = ((result and 0xaaaaaaaa.toInt()) ushr 1) or ((result and 0x55555555) shl 1)
         return result
     }
 }
