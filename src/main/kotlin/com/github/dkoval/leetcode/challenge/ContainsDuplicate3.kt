31c4313b67e7c20df6a3e4a2a532fb8da3941c82
dkoval <koval.dmitriy@gmail.com>
2020-09-02 23:46:22 +0200

    Solution to "Contains Duplicate III" problem

---
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/ContainsDuplicate3.kt
@@ -0,0 +1,51 @@
+package com.github.dkoval.leetcode.challenge
+
+import java.util.*
+import kotlin.math.abs
+
+/**
+ * [Contains Duplicate III](https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3446/)
+ *
+ * Given an array of integers, find out whether there are two distinct indices `i` and `j` in the array such that
+ * the absolute difference between ```nums[i]``` and ```nums[j]``` is at most `t` and
+ * the absolute difference between `i` and `j` is at most `k`.
+ */
+interface ContainsDuplicate3 {
+
+    fun containsNearbyAlmostDuplicate(nums: IntArray, k: Int, t: Int): Boolean
+}
+
+object ContainsDuplicate3BruteForce : ContainsDuplicate3 {
+
+    override fun containsNearbyAlmostDuplicate(nums: IntArray, k: Int, t: Int): Boolean {
+        for (i in 0 until nums.size - 1) {
+            for (j in i + 1 until nums.size) {
+                if (abs(nums[i].toLong() - nums[j].toLong()) <= t && abs(i - j) <= k) return true
+            }
+        }
+        return false
+    }
+}
+
+// Time complexity: O(N * logK)
+object ContainsDuplicate3UsingTreeSet : ContainsDuplicate3 {
+
+    override fun containsNearbyAlmostDuplicate(nums: IntArray, k: Int, t: Int): Boolean {
+        // the problem statement | nums[i] - nums[j] | <= t inequality reduces to 2 cases:
+        // 1. nums[i] - nums[j] <= t  <-> nums[i] <= nums[j] + t
+        // 2. nums[i] - nums[j] >= -t <-> nums[i] >= nums[j] - t
+        val kElements = TreeSet<Long>()
+        for (i in nums.indices) {
+            val num = nums[i].toLong() // to prevent Int overflow
+            val floor = kElements.floor(num + t)
+            if (floor != null && nums[i] <= floor) return true
+            val ceiling = kElements.ceiling(num - t)
+            if (ceiling != null && nums[i] >= ceiling) return true
+            kElements.add(num)
+            if (i >= k) {
+                kElements.remove(nums[i - k].toLong())
+            }
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ContainsDuplicate3Test.kt
@@ -0,0 +1,71 @@
+package com.github.dkoval.leetcode.challenge
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class ContainsDuplicate3Test {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 3, 1), 3, 0, true
+            ),
+            Arguments.of(
+                intArrayOf(1, 0, 1, 1), 1, 2, true
+            ),
+            Arguments.of(
+                intArrayOf(1, 5, 9, 1, 5, 9), 2, 3, false
+            ),
+            Arguments.of(
+                intArrayOf(-1, 2147483647),
+                1,
+                2147483647,
+                false
+            ),
+            Arguments.of(
+                intArrayOf(0),
+                0,
+                0,
+                false
+            ),
+            Arguments.of(
+                intArrayOf(2, 2),
+                3,
+                0,
+                true
+            )
+        )
+    }
+
+    @Nested
+    inner class ContainsDuplicate3BruteForceTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should test containsNearbyAlmostDuplicate() method`(nums: IntArray, k: Int, t: Int, expected: Boolean) {
+            ContainsDuplicate3BruteForce.test(nums, k, t, expected)
+        }
+    }
+
+    @Nested
+    inner class ContainsDuplicate3UsingTreeSetTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should test containsNearbyAlmostDuplicate() method`(nums: IntArray, k: Int, t: Int, expected: Boolean) {
+            ContainsDuplicate3UsingTreeSet.test(nums, k, t, expected)
+        }
+    }
+
+    private fun ContainsDuplicate3.test(nums: IntArray, k: Int, t: Int, expected: Boolean) {
+        val actual = containsNearbyAlmostDuplicate(nums, k, t)
+        assertEquals(expected, actual)
+    }
+}
