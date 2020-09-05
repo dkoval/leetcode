a481a8f81c4deeaf3984c1aa9aae7cdbb0e649a9
dkoval <koval.dmitriy@gmail.com>
2020-09-06 00:05:27 +0200

    Optimize solution to "All Elements in Two Binary Search Trees" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/AllElementsInTwoBinarySearchTrees.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/AllElementsInTwoBinarySearchTrees.kt
@@ -9,22 +9,55 @@ import com.github.dkoval.leetcode.TreeNode
  *
  * Return a list containing all the integers from both trees sorted in ascending order.
  */
-object AllElementsInTwoBinarySearchTrees {
+interface AllElementsInTwoBinarySearchTrees {
 
-    fun getAllElements(root1: TreeNode?, root2: TreeNode?): List<Int> {
-        var result = mutableListOf<Int>()
-        result = traverseInorder(root1, result)
-        result = traverseInorder(root2, result)
-        return result.apply { sort() }
+    fun getAllElements(root1: TreeNode?, root2: TreeNode?): List<Int>
+}
+
+// Space complexity: O(M + N), space complexity: O(M + N) - required to merge 2 sorted lists
+object AllElementsInTwoBinarySearchTreesUsingInorderTraversalWithMerge : AllElementsInTwoBinarySearchTrees {
+
+    override fun getAllElements(root1: TreeNode?, root2: TreeNode?): List<Int> {
+        // Inorder traversal of a BST generates a list sorted in ascending order
+        val list1 = traverseInorder(root1)
+        val list2 = traverseInorder(root2)
+        return mergeSortedLists(list1, list2)
     }
 
-    private fun traverseInorder(root: TreeNode?, result: MutableList<Int>): MutableList<Int> {
+    private fun traverseInorder(root: TreeNode?): List<Int> {
         fun doTraverseInorder(root: TreeNode?, result: MutableList<Int>) {
             if (root == null) return
             doTraverseInorder(root.left, result)
             result.add(root.`val`)
             doTraverseInorder(root.right, result)
         }
-        return result.also { doTraverseInorder(root, it) }
+        return mutableListOf<Int>().also { doTraverseInorder(root, it) }
+    }
+
+    private fun mergeSortedLists(list1: List<Int>, list2: List<Int>): List<Int> {
+        val result = ArrayList<Int>(list1.size + list2.size)
+        var i = 0
+        var j = 0
+        // iterate over the smaller of 2 lists
+        while (i < list1.size && j < list2.size) {
+            if (list1[i] < list2[j]) {
+                result.add(list1[i])
+                i++
+            } else {
+                result.add(list2[j])
+                j++
+            }
+        }
+        // if list1 is larger, append remaining elements to the result
+        while (i < list1.size) {
+            result.add(list1[i])
+            i++
+        }
+        // if list2 is larger, append remaining elements to the result
+        while (j < list2.size) {
+            result.add(list2[j])
+            j++
+        }
+        return result
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/AllElementsInTwoBinarySearchTreesTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/AllElementsInTwoBinarySearchTreesTest.kt
@@ -2,15 +2,19 @@ package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class AllElementsInTwoBinarySearchTreesTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 TreeNode(2).apply {
                     left = TreeNode(1)
@@ -47,17 +51,26 @@ internal class AllElementsInTwoBinarySearchTreesTest {
                 },
                 listOf(1, 1, 8, 8)
             )
+
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return a list containing all the integers from both trees sorted in ascending order`(
-        root1: TreeNode?,
-        root2: TreeNode?,
-        expected: List<Int>
-    ) {
-        val actual = AllElementsInTwoBinarySearchTrees.getAllElements(root1, root2)
+    @Nested
+    inner class AllElementsInTwoBinarySearchTreesUsingInorderTraversalWithMergeTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return a list containing all the integers from both trees sorted in ascending order`(
+            root1: TreeNode?,
+            root2: TreeNode?,
+            expected: List<Int>
+        ) {
+            AllElementsInTwoBinarySearchTreesUsingInorderTraversalWithMerge.test(root1, root2, expected)
+        }
+    }
+
+    private fun AllElementsInTwoBinarySearchTrees.test(root1: TreeNode?, root2: TreeNode?, expected: List<Int>) {
+        val actual = getAllElements(root1, root2)
         assertEquals(expected, actual)
     }
 }
