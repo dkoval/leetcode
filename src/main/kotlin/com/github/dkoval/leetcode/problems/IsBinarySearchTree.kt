7f447d2a994e5335f9550d18563d02d24e084b77
dkoval <koval.dmitriy@gmail.com>
2020-09-06 15:18:47 +0200

    Solution to "Validate Binary Search Tree" problem

---
+++ src/main/kotlin/com/github/dkoval/leetcode/problems/IsBinarySearchTree.kt
@@ -0,0 +1,68 @@
+package com.github.dkoval.leetcode.problems
+
+import com.github.dkoval.leetcode.TreeNode
+
+/**
+ * [Validate Binary Search Tree](https://leetcode.com/problems/validate-binary-search-tree/)
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ * - The left subtree of a node contains only nodes with keys less than the node's key.
+ * - The right subtree of a node contains only nodes with keys greater than the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ */
+interface IsBinarySearchTree {
+
+    fun isValidBST(root: TreeNode?): Boolean
+}
+
+// Time complexity: O(N^2)
+object IsBinarySearchTreeRecursivelyBruteForce : IsBinarySearchTree {
+
+    override fun isValidBST(root: TreeNode?): Boolean {
+        if (root == null) return true
+        if (isSubtreeLesser(root.left, root.`val`)
+            && isSubtreeGreater(root.right, root.`val`)
+            && isValidBST(root.left)
+            && isValidBST(root.right)
+        ) return true
+        return false
+    }
+
+    private fun isSubtreeLesser(root: TreeNode?, value: Int): Boolean {
+        if (root == null) return true
+        if (root.`val` < value
+            && isSubtreeLesser(root.left, value)
+            && isSubtreeLesser(root.right, value)
+        ) return true
+        return false
+    }
+
+    private fun isSubtreeGreater(root: TreeNode?, value: Int): Boolean {
+        if (root == null) return true
+        if (root.`val` > value
+            && isSubtreeGreater(root.left, value)
+            && isSubtreeGreater(root.right, value)
+        ) return true
+        return false
+    }
+}
+
+// Time complexity: O(N) - since we visit each node exactly once,
+// Space complexity: O(N) - since we keep up to the entire tree
+object IsBinarySearchTreeRecursivelyWithRanges : IsBinarySearchTree {
+
+    override fun isValidBST(root: TreeNode?): Boolean = doIsValidBST(root, null, null)
+
+    private fun doIsValidBST(root: TreeNode?, minValue: Int?, maxValue: Int?): Boolean {
+        if (root == null) return true
+
+        // check for BST constraint violation: root.`val` must be in (minValue; maxValue) range
+        if (minValue != null && root.`val` <= minValue
+            || maxValue != null && root.`val` >= maxValue) return false
+
+        return doIsValidBST(root.left, minValue, root.`val`)
+                && doIsValidBST(root.right, root.`val`, maxValue)
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/IsBinarySearchTreeTest.kt
@@ -0,0 +1,90 @@
+package com.github.dkoval.leetcode.problems
+
+import com.github.dkoval.leetcode.TreeNode
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class IsBinarySearchTreeTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(2).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(3)
+                },
+                true
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(4).apply {
+                        left = TreeNode(3)
+                        right = TreeNode(6)
+                    }
+                },
+                false // The root node's value is 5 but its right child's value is 4.
+            ),
+            Arguments.of(
+                TreeNode(1),
+                true
+            ),
+            Arguments.of(
+                null,
+                true
+            ),
+            Arguments.of(
+                TreeNode(Int.MIN_VALUE),
+                true
+            ),
+            Arguments.of(
+                TreeNode(Int.MAX_VALUE),
+                true
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(1)
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    right = TreeNode(1)
+                },
+                false
+            )
+        )
+    }
+
+    @Nested
+    inner class IsBinarySearchTreeRecursivelyBruteForceTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if a given tree is a binary search tree (BST)`(root: TreeNode?, expected: Boolean) {
+            IsBinarySearchTreeRecursivelyBruteForce.test(root, expected)
+        }
+    }
+
+    @Nested
+    inner class IsBinarySearchTreeRecursivelyWithRangesTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if a given tree is a binary search tree (BST)`(root: TreeNode?, expected: Boolean) {
+            IsBinarySearchTreeRecursivelyWithRanges.test(root, expected)
+        }
+    }
+
+    private fun IsBinarySearchTree.test(root: TreeNode?, expected: Boolean) {
+        val actual = isValidBST(root)
+        assertEquals(expected, actual)
+    }
+}
