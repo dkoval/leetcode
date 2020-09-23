241e364b7cb6b77160cc188a6a2d7c8b57e8300a
dkoval <koval.dmitriy@gmail.com>
2020-09-24 00:15:02 +0200

    Refactor solutions to "Kth Smallest Element in a BST" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/KthSmallestElementInBST.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/KthSmallestElementInBST.kt
@@ -12,27 +12,29 @@ import com.github.dkoval.leetcode.TreeNode
  */
 object KthSmallestElementInBST {
 
-    fun kthSmallest(root: TreeNode?, k: Int): Int = doKthSmallest(root, k, MutableInt(0))
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        val i = IndexAndValue(0, Int.MAX_VALUE)
+        inorder(root, k, i);
+        return i.value
+    }
+
+    private data class IndexAndValue(var index: Int, var value: Int)
 
     // Fact: In-order traversal of a BST returns the node in ascending order.
     // Idea: Keep track of the number of nodes processed so far while traversing the BST in in-order fashion.
     // When the number of nodes processed becomes equal to k, the current node is k-th smallest.
-    private fun doKthSmallest(root: TreeNode?, k: Int, i: MutableInt): Int {
+    private fun inorder(root: TreeNode?, k: Int, i: IndexAndValue) {
         if (root == null) {
-            return Int.MAX_VALUE
+            return
         }
         // search in the left subtree
-        val left = doKthSmallest(root.left, k, i)
-        if (left != Int.MAX_VALUE) {
-            return left
-        }
+        inorder(root.left, k, i)
         // if the current node is k-th, return its node
-        if (++i.value == k) {
-            return root.`val`
+        if (++i.index == k) {
+            i.value = root.`val`
+            return
         }
         // search in the right subtree
-        return doKthSmallest(root.right, k, i)
+        inorder(root.right, k, i)
     }
-
-    private class MutableInt(var value: Int)
 }
