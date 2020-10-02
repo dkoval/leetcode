206c621d6a7904ccc0d4ff414f7c8b71bdf3d45e
dkoval <koval.dmitriy@gmail.com>
2020-10-02 07:52:46 +0200

    Solution to "Maximum Distance in Arrays" problem in Java

--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumDistanceInArraysTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumDistanceInArraysTest.kt
@@ -1,15 +1,19 @@
 package com.github.dkoval.leetcode.challenge
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MaximumDistanceInArraysTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 listOf(
                     listOf(1, 2, 3),
@@ -35,10 +39,24 @@ internal class MaximumDistanceInArraysTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find the maximum distance in arrays`(arrays: List<List<Int>>, expected: Int) {
-        val actual = MaximumDistanceInArrays.maxDistance(arrays)
-        assertEquals(expected, actual)
+    @Nested
+    inner class MaximumDistanceInArraysTest {
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the maximum distance in arrays`(arrays: List<List<Int>>, expected: Int) {
+            val actual = MaximumDistanceInArrays.maxDistance(arrays)
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Nested
+    inner class MaximumDistanceInArraysJavaTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the maximum distance in arrays`(arrays: List<List<Int>>, expected: Int) {
+            val actual = MaximumDistanceInArraysJava().maxDistance(arrays)
+            assertEquals(expected, actual)
+        }
     }
 }
