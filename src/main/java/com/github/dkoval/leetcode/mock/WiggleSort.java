0fd0aa8ef6a046e6adeada9f8d74280bccc81e55
dkoval <koval.dmitriy@gmail.com>
2020-10-07 01:36:28 +0200

    Solutions to "Wiggle Sort" problem

--- src/main/java/com/github/dkoval/leetcode/mock/WiggleSort.java
+++ src/main/java/com/github/dkoval/leetcode/mock/WiggleSort.java
@@ -7,21 +7,57 @@ import java.util.Arrays;
  *
  * Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]...
  */
-public class WiggleSort {
+public abstract class WiggleSort {
 
-    // Time complexity: O(NlogN)
-    public void wiggleSort(int[] nums) {
-        // Sort the array first, then swap elements pair-wise starting from the second element.
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length - 1; i += 2) {
-            swap(nums, i, i + 1);
+    private WiggleSort() { /* prevents instantiation from the outside */ }
+
+    public abstract void wiggleSort(int[] nums);
+
+    static class WiggleSortInNLogNTime extends WiggleSort {
+
+        // Time complexity: O(NlogN)
+        @Override
+        public void wiggleSort(int[] nums) {
+            // Sort the array first, then swap elements pair-wise starting from the second element.
+            Arrays.sort(nums);
+            for (int i = 1; i < nums.length - 1; i += 2) {
+                swap(nums, i, i + 1);
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            if (i == j) return;
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
-        if (i == j) return;
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    static class WiggleSortInLinearTime extends WiggleSort {
+
+        // Time complexity: O(N)
+        @Override
+        public void wiggleSort(int[] nums) {
+            boolean less = true;
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (less) {
+                    if (nums[i] > nums[i + 1]) {
+                        swap(nums, i, i + 1);
+                    }
+                } else {
+                    if (nums[i] < nums[i + 1]) {
+                        swap(nums, i, i + 1);
+                    }
+                }
+                less = !less;
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            if (i == j) return;
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
     }
 }
--- src/test/java/com/github/dkoval/leetcode/mock/WiggleSortTest.java
+++ src/test/java/com/github/dkoval/leetcode/mock/WiggleSortTest.java
@@ -1,5 +1,9 @@
 package com.github.dkoval.leetcode.mock;
 
+import com.github.dkoval.leetcode.mock.WiggleSort.WiggleSortInLinearTime;
+import com.github.dkoval.leetcode.mock.WiggleSort.WiggleSortInNLogNTime;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,20 +15,48 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class WiggleSortTest {
 
-    private final WiggleSort solution = new WiggleSort();
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WiggleSortInNLogNTimeTest {
 
-    static List<Arguments> input() {
-        return Collections.singletonList(
-                Arguments.of(
-                        new int[]{3, 5, 2, 1, 6, 4},
-                        new int[]{1, 3, 2, 5, 4, 6}
-                )
-        );
+        List<Arguments> input() {
+            return Collections.singletonList(
+                    Arguments.of(
+                            new int[]{3, 5, 2, 1, 6, 4},
+                            new int[]{1, 3, 2, 5, 4, 6}
+                    )
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("input")
+        void verifyRequiredReordering(int[] nums, int[] expected) {
+            test(new WiggleSortInNLogNTime(), nums, expected);
+        }
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    void verifyRequiredReordering(int[] nums, int[] expected) {
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WiggleSortInLinearTimeTest {
+
+        List<Arguments> input() {
+            return Collections.singletonList(
+                    Arguments.of(
+                            new int[]{3, 5, 2, 1, 6, 4},
+                            new int[]{3, 5, 1, 6, 2, 4}
+                    )
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("input")
+        void verifyRequiredReordering(int[] nums, int[] expected) {
+            test(new WiggleSortInLinearTime(), nums, expected);
+        }
+
+    }
+
+    private static void test(WiggleSort solution, int[] nums, int[] expected) {
         solution.wiggleSort(nums);
         assertArrayEquals(expected, nums);
     }
