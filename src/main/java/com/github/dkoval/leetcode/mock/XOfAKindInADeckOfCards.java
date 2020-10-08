963e2775faf92f601559ed41182f15b986880208
dkoval <koval.dmitriy@gmail.com>
2020-10-09 00:34:19 +0200

    Solution to "X of a Kind in a Deck of Cards" problem

---
+++ src/main/java/com/github/dkoval/leetcode/mock/XOfAKindInADeckOfCards.java
@@ -0,0 +1,73 @@
+package com.github.dkoval.leetcode.mock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards">X of a Kind in a Deck of Cards</a>
+ *
+ * In a deck of cards, each card has an integer written on it.
+ *
+ * Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into
+ * 1 or more groups of cards, where:
+ *
+ * - Each group has exactly X cards.
+ * - All the cards in each group have the same integer.
+ */
+public abstract class XOfAKindInADeckOfCards {
+
+    private XOfAKindInADeckOfCards() { /* prevents instantiation from the outside */ }
+
+    public abstract boolean hasGroupsSizeX(int[] deck);
+
+    public static class XOfAKindInADeckOfCardsBruteForce extends XOfAKindInADeckOfCards {
+
+        @Override
+        public boolean hasGroupsSizeX(int[] deck) {
+            Map<Integer, Integer> count = new HashMap<>();
+            for (int card : deck) {
+                count.put(card, count.getOrDefault(card, 0) + 1);
+            }
+            // brute force: try all possible X values from 2..N range
+            for (int x = 2; x <= deck.length; x++) {
+                if (deck.length % x != 0) continue;
+                boolean found = true;
+                for (int v : count.values()) {
+                    if (v % x != 0) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) return true;
+            }
+            return false;
+        }
+    }
+
+    public static class XOfAKindInADeckOfCardsGCD extends XOfAKindInADeckOfCards {
+
+        @Override
+        public boolean hasGroupsSizeX(int[] deck) {
+            Map<Integer, Integer> count = new HashMap<>();
+            for (int card : deck) {
+                count.put(card, count.getOrDefault(card, 0) + 1);
+            }
+            int gcd = -1;
+            for (int v : count.values()) {
+                if (gcd == -1) {
+                    gcd = v;
+                } else {
+                    gcd = gcd(gcd, v);
+                }
+            }
+            return gcd >= 2;
+        }
+
+        private int gcd(int x, int y) {
+            if (y == 0) {
+                return x;
+            }
+            return gcd(y,  x % y);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/mock/XOfAKindInADeckOfCardsTest.kt
@@ -0,0 +1,66 @@
+package com.github.dkoval.leetcode.mock
+
+import com.github.dkoval.leetcode.mock.XOfAKindInADeckOfCards.XOfAKindInADeckOfCardsBruteForce
+import com.github.dkoval.leetcode.mock.XOfAKindInADeckOfCards.XOfAKindInADeckOfCardsGCD
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class XOfAKindInADeckOfCardsTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 3, 4, 4, 3, 2, 1),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 1, 2, 2, 2, 3, 3),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(1),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(1, 1),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 2, 2, 2, 2),
+                true
+            )
+        )
+    }
+
+    @Nested
+    inner class XOfAKindInADeckOfCardsBruteForceTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun hasGroupsSizeX(deck: IntArray, expected: Boolean) {
+            XOfAKindInADeckOfCardsBruteForce().test(deck, expected)
+        }
+    }
+
+    @Nested
+    inner class XOfAKindInADeckOfCardsGCDTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun hasGroupsSizeX(deck: IntArray, expected: Boolean) {
+            XOfAKindInADeckOfCardsGCD().test(deck, expected)
+        }
+    }
+
+    private fun XOfAKindInADeckOfCards.test(deck: IntArray, expected: Boolean) {
+        val actual = hasGroupsSizeX(deck)
+        assertEquals(expected, actual)
+    }
+}
