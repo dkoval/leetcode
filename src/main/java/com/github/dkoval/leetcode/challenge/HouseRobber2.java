ad9b143ccca6cfc493c9df9264aa9c59898068ad
dkoval <koval.dmitriy@gmail.com>
2020-10-14 22:15:19 +0200

    Solution to "House Robber II" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/HouseRobber2.java
@@ -0,0 +1,66 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/explore/featured/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3494/">House Robber II</a>
+ * <p>
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed.
+ * All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
+ * Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if
+ * two adjacent houses were broken into on the same night.
+ * <p>
+ * Given a list of non-negative integers nums representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the police.
+ */
+public abstract class HouseRobber2 {
+
+    public abstract int rob(int[] nums);
+
+    public static class HouseRobber2DPArray extends HouseRobber2 {
+
+        @Override
+        public int rob(int[] nums) {
+            if (nums.length == 0) return 0;
+            if (nums.length == 1) return nums[0];
+            return Math.max(doRob(nums, 0, nums.length - 2), doRob(nums, 1, nums.length - 1));
+        }
+
+        private int doRob(int[] nums, int start, int end) {
+            int length = end - start + 1;
+            // dp[i] represents max amount of money a one can rob up to i-th house (including i-th house)
+            int[] dp = new int[length];
+            if (length >= 1) {
+                dp[0] = nums[start];
+            }
+            if (length >= 2) {
+                dp[1] = Math.max(nums[start], nums[start + 1]);
+            }
+            for (int i = 2; i < length; i++) {
+                dp[i] = Math.max(nums[start + i] + dp[i - 2], dp[i - 1]);
+            }
+            return dp[length - 1];
+        }
+    }
+
+    public static class HouseRobber2DPSpaceOptimized extends HouseRobber2 {
+
+        @Override
+        public int rob(int[] nums) {
+            if (nums.length == 0) return 0;
+            if (nums.length == 1) return nums[0];
+            return Math.max(doRob(nums, 0, nums.length - 2), doRob(nums, 1, nums.length - 1));
+        }
+
+        private int doRob(int[] nums, int start, int end) {
+            int length = end - start + 1;
+            if (length == 1) return nums[start];
+            if (length == 2) return Math.max(nums[start], nums[start + 1]);
+            int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+            for (int i = start + 2; i < start + length; i++) {
+                int third = Math.max(first + nums[i], second);
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/HouseRobber2Test.kt
@@ -0,0 +1,76 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.HouseRobber2.HouseRobber2DPArray
+import com.github.dkoval.leetcode.challenge.HouseRobber2.HouseRobber2DPSpaceOptimized
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class HouseRobber2Test {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(),
+                0
+            ),
+            Arguments.of(
+                intArrayOf(3, 5),
+                5
+            ),
+            Arguments.of(
+                intArrayOf(2, 3, 2),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3, 1),
+                4
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3, 1),
+                4
+            ),
+            Arguments.of(
+                intArrayOf(200, 3, 140, 20, 10),
+                340
+            )
+        )
+    }
+
+    @Nested
+    inner class HouseRobber2DPArrayTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum amount of money you can rob tonight without alerting the police`(
+            nums: IntArray,
+            expected: Int
+        ) {
+            HouseRobber2DPArray().test(nums, expected)
+        }
+    }
+
+    @Nested
+    inner class HouseRobber2DPSpaceOptimizedTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum amount of money you can rob tonight without alerting the police`(
+            nums: IntArray,
+            expected: Int
+        ) {
+            HouseRobber2DPSpaceOptimized().test(nums, expected)
+        }
+    }
+
+    private fun HouseRobber2.test(nums: IntArray, expected: Int) {
+        val actual = rob(nums)
+        assertEquals(expected, actual)
+    }
+}
