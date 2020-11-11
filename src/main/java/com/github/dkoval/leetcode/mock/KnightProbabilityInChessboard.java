e7c7f85ae94101730bb9a575c393f1636fb320fe
dkoval <koval.dmitriy@gmail.com>
2020-11-11 21:55:49 +0100

    Document solution

--- src/main/java/com/github/dkoval/leetcode/mock/KnightProbabilityInChessboard.java
+++ src/main/java/com/github/dkoval/leetcode/mock/KnightProbabilityInChessboard.java
@@ -1,5 +1,20 @@
 package com.github.dkoval.leetcode.mock;
 
+/**
+ * <a href="https://leetcode.com/problems/knight-probability-in-chessboard/">Knight Probability in Chessboard</a>
+ * <p>
+ * On an NxN chessboard, a knight starts at the r-th row and c-th column and attempts to make exactly K moves.
+ * The rows and columns are 0 indexed, so the top-left square is (0, 0), and the bottom-right square is (N-1, N-1).
+ * <p>
+ * A chess knight has 8 possible moves it can make, as illustrated below. Each move is two squares in a cardinal direction,
+ * then one square in an orthogonal direction.
+ * <p>
+ * Each time the knight is to move, it chooses one of eight possible moves uniformly at random
+ * (even if the piece would go off the chessboard) and moves there.
+ * <p>
+ * The knight continues moving until it has made exactly K moves or has moved off the chessboard.
+ * Return the probability that the knight remains on the board after it has stopped moving.
+ */
 public abstract class KnightProbabilityInChessboard {
 
     public abstract double knightProbability(int N, int K, int r, int c);
@@ -8,7 +23,7 @@ public abstract class KnightProbabilityInChessboard {
 
         private static final int[][] dirs = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
 
-        // LeetCode: https://leetcode.com/problems/knight-probability-in-chessboard/
+        // Resource: https://www.youtube.com/watch?v=OrS7PaJ-5ck
         public double knightProbability(int N, int K, int r, int c) {
             // dp[i][j] - probability of being on square (i, j)
             double[][] dp = new double[N][N];
