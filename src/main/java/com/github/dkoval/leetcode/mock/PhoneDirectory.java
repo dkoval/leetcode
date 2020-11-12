e911c6de3020c43389d3f45c6ed89f571c322188
dkoval <koval.dmitriy@gmail.com>
2020-11-12 23:44:27 +0100

    Revised solution to "Phone Directory" problem in Java

--- src/main/java/com/github/dkoval/leetcode/mock/PhoneDirectory.java
+++ src/main/java/com/github/dkoval/leetcode/mock/PhoneDirectory.java
@@ -14,10 +14,9 @@ import java.util.Set;
  * release: Recycle or release a number.
  */
 public class PhoneDirectory {
-    private final Set<Integer> occupied = new LinkedHashSet<>();
-    private final Set<Integer> released = new LinkedHashSet<>();
     private final int maxNumbers;
-    private int lastAssignedNumber = -1;
+    private final Set<Integer> releasedNumbers = new LinkedHashSet<>();
+    private int nextNumber = 0;
 
     /** Initialize your data structure here
      @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
@@ -28,29 +27,27 @@ public class PhoneDirectory {
     /** Provide a number which is not assigned to anyone.
      @return - Return an available number. Return -1 if none is available. */
     public int get() {
-        if (released.isEmpty() && lastAssignedNumber == maxNumbers - 1) {
-            return -1;
-        }
-        if (!released.isEmpty()) {
-            Iterator<Integer> it = released.iterator();
-            int number = it.next();
+        if (!releasedNumbers.isEmpty()) {
+            Iterator<Integer> it = releasedNumbers.iterator();
+            int releasedNumber = it.next();
             it.remove();
-            return number;
+            return releasedNumber;
         }
-        int number = ++lastAssignedNumber;
-        occupied.add(number);
-        return number;
+        if (nextNumber < maxNumbers) {
+            return nextNumber++;
+        }
+        return -1;
     }
 
     /** Check if a number is available or not. */
     public boolean check(int number) {
-        return !occupied.contains(number) || released.contains(number);
+        return releasedNumbers.contains(number) || (number >= nextNumber && number < maxNumbers);
     }
 
     /** Recycle or release a number. */
     public void release(int number) {
-        if (number <= lastAssignedNumber) {
-            released.add(number);
+        if (number < nextNumber) {
+            releasedNumbers.add(number);
         }
     }
 }
