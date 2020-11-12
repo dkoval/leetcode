9aba224c05555a938a3a251efab22e2ebdc464e9
dkoval <koval.dmitriy@gmail.com>
2020-11-13 00:09:05 +0100

    Refactor solution to "Strobogrammatic Number" problem in Java

--- src/main/java/com/github/dkoval/leetcode/mock/StrobogrammaticNumber.java
+++ src/main/java/com/github/dkoval/leetcode/mock/StrobogrammaticNumber.java
@@ -1,8 +1,5 @@
 package com.github.dkoval.leetcode.mock;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <a href="https://leetcode.com/problems/strobogrammatic-number/">Strobogrammatic Number</a>
  * <p>
@@ -12,28 +9,15 @@ import java.util.Map;
  */
 public class StrobogrammaticNumber {
 
-    private final Map<Character, Character> lookUpsideDown = new HashMap<Character, Character>() {{
-        put('0', '0');
-        put('1', '1');
-        put('6', '9');
-        put('8', '8');
-        put('9', '6');
-    }};
-
     public boolean isStrobogrammatic(String num) {
         int n = num.length();
-        if (n == 1) {
-            char x = num.charAt(0);
-            if (x != '0' && x != '1' && x != '8') {
-                return false;
-            }
-        }
-        for (int i = 0; i <= n / 2; i++) {
-            char x = num.charAt(i);
-            Character y = lookUpsideDown.get(x);
-            if (y == null || y != num.charAt(n - i - 1)) {
-                return false;
-            }
+        int l = 0, r = num.length() - 1;
+        while (l <= r) {
+            char lc = num.charAt(l++);
+            char rc = num.charAt(r--);
+            if (lc == rc && (lc == '0' || lc == '1' || lc == '8')) continue;
+            if (lc == '6' && rc == '9' || lc == '9' && rc == '6') continue;
+            return false;
         }
         return true;
     }
