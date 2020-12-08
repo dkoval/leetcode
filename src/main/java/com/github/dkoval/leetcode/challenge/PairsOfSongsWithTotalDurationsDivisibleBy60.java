2dcc7717ded6dfe06929d1b8f97f513868f87abd
dkoval <koval.dmitriy@gmail.com>
2020-12-08 19:47:34 +0100

    Solution to "Pairs of Songs With Total Durations Divisible by 60" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -0,0 +1,59 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/570/week-2-december-8th-december-14th/3559/">Pairs of Songs With Total Durations Divisible by 60</a>
+ * <p>
+ * You are given a list of songs where the ith song has a duration of time[i] seconds.
+ * <p>
+ * Return the number of pairs of songs for which their total duration in seconds is divisible by 60.
+ * Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
+ * <p>
+ * Constraints:
+ * <ul>
+ * <li>1 <= time.length <= 6 * 10^4</li>
+ * <li>1 <= time[i] <= 500</li>
+ * </ul>
+ */
+public abstract class PairsOfSongsWithTotalDurationsDivisibleBy60 {
+
+    public abstract int numPairsDivisibleBy60(int[] time);
+
+    // Brute force: O(N^2) time | O(1) space
+    // Results in TLE
+    public static class PairsOfSongsWithTotalDurationsDivisibleBy60BruteForce extends PairsOfSongsWithTotalDurationsDivisibleBy60 {
+
+        @Override
+        public int numPairsDivisibleBy60(int[] time) {
+            int numPairs = 0;
+            for (int i = 0; i < time.length; i++) {
+                for (int j = i + 1; j < time.length; j++) {
+                    if ((time[i] + time[j]) % 60 == 0) {
+                        numPairs++;
+                    }
+                }
+            }
+            return numPairs;
+        }
+    }
+
+    public static class PairsOfSongsWithTotalDurationsDivisibleBy60CountingMods extends PairsOfSongsWithTotalDurationsDivisibleBy60 {
+
+        @Override
+        public int numPairsDivisibleBy60(int[] time) {
+            int[] mods = new int[60];
+            for (int t : time) {
+                mods[t % 60]++;
+            }
+            int count = choose2(mods[0]) + choose2(mods[30]);
+            for (int i = 1; i <= 29; i++) {
+                count += mods[i] * mods[60 - i];
+            }
+            return count;
+        }
+
+        private int choose2(int n) {
+            // C(n, 2) = n * (n - 1) / 2
+            return n * (n - 1) / 2;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/PairsOfSongsWithTotalDurationsDivisibleBy60Test.kt
@@ -0,0 +1,64 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.PairsOfSongsWithTotalDurationsDivisibleBy60.PairsOfSongsWithTotalDurationsDivisibleBy60BruteForce
+import com.github.dkoval.leetcode.challenge.PairsOfSongsWithTotalDurationsDivisibleBy60.PairsOfSongsWithTotalDurationsDivisibleBy60CountingMods
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class PairsOfSongsWithTotalDurationsDivisibleBy60Test {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(30, 20, 150, 100, 40),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(60, 60, 60),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(418, 204, 77, 278, 239, 457, 284, 263, 372, 279, 476, 416, 360, 18),
+                1
+            )
+        )
+    }
+
+    @Nested
+    inner class PairsOfSongsWithTotalDurationsDivisibleBy60BruteForceTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of pairs of songs for which their total duration in seconds is divisible by 60`(
+            time: IntArray,
+            expected: Int
+        ) {
+            PairsOfSongsWithTotalDurationsDivisibleBy60BruteForce().test(time, expected)
+        }
+    }
+
+    @Nested
+    inner class PairsOfSongsWithTotalDurationsDivisibleBy60CountingModsTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of pairs of songs for which their total duration in seconds is divisible by 60`(
+            time: IntArray,
+            expected: Int
+        ) {
+            PairsOfSongsWithTotalDurationsDivisibleBy60CountingMods().test(time, expected)
+        }
+    }
+
+    private fun PairsOfSongsWithTotalDurationsDivisibleBy60.test(time: IntArray, expected: Int) {
+        val actual = numPairsDivisibleBy60(time)
+        assertEquals(expected, actual)
+    }
+}
