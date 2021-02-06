5dae12fb8c44d587c54f6fe119f1975674044b29
dkoval <koval.dmitriy@gmail.com>
2021-02-06 23:49:20 +0100

    Solution to "Binary Tree Right Side View" problem leveraging level-order traversal

--- src/main/java/com/github/dkoval/leetcode/challenge/BinaryTreeRightSideView.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/BinaryTreeRightSideView.java
@@ -3,7 +3,9 @@ package com.github.dkoval.leetcode.challenge;
 import com.github.dkoval.leetcode.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * <a href="https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3630/">Binary Tree Right Side View</a>
@@ -11,22 +13,58 @@ import java.util.List;
  * Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see
  * ordered from top to bottom.
  */
-public class BinaryTreeRightSideView {
+public abstract class BinaryTreeRightSideView {
 
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        preorder(root, 0, result);
-        return result;
+    public abstract List<Integer> rightSideView(TreeNode root);
+
+    public static class BinaryTreeRightSideViewLevelOrderTraversal extends BinaryTreeRightSideView {
+
+        @Override
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+            while (!q.isEmpty()) {
+                int size = q.size();
+                while (size-- > 0) {
+                    TreeNode node = q.poll();
+                    if (size == 0) {
+                        // last element of i-th level
+                        result.add(node.val);
+                    }
+                    if (node.left != null) {
+                        q.add(node.left);
+                    }
+                    if (node.right != null) {
+                        q.add(node.right);
+                    }
+                }
+            }
+            return result;
+        }
     }
 
-    private void preorder(TreeNode root, int depth, List<Integer> result) {
-        if (root == null) {
-            return;
+    public static class BinaryTreeRightSideViewPreorderTraversal extends BinaryTreeRightSideView {
+
+        @Override
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            preorder(root, 0, result);
+            return result;
         }
-        if (result.size() <= depth) {
-            result.add(root.val);
+        private void preorder(TreeNode root, int depth, List<Integer> result) {
+            if (root == null) {
+                return;
+            }
+            if (result.size() == depth) {
+                result.add(root.val);
+            }
+            preorder(root.right, depth + 1, result);
+            preorder(root.left, depth + 1, result);
         }
-        preorder(root.right, depth + 1, result);
-        preorder(root.left, depth + 1, result);
+
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/BinaryTreeRightSideViewTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/BinaryTreeRightSideViewTest.kt
@@ -1,16 +1,22 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.BinaryTreeRightSideView.BinaryTreeRightSideViewLevelOrderTraversal
+import com.github.dkoval.leetcode.challenge.BinaryTreeRightSideView.BinaryTreeRightSideViewPreorderTraversal
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class BinaryTreeRightSideViewTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 TreeNode(1).apply {
                     left = TreeNode(2).apply {
@@ -25,13 +31,34 @@ internal class BinaryTreeRightSideViewTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the values of the nodes of the binary tree right side view`(
-        root: TreeNode?,
-        expected: List<Int>
-    ) {
-        val actual = BinaryTreeRightSideView().rightSideView(root)
+    @Nested
+    inner class BinaryTreeRightSideViewLevelOrderTraversalTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the values of the nodes of the binary tree right side view`(
+            root: TreeNode?,
+            expected: List<Int>
+        ) {
+            BinaryTreeRightSideViewLevelOrderTraversal().test(root, expected)
+        }
+    }
+
+    @Nested
+    inner class BinaryTreeRightSideViewPreorderTraversalTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the values of the nodes of the binary tree right side view`(
+            root: TreeNode?,
+            expected: List<Int>
+        ) {
+            BinaryTreeRightSideViewPreorderTraversal().test(root, expected)
+        }
+    }
+
+    private fun BinaryTreeRightSideView.test(root: TreeNode?, expected: List<Int>) {
+        val actual = rightSideView(root)
         assertEquals(expected, actual)
     }
 }
