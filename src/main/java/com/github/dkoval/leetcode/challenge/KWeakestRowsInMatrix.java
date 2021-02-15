20857d07578434b0a09afd378aad2f8da68c6870
dkoval <koval.dmitriy@gmail.com>
2021-02-15 22:47:16 +0100

    Solution to "The K Weakest Rows in a Matrix" problem using a PriorityQueue

--- src/main/java/com/github/dkoval/leetcode/challenge/KWeakestRowsInMatrix.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/KWeakestRowsInMatrix.java
@@ -2,6 +2,7 @@ package com.github.dkoval.leetcode.challenge;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * <a href="https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3641/">The K Weakest Rows in a Matrix</a>
@@ -13,43 +14,89 @@ import java.util.Comparator;
  * or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of a row,
  * that is, always ones may appear first and then zeros.
  */
-public class KWeakestRowsInMatrix {
+public abstract class KWeakestRowsInMatrix {
 
-    public int[] kWeakestRows(int[][] mat, int k) {
-        int m = mat.length;
-        Integer[] rows = new Integer[m];
-        for (int i = 0; i < m; i++) {
-            rows[i] = i;
+    public abstract int[] kWeakestRows(int[][] mat, int k);
+
+    public static class KWeakestRowsInMatrixBySortingRows extends KWeakestRowsInMatrix {
+
+        @Override
+        public int[] kWeakestRows(int[][] mat, int k) {
+            int m = mat.length;
+            Integer[] rows = new Integer[m];
+            for (int i = 0; i < m; i++) {
+                rows[i] = i;
+            }
+
+            Arrays.sort(rows, new WeakestRowsComparator(mat));
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++) {
+                result[i] = rows[i];
+            }
+            return result;
         }
 
-        Arrays.sort(rows, new WeakestRowsComparator(mat));
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = rows[i];
+        private static class WeakestRowsComparator implements Comparator<Integer> {
+            final int[][] mat;
+
+            WeakestRowsComparator(int[][] mat) {
+                this.mat = mat;
+            }
+
+            public int compare(Integer row1, Integer row2) {
+                int count1 = countSoldiers(row1);
+                int count2 = countSoldiers(row2);
+                return (count1 != count2) ? count1 - count2 : row1 - row2;
+            }
+
+            private int countSoldiers(int row) {
+                int count = 0;
+                int col = 0;
+                while (col < mat[row].length && mat[row][col++] == 1) {
+                    count++;
+                }
+                return count;
+            }
         }
-        return result;
     }
 
-    private static class WeakestRowsComparator implements Comparator<Integer> {
-        final int[][] mat;
+    public static class KWeakestRowsInMatrixUsingPriorityQueue extends KWeakestRowsInMatrix {
 
-        WeakestRowsComparator(int[][] mat) {
-            this.mat = mat;
-        }
+        @Override
+        public int[] kWeakestRows(int[][] mat, int k) {
+            PriorityQueue<Row> pq = new PriorityQueue<>(
+                    k,
+                    (row1, row2) -> (row1.numSoldiers != row2.numSoldiers)
+                            ? row1.numSoldiers - row2.numSoldiers
+                            : row1.idx - row2.idx);
 
-        public int compare(Integer row1, Integer row2) {
-            int count1 = countSoldiers(row1);
-            int count2 = countSoldiers(row2);
-            return (count1 != count2) ? count1 - count2 : row1 - row2;
-        }
-
-        private int countSoldiers(int row) {
-            int count = 0;
-            int col = 0;
-            while (col < mat[row].length && mat[row][col++] == 1) {
-                count++;
+            for (int i = 0; i < mat.length; i++) {
+                int numSoldiers = 0;
+                for (int j = 0; j < mat[0].length; j++) {
+                    if (mat[i][j] == 1) {
+                        numSoldiers++;
+                    } else {
+                        break;
+                    }
+                }
+                pq.offer(new Row(i, numSoldiers));
             }
-            return count;
+
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++) {
+                result[i] = pq.poll().idx;
+            }
+            return result;
+        }
+
+        private static class Row {
+            final int idx;
+            final int numSoldiers;
+
+            Row(int idx, int numSoldiers) {
+                this.idx = idx;
+                this.numSoldiers = numSoldiers;
+            }
         }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/KWeakestRowsInMatrixTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/KWeakestRowsInMatrixTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.KWeakestRowsInMatrix.KWeakestRowsInMatrixBySortingRows
+import com.github.dkoval.leetcode.challenge.KWeakestRowsInMatrix.KWeakestRowsInMatrixUsingPriorityQueue
 import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class KWeakestRowsInMatrixTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     intArrayOf(1, 1, 0, 0, 0),
@@ -34,14 +40,36 @@ internal class KWeakestRowsInMatrixTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest`(
-        mat: Array<IntArray>,
-        k: Int,
-        expected: IntArray
-    ) {
-        val actual = KWeakestRowsInMatrix().kWeakestRows(mat, k)
+    @Nested
+    inner class KWeakestRowsInMatrixUsingPriorityQueueTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest`(
+            mat: Array<IntArray>,
+            k: Int,
+            expected: IntArray
+        ) {
+            KWeakestRowsInMatrixUsingPriorityQueue().test(mat, k, expected)
+        }
+    }
+
+    @Nested
+    inner class KWeakestRowsInMatrixBySortingRowsTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest`(
+            mat: Array<IntArray>,
+            k: Int,
+            expected: IntArray
+        ) {
+            KWeakestRowsInMatrixBySortingRows().test(mat, k, expected)
+        }
+    }
+
+    private fun KWeakestRowsInMatrix.test(mat: Array<IntArray>, k: Int, expected: IntArray) {
+        val actual = kWeakestRows(mat, k)
         assertArrayEquals(expected, actual)
     }
 }
