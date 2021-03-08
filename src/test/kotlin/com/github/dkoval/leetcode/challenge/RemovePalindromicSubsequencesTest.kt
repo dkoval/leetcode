bc085bdeac81f26b3cc0e5313df9e3ea8331eef7
dkoval <koval.dmitriy@gmail.com>
2021-03-08 22:46:15 +0100

    Solution to "Remove Palindromic Subsequences" problem

---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/RemovePalindromicSubsequencesTest.kt
@@ -0,0 +1,38 @@
+package com.github.dkoval.leetcode.challenge
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class RemovePalindromicSubsequencesTest {
+
+    companion object {
+        @JvmStatic
+        fun input(): List<Arguments> = listOf(
+            Arguments.of(
+                "ababa",
+                1
+            ),
+            Arguments.of(
+                "abb",
+                2
+            ),
+            Arguments.of(
+                "baabb",
+                2
+            ),
+            Arguments.of(
+                "",
+                0
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    fun `should Return the minimum number of steps to make the given string empty`(s: String, expected: Int) {
+        val actual = RemovePalindromicSubsequences().removePalindromeSub(s)
+        assertEquals(expected, actual)
+    }
+}
