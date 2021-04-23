c57266945c07a388f2f655d9c4b2f7f59dbe77c1
dkoval <koval.dmitriy@gmail.com>
2021-04-23 21:43:12 +0200

    Optimize solution to "Count Binary Substrings" problem to achieve O(N) time and O(1) space complexity

--- src/main/java/com/github/dkoval/leetcode/challenge/CountBinarySubstrings.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/CountBinarySubstrings.java
@@ -1,8 +1,5 @@
 package com.github.dkoval.leetcode.challenge;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * <a href="https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/596/week-4-april-22nd-april-28th/3718/">Count Binary Substrings</a>
  * <p>
@@ -10,27 +7,36 @@ import java.util.List;
  * and all the 0's and all the 1's in these substrings are grouped consecutively.
  * <p>
  * Substrings that occur multiple times are counted the number of times they occur.
+ * <p>
+ * Note:
+ * <ul>
+ *  <li>s.length will be between 1 and 50,000.</li>
+ *  <li>s will only consist of "0" or "1" characters.</li>
+ * </ul>
  */
 public class CountBinarySubstrings {
 
+    // O(N) time | O(1) space
     public int countBinarySubstrings(String s) {
-        // group is a contiguous sequence of either 00...0 or 11...1
-        List<Integer> groupLengths = new ArrayList<>();
+        // Let group be a contiguous sequence of 0's (00...0) or 1's (11...1).
+        // Given two groups G1 = 000... and G2 = 11..., the number of substrings formed from concatenated G1 + G2 string
+        // that have the same number of 0's and 1's is N = min(G1.length, G2.length).
+        // Applying the above formula to all (G[i], G[i + 1]) pairs gives us the final count.
+        int prevGroupLength = -1;
         int currGroupLength = 1;
+        int count = 0;
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) != s.charAt(i - 1)) {
-                groupLengths.add(currGroupLength);
+                if (prevGroupLength != -1) {
+                    count += Math.min(prevGroupLength, currGroupLength);
+                }
+                prevGroupLength = currGroupLength;
                 currGroupLength = 1;
             } else {
                 currGroupLength++;
             }
         }
-        groupLengths.add(currGroupLength);
-
-        int count = 0;
-        for (int i = 0; i < groupLengths.size() - 1; i++) {
-            count += Math.min(groupLengths.get(i), groupLengths.get(i + 1));
-        }
+        count += (prevGroupLength > 0) ? Math.min(prevGroupLength, currGroupLength) : 0;
         return count;
     }
 }
