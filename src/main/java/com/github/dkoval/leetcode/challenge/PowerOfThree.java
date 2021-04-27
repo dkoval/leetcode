7f689f90977bead5f0f53d026ad13e7decc9ad6d
dkoval <koval.dmitriy@gmail.com>
2021-04-27 18:16:05 +0200

    Improve solution to "Power of Three" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/PowerOfThree.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/PowerOfThree.java
@@ -12,15 +12,12 @@ package com.github.dkoval.leetcode.challenge;
 public class PowerOfThree {
 
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        while (n > 1) {
+        while (n >= 3) {
             if (n % 3 != 0) {
                 return false;
             }
             n /= 3;
         }
-        return true;
+        return (n == 1);
     }
 }
