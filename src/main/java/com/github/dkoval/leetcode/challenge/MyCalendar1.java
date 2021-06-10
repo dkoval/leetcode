3bef5bf49f7a0454cf9c751688711ab5eff98c20
dkoval <koval.dmitriy@gmail.com>
2021-06-11 00:48:36 +0200

    Solution to "My Calendar I" using Java TreeMap

--- src/main/java/com/github/dkoval/leetcode/challenge/MyCalendar1.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MyCalendar1.java
@@ -2,12 +2,16 @@ package com.github.dkoval.leetcode.challenge;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public interface MyCalendar1 {
 
     boolean book(int start, int end);
 
     class MyCalendar1BruteForce implements MyCalendar1 {
+        // PriorityQueue is used here to keep bookings sorted by their start time
+        private final PriorityQueue<Interval> bookings = new PriorityQueue<>(
+                Comparator.comparingInt(booking -> booking.start));
 
         private static class Interval {
             final int start;
@@ -18,10 +22,6 @@ public interface MyCalendar1 {
                 this.end = end;
             }
         }
-
-        // PriorityQueue is used here to keep bookings sorted by their start time
-        private final PriorityQueue<Interval> bookings = new PriorityQueue<>(
-                Comparator.comparingInt(booking -> booking.start));
 
         @Override
         public boolean book(int start, int end) {
@@ -37,6 +37,26 @@ public interface MyCalendar1 {
 
         private boolean overlaps(Interval booking1, Interval booking2) {
             return (booking2.start < booking1.end) && (booking2.end > booking1.start);
+        }
+    }
+
+    class MyCalendar1UsingTreeMap implements MyCalendar1 {
+        private final TreeMap<Integer, Integer> bookings = new TreeMap<>();
+
+        @Override
+        public boolean book(int start, int end) {
+            Integer prevBookingStart = bookings.floorKey(start);
+            if (prevBookingStart != null && bookings.get(prevBookingStart) > start) {
+                return false;
+            }
+
+            Integer nextBookingEnd = bookings.ceilingKey(start);
+            if (nextBookingEnd != null && end > nextBookingEnd) {
+                return false;
+            }
+
+            bookings.put(start, end);
+            return true;
         }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MyCalendar1Test.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MyCalendar1Test.kt
@@ -1,6 +1,7 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.challenge.MyCalendar1.MyCalendar1BruteForce
+import com.github.dkoval.leetcode.challenge.MyCalendar1.MyCalendar1UsingTreeMap
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -16,20 +17,30 @@ internal class MyCalendar1Test {
 
         override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of(
-                10, 20, true
+                listOf(
+                    BookCommand(10, 20, true),
+                    BookCommand(15, 25, false),
+                    BookCommand(20, 30, true)
+                )
             ),
             Arguments.of(
-                15, 25, false
-            ),
-            Arguments.of(
-                20, 30, true
+                listOf(
+                    BookCommand(47, 50, true),
+                    BookCommand(33, 41, true),
+                    BookCommand(39, 45, false),
+                    BookCommand(33, 42, false),
+                    BookCommand(25, 32, true),
+                    BookCommand(26, 35, false),
+                    BookCommand(19, 25, true),
+                    BookCommand(3, 8, true),
+                    BookCommand(8, 13, true),
+                    BookCommand(18, 27, false)
+                )
             )
         )
     }
 
-    companion object {
-        private val myCalendar1BruteForce: MyCalendar1 = MyCalendar1BruteForce()
-    }
+    class BookCommand(val start: Int, val end: Int, val expected: Boolean)
 
     @Nested
     inner class MyCalendar1BruteForceTest {
@@ -37,16 +48,28 @@ internal class MyCalendar1Test {
         @ParameterizedTest
         @ArgumentsSource(InputArgumentsProvider::class)
         fun `should  return true if the event can be added to the calendar successfully without causing a double booking`(
-            start: Int,
-            end: Int,
-            expected: Boolean
+            commands: List<BookCommand>
         ) {
-            myCalendar1BruteForce.test(start, end, expected)
+            MyCalendar1BruteForce().test(commands)
         }
     }
 
-    private fun MyCalendar1.test(start: Int, end: Int, expected: Boolean) {
-        val actual = book(start, end)
-        assertEquals(expected, actual)
+    @Nested
+    inner class MyCalendar1UsingTreeMapTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should  return true if the event can be added to the calendar successfully without causing a double booking`(
+            commands: List<BookCommand>
+        ) {
+            MyCalendar1UsingTreeMap().test(commands)
+        }
+    }
+
+    private fun MyCalendar1.test(commands: List<BookCommand>) {
+        for (command in commands) {
+            val actual = book(command.start, command.end)
+            assertEquals(command.expected, actual)
+        }
     }
 }
