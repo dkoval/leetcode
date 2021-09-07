1c4fc70ab89b2d7bdbe4762800364203f2497630
dkoval <koval.dmitriy@gmail.com>
2021-09-07 21:30:25 +0200

    Iterative and recursive solutions to "Reverse Linked List" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/ReverseLinkedList.java
@@ -0,0 +1,56 @@
+package com.github.dkoval.leetcode.challenge;
+
+import com.github.dkoval.leetcode.ListNode;
+
+/**
+ * <a href="https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3966/">Reverse Linked List</a>
+ * <p>
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the list is the range [0, 5000]</li>
+ *  <li>-5000 <= Node.val <= 5000</li>
+ * </ul>
+ * Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+public interface ReverseLinkedList {
+
+    ListNode reverseList(ListNode head);
+
+    // O(N) time | O(1) space
+    class ReverseLinkedListIterative implements ReverseLinkedList {
+
+        @Override
+        public ListNode reverseList(ListNode head) {
+            ListNode curr = head;
+            ListNode prev = null;
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
+
+    // O(N) time | O(N) space for recursion
+    class ReverseLinkedListRecursive implements ReverseLinkedList {
+
+        @Override
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            // n[1] -> ... -> n[k] <- n[k + 1] <- ... <- n[N]
+            //                        ^ already reversed ^
+            // we want n[k + 1] to point to n[k], where n[k] is the current `head`
+            ListNode reversed = reverseList(head.next);
+            head.next.next = head;
+            head.next = null; // to avoid cycles
+            return reversed; // head of the reversed list, i.e. n[N]
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ReverseLinkedListTest.kt
@@ -0,0 +1,70 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.ListNode
+import com.github.dkoval.leetcode.challenge.ReverseLinkedList.ReverseLinkedListIterative
+import com.github.dkoval.leetcode.challenge.ReverseLinkedList.ReverseLinkedListRecursive
+import com.github.dkoval.leetcode.toList
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class ReverseLinkedListTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                ListNode(1).apply {
+                    next = ListNode(2).apply {
+                        next = ListNode(3).apply {
+                            next = ListNode(4).apply {
+                                next = ListNode(5)
+                            }
+                        }
+                    }
+                },
+                listOf(5, 4, 3, 2, 1)
+            ),
+            Arguments.of(
+                ListNode(1).apply {
+                    next = ListNode(2)
+                },
+                listOf(2, 1)
+            ),
+            Arguments.of(
+                null,
+                listOf<Int>()
+            )
+        )
+    }
+
+    @Nested
+    inner class ReverseLinkedListIterativeTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should reverse a singly linked list`(head: ListNode?, expected: List<Int>) {
+            ReverseLinkedListIterative().test(head, expected)
+        }
+    }
+
+    @Nested
+    inner class ReverseLinkedListRecursiveTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should reverse a singly linked list`(head: ListNode?, expected: List<Int>) {
+            ReverseLinkedListRecursive().test(head, expected)
+        }
+    }
+
+    private fun ReverseLinkedList.test(head: ListNode?, expected: List<Int>) {
+        val actual = reverseList(head)
+        assertEquals(expected, actual.toList())
+    }
+}
