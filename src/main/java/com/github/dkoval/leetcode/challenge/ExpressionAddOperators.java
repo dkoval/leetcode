ee575715a474e3ef9d91c073599f72fda55568b7
dkoval <koval.dmitriy@gmail.com>
2021-09-18 20:49:42 +0200

    Document solution to "Expression Add Operators" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ExpressionAddOperators.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ExpressionAddOperators.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3979/">Expression Add Operators (Hard)</a>
+ * <a href="https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3979/">           (Hard)</a>
  * <p>
  * Given a string num that contains only digits and an integer target, return all possibilities
  * to add the binary operators '+', '-', or '*' between the digits of num so that the resultant expression evaluates
@@ -19,6 +19,7 @@ import java.util.List;
  */
 public class ExpressionAddOperators {
 
+    // (N * 4^N) time | O(N) space
     public List<String> addOperators(String num, int target) {
         List<String> answer = new ArrayList<>();
         dfs(num, target, 0, "", 0, 0, answer);
@@ -45,8 +46,12 @@ public class ExpressionAddOperators {
                 // hence 4 + 3 * 2 = 10. However, the expression gets evaluated like (4 + 3) * 2 = 14,
                 // which is obviously wrong. Therefore, to fix the math, we keep track of previous and current evaluations.
                 // At the time we get to (...) * 2 step, prevEval = 4, currEval = 4 + 3 = 7, x = 2.
-                // To correct evaluation of the current expression, use the formula
+                // To correct evaluation of the current expression, use the formula:
                 // prevEval + (currEval - prevEval) * x = 4 + (7 - 4) * 2 = 10.
+                // Note that instead of prevEval we could have kept track of the previous number.
+                // In this case the formula would change to:
+                // currEval - prevNum +  prevNum * x = 7 - 3 + 3 * 2 = 4 + 3 * 2 = 10.
+                // Update prevNum to prevNum * x to proceed to the next iteration.
                 dfs(num, target, i + 1, expr + "*" + s, prevEval, prevEval + (currEval - prevEval) * x, answer);
             }
 
