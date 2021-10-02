eaa141f1692b7ed7a30d9dd81f7ff62906b3571d
dkoval <koval.dmitriy@gmail.com>
2021-10-02 20:49:55 +0200

    Re-factor solution to "Dungeon Game" problem using DP array

--- src/main/java/com/github/dkoval/leetcode/challenge/DungeonGame.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/DungeonGame.java
@@ -60,30 +60,36 @@ public interface DungeonGame {
             int m = dungeon.length;
             int n = dungeon[0].length;
 
-            // hp[i][j] - min health points needed to reach the bottom-right corner from (i, j).
-            // Idea is to solve the problem in reverse.
+            // hp[i][j] - min health points needed in the bottom-right for the knight to stay alive
             int[][] hp = new int[m][n];
 
-            // Step #0: min health points needed int the bottom-right corner for the knight to survive.
+            // base case
             hp[m - 1][n - 1] = (dungeon[m - 1][n - 1] > 0) ? 1 : 1 - dungeon[m - 1][n - 1];
 
-            // Step #1: solve for the last column going bottom-up.
-            for (int i = m - 2; i >= 0; i--) {
-                hp[i][n - 1] = Math.max(hp[i + 1][n - 1] - dungeon[i][n - 1], 1);
-            }
+            // solve for the remaining cells going bottom-up and right-to-left
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= 0; j--) {
+                    if (i == m - 1 && j == n - 1) {
+                        continue;
+                    }
 
-            // Step #2: solve for the last row going from right to left.
-            for (int j = n - 2; j >= 0; j--) {
-                hp[m - 1][j] = Math.max(hp[m - 1][j + 1] - dungeon[m - 1][j], 1);
-            }
-
-            // Step #3: solve for the remaining cells going bottom-up
-            for (int i = m - 2; i >= 0; i--) {
-                for (int j = n - 2; j >= 0; j--) {
-                    hp[i][j] = Math.max(Math.min(hp[i + 1][j], hp[i][j + 1]) - dungeon[i][j], 1);
+                    int best = Integer.MAX_VALUE;
+                    // go right
+                    if (j + 1 < n) {
+                        best = Math.min(best, ensureAlive(hp[i][j + 1] - dungeon[i][j]));
+                    }
+                    // go down
+                    if (i + 1 < m) {
+                        best = Math.min(best, ensureAlive(hp[i + 1][j] - dungeon[i][j]));
+                    }
+                    hp[i][j] = best;
                 }
             }
             return hp[0][0];
+        }
+
+        private int ensureAlive(int healthPoints) {
+            return Math.max(healthPoints, 1);
         }
     }
 }
