6f7cc296e286c45ada684316562fc17038cf64af
dkoval <koval.dmitriy@gmail.com>
2021-10-07 17:36:46 +0200

    Revise solution to "Word Search" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/WordSearch.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/WordSearch.kt
@@ -11,11 +11,18 @@ package com.github.dkoval.leetcode.challenge
  */
 object WordSearch {
 
+    private val directions = arrayOf(
+        intArrayOf(-1, 0),
+        intArrayOf(1, 0),
+        intArrayOf(0, -1),
+        intArrayOf(0, 1)
+    )
+
     // Resource: https://www.youtube.com/watch?v=X0kX7PMOYi0&t=2s
     fun exist(board: Array<CharArray>, word: String): Boolean {
-        for (i in board.indices) {
-            for (j in board[0].indices) {
-                if (board[i][j] == word[0] && existDFS(board, i, j, word, 0)) {
+        for (row in board.indices) {
+            for (col in board[0].indices) {
+                if (board[row][col] == word[0] && existDFS(board, row, col, word, 0)) {
                     return true
                 }
             }
@@ -23,25 +30,38 @@ object WordSearch {
         return false
     }
 
-    private fun existDFS(board: Array<CharArray>, i: Int, j: Int, word: String, idx: Int): Boolean {
-        if (idx == word.lastIndex) {
+    private fun existDFS(board: Array<CharArray>, row: Int, col: Int, word: String, idx: Int): Boolean {
+        // base case
+        if (idx == word.length) {
             return true
         }
 
-        val ch = board[i][j]
-        board[i][j] = '#' // mark cell as visited
-        val nextIdx = idx + 1
-        val nextCh = word[nextIdx]
-        if (i > 0 && board[i - 1][j] == nextCh && existDFS(board, i - 1, j, word, nextIdx) ||
-            j > 0 && board[i][j - 1] == nextCh && existDFS(board, i, j - 1, word, nextIdx) ||
-            i < board.lastIndex && board[i + 1][j] == nextCh && existDFS(board, i + 1, j, word, nextIdx) ||
-            j < board[0].lastIndex && board[i][j + 1] == nextCh && existDFS(board, i, j + 1, word, nextIdx)
-        ) {
-            board[i][j] = ch // restore cell
-            return true
+        // boundary check
+        val m = board.size
+        val n = board[0].size
+        if (row !in 0 until m || col !in 0 until n) {
+            return false
         }
 
-        board[i][j] = ch // restore cell
+        if (board[row][col] != word[idx]) {
+            return false
+        }
+
+        // mark cell as visited
+        val c = board[row][col]
+        board[row][col] = '#'
+
+        // explore 4 adjacent directions
+        for ((dx, dy) in directions) {
+            if (existDFS(board, row + dx, col + dy, word, idx + 1)) {
+                // restore cell
+                board[row][col] = c
+                return true
+            }
+        }
+
+        // restore cell
+        board[row][col] = c
         return false
     }
 }
