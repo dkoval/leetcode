dfdaf4ccb359eef027d2e668d1d277f57e610da2
dkoval <koval.dmitriy@gmail.com>
2021-10-21 11:36:56 +0200

    Revise solution to "Randomized Set" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/RandomizedSet.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/RandomizedSet.kt
@@ -11,26 +11,32 @@ import kotlin.random.Random
  * - getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
  */
 class RandomizedSet {
-    private val values = ArrayList<Int>()
-    private val indexByValue = HashMap<Int, Int>()
+    private val values = arrayListOf<Int>()
+    private val indexByValue = hashMapOf<Int, Int>()
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     fun insert(`val`: Int): Boolean {
-        val exists = indexByValue.containsKey(`val`)
-        if (exists) {
+        if (exists(`val`)) {
             return false
         }
-        values.add(`val`)
+        values += `val`
         indexByValue[`val`] = values.lastIndex
         return true
     }
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     fun remove(`val`: Int): Boolean {
-        val index = indexByValue[`val`] ?: return false
-        values[index] = values.last()
-        indexByValue[values[index]] = index
+        if (!exists(`val`)) {
+            return false
+        }
+        val index = indexByValue[`val`]!!
+        val last = values.last()
+        // put the last element of `values` at the current index
+        values[index] = last
+        indexByValue[last] = index
+        // O(1) time: remove the last element from `values` list
         values.removeAt(values.lastIndex)
+        // O(1) time: remove `val` from `indexByValue` map
         indexByValue.remove(`val`)
         return true
     }
@@ -40,4 +46,6 @@ class RandomizedSet {
         val index = Random.nextInt(0, values.size)
         return values[index]
     }
+
+    private fun exists(`val`: Int): Boolean = `val` in indexByValue
 }
