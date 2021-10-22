71c3816bc62934beecba2c24f759d329e43583a1
dkoval <koval.dmitriy@gmail.com>
2021-10-22 09:46:05 +0200

    Revise solution to "Sort Characters By Frequency" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SortCharactersByFrequencyJava.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SortCharactersByFrequencyJava.java
@@ -2,24 +2,37 @@ package com.github.dkoval.leetcode.challenge;
 
 import java.util.*;
 
+/**
+ * <a href="https://leetcode.com/problems/sort-characters-by-frequency/">Sort Characters By Frequency</a>
+ * <p>
+ * Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+ * <p>
+ * Return the sorted string. If there are multiple answers, return any of them.
+ */
 public class SortCharactersByFrequencyJava {
 
+    // O(N * logN) time | O(alpha) space, where
+    // N - number of characters in input string s
+    // alpha - size of the alphabet
     public static String frequencySort(String s) {
         Map<Character, Integer> frequencies = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            frequencies.put(ch, frequencies.getOrDefault(ch, 0) + 1);
+            char c = s.charAt(i);
+            frequencies.put(c, frequencies.getOrDefault(c, 0) + 1);
         }
 
-        List<Map.Entry<Character, Integer>> sortedByFrequency = new ArrayList<>(frequencies.entrySet());
-        sortedByFrequency.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        // sort list of (char, frequency) pairs by their frequencies in decreasing order
+        List<Map.Entry<Character, Integer>> sorted = new ArrayList<>(frequencies.entrySet());
+        sorted.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
 
-        StringBuilder result = new StringBuilder();
-        for (Map.Entry<Character, Integer> charAndFrequency : sortedByFrequency) {
-            for (int i = 0; i < charAndFrequency.getValue(); i++) {
-                result.append(charAndFrequency.getKey());
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : sorted) {
+            char c = entry.getKey();
+            int frequency = entry.getValue();
+            while (frequency-- > 0) {
+                sb.append(c);
             }
         }
-        return result.toString();
+        return sb.toString();
     }
 }
