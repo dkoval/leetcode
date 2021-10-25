5eb28cd8b48bdf5c7ebee9813d9caa593409bc1b
dkoval <koval.dmitriy@gmail.com>
2021-10-25 09:01:33 +0200

    DP top-down and bottom-up solution to "Longest Common Subsequence" problem

--- src/main/java/com/github/dkoval/leetcode/problems/LongestCommonSubsequence.java
+++ src/main/java/com/github/dkoval/leetcode/problems/LongestCommonSubsequence.java
@@ -11,25 +11,67 @@ package com.github.dkoval.leetcode.problems;
  * <p>
  * If there is no common subsequence, return 0.
  */
-public class LongestCommonSubsequence {
+public interface LongestCommonSubsequence {
 
-    // Time complexity: O(M*N)
-    // Space complexity: O(M*N)
-    public int longestCommonSubsequence(String text1, String text2) {
-        // dp[i][j] - denotes solution to lcs(text1[0:i], text2[0:j]) sub-problem
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for (int i = 1; i <= text1.length(); i++) {
-            for (int j = 1; j <= text2.length(); j++) {
-                char c1 = text1.charAt(i - 1);
-                char c2 = text2.charAt(j - 1);
-                dp[i][j] = (c1 == c2)
-                        // remove last character from text1 and text2
-                        ? 1 + dp[i - 1][j - 1]
-                        // either remove last character from text1 and leave text2 unmodified, or
-                        // remove last character from text2 and leave text1 unmodified
-                        : Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
+    int longestCommonSubsequence(String text1, String text2);
+
+    // O(M*N) time | O(M*N) space
+    class LongestCommonSubsequenceDPTopDown implements LongestCommonSubsequence {
+
+        @Override
+        public int longestCommonSubsequence(String text1, String text2) {
+            int n1 = text1.length();
+            int n2 = text2.length();
+            return longestCommonSubsequence(text1, n1 - 1, text2, n2 - 1, new Integer[n1][n2]);
         }
-        return dp[text1.length()][text2.length()];
+
+        // returns solution to the longest common subsequence problem for text1[0:i] and text2[0:j] prefixes
+        private int longestCommonSubsequence(String text1, int i, String text2, int j, Integer[][] memo) {
+            if (i < 0 || j < 0) {
+                return 0;
+            }
+
+            if (memo[i][j] != null) {
+                return memo[i][j];
+            }
+
+            if (text1.charAt(i) == text2.charAt(j)) {
+                // remove i-th and j-th characters from text1 and text2 respectively
+                memo[i][j] = 1 + longestCommonSubsequence(text1, i - 1, text2, j - 1, memo);
+            } else {
+                // either remove i-th character from text1 and leave text2 unmodified, or
+                // remove j-th character from text2 and leave text1 unmodified
+                memo[i][j] = Math.max(
+                        longestCommonSubsequence(text1, i - 1, text2, j, memo),
+                        longestCommonSubsequence(text1, i, text2, j - 1, memo));
+            }
+            return memo[i][j];
+        }
+    }
+
+    // O(M*N) time | O(M*N) space
+    class LongestCommonSubsequenceDPBottomUp implements LongestCommonSubsequence {
+
+        @Override
+        public int longestCommonSubsequence(String text1, String text2) {
+            // dp[i][j] - denotes solution to lcs(text1[0:i], text2[0:j]) sub-problem
+            int n1 = text1.length();
+            int n2 = text2.length();
+            int[][] dp = new int[n1 + 1][n2 + 1];
+
+            for (int i = 1; i <= n1; i++) {
+                for (int j = 1; j <= n2; j++) {
+                    char c1 = text1.charAt(i - 1);
+                    char c2 = text2.charAt(j - 1);
+                    dp[i][j] = (c1 == c2)
+                            // remove last characters from text1[0:i] and text2[0:j] respectively
+                            ? 1 + dp[i - 1][j - 1]
+                            // either remove last character from text1[0:i] and leave text2[0:j] unmodified, or
+                            // remove last character from text2[0:j] and leave text1[0:i] unmodified
+                            : Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+            return dp[n1][n2];
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/problems/LongestCommonSubsequenceTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/LongestCommonSubsequenceTest.kt
@@ -1,25 +1,57 @@
 package com.github.dkoval.leetcode.problems
 
+import com.github.dkoval.leetcode.problems.LongestCommonSubsequence.LongestCommonSubsequenceDPBottomUp
+import com.github.dkoval.leetcode.problems.LongestCommonSubsequence.LongestCommonSubsequenceDPTopDown
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class LongestCommonSubsequenceTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of("abcde", "ace", 3),
             Arguments.of("abc", "abc", 3),
             Arguments.of("abc", "def", 0),
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the length of their longest common subsequence`(text1: String, text2: String, expected: Int) {
-        val actual = LongestCommonSubsequence().longestCommonSubsequence(text1, text2)
+    @Nested
+    inner class LongestCommonSubsequenceDPTopDownTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the length of their longest common subsequence`(
+            text1: String,
+            text2: String,
+            expected: Int
+        ) {
+            LongestCommonSubsequenceDPTopDown().test(text1, text2, expected)
+        }
+    }
+
+    @Nested
+    inner class LongestCommonSubsequenceDPBottomUpTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the length of their longest common subsequence`(
+            text1: String,
+            text2: String,
+            expected: Int
+        ) {
+            LongestCommonSubsequenceDPBottomUp().test(text1, text2, expected)
+        }
+    }
+
+    private fun LongestCommonSubsequence.test(text1: String, text2: String, expected: Int) {
+        val actual = longestCommonSubsequence(text1, text2)
         assertEquals(expected, actual)
     }
 }
