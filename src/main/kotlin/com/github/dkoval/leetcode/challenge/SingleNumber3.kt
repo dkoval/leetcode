94ed12825e5bfbe9118f18a1058a83d0264d7622
dkoval <koval.dmitriy@gmail.com>
2021-11-06 16:25:23 +0100

    Document solution

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/SingleNumber3.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/SingleNumber3.kt
@@ -24,17 +24,22 @@ object SmartSingleNumber3 : SingleNumber3 {
 
     // Resource: https://www.youtube.com/watch?v=L-EaPf5tD5A
     override fun singleNumber(nums: IntArray): IntArray {
+        // xy denotes x XOR y here, where x and y refer to two elements that appear only once in nums[].
+        // Note important property of XOR operator: x XOR x = 0
         var xy = 0
         for (num in nums) {
             xy = xy xor num
         }
-        val d = xy and -xy // find first least significant i-th bit set, i.e. 2^i = d
+
+        // Now, take any set bit in xy.
+        // Here we take the least significant bit: LSB = 2^i = x AND -x
+        val lsb = xy and -xy
         val result = IntArray(2)
+        // Take XOR of all elements in nums[] once again, but this time elements into 2 groups:
+        // with and without i-th bit set. Clearly, x and y will follow into different groups.
         for (num in nums) {
-            // split numbers into 2 groups:
             // 1st group with i-th bit set, 2nd group with i-th bit unset
-            // clearly, x and y will follow into different groups
-            if (num and d == 0) {
+            if (num and lsb == 0) {
                 result[0] = result[0] xor num
             } else {
                 result[1] = result[1] xor num
