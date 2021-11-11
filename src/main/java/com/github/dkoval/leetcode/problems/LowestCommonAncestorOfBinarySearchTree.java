434acb38c552775dab2a87e123693e5d31dfdf9c
dkoval <koval.dmitriy@gmail.com>
2021-11-11 18:31:24 +0100

    Solution to "Lowest Common Ancestor of a Binary Search Tree" problem

---
+++ src/main/java/com/github/dkoval/leetcode/problems/LowestCommonAncestorOfBinarySearchTree.java
@@ -0,0 +1,62 @@
+package com.github.dkoval.leetcode.problems;
+
+import com.github.dkoval.leetcode.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * <a href="https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/">Lowest Common Ancestor of a Binary Search Tree</a>
+ * <p>
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * <p>
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q
+ * as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree is in the range [2, 10^5]</li>
+ *  <li>-10^9 <= Node.val <= 10^9</li>
+ *  <li>All Node.val are unique</li>
+ *  <li>p != q</li>
+ *  <li>p and q will exist in the BST</li>
+ * </ul>
+ */
+public class LowestCommonAncestorOfBinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pPath = new ArrayList<>();
+        List<TreeNode> qPath = new ArrayList<>();
+
+        findInBST(root, p, pPath);
+        findInBST(root, q, qPath);
+
+        TreeNode lca = root;
+        int pIter = 0;
+        int qIter = 0;
+        while (pIter < pPath.size() && qIter < qPath.size()) {
+            TreeNode pNode = pPath.get(pIter++);
+            TreeNode qNode = qPath.get(qIter++);
+            if (pNode != qNode) {
+                return lca;
+            }
+            lca = pNode;
+        }
+        return lca;
+    }
+
+    private boolean findInBST(TreeNode root, TreeNode target, List<TreeNode> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+
+        if (root.val < target.val) {
+            return findInBST(root.right, target, path);
+        } else if (root.val > target.val) {
+            return findInBST(root.left, target, path);
+        } else {
+            return true;
+        }
+    }
+}
