3227428b1107c05653fa3bd639083d38b2b09d30
dkoval <koval.dmitriy@gmail.com>
2021-11-16 14:22:12 +0100

    Solution to "Product of Array Except Self" problem using O(N) extra space

---
+++ src/main/java/com/github/dkoval/leetcode/problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,47 @@
+package com.github.dkoval.leetcode.problems;
+
+/**
+ * <a href="https://leetcode.com/problems/product-of-array-except-self/">Product of Array Except Self</a>
+ * <p>
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ * <p>
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= nums.length <= 10^5</li>
+ *  <li>-30 <= nums[i] <= 30</li>
+ *  <li>The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer</li>
+ * </ul>
+ * <p>
+ * Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+ */
+public interface ProductOfArrayExceptSelf {
+
+    int[] productExceptSelf(int[] nums);
+
+    // O(N) time | O(N) extra space
+    class ProductOfArrayExceptSelfUsingExtraSpace implements ProductOfArrayExceptSelf {
+
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] prefixProduct = new int[n];
+            int[] suffixProduct = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                prefixProduct[i] = nums[i] * (i > 0 ? prefixProduct[i - 1] : 1);
+                suffixProduct[n - i - 1] = nums[n - i - 1] * (i > 0 ? suffixProduct[n - i] : 1);
+            }
+
+            int[] ans = new int[n];
+            ans[0] = suffixProduct[1];
+            ans[n - 1] = prefixProduct[n - 2];
+            for (int i = 1; i < n - 1; i++) {
+                ans[i] = prefixProduct[i - 1] * suffixProduct[i + 1];
+            }
+            return ans;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/ProductOfArrayExceptSelfTest.kt
@@ -0,0 +1,46 @@
+package com.github.dkoval.leetcode.problems
+
+import com.github.dkoval.leetcode.problems.ProductOfArrayExceptSelf.ProductOfArrayExceptSelfUsingExtraSpace
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class ProductOfArrayExceptSelfTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(24, 12, 8, 6)
+            ),
+            Arguments.of(
+                intArrayOf(-1, 1, 0, -3, 3),
+                intArrayOf(0, 0, 9, 0, 0)
+            )
+        )
+    }
+
+    @Nested
+    inner class ProductOfArrayExceptSelfUsingExtraSpaceTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return an array answer such that answer(i) is equal to the product of all the elements of nums except nums(i)`(
+            nums: IntArray,
+            expected: IntArray
+        ) {
+            ProductOfArrayExceptSelfUsingExtraSpace().test(nums, expected)
+        }
+    }
+
+    private fun ProductOfArrayExceptSelf.test(nums: IntArray, expected: IntArray) {
+        val actual = productExceptSelf(nums)
+        assertArrayEquals(expected, actual)
+    }
+}
