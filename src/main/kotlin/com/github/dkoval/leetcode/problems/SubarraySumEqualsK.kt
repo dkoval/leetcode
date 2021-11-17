c0975aaf955103726c9ec5da7e2a68cd86ac3d3b
dkoval <koval.dmitriy@gmail.com>
2021-11-17 23:34:49 +0100

    Document solution to "Subarray Sum Equals K" using prefix sum counting approach

--- src/main/kotlin/com/github/dkoval/leetcode/problems/SubarraySumEqualsK.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/problems/SubarraySumEqualsK.kt
@@ -33,16 +33,30 @@ object SubarraySumEqualsKBruteForce : SubarraySumEqualsK {
 object SubarraySumEqualsKUsingHashMap : SubarraySumEqualsK {
 
     override fun subarraySum(nums: IntArray, k: Int): Int {
-        // sum[i:j] = sum[0:j] - sum[0:i - 1]
-        var count = 0
+        // Prefix sum of first i elements of nums, i.e.
+        // sum(nums[0 : i]) = nums[0] + nums[1] + ... + nums[i]
         var sum = 0
-        // stores (sum[i], number of occurrences of sum[i]) pairs
-        val sumOccurrences = mutableMapOf<Int, Int>().apply { this[0] = 1 }
+
+        // The below map stores (sum[i] -> number of occurrences of sum[i]) pairs.
+        // The reason for counting occurrences of a prefix sum is because nums[i] can be negative, therefore
+        // we can have multiple prefixes that sum up to the same value, for example:
+        // nums = [1, -1, 1, ...], sum([1]) = sum([1, -1, 1]) = 1
+        val sumCounts = mutableMapOf<Int, Int>()
+        sumCounts[0] = 1
+
+        // sum(nums[i : j]) = sum(nums[0 : j]) - sum(nums[0 : i - 1])
+        var ans = 0
         for (num in nums) {
             sum += num
-            sumOccurrences[sum - k]?.also { numOccurrences -> count += numOccurrences }
-            sumOccurrences[sum] = sumOccurrences.getOrDefault(sum, 0) + 1
+
+            // Can we chop off x first elements from the current prefix to make sum == k?
+            val complementSum = sum - k
+            if (complementSum in sumCounts) {
+                ans += sumCounts[complementSum]!!
+            }
+
+            sumCounts[sum] = (sumCounts[sum] ?: 0) + 1
         }
-        return count
+        return ans
     }
 }
