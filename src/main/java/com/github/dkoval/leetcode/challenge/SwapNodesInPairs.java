a6f964602a3192c59587c9639a0ce1b8967f9a77
dkoval <koval.dmitriy@gmail.com>
2021-11-24 00:52:59 +0100

    Yet another solution to "Swap Nodes in Pairs" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SwapNodesInPairs.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SwapNodesInPairs.java
@@ -9,40 +9,84 @@ import com.github.dkoval.leetcode.ListNode;
  * <p>
  * You may not modify the values in the list's nodes. Only nodes itself may be changed.
  */
-public class SwapNodesInPairs {
+public interface SwapNodesInPairs {
+
+    ListNode swapPairs(ListNode head);
 
     // Resource: https://www.youtube.com/watch?v=o811TZLAWOo
-    // O(N) time | O(1) space
-    public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(-1, head);
-        ListNode prev = dummy;
-        ListNode curr = head;
+    class SwapNodesInPairsUsingDummyNode implements SwapNodesInPairs {
 
-        // make sure we have at lest 2 nodes to work with
-        while (curr != null && curr.next != null) {
-            // save pointers that are going to be altered
-            ListNode next = curr.next;
-            ListNode nextPair = next.next;
+        // O(N) time | O(1) space
+        @Override
+        public ListNode swapPairs(ListNode head) {
+            ListNode dummy = new ListNode(-1, head);
+            ListNode prev = dummy;
+            ListNode curr = head;
 
-            // Before: D -> 1 -> 2 | -> 3 -> 4 | -> ...
-            //         ^    ^    ^
-            //       prev curr next
-            // ---
-            // After:  D -> 2 -> 1 | -> 3 -> 4 | -> ...
-            //                   ^      ^
-            //                 prev    curr
+            // make sure we have at lest 2 nodes to work with
+            while (curr != null && curr.next != null) {
+                // save pointers that are going to be altered
+                ListNode next = curr.next;
+                ListNode nextPair = next.next;
 
-            // reverse (curr, next) pair of nodes
-            next.next = curr;
-            curr.next = nextPair;
+                // Before: D -> 1 -> 2 | -> 3 -> 4 | -> ...
+                //         ^    ^    ^
+                //       prev curr next
+                // ---
+                // After:  D -> 2 -> 1 | -> 3 -> 4 | -> ...
+                //                   ^      ^
+                //                 prev    curr
 
-            // connect previous pair
-            prev.next = next;
+                // reverse (curr, next) pair of nodes
+                next.next = curr;
+                curr.next = nextPair;
 
-            // prepare for the next iteration
-            prev = curr;
-            curr = nextPair;
+                // connect previous pair
+                prev.next = next;
+
+                // prepare for the next iteration
+                prev = curr;
+                curr = nextPair;
+            }
+            return dummy.next;
         }
-        return dummy.next;
     }
+
+     class SwapNodesInPairsHandlingEdgeCases implements SwapNodesInPairs {
+
+         // O(N) time | O(1) space
+        @Override
+         public ListNode swapPairs(ListNode head) {
+            // len(list) <= 1
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            // len(list) >= 2
+            ListNode prev = null;
+            ListNode curr = head;
+
+            // 2nd node is going to be the head of the modified list
+            head = head.next;
+
+            // make sure we have at lest 2 nodes to work with
+            while (curr != null && curr.next != null) {
+                // save pointers that are going to be updated
+                ListNode next = curr.next;
+                ListNode nextPair = next.next;
+
+                // reverse (curr, next) pair of nodes
+                next.next = curr;
+                curr.next = nextPair;
+                if (prev != null) {
+                    prev.next = next;
+                }
+
+                // prepare for the next iteration
+                prev = curr;
+                curr = nextPair;
+            }
+            return head;
+        }
+     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/SwapNodesInPairsTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/SwapNodesInPairsTest.kt
@@ -1,13 +1,57 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.ListNode
+import com.github.dkoval.leetcode.challenge.SwapNodesInPairs.SwapNodesInPairsHandlingEdgeCases
+import com.github.dkoval.leetcode.challenge.SwapNodesInPairs.SwapNodesInPairsUsingDummyNode
 import com.github.dkoval.leetcode.toList
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class SwapNodesInPairsTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                null,
+                listOf<Int>()
+            ),
+            Arguments.of(
+                ListNode(1),
+                listOf(1)
+            ),
+            Arguments.of(
+                ListNode(1).apply {
+                    next = ListNode(2)
+                },
+                listOf(2, 1)
+            ),
+            Arguments.of(
+                ListNode(1).apply {
+                    next = ListNode(2).apply {
+                        next = ListNode(3)
+                    }
+                },
+                listOf(2, 1, 3)
+            ),
+            Arguments.of(
+                ListNode(1).apply {
+                    next = ListNode(2).apply {
+                        next = ListNode(3).apply {
+                            next = ListNode(4)
+                        }
+                    }
+                },
+                listOf(2, 1, 4, 3)
+            )
+        )
+    }
 
     companion object {
         @JvmStatic
@@ -47,13 +91,34 @@ internal class SwapNodesInPairsTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should swap every two adjacent nodes and return the head of modified list`(
-        head: ListNode?,
-        expected: List<Int>
-    ) {
-        val actual = SwapNodesInPairs().swapPairs(head)
+    @Nested
+    inner class SwapNodesInPairsUsingDummyNodeTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should swap every two adjacent nodes and return the head of modified list`(
+            head: ListNode?,
+            expected: List<Int>
+        ) {
+            SwapNodesInPairsUsingDummyNode().test(head, expected)
+        }
+    }
+
+    @Nested
+    inner class SwapNodesInPairsHandlingEdgeCasesTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should swap every two adjacent nodes and return the head of modified list`(
+            head: ListNode?,
+            expected: List<Int>
+        ) {
+            SwapNodesInPairsHandlingEdgeCases().test(head, expected)
+        }
+    }
+
+    private fun SwapNodesInPairs.test(head: ListNode?, expected: List<Int>) {
+        val actual = swapPairs(head)
         assertEquals(expected, actual.toList())
     }
 }
