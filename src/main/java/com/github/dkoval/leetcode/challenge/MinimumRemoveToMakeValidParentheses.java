8fb55ca3df62918f2d2ceb9b1f222bad12cd7f06
dkoval <koval.dmitriy@gmail.com>
2021-11-25 22:13:00 +0100

    Solution to "Minimum Remove to Make Valid Parentheses" problem using a stack

--- src/main/java/com/github/dkoval/leetcode/challenge/MinimumRemoveToMakeValidParentheses.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumRemoveToMakeValidParentheses.java
@@ -1,5 +1,9 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * <a href="https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3645/">Minimum Remove to Make Valid Parentheses</a>
  *
@@ -15,47 +19,102 @@ package com.github.dkoval.leetcode.challenge;
  *  <li>It can be written as (A), where A is a valid string.</li>
  * </ul>
  */
-public class MinimumRemoveToMakeValidParentheses {
+public interface MinimumRemoveToMakeValidParentheses {
 
-    private final static char MARKER = '#';
+    String minRemoveToMakeValid(String s);
 
-    public String minRemoveToMakeValid(String s) {
-        char[] chars = s.toCharArray();
+    class MinimumRemoveToMakeValidParenthesesUsingCounter implements MinimumRemoveToMakeValidParentheses {
 
-        // scan chars[] left-to-right to mark ')' chars that do not have a match
-        matchParentheses(chars, true);
-        // scan chars[] right-to-left to mark '(' chars that do not have a match
-        matchParentheses(chars, false);
+        private final static char MARKER = '#';
 
-        StringBuilder sb = new StringBuilder();
-        for (char c : chars) {
-            if (c != MARKER) {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
+        @Override
+        public String minRemoveToMakeValid(String s) {
+            char[] chars = s.toCharArray();
 
-    private void matchParentheses(char[] chars, boolean forward) {
-        int i = forward ? 0 : chars.length - 1;
-        final int end = forward ? chars.length : -1;
-        final int step = forward ? 1 : -1;
+            // scan chars[] left-to-right to mark ')' chars that do not have a match
+            matchParentheses(chars, true);
+            // scan chars[] right-to-left to mark '(' chars that do not have a match
+            matchParentheses(chars, false);
 
-        final char parenthesisToMatchWith = forward ? '(' : ')';
-        final char matchingParenthesis = forward ? ')' : '(';
-
-        int count = 0;
-        while (i != end) {
-            if (chars[i] == parenthesisToMatchWith) {
-                count++;
-            } else if (chars[i] == matchingParenthesis) {
-                if (count > 0) {
-                    count--;
-                } else {
-                    chars[i] = MARKER;
+            StringBuilder sb = new StringBuilder();
+            for (char c : chars) {
+                if (c != MARKER) {
+                    sb.append(c);
                 }
             }
-            i += step;
+            return sb.toString();
+        }
+
+        private void matchParentheses(char[] chars, boolean forward) {
+            int i = forward ? 0 : chars.length - 1;
+            final int end = forward ? chars.length : -1;
+            final int step = forward ? 1 : -1;
+
+            final char parenthesisToMatchWith = forward ? '(' : ')';
+            final char matchingParenthesis = forward ? ')' : '(';
+
+            int count = 0;
+            while (i != end) {
+                if (chars[i] == parenthesisToMatchWith) {
+                    count++;
+                } else if (chars[i] == matchingParenthesis) {
+                    if (count > 0) {
+                        count--;
+                    } else {
+                        chars[i] = MARKER;
+                    }
+                }
+                i += step;
+            }
+        }
+    }
+
+    class MinimumRemoveToMakeValidParenthesesUsingStack implements MinimumRemoveToMakeValidParentheses {
+
+        @Override
+        public String minRemoveToMakeValid(String s) {
+            int n = s.length();
+            Stack<Character> stack = new Stack<>();
+            Set<Integer> badIndices = new HashSet<>();
+
+            // step #1: going from left to right, find obsolete ')' parentheses
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                if (c == '(') {
+                    stack.push(c);
+                } else if (c == ')') {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    } else {
+                        badIndices.add(i);
+                    }
+                }
+            }
+
+            stack.clear();
+
+            // step #2: going from right to left, find obsolete '(' parentheses
+            for (int i = n - 1; i >= 0; i--) {
+                char c = s.charAt(i);
+                if (c == ')') {
+                    stack.push(c);
+                } else if (c == '(') {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    } else {
+                        badIndices.add(i);
+                    }
+                }
+            }
+
+            // finally, construct the result
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                if (!badIndices.contains(i)) {
+                    sb.append(s.charAt(i));
+                }
+            }
+            return sb.toString();
         }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumRemoveToMakeValidParenthesesTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumRemoveToMakeValidParenthesesTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MinimumRemoveToMakeValidParentheses.MinimumRemoveToMakeValidParenthesesUsingCounter
+import com.github.dkoval.leetcode.challenge.MinimumRemoveToMakeValidParentheses.MinimumRemoveToMakeValidParenthesesUsingStack
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MinimumRemoveToMakeValidParenthesesTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 "lee(t(c)o)de)",
                 "lee(t(c)o)de"
@@ -29,13 +35,34 @@ internal class MinimumRemoveToMakeValidParenthesesTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should remove the minimum number of parentheses so that the resulting parentheses string is valid`(
-        s: String,
-        expected: String
-    ) {
-        val actual = MinimumRemoveToMakeValidParentheses().minRemoveToMakeValid(s)
+    @Nested
+    inner class MinimumRemoveToMakeValidParenthesesUsingCounterTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should remove the minimum number of parentheses so that the resulting parentheses string is valid`(
+            s: String,
+            expected: String
+        ) {
+            MinimumRemoveToMakeValidParenthesesUsingCounter().test(s, expected)
+        }
+    }
+
+    @Nested
+    inner class MinimumRemoveToMakeValidParenthesesUsingStackTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should remove the minimum number of parentheses so that the resulting parentheses string is valid`(
+            s: String,
+            expected: String
+        ) {
+            MinimumRemoveToMakeValidParenthesesUsingStack().test(s, expected)
+        }
+    }
+
+    private fun MinimumRemoveToMakeValidParentheses.test(s: String, expected: String) {
+        val actual = minRemoveToMakeValid(s)
         assertEquals(expected, actual)
     }
 }
