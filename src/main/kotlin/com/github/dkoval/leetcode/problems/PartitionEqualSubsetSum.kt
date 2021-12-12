3bf7af38039da85422d00f7a8bf2344af05d02ac
dkoval <koval.dmitriy@gmail.com>
2021-12-12 20:33:45 +0100

    Cosmetic improvements

--- src/main/kotlin/com/github/dkoval/leetcode/problems/PartitionEqualSubsetSum.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/problems/PartitionEqualSubsetSum.kt
@@ -13,10 +13,7 @@ package com.github.dkoval.leetcode.problems
 object PartitionEqualSubsetSum {
 
     fun canPartition(nums: IntArray): Boolean {
-        var sum = 0
-        for (num in nums) {
-            sum += num
-        }
+        val sum = nums.sum()
         if (sum % 2 != 0) return false
         return canPartition(nums, 0, 0, sum, mutableMapOf())
     }
@@ -32,9 +29,12 @@ object PartitionEqualSubsetSum {
     ): Boolean {
         if (currSum == targetSum / 2) return true
         if (index == nums.size || currSum > targetSum / 2) return false
+
         val key = Key(index, currSum)
-        val prevResult = memo[key]
-        if (prevResult != null) return prevResult
+        if (key in memo) {
+            return memo[key]!!
+        }
+
         // include/exclude nums[index] element
         val result = canPartition(nums, index + 1, currSum + nums[index], targetSum, memo)
                 || canPartition(nums, index + 1, currSum, targetSum, memo)
