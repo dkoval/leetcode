3c2264c9ab4bfe9327da4b6964cd26c92ef36c3f
dkoval <koval.dmitriy@gmail.com>
2021-12-30 17:03:05 +0100

    Fix solution to "Smallest Integer Divisible by K" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SmallestIntegerDivisibleByK.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SmallestIntegerDivisibleByK.java
@@ -26,15 +26,20 @@ public interface SmallestIntegerDivisibleByK {
             int rem = 1 % k;
             int length = 1;
 
+            // Properties of modular arithmetic:
+            // (a + b) % c = (a % c + b % c) % c
+            // (a * b) % c = (a % c * b % c) % c
+
             // Given N1 = 11...1, rem1 = N1 % k, where k >= 1, let's compute next N2 and rem2 values:
             // N2 = N1 * 10 + 1 (add digit 1 to the right of N1)
-            // rem2 = N2 % k = (N1 * 10 + 1) % k = ((N1 % k) * 10 + 1) % k = (rem1 * 10 + 1) % k
+            // rem2 = N2 % k = (N1 * 10 + 1) % k = ((N1 * 10) % k + 1 % k) % k = ((N1 % k * 10 % k) % k + 1 % k) % k =
+            // = (N1 % k * 10 % k + 1) % k = (rem1 * 10 % k + 1) % k
 
             // seen[] stores all possible remainders of N % k, i.e. {0, 1, ..., k - 1}.
             // The array is used to avoid running into infinite loop if N = 11...1 such that N % k = 0 doesn't exist.
             boolean[] seen = new boolean[k];
             while (rem != 0) {
-                rem = (rem * 10 + 1) % k;
+                rem = (rem * 10 % k + 1) % k;
                 length++;
                 if (seen[rem]) {
                     return -1;
@@ -53,12 +58,10 @@ public interface SmallestIntegerDivisibleByK {
             // Observation. In the previous solution seen[] stores k possible remainders of N % k, which means that
             // if N = 11...1 such that N % k = 0 exists, the while-loop executes <= k times.
             int rem = 0;
-            int length = 0;
             for (int i = 1; i <= k; i++) {
-                rem = (rem * 10 + 1) % k;
-                length++;
+                rem = (rem * 10 % k + 1) % k;
                 if (rem == 0) {
-                    return length;
+                    return i;
                 }
             }
             return -1;
