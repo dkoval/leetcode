c4c0e7548c545d24404d5528ad22f5c7f5df8b23
dkoval <koval.dmitriy@gmail.com>
2022-01-01 23:21:03 +0100

    Re-implement solution to "Burst Balloons" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/BurstBalloons.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/BurstBalloons.java
@@ -15,39 +15,74 @@ package com.github.dkoval.leetcode.challenge;
  * You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
  * 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
  */
-public class BurstBalloons {
+public interface BurstBalloons {
 
-    public int maxCoins(int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
-        }
+    int maxCoins(int[] nums);
 
-        int[] balloons = new int[n + 2];
-        balloons[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            balloons[i] = nums[i - 1];
-        }
-        balloons[n + 1] = 1;
+    class BurstBalloonsInitial implements BurstBalloons {
 
-        int[][] dp = new int[n + 1][n + 1];
-        for (int left = n; left >= 1; left--) {
-            for (int right = left; right <= n; right++) {
-                if (left == right) {
-                    // burst left balloon
-                    dp[left][right] = balloons[left - 1] * balloons[left] * balloons[left + 1];
-                } else {
-                    // p == left
-                    dp[left][right] = dp[left + 1][right] + balloons[left - 1] * balloons[left] * balloons[right + 1];
-                    // p == right
-                    dp[left][right] = Math.max(dp[left][right], dp[left][right - 1] + balloons[left - 1] * balloons[right] * balloons[right + 1]);
-                    // left < p < right
-                    for (int p = left + 1; p < right; p++) {
-                        dp[left][right] = Math.max(dp[left][right], dp[left][p - 1] + dp[p + 1][right] + balloons[p] * balloons[left - 1] * balloons[right + 1]);
+        @Override
+        public int maxCoins(int[] nums) {
+            int n = nums.length;
+            if (n == 0) {
+                return 0;
+            }
+
+            int[] balloons = new int[n + 2];
+            balloons[0] = 1;
+            for (int i = 1; i <= n; i++) {
+                balloons[i] = nums[i - 1];
+            }
+            balloons[n + 1] = 1;
+
+            int[][] dp = new int[n + 1][n + 1];
+            for (int left = n; left >= 1; left--) {
+                for (int right = left; right <= n; right++) {
+                    if (left == right) {
+                        // burst left balloon
+                        dp[left][right] = balloons[left - 1] * balloons[left] * balloons[left + 1];
+                    } else {
+                        // p == left
+                        dp[left][right] = dp[left + 1][right] + balloons[left - 1] * balloons[left] * balloons[right + 1];
+                        // p == right
+                        dp[left][right] = Math.max(dp[left][right], dp[left][right - 1] + balloons[left - 1] * balloons[right] * balloons[right + 1]);
+                        // left < p < right
+                        for (int p = left + 1; p < right; p++) {
+                            dp[left][right] = Math.max(dp[left][right], dp[left][p - 1] + dp[p + 1][right] + balloons[p] * balloons[left - 1] * balloons[right + 1]);
+                        }
                     }
                 }
             }
+            return dp[1][n];
         }
-        return dp[1][n];
+    }
+
+    // O(N^3) time | O(N^2) space
+    class BurstBalloonsRemastered implements BurstBalloons {
+
+        // Explanation of the algorithm: https://www.youtube.com/watch?v=IFNibRVgFBo
+        @Override
+        public int maxCoins(int[] nums) {
+            int n = nums.length;
+
+            // dp[i][j] - the maximum coins you can collect considering balloons[i:j]
+            int[][] dp = new int[n][n];
+            // consider all sub-arrays of balloons[i:j] and find the last balloon to burst in the sub-array to maximize the number of collected coins
+            for (int len = 1; len <= n; len++) {
+                for (int i = 0; i <= n - len; i++) {
+                    int j = i + len - 1;
+                    // balloons[k] is the last balloon to burst in balloons[i:j]
+                    for (int k = i; k <= j; k++) {
+                        int numCoins = (k == i ? 0 : dp[i][k - 1]) // left side: if k = i, there is nothing to the left
+                                + (i == 0 ? 1 : nums[i - 1]) * nums[k] * (j == n - 1 ? 1 : nums[j + 1]) // balloon to burst
+                                + (k == j ? 0 : dp[k + 1][j]); // right side: if k = j, there is nothing to the right
+
+                        //System.out.printf("Length = %d, sub-array = [%d:%d], last balloon to burst = %d, coins = %d\n", len, i, j, k, numCoins);
+                        dp[i][j] = Math.max(dp[i][j], numCoins);
+                    }
+                }
+            }
+            return dp[0][n - 1];
+        }
     }
 }
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/BurstBalloonsTest.kt
@@ -0,0 +1,60 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.BurstBalloons.BurstBalloonsInitial
+import com.github.dkoval.leetcode.challenge.BurstBalloons.BurstBalloonsRemastered
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class BurstBalloonsTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(3, 1, 5, 8),
+                167
+            ),
+            Arguments.of(
+                intArrayOf(1, 5),
+                10
+            )
+        )
+    }
+
+    @Nested
+    inner class BurstBalloonsInitialTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum coins you can collect by bursting the balloons wisely`(
+            nums: IntArray,
+            expected: Int
+        ) {
+            BurstBalloonsInitial().test(nums, expected)
+        }
+    }
+
+    @Nested
+    inner class BurstBalloonsRemasteredTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum coins you can collect by bursting the balloons wisely`(
+            nums: IntArray,
+            expected: Int
+        ) {
+            BurstBalloonsRemastered().test(nums, expected)
+        }
+    }
+
+    private fun BurstBalloons.test(nums: IntArray, expected: Int) {
+        val actual = maxCoins(nums)
+        assertEquals(expected, actual)
+    }
+}
