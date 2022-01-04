52e72150ba2a45b1f3eba7902bbc2dd130d45b10
dkoval <koval.dmitriy@gmail.com>
2022-01-04 15:31:25 +0100

    Polish implementation details of a solution to "Design HashMap" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/DesignHashMap.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/DesignHashMap.java
@@ -2,6 +2,7 @@ package com.github.dkoval.leetcode.challenge;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * <a href="https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3663/">Design HashMap</a>
@@ -24,9 +25,7 @@ import java.util.LinkedList;
 public class DesignHashMap {
 
     public static class MyHashMap {
-        private static final int DEFAULT_SIZE = 1000;
-
-        private final LinkedList<Entry>[] table = new LinkedList[DEFAULT_SIZE];
+        private static final int DEFAULT_SIZE = 1009; // prime number
 
         private static class Entry {
             final int key;
@@ -38,36 +37,35 @@ public class DesignHashMap {
             }
         }
 
+        @SuppressWarnings("unchecked")
+        private final List<Entry>[] table = new List[DEFAULT_SIZE];
+
         /** Initialize your data structure here. */
         public MyHashMap() {
-            // NOOP
+            // nothing to do here
         }
 
         /** value will always be non-negative. */
         public void put(int key, int value) {
-            int idx = key % table.length;
-            LinkedList<Entry> bucket = table[idx];
-            if (bucket != null) {
+            List<Entry> bucket = getBucket(key);
+            if (!bucket.isEmpty()) {
                 for (Entry entry : bucket) {
-                    if (entry.key == key) {
+                    if (key == entry.key) {
                         entry.value = value;
                         return;
                     }
                 }
-            } else {
-                bucket = new LinkedList<>();
-                table[idx] = bucket;
             }
+            // add a new entry if the bucket is either empty, or the given key is yet not in the bucket
             bucket.add(new Entry(key, value));
         }
 
         /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
         public int get(int key) {
-            int idx = key % table.length;
-            LinkedList<Entry> bucket = table[idx];
-            if (bucket != null) {
+            List<Entry> bucket = getBucket(key);
+            if (!bucket.isEmpty()) {
                 for (Entry entry : bucket) {
-                    if (entry.key == key) {
+                    if (key == entry.key) {
                         return entry.value;
                     }
                 }
@@ -77,18 +75,27 @@ public class DesignHashMap {
 
         /** Removes the mapping of the specified value key if this map contains a mapping for the key */
         public void remove(int key) {
-            int idx = key % table.length;
-            LinkedList<Entry> bucket = table[idx];
-            if (bucket != null) {
+            List<Entry> bucket = getBucket(key);
+            if (!bucket.isEmpty()) {
                 Iterator<Entry> it = bucket.iterator();
                 while (it.hasNext()) {
                     Entry entry = it.next();
-                    if (entry.key == key) {
+                    if (key == entry.key) {
                         it.remove();
                         return;
                     }
                 }
             }
+        }
+
+        private List<Entry> getBucket(int key) {
+            int idx = key % table.length;
+            List<Entry> bucket = table[idx];
+            if (bucket == null) {
+                bucket = new LinkedList<>();
+                table[idx] = bucket;
+            }
+            return bucket;
         }
     }
 }
