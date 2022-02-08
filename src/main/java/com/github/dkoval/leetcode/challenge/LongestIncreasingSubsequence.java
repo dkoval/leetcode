3903f4866566813eec57d3e5f7764df1d71ad8a9
dkoval <koval.dmitriy@gmail.com>
2022-02-08 13:08:06 +0100

    Code readability improvements

--- src/main/java/com/github/dkoval/leetcode/challenge/LongestIncreasingSubsequence.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/LongestIncreasingSubsequence.java
@@ -1,7 +1,5 @@
 package com.github.dkoval.leetcode.challenge;
 
-import java.util.Arrays;
-
 /**
  * <a href="https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/609/week-2-july-8th-july-14th/3808/">Longest Increasing Subsequence</a>
  * <p>
@@ -25,11 +23,11 @@ public class LongestIncreasingSubsequence {
 
         // dp[i] is the length of LIS ending at index i
         int[] dp = new int[n];
-        Arrays.fill(dp, 1);
 
         int best = 1;
-        for (int i = 1; i < n; i++) {
-            // check numbers before the i-th index to see if we can we further extend LIS ending at index i
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            // check numbers before the i-th index to see if we can further extend a LIS ending at index i
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     // append nums[i] to LIS ending at index j
