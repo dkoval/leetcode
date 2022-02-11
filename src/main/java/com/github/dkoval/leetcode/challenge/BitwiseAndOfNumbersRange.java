66d1fdbf3daa0ea6dd8060f2ee3d6ca35a03e64e
dkoval <koval.dmitriy@gmail.com>
2022-02-11 10:26:25 +0100

    Revise solution to "Bitwise AND of Numbers Range" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/BitwiseAndOfNumbersRange.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/BitwiseAndOfNumbersRange.java
@@ -10,38 +10,68 @@ package com.github.dkoval.leetcode.challenge;
  * <p>
  * 0 <= left <= right <= 2^31 - 1
  */
-public class BitwiseAndOfNumbersRange {
+public interface BitwiseAndOfNumbersRange {
+
+    int rangeBitwiseAnd(int left, int right);
+
+    // Resource: https://www.youtube.com/watch?v=6aHmj9ihjMY
+    class BitwiseAndOfNumbersRangeByFindingCommonPrefix implements BitwiseAndOfNumbersRange {
+
+        @Override
+        public int rangeBitwiseAnd(int left, int right) {
+            // idea: find common prefix in left and right
+            int ans = 0;
+            // iterate from MSB to LSB
+            // 0 <= left <= right <= 2^31 - 1, therefore we can skip 31st bit
+            for (int i = 30; i >= 0; i--) {
+                int bitInLeft = left & (1 << i);
+                int bitInRight = right & (1 << i);
+                if (bitInLeft != bitInRight) {
+                    // there will be x in [left, right] with (i - 1) .. 0 bits set to 0, therefore bitwise AND will yield 0
+                    // x = [prefix] b [00...0]
+                    //              ^ i-th bit is a mismatch
+                    break;
+                }
+                ans |= bitInLeft;
+            }
+            return ans;
+        }
+    }
 
     // Resource: https://www.youtube.com/watch?v=VHWd8STTVZs
-    // M = log2(left) - number of bits in `left`
-    // N = log2(right) - number of bits in `right`
-    // O(max(M, N)) time | O(max(M, N)) space
-    public int rangeBitwiseAnd(int left, int right) {
-        // consider one bit at a time, going from LSB to MSB
-        int answer = 0;
-        int mask = 1;
-        while (left > 0 && right > 0) {
-            boolean onlyOnes = onlyOnes(left, right);
-            if (onlyOnes) {
-                answer |= mask;
+    class BitwiseAndOfNumbersLarry implements BitwiseAndOfNumbersRange {
+
+        // M = log2(left) - number of bits in `left`
+        // N = log2(right) - number of bits in `right`
+        // O(max(M, N)) time | O(max(M, N)) space
+        @Override
+        public int rangeBitwiseAnd(int left, int right) {
+            // consider one bit at a time, going from LSB to MSB
+            int answer = 0;
+            int mask = 1;
+            while (left > 0 && right > 0) {
+                boolean onlyOnes = onlyOnes(left, right);
+                if (onlyOnes) {
+                    answer |= mask;
+                }
+                mask <<= 1;
+                left = dropLastBit(left);
+                right = dropLastBit(right);
             }
-            mask <<= 1;
-            left = dropLastBit(left);
-            right = dropLastBit(right);
+            return answer;
         }
-        return answer;
-    }
 
-    private boolean onlyOnes(int x, int y) {
-        return getLastBit(x) == 1 && getLastBit(y) == 1 // compare last bits of x and y
-                && dropLastBit(x) == dropLastBit(y); // compare "prefixes" of x and y
-    }
+        private boolean onlyOnes(int x, int y) {
+            return getLastBit(x) == 1 && getLastBit(y) == 1 // compare last bits of x and y
+                    && dropLastBit(x) == dropLastBit(y); // compare "prefixes" of x and y
+        }
 
-    private int getLastBit(int x) {
-        return x & 1;
-    }
+        private int getLastBit(int x) {
+            return x & 1;
+        }
 
-    private int dropLastBit(int x) {
-        return x >> 1;
+        private int dropLastBit(int x) {
+            return x >> 1;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/BitwiseAndOfNumbersRangeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/BitwiseAndOfNumbersRangeTest.kt
@@ -1,25 +1,57 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.BitwiseAndOfNumbersRange.BitwiseAndOfNumbersLarry
+import com.github.dkoval.leetcode.challenge.BitwiseAndOfNumbersRange.BitwiseAndOfNumbersRangeByFindingCommonPrefix
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class BitwiseAndOfNumbersRangeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(5, 7, 4),
             Arguments.of(0, 0, 0),
             Arguments.of(1, 2147483647, 0)
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the bitwise AND of all numbers in the given range, inclusive`(left: Int, right: Int, expected: Int) {
-        val actual = BitwiseAndOfNumbersRange().rangeBitwiseAnd(left, right)
+    @Nested
+    inner class BitwiseAndOfNumbersRangeByFindingCommonPrefixTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the bitwise AND of all numbers in the given range, inclusive`(
+            left: Int,
+            right: Int,
+            expected: Int
+        ) {
+            BitwiseAndOfNumbersRangeByFindingCommonPrefix().test(left, right, expected)
+        }
+    }
+
+    @Nested
+    inner class BitwiseAndOfNumbersLarryTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the bitwise AND of all numbers in the given range, inclusive`(
+            left: Int,
+            right: Int,
+            expected: Int
+        ) {
+            BitwiseAndOfNumbersLarry().test(left, right, expected)
+        }
+    }
+
+    private fun BitwiseAndOfNumbersRange.test(left: Int, right: Int, expected: Int) {
+        val actual = rangeBitwiseAnd(left, right)
         assertEquals(expected, actual)
     }
 }
