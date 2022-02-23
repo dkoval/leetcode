6c60421148b8c31d968ce3b84d418a556a4e6297
dkoval <koval.dmitriy@gmail.com>
2022-02-23 22:59:39 +0100

    Rewrite solution to "Lowest Common Ancestor of a Binary Tree" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/LowestCommonAncestorOfBinaryTree.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/LowestCommonAncestorOfBinaryTree.java
@@ -2,61 +2,124 @@ package com.github.dkoval.leetcode.challenge;
 
 import com.github.dkoval.leetcode.TreeNode;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/607/week-5-june-29th-june-30th/3797/">Lowest Common Ancestor of a Binary Tree</a>
+ * <a href="https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/">Lowest Common Ancestor of a Binary Tree</a>
  * <p>
  * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
  * <p>
  * According to the definition of LCA on Wikipedia:
  * “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q
  * as descendants (where we allow a node to be a descendant of itself).”
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree is in the range [2, 10^5]</li>
+ *  <li>-10^9 <= Node.val <= 10^9</li>
+ *  <li>All Node.val are unique</li>
+ *  <li>p != q</li>
+ *  <li>p and q will exist in the BST</li>
+ * </ul>
  */
-public class LowestCommonAncestorOfBinaryTree {
+public interface LowestCommonAncestorOfBinaryTree {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Deque<TreeNode> pPath = find(root, p);
-        Deque<TreeNode> qPath = find(root, q);
+    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q);
 
-        // While traversing p- and q- paths, find the first p != q pair.
-        // If found, LCA is the previous node in the path, otherwise - the root node.
-        TreeNode lca = root;
-        while (!pPath.isEmpty() && !qPath.isEmpty()) {
-            TreeNode pNode = pPath.pollFirst();
-            TreeNode qNode = qPath.pollFirst();
-            if (pNode.val != qNode.val) {
-                return lca;
+    class LowestCommonAncestorOfBinaryTreeUsingListToStorePath implements LowestCommonAncestorOfBinaryTree {
+
+        @Override
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            List<TreeNode> pPath = path(root, p);
+            List<TreeNode> qPath = path(root, q);
+
+            TreeNode lca = root;
+            int i = 0;
+            while (i < pPath.size() && i < qPath.size()) {
+                TreeNode pNode = pPath.get(i);
+                TreeNode qNode = qPath.get(i);
+                if (pNode.val != qNode.val) {
+                    break;
+                }
+                lca = pNode;
+                i++;
             }
-            lca = pNode;
-        }
-        return lca;
-    }
-
-    private Deque<TreeNode> find(TreeNode root, TreeNode target) {
-        if (root == null) {
-            return new LinkedList<>();
+            return lca;
         }
 
-        if (root.val == target.val) {
-            Deque<TreeNode> path = new LinkedList<>();
-            path.addFirst(root);
+        private List<TreeNode> path(TreeNode root, TreeNode target) {
+            List<TreeNode> path = new ArrayList<>();
+            dfs(root, target, path);
             return path;
         }
 
-        Deque<TreeNode> left = find(root.left, target);
-        if (!left.isEmpty()) {
-            left.addFirst(root);
-            return left;
+        private boolean dfs(TreeNode root, TreeNode target, List<TreeNode> path) {
+            if (root == null) {
+                return false;
+            }
+
+            path.add(root);
+            if (root.val == target.val) {
+                return true;
+            }
+
+            boolean found = dfs(root.left, target, path) || dfs(root.right, target, path);
+            if (!found) {
+                // backtrack
+                path.remove(path.size() - 1);
+            }
+            return found;
+        }
+    }
+
+    class LowestCommonAncestorOfBinaryTreeUsingDequeToStorePath implements LowestCommonAncestorOfBinaryTree {
+
+        @Override
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            Deque<TreeNode> pPath = path(root, p);
+            Deque<TreeNode> qPath = path(root, q);
+
+            // While traversing p- and q- paths, find the first p != q pair.
+            // If found, LCA is the previous node in the path, otherwise - the root node.
+            TreeNode lca = root;
+            while (!pPath.isEmpty() && !qPath.isEmpty()) {
+                TreeNode pNode = pPath.pollFirst();
+                TreeNode qNode = qPath.pollFirst();
+                if (pNode.val != qNode.val) {
+                    return lca;
+                }
+                lca = pNode;
+            }
+            return lca;
         }
 
-        Deque<TreeNode> right = find(root.right, target);
-        if (!right.isEmpty()) {
-            right.addFirst(root);
-            return right;
-        }
+        private Deque<TreeNode> path(TreeNode root, TreeNode target) {
+            if (root == null) {
+                return new LinkedList<>();
+            }
 
-        return new LinkedList<>();
+            if (root.val == target.val) {
+                Deque<TreeNode> path = new LinkedList<>();
+                path.addFirst(root);
+                return path;
+            }
+
+            Deque<TreeNode> left = path(root.left, target);
+            if (!left.isEmpty()) {
+                left.addFirst(root);
+                return left;
+            }
+
+            Deque<TreeNode> right = path(root.right, target);
+            if (!right.isEmpty()) {
+                right.addFirst(root);
+                return right;
+            }
+
+            return new LinkedList<>();
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/LowestCommonAncestorOfBinaryTreeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/LowestCommonAncestorOfBinaryTreeTest.kt
@@ -1,16 +1,22 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.LowestCommonAncestorOfBinaryTree.LowestCommonAncestorOfBinaryTreeUsingDequeToStorePath
+import com.github.dkoval.leetcode.challenge.LowestCommonAncestorOfBinaryTree.LowestCommonAncestorOfBinaryTreeUsingListToStorePath
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class LowestCommonAncestorOfBinaryTreeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 TreeNode(3).apply {
                     left = TreeNode(5).apply {
@@ -58,15 +64,38 @@ class LowestCommonAncestorOfBinaryTreeTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find the lowest common ancestor of two given nodes in the tree`(
-        root: TreeNode,
-        p: Int,
-        q: Int,
-        expected: Int
-    ) {
-        val actual = LowestCommonAncestorOfBinaryTree().lowestCommonAncestor(root, TreeNode(p), TreeNode(q))
+    @Nested
+    inner class LowestCommonAncestorOfBinaryTreeUsingListToStorePathTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the lowest common ancestor of two given nodes in the tree`(
+            root: TreeNode,
+            p: Int,
+            q: Int,
+            expected: Int
+        ) {
+            LowestCommonAncestorOfBinaryTreeUsingListToStorePath().test(root, p, q, expected)
+        }
+    }
+
+    @Nested
+    inner class LowestCommonAncestorOfBinaryTreeUsingDequeToStorePathTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the lowest common ancestor of two given nodes in the tree`(
+            root: TreeNode,
+            p: Int,
+            q: Int,
+            expected: Int
+        ) {
+            LowestCommonAncestorOfBinaryTreeUsingDequeToStorePath().test(root, p, q, expected)
+        }
+    }
+
+    private fun LowestCommonAncestorOfBinaryTree.test(root: TreeNode, p: Int, q: Int, expected: Int) {
+        val actual = lowestCommonAncestor(root, TreeNode(p), TreeNode(q))
         assertEquals(expected, actual.`val`)
     }
 }
