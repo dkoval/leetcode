57144ec94206686fd449403f55640eb20e8ced9b
dkoval <koval.dmitriy@gmail.com>
2022-03-12 17:47:39 +0100

    Yet another solution to "Copy List with Random Pointer" problem

--- src/main/java/com/github/dkoval/leetcode/interview/linkedlist/CopyListWithRandomPointer.java
+++ src/main/java/com/github/dkoval/leetcode/interview/linkedlist/CopyListWithRandomPointer.java
@@ -1,5 +1,8 @@
 package com.github.dkoval.leetcode.interview.linkedlist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/explore/interview/card/google/60/linked-list-5/3066/">Copy List with Random Pointer</a>
  * <p>
@@ -12,9 +15,9 @@ package com.github.dkoval.leetcode.interview.linkedlist;
  * val: an integer representing Node.val
  * random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
  */
-public class CopyListWithRandomPointer {
+public interface CopyListWithRandomPointer {
 
-    static class Node {
+    class Node {
         int val;
         Node next;
         Node random;
@@ -26,35 +29,78 @@ public class CopyListWithRandomPointer {
         }
     }
 
-    public Node copyRandomList(Node head) {
-        // tweak the original linked list by interweaving the nodes of the old and copied list:
-        // A --> A' --> B --> B' --> C --> C' --> D --> D'
-        Node curr = head;
-        while (curr != null) {
-            Node next = curr.next;
-            Node copy = new Node(curr.val);
-            curr.next = copy;
-            copy.next = next;
-            curr = next;
-        }
-        // resolve random pointers
-        curr = head;
-        while (curr != null && curr.next != null) {
-            Node next = curr.next.next;
-            if (curr.random != null) {
-                curr.next.random = curr.random.next;
+    Node copyRandomList(Node head);
+
+    // O(N) time | O(N) extra space
+    class CopyListWithRandomPointerUsingExtraSpace implements CopyListWithRandomPointer {
+
+        @Override
+        public Node copyRandomList(Node head) {
+            if (head == null) {
+                return null;
             }
-            curr = next;
+
+            Map<Node, Node> copied = new HashMap<>();
+
+            // 1st pass - copy nodes
+            Node dummy = new Node(42);
+            Node lastCopy = dummy;
+            Node currOld = head;
+            while (currOld != null) {
+                Node copy = new Node(currOld.val);
+                copied.put(currOld, copy);
+                lastCopy.next = copy;
+                lastCopy = lastCopy.next;
+                currOld = currOld.next;
+            }
+
+            // 2nd pass - resolve `random` pointers
+            currOld = head;
+            while (currOld != null) {
+                if (currOld.random != null) {
+                    Node copy = copied.get(currOld);
+                    copy.random = copied.get(currOld.random);
+                }
+                currOld = currOld.next;
+            }
+            return dummy.next;
         }
-        // collect copied nodes while restoring the original list
-        curr = head;
-        Node dummy = new Node(42), copy = dummy;
-        while (curr != null && curr.next != null) {
-            copy.next = curr.next;
-            copy = copy.next;
-            curr.next = curr.next.next;
-            curr = curr.next;
+    }
+
+    // O(N) time | O(1) extra space
+    class CopyListWithRandomPointerSpaceOptimized implements CopyListWithRandomPointer {
+
+        @Override
+        public Node copyRandomList(Node head) {
+            // tweak the original linked list by interweaving the nodes of the old and copied list:
+            // A --> A' --> B --> B' --> C --> C' --> D --> D'
+            Node curr = head;
+            while (curr != null) {
+                Node next = curr.next;
+                Node copy = new Node(curr.val);
+                curr.next = copy;
+                copy.next = next;
+                curr = next;
+            }
+            // resolve random pointers
+            curr = head;
+            while (curr != null && curr.next != null) {
+                Node next = curr.next.next;
+                if (curr.random != null) {
+                    curr.next.random = curr.random.next;
+                }
+                curr = next;
+            }
+            // collect copied nodes while restoring the original list
+            curr = head;
+            Node dummy = new Node(42), copy = dummy;
+            while (curr != null && curr.next != null) {
+                copy.next = curr.next;
+                copy = copy.next;
+                curr.next = curr.next.next;
+                curr = curr.next;
+            }
+            return dummy.next;
         }
-        return dummy.next;
     }
 }
