e8be4c78f651dcc7e721bcae48c4fad7fe375241
dkoval <koval.dmitriy@gmail.com>
2022-03-29 10:03:00 +0200

    Revise solution to "Find the Duplicate Number"

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/FindDuplicateNumber.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/FindDuplicateNumber.kt
@@ -1,27 +1,32 @@
 package com.github.dkoval.leetcode.challenge
 
 /**
- * [Find the Duplicate Number](https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3371/)
+ * [Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number)
  *
- * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
- * prove that at least one duplicate number must exist. Assume that there is only one duplicate number,
- * find the duplicate one.
+ * Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
  *
- * Note:
- * - You must not modify the array (assume the array is read only).
- * - You must use only constant, O(1) extra space.
- * - Your runtime complexity should be less than O(n2).
- * - There is only one duplicate number in the array, but it could be repeated more than once.
+ * There is only one repeated number in nums, return this repeated number.
+ *
+ * You must solve the problem without modifying the array nums and uses only constant extra space.
+ *
+ * Constraints:
+ * - 1 <= n <= 10^5
+ * - nums.length == n + 1
+ * - 1 <= nums(i) <= n
+ * - All the integers in nums appear only once except for precisely one integer which appears two or more times
  */
 object FindDuplicateNumber {
 
     fun findDuplicate(nums: IntArray): Int {
         val n = nums.size
-        for (i in nums.indices) {
-            val idx = nums[i] % n
+        for (x in nums) {
+            // map x from [1 : n] range to index from [0 : n - 1] range
+            val idx = x % n - 1
+            // don't worry, nums[] will be restored in the 2nd pass
             nums[idx] += n
         }
-        // find at which index max is occurring
+
+        // find at which index max occurs
         var maxVal = Int.MIN_VALUE
         var maxIdx = -1
         for (i in nums.indices) {
@@ -29,9 +34,11 @@ object FindDuplicateNumber {
                 maxVal = nums[i]
                 maxIdx = i
             }
-            // revert to original input array
+            // restore the original value at index i
             nums[i] %= n
         }
-        return maxIdx
+
+        // map index back to number
+        return maxIdx + 1
     }
 }
