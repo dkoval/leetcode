760e7cd2e2cae1dc4de281c22f4fd170bda53579
dkoval <koval.dmitriy@gmail.com>
2022-04-05 22:59:02 +0200

    Revise solution to "Longest Substring Without Repeating Characters" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/LongestSubstringWithoutRepeatingCharacters.java
@@ -4,30 +4,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3595/">Longest Substring Without Repeating Characters</a>
+ * <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">Longest Substring Without Repeating Characters</a>
  * <p>
  * Given a string s, find the length of the longest substring without repeating characters.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>0 <= s.length <= 5 * 10^4</li>
+ *  <li>s consists of English letters, digits, symbols and spaces</li>
+ * </ul>
  */
-public class LongestSubstringWithoutRepeatingCharacters {
+public interface LongestSubstringWithoutRepeatingCharacters {
 
-    // O(N) time | O(min(alphabet, N)) space
-    public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0;
-        Map<Character, Integer> window = new HashMap<>();
-        int start = 0;
-        for (int end = 0; end < s.length(); end++) {
-            char c = s.charAt(end);
-            if (window.containsKey(c)) {
-                int idx = window.get(c);
-                // shrink the window, i.e. remove characters before s[idx + 1]
-                for (int i = start; i <= idx; i++) {
-                    window.remove(s.charAt(i));
+    int lengthOfLongestSubstring(String s);
+
+    class LongestSubstringWithoutRepeatingCharactersRev1 implements LongestSubstringWithoutRepeatingCharacters {
+
+        // O(N) time | O(min(alphabet, N)) space
+        @Override
+        public int lengthOfLongestSubstring(String s) {
+            int maxLength = 0;
+            Map<Character, Integer> window = new HashMap<>();
+            int start = 0;
+            for (int end = 0; end < s.length(); end++) {
+                char c = s.charAt(end);
+                if (window.containsKey(c)) {
+                    int idx = window.get(c);
+                    // shrink the window, i.e. remove characters before s[idx + 1]
+                    for (int i = start; i <= idx; i++) {
+                        window.remove(s.charAt(i));
+                    }
+                    start = idx + 1;
                 }
-                start = idx + 1;
+                window.put(c, end);
+                maxLength = Math.max(maxLength, window.size());
             }
-            window.put(c, end);
-            maxLength = Math.max(maxLength, window.size());
+            return maxLength;
         }
-        return maxLength;
+    }
+
+    class LongestSubstringWithoutRepeatingCharactersRev2 implements LongestSubstringWithoutRepeatingCharacters {
+
+        // O(N) time | O(min(alphabet, N)) space
+        @Override
+        public int lengthOfLongestSubstring(String s) {
+            int maxLength = 0;
+            Map<Character, Integer> seen = new HashMap<>();
+            int start = 0;
+            for (int end = 0; end < s.length(); end++) {
+                char c = s.charAt(end);
+                if (seen.containsKey(c)) {
+                    int idx = seen.get(c);
+                    if (idx >= start) {
+                        maxLength = Math.max(maxLength, end - start);
+                        start = idx + 1;
+                    }
+                }
+                seen.put(c, end);
+            }
+
+            if (start < s.length()) {
+                maxLength = Math.max(maxLength, s.length() - start);
+            }
+            return maxLength;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/LongestSubstringWithoutRepeatingCharactersTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/LongestSubstringWithoutRepeatingCharactersTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.LongestSubstringWithoutRepeatingCharacters.LongestSubstringWithoutRepeatingCharactersRev1
+import com.github.dkoval.leetcode.challenge.LongestSubstringWithoutRepeatingCharacters.LongestSubstringWithoutRepeatingCharactersRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class LongestSubstringWithoutRepeatingCharactersTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of("abcabcbb", 3),
             Arguments.of("bbbbb", 1),
             Arguments.of("pwwkew", 3),
@@ -17,10 +23,28 @@ internal class LongestSubstringWithoutRepeatingCharactersTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find the length of the longest substring without repeating characters`(s: String, expected: Int) {
-        val actual = LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(s)
+    @Nested
+    inner class LongestSubstringWithoutRepeatingCharactersRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the length of the longest substring without repeating characters`(s: String, expected: Int) {
+            LongestSubstringWithoutRepeatingCharactersRev1().test(s, expected)
+        }
+    }
+
+    @Nested
+    inner class LongestSubstringWithoutRepeatingCharactersRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the length of the longest substring without repeating characters`(s: String, expected: Int) {
+            LongestSubstringWithoutRepeatingCharactersRev2().test(s, expected)
+        }
+    }
+
+    private fun LongestSubstringWithoutRepeatingCharacters.test(s: String, expected: Int) {
+        val actual = lengthOfLongestSubstring(s)
         assertEquals(expected, actual)
     }
 }
