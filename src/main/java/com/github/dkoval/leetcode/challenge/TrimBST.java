6a1133fca171b2bf2896c5afae7a9d578e323ddd
dkoval <koval.dmitriy@gmail.com>
2022-04-15 14:07:31 +0200

    Edit problem description and add complexity analysis

--- src/main/java/com/github/dkoval/leetcode/challenge/TrimBST.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/TrimBST.java
@@ -3,14 +3,27 @@ package com.github.dkoval.leetcode.challenge;
 import com.github.dkoval.leetcode.TreeNode;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3626/">Trim a Binary Search Tree</a>
+ * <a href="https://leetcode.com/problems/trim-a-binary-search-tree/">Trim a Binary Search Tree</a>
  * <p>
- * Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+ * Given the root of a binary search tree and the lowest and highest boundaries as low and high,
+ * trim the tree so that all its elements lies in [low, high].
+ * Trimming the tree should not change the relative structure of the elements that will remain in the tree
+ * (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
  * <p>
  * Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree in the range [1, 10^4]</li>
+ *  <li>0 <= Node.val <= 10^4</li>
+ *  <li>The value of each node in the tree is unique</li>
+ *  <li>root is guaranteed to be a valid binary search tree</li>
+ *  <li>0 <= low <= high <= 10^4</li>
+ * </ul>
  */
 public class TrimBST {
 
+    // O(N) time | O(H) space, where H is the height of the tree
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) {
             return null;
