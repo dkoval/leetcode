d5e497a3262a1b35d6e531c22cc6da7f463e4934
dkoval <koval.dmitriy@gmail.com>
2022-05-02 10:26:36 +0200

    Code readability improvements

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/SortArrayByParity.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/SortArrayByParity.kt
@@ -3,12 +3,16 @@ package com.github.dkoval.leetcode.challenge
 import java.util.*
 
 /**
- * [Sort Array By Parity](https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3431/)
+ * [Sort Array By Parity](https://leetcode.com/problems/sort-array-by-parity/)
  *
  * Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by
  * all the odd elements of A.
  *
  * You may return any answer array that satisfies this condition.
+ *
+ * Constraints:
+ * - 1 <= nums.length <= 5000
+ * - 0 <= nums(i) <= 5000
  */
 interface SortArrayByParity {
 
@@ -19,23 +23,21 @@ interface SortArrayByParity {
 object SortArrayByParityNaive: SortArrayByParity {
 
     override fun sortArrayByParity(A: IntArray): IntArray {
-        val even: Queue<Int> = LinkedList()
-        val odd: Queue<Int> = LinkedList()
+        val evens: Queue<Int> = ArrayDeque()
+        val odds: Queue<Int> = ArrayDeque()
         for (a in A) {
             if (a % 2 == 0) {
-                even.add(a)
+                evens.add(a)
             } else {
-                odd.add(a)
+                odds.add(a)
             }
         }
-        val result = IntArray(A.size)
+
         var i = 0
-        val n = even.size
-        while (!even.isEmpty() || !odd.isEmpty()) {
-            result[i] = if (i < n) even.poll() else odd.poll()
-            i++
+        while (!evens.isEmpty() || !odds.isEmpty()) {
+            A[i++] = if (!evens.isEmpty()) evens.poll() else odds.poll()
         }
-        return result
+        return A
     }
 }
 
@@ -43,15 +45,21 @@ object SortArrayByParityNaive: SortArrayByParity {
 object SortArrayByParityOptimal: SortArrayByParity {
 
     override fun sortArrayByParity(A: IntArray): IntArray {
-        var i = 0 // to point to the last element of the `even` part
-        for (j in A.indices) {
-            if (A[j] % 2 == 0) {
-                val tmp = A[i]
-                A[i] = A[j]
-                A[j] = tmp
-                i++
+        var writeIdx = 0 // points to the next vacant position of the `even` part
+        for (i in A.indices) {
+            if (A[i] % 2 == 0) {
+                A.swap(writeIdx, i)
+                writeIdx++
             }
         }
         return A
+    }
+
+    private fun IntArray.swap(i: Int, j: Int) {
+        if (i != j) {
+            val tmp = this[i]
+            this[i] = this[j]
+            this[j] = tmp
+        }
     }
 }
