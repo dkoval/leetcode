23bbf9a8dbbd0137dda9fa7413596235e6130900
dkoval <koval.dmitriy@gmail.com>
2022-05-20 23:01:43 +0200

    Solution to "Min Cost to Connect All Points" problem using Prim's algorithm

--- src/main/java/com/github/dkoval/leetcode/challenge/MinCostToConnectAllPoints.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinCostToConnectAllPoints.java
@@ -1,6 +1,8 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 /**
@@ -19,69 +21,137 @@ import java.util.PriorityQueue;
  *  <li>All pairs (xi, yi) are distinct</li>
  * </ul>
  */
-public class MinCostToConnectAllPoints {
+public interface MinCostToConnectAllPoints {
 
-    private static class PairOfPoints {
-        final int i; // index of the 1st point, i.e. points[i]
-        final int j; // index of the 2nd point, i.e. points[j]
-        final int distance; // distance between points[i] and points[j]
+    int minCostConnectPoints(int[][] points);
 
-        PairOfPoints(int i, int j, int distance) {
-            this.i = i;
-            this.j = j;
-            this.distance = distance;
+    class MinCostToConnectAllPointsUsingUnionFind implements MinCostToConnectAllPoints {
+
+        private static class PairOfPoints {
+            final int i; // index of the 1st point, i.e. points[i]
+            final int j; // index of the 2nd point, i.e. points[j]
+            final int distance; // distance between points[i] and points[j]
+
+            PairOfPoints(int i, int j, int distance) {
+                this.i = i;
+                this.j = j;
+                this.distance = distance;
+            }
+        }
+
+        private static class UnionFind {
+            // parent[i] is the parent of i
+            final int[] parent;
+
+            UnionFind(int n) {
+                parent = new int[n];
+                // initially, every is the parent to itself
+                for (int i = 0; i < n; i++) {
+                    parent[i] = i;
+                }
+            }
+
+            int find(int x) {
+                if (parent[x] != x) {
+                    parent[x] = find(parent[x]);
+                }
+                return parent[x];
+            }
+
+            boolean union(int x, int y) {
+                int px = find(x);
+                int py = find(y);
+                if (px != py) {
+                    parent[px] = py;
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        @Override
+        public int minCostConnectPoints(int[][] points) {
+            int n = points.length;
+            UnionFind uf = new UnionFind(n);
+
+            // generate all possible pairs of points and sort them by distance
+            PriorityQueue<PairOfPoints> pq = new PriorityQueue<>(Comparator.comparingInt(pair -> pair.distance));
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int distance = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                    pq.offer(new PairOfPoints(i, j, distance));
+                }
+            }
+
+            int ans = 0; // the minimum cost to make all points connected
+            int numConnectedPoints = 0;
+            while (!pq.isEmpty() && numConnectedPoints < n - 1) {
+                PairOfPoints pair = pq.poll();
+                if (uf.union(pair.i, pair.j)) {
+                    numConnectedPoints++;
+                    ans += pair.distance;
+                }
+            }
+            return ans;
         }
     }
 
-    private static class UnionFind {
-        // parent[i] is the parent of i
-        final int[] parent;
+    class MinCostToConnectAllPointsUsingPrim implements MinCostToConnectAllPoints {
 
-        UnionFind(int n) {
-            parent = new int[n];
-            // initially, every is the parent to itself
+        private static class Point {
+            // index of point in points[]
+            final int idx;
+            // distance to this point from another point
+            final int distance;
+
+            Point(int idx, int distance) {
+                this.idx = idx;
+                this.distance = distance;
+            }
+        }
+
+        @Override
+        public int minCostConnectPoints(int[][] points) {
+            int n = points.length;
+
+            // Form a graph by connecting all possible pair of points
+            List<Point>[] graph = new List[n];
             for (int i = 0; i < n; i++) {
-                parent[i] = i;
+                graph[i] = new ArrayList<>();
             }
-        }
 
-        int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int distance = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                    graph[i].add(new Point(j, distance));
+                    graph[j].add(new Point(i, distance));
+                }
             }
-            return parent[x];
-        }
 
-        void union(int x, int y) {
-            int px = find(x);
-            int py = find(y);
-            parent[px] = py;
-        }
-    }
+            // Prim's algorithm for computing the minimum spanning tree
+            int cost = 0;
+            int numConnectedPoints = 0;
+            boolean[] visited = new boolean[n];
+            PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingInt(point -> point.distance)); // min heap
+            pq.offer(new Point(0, 0));
+            while (numConnectedPoints < n) {
+                Point curr = pq.poll();
 
-    public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        UnionFind uf = new UnionFind(n);
+                // we are going to be adding duplicates to the min heap, therefore `is visited?` check is needed here
+                if (visited[curr.idx]) {
+                    continue;
+                }
 
-        // generate all possible pairs of points and sort them by distance
-        PriorityQueue<PairOfPoints> pq = new PriorityQueue<>(Comparator.comparingInt(pair -> pair.distance));
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int distance = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                pq.offer(new PairOfPoints(i, j, distance));
-            }
-        }
-
-        int ans = 0; // the minimum cost to make all points connected
-        int numConnectedPoints = 0;
-        while (!pq.isEmpty() && numConnectedPoints < n - 1) {
-            PairOfPoints pair = pq.poll();
-            if (uf.find(pair.i) != uf.find(pair.j)) {
-                uf.union(pair.i, pair.j);
+                cost += curr.distance;
                 numConnectedPoints++;
-                ans += pair.distance;
+                visited[curr.idx] = true;
+                for (Point neighbor : graph[curr.idx]) {
+                    if (!visited[neighbor.idx]) {
+                        pq.offer(neighbor);
+                    }
+                }
             }
+            return cost;
         }
-        return ans;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MinCostToConnectAllPointsTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinCostToConnectAllPointsTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MinCostToConnectAllPoints.MinCostToConnectAllPointsUsingPrim
+import com.github.dkoval.leetcode.challenge.MinCostToConnectAllPoints.MinCostToConnectAllPointsUsingUnionFind
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MinCostToConnectAllPointsTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     intArrayOf(0, 0),
@@ -31,10 +37,28 @@ internal class MinCostToConnectAllPointsTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the minimum cost to make all points connected`(points: Array<IntArray>, expected: Int) {
-        val actual = MinCostToConnectAllPoints().minCostConnectPoints(points)
+    @Nested
+    inner class MinCostToConnectAllPointsUsingUnionFindTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum cost to make all points connected`(points: Array<IntArray>, expected: Int) {
+            MinCostToConnectAllPointsUsingUnionFind().test(points, expected)
+        }
+    }
+
+    @Nested
+    inner class MinCostToConnectAllPointsUsingPrimTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum cost to make all points connected`(points: Array<IntArray>, expected: Int) {
+            MinCostToConnectAllPointsUsingPrim().test(points, expected)
+        }
+    }
+
+    private fun MinCostToConnectAllPoints.test(points: Array<IntArray>, expected: Int) {
+        val actual = minCostConnectPoints(points)
         assertEquals(expected, actual)
     }
 }
