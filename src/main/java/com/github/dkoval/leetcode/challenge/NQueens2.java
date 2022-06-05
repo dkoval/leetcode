e180c4a3b985016687ac4f75b4c357cf47839440
dkoval <koval.dmitriy@gmail.com>
2022-06-05 12:33:07 +0200

    Re-solve "N-Queens II" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/NQueens2.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/NQueens2.java
@@ -1,48 +1,106 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * <a href="https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/602/week-5-may-29th-may-31st/3760/">N-Queens II</a>
+ * <a href="https://leetcode.com/problems/n-queens-ii/">N-Queens II</a>
  * <p>
  * The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
  * <p>
  * Given an integer n, return the number of distinct solutions to the n-queens puzzle.
  */
-public class NQueens2 {
+public interface NQueens2 {
 
-    public int totalNQueens(int n) {
-        return placeQueens(0, new int[n]);
+    int totalNQueens(int n);
+
+    class NQueens2Rev1 implements NQueens2 {
+
+        @Override
+        public int totalNQueens(int n) {
+            return placeQueen(0, createBoard(n), new HashSet<>(), new HashSet<>(), new HashSet<>());
+        }
+
+        private char[][] createBoard(int n) {
+            char[][] board = new char[n][n];
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    board[row][col] = '.';
+                }
+            }
+            return board;
+        }
+
+        private int placeQueen(int row, char[][] board, Set<Integer> usedCols, Set<Integer> usedDiags1, Set<Integer> usedDiags2) {
+            int n = board.length;
+
+            if (row == n) {
+                return 1;
+            }
+
+            int count = 0;
+            for (int col = 0; col < n; col++) {
+                // check column and 2 diagonals
+                // trick: use row + col = c1 and row - col = c2 equations to label diagonals
+                if (!usedCols.contains(col) && !usedDiags1.contains(row - col) && !usedDiags2.contains(row + col)) {
+                    // place a queen
+                    board[row][col] = 'Q';
+                    usedCols.add(col);
+                    usedDiags1.add(row - col);
+                    usedDiags2.add(row + col);
+
+                    // go to the next row
+                    count += placeQueen(row + 1, board, usedCols, usedDiags1, usedDiags2);
+
+                    // backtrack
+                    board[row][col] = '.';
+                    usedCols.remove(col);
+                    usedDiags1.remove(row - col);
+                    usedDiags2.remove(row + col);
+                }
+            }
+            return count;
+        }
     }
 
-    private int placeQueens(int row, int[] placements) {
-        // placements[i] = j denotes a queen placed at i-th row and j-th column
-        int n = placements.length;
-        if (row == n) {
-            return 1;
-        }
-        // generate all possible valid queen placements in the current row
-        int numSolutions = 0;
-        for (int col = 0; col < n; col++) {
-            if (isValidPlacement(row, col, placements)) {
-                placements[row] = col;
-                numSolutions += placeQueens(row + 1, placements);
-            }
-        }
-        return numSolutions;
-    }
+    class NQueens2Rev2 implements NQueens2 {
 
-    private boolean isValidPlacement(int row, int col, int[] placements) {
-        // check all previous rows
-        for (int currRow = 0; currRow < row; currRow++) {
-            int currCol = placements[currRow];
-            if (currCol == col) {
-                // same column
-                return false;
-            }
-            if (Math.abs(row - currRow) == Math.abs(col - currCol)) {
-                // same diagonal
-                return false;
-            }
+        @Override
+        public int totalNQueens(int n) {
+            return placeQueen(0, new int[n]);
         }
-        return true;
+
+        private int placeQueen(int row, int[] placements) {
+            // placements[i] = j denotes a queen placed at i-th row and j-th column
+            int n = placements.length;
+            if (row == n) {
+                return 1;
+            }
+            // generate all possible valid queen placements in the current row
+            int numSolutions = 0;
+            for (int col = 0; col < n; col++) {
+                if (isValidPlacement(row, col, placements)) {
+                    placements[row] = col;
+                    numSolutions += placeQueen(row + 1, placements);
+                }
+            }
+            return numSolutions;
+        }
+
+        private boolean isValidPlacement(int row, int col, int[] placements) {
+            // check all previous rows
+            for (int currRow = 0; currRow < row; currRow++) {
+                int currCol = placements[currRow];
+                if (currCol == col) {
+                    // same column
+                    return false;
+                }
+                if (Math.abs(row - currRow) == Math.abs(col - currCol)) {
+                    // same diagonal
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/NQueens2Test.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/NQueens2Test.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.NQueens2.NQueens2Rev1
+import com.github.dkoval.leetcode.challenge.NQueens2.NQueens2Rev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class NQueens2Test {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(1, 1),
             Arguments.of(2, 0),
             Arguments.of(3, 0),
@@ -18,10 +24,28 @@ internal class NQueens2Test {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the number of distinct solutions to the n-queens puzzle`(n: Int, expected: Int) {
-        val actual = NQueens2().totalNQueens(n)
+    @Nested
+    inner class NQueens2Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of distinct solutions to the n-queens puzzle`(n: Int, expected: Int) {
+            NQueens2Rev1().test(n, expected)
+        }
+    }
+
+    @Nested
+    inner class NQueens2Rev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of distinct solutions to the n-queens puzzle`(n: Int, expected: Int) {
+            NQueens2Rev2().test(n, expected)
+        }
+    }
+
+    private fun NQueens2.test(n: Int, expected: Int) {
+        val actual = totalNQueens(n)
         assertEquals(expected, actual)
     }
 }
