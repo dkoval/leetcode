12e257a03db4f9d1ed4312f4a5ff59b1df95d209
dkoval <koval.dmitriy@gmail.com>
2022-06-20 09:50:57 +0200

    Update docs

--- src/main/java/com/github/dkoval/leetcode/challenge/ShortEncodingOfWords.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ShortEncodingOfWords.java
@@ -1,11 +1,12 @@
 package com.github.dkoval.leetcode.challenge;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3662/">Short Encoding of Words</a>
+ * <a href="https://leetcode.com/problems/short-encoding-of-words/">Short Encoding of Words</a>
  * <p>
  * A valid encoding of an array of words is any reference string s and array of indices indices such that:
  * <ul>
@@ -14,24 +15,32 @@ import java.util.Set;
  *  <li>For each index indices[i], the substring of s starting from indices[i] and up to (but not including) the next '#' character is equal to words[i].</li>
  * </ul>
  * Given an array of words, return the length of the shortest reference string s possible of any valid encoding of words.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= words.length <= 2000</li>
+ *  <li>1 <= words[i].length <= 7</li>
+ *  <li>words[i] consists of only lowercase letters</li>
+ * </ul>
  */
 public class ShortEncodingOfWords {
 
     public int minimumLengthEncoding(String[] words) {
         // Sort words[] by length in desc order
-        Arrays.sort(words, (w1, w2) -> w2.length() - w1.length());
-        int answer = 0;
-        // Given a word, stores all of its prefixes, e.g.
-        // word = "time", prefixes = ["time", "ime", "me", "e"]
-        Set<String> prefixes = new HashSet<>();
+        Arrays.sort(words, Comparator.comparingInt(String::length).reversed());
+
+        int maxLength = 0;
+        // Given a word, store all of its suffixes, e.g.
+        // word = "time", suffixes = ["time", "ime", "me", "e"]
+        Set<String> suffixes = new HashSet<>();
         for (String word : words) {
-            if (!prefixes.contains(word)) {
-                answer += word.length() + 1; // +1 for '#' character
-                for (int i = 0; i < word.length(); i++) {
-                    prefixes.add(word.substring(i));
+            if (!suffixes.contains(word)) {
+                maxLength += word.length() + 1; // +1 for '#' character
+                for (int offset = 0; offset < word.length(); offset++) {
+                    suffixes.add(word.substring(offset));
                 }
             }
         }
-        return answer;
+        return maxLength;
     }
 }
