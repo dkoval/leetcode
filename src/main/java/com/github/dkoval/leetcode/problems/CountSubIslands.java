71a93b66285a6789b62eb6f5058e447f8fc158fd
dkoval <koval.dmitriy@gmail.com>
2022-06-21 22:24:26 +0200

    Solution to "Count Sub Islands" problem

---
+++ src/main/java/com/github/dkoval/leetcode/problems/CountSubIslands.java
@@ -0,0 +1,69 @@
+package com.github.dkoval.leetcode.problems;
+
+/**
+ * <a href="https://leetcode.com/problems/count-sub-islands/">Count Sub Islands</a>
+ * <p>
+ * You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water) and 1's (representing land).
+ * An island is a group of 1's connected 4-directionally (horizontal or vertical). Any cells outside of the grid are considered water cells.
+ * <p>
+ * An island in grid2 is considered a sub-island if there is an island in grid1 that contains all the cells that make up this island in grid2.
+ * <p>
+ * Return the number of islands in grid2 that are considered sub-islands.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>m == grid1.length == grid2.length</li>
+ *  <li>n == grid1[i].length == grid2[i].length</li>
+ *  <li>1 <= m, n <= 500</li>
+ *  <li>grid1[i][j] and grid2[i][j] are either 0 or 1</li>
+ * </ul>
+ */
+public interface CountSubIslands {
+
+    int countSubIslands(int[][] grid1, int[][] grid2);
+
+    class CountSubIslandsDFS implements CountSubIslands {
+
+        private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+        @Override
+        public int countSubIslands(int[][] grid1, int[][] grid2) {
+            int m = grid2.length;
+            int n = grid2[0].length;
+
+            int count = 0;
+            for (int row = 0; row < m; row++) {
+                for (int col = 0; col < n; col++) {
+                    if (grid2[row][col] == 1) {
+                        count += dfs(grid2, row, col, grid1) ? 1 : 0;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private boolean dfs(int[][] grid2, int row, int col, int[][] grid1) {
+            int m = grid2.length;
+            int n = grid2[0].length;
+
+            boolean isSubIsland = true;
+            grid2[row][col] = 2;
+
+            if (grid1[row][col] == 0) {
+                isSubIsland = false;
+            }
+
+            for (int[] d : DIRS) {
+                int nextRow = row + d[0];
+                int nextCol = col + d[1];
+
+                if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n || grid2[nextRow][nextCol] != 1) {
+                    continue;
+                }
+
+                isSubIsland &= dfs(grid2, nextRow, nextCol, grid1);
+            }
+            return isSubIsland;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/CountSubIslandsTest.kt
@@ -0,0 +1,73 @@
+package com.github.dkoval.leetcode.problems
+
+import com.github.dkoval.leetcode.problems.CountSubIslands.CountSubIslandsDFS
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class CountSubIslandsTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 1, 1, 0, 0),
+                    intArrayOf(0, 1, 1, 1, 1),
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 0, 1, 1)
+                ),
+                arrayOf(
+                    intArrayOf(1, 1, 1, 0, 0),
+                    intArrayOf(0, 0, 1, 1, 1),
+                    intArrayOf(0, 1, 0, 0, 0),
+                    intArrayOf(1, 0, 1, 1, 0),
+                    intArrayOf(0, 1, 0, 1, 0)
+                ),
+                3
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 0, 1, 0, 1),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(1, 0, 1, 0, 1)
+                ),
+                arrayOf(
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(0, 1, 0, 1, 0),
+                    intArrayOf(0, 1, 0, 1, 0),
+                    intArrayOf(1, 0, 0, 0, 1)
+                ),
+                2
+            )
+        )
+    }
+
+    @Nested
+    inner class CountSubIslandsDFSTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of islands in grid2 that are considered sub-islands`(
+            grid1: Array<IntArray>,
+            grid2: Array<IntArray>,
+            expected: Int
+        ) {
+            CountSubIslandsDFS().test(grid1, grid2, expected)
+        }
+    }
+
+    private fun CountSubIslands.test(grid1: Array<IntArray>, grid2: Array<IntArray>, expected: Int) {
+        val actual = countSubIslands(grid1, grid2)
+        assertEquals(expected, actual)
+    }
+}
