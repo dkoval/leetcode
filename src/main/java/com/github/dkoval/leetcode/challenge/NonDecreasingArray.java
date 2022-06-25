2ac03812cac8dad227b2a0ee637a9fcb75bcd426
dkoval <koval.dmitriy@gmail.com>
2022-06-25 20:03:02 +0200

    Revise "Non-decreasing Array" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/NonDecreasingArray.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/NonDecreasingArray.java
@@ -1,7 +1,7 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge-2021/598/week-1-may-1st-may-7th/3731/">Non-decreasing Array</a>
+ * <a href="https://leetcode.com/problems/non-decreasing-array/>Non-decreasing Array</a>
  * <p>
  * Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
  * <p>
@@ -14,26 +14,71 @@ package com.github.dkoval.leetcode.challenge;
  * <li>-10^5 <= nums[i] <= 10^5</li>
  * </ul>
  */
-public class NonDecreasingArray {
+public interface NonDecreasingArray {
 
-    public boolean checkPossibility(int[] nums) {
-        // Idea of the algorithm
-        // 1. Invariant to keep nums[] sorted in non-decreasing order: nums[i - 1] <= nums[i] <= nums[i + 1]
-        // 2. It's allowed to "fix" at most 1 element in nums[].
-        boolean changedOneElement = false;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                if (changedOneElement) {
-                    return false;
+    boolean checkPossibility(int[] nums);
+
+    // O(N) time | O(1) space
+    class NonDecreasingArrayRev1 implements NonDecreasingArray {
+
+        @Override
+        public boolean checkPossibility(int[] nums) {
+            int n = nums.length;
+            boolean changed = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    if (changed) {
+                        return false;
+                    }
+
+                    // Note: don't technically need to even update nums[i] for the algorithm to work;
+                    // However, updating nums[i + 1] is important.
+                    if (i > 0) {
+                        if (nums[i - 1] > nums[i + 1]) {
+                            // case #1: prev > next
+                            // [..., 3, x, 2, ...] -> [..., 3, x, _x_, ...]
+                            nums[i + 1] = nums[i];
+                        } else if (nums[i - 1] <= nums[i + 1]) {
+                            // case #2: prev < next
+                            // [..., 3, x, 4, ...] -> [..., 3, _4_, 4, ...]
+                            // case #3: prev == next
+                            // [..., 3, x, 3, ...] -> [..., 3, _3_, 3, ...]
+                            nums[i] = nums[i + 1];
+                        }
+                    } else {
+                        // i == 0
+                        nums[i] = nums[i + 1];
+                    }
+                    changed = true;
                 }
-                // [..., 3, 4, 2, ...] -> [..., 3, 4, 4, ...]
-                //       |  ^  |                |  ^  |
-                if (i > 0 && nums[i - 1] > nums[i + 1]) {
-                    nums[i + 1] = nums[i];
-                }
-                changedOneElement = true;
             }
+            return true;
         }
-        return true;
+    }
+
+    // O(N) time | O(1) space
+    class NonDecreasingArrayRev2 implements NonDecreasingArray {
+
+        @Override
+        public boolean checkPossibility(int[] nums) {
+            // Idea of the algorithm
+            // 1. Invariant to keep nums[] sorted in non-decreasing order: nums[i - 1] <= nums[i] <= nums[i + 1]
+            // 2. It's allowed to "fix" at most 1 element in nums[].
+            boolean changedOneElement = false;
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    if (changedOneElement) {
+                        return false;
+                    }
+                    // [..., 3, 4, 2, ...] -> [..., 3, 4, 4, ...]
+                    //       |  ^  |                |  ^  |
+                    if (i > 0 && nums[i - 1] > nums[i + 1]) {
+                        nums[i + 1] = nums[i];
+                    }
+                    changedOneElement = true;
+                }
+            }
+            return true;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/NonDecreasingArrayTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/NonDecreasingArrayTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.NonDecreasingArray.NonDecreasingArrayRev1
+import com.github.dkoval.leetcode.challenge.NonDecreasingArray.NonDecreasingArrayRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class NonDecreasingArrayTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(4, 2, 3),
                 true
@@ -37,13 +43,35 @@ internal class NonDecreasingArrayTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should check if nums array could become non-decreasing by modifying at most one element`(
-        nums: IntArray,
-        expected: Boolean
-    ) {
-        val actual = NonDecreasingArray().checkPossibility(nums)
+    @Nested
+    inner class NonDecreasingArrayRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if nums array could become non-decreasing by modifying at most one element`(
+            nums: IntArray,
+            expected: Boolean
+        ) {
+            NonDecreasingArrayRev2().test(nums, expected)
+        }
+    }
+
+    @Nested
+    inner class NonDecreasingArrayRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if nums array could become non-decreasing by modifying at most one element`(
+            nums: IntArray,
+            expected: Boolean
+        ) {
+            NonDecreasingArrayRev1().test(nums, expected)
+        }
+    }
+
+
+    private fun NonDecreasingArray.test(nums: IntArray, expected: Boolean) {
+        val actual = checkPossibility(nums)
         assertEquals(expected, actual)
     }
 }
