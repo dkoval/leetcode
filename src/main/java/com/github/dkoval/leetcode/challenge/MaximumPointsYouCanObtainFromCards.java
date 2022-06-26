61c8e094901998a9aa4aa9a9fe2d4a5ccefdbe44
dkoval <koval.dmitriy@gmail.com>
2022-06-26 11:01:27 +0200

    Add alternative solution to "Maximum Points You Can Obtain from Cards" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MaximumPointsYouCanObtainFromCards.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MaximumPointsYouCanObtainFromCards.java
@@ -1,7 +1,7 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3739/">Maximum Points You Can Obtain from Cards</a>
+ * <a href="https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/">Maximum Points You Can Obtain from Cards</a>
  * <p>
  * There are several cards arranged in a row, and each card has an associated number of points.
  * The points are given in the integer array cardPoints.
@@ -19,27 +19,61 @@ package com.github.dkoval.leetcode.challenge;
  *  <li>1 <= k <= cardPoints.length</li>
  * </ul>
  */
-public class MaximumPointsYouCanObtainFromCards {
+public interface MaximumPointsYouCanObtainFromCards {
 
-    // O(N) time | O(1) space
-    public int maxScore(int[] cardPoints, int k) {
-        // Keep a window of size m = n - k over the array. The answer is max(answer, sumOfArray - sumOfCurrentWindow)
-        int totalSum = 0;
-        for (int cardPoint : cardPoints) {
-            totalSum += cardPoint;
-        }
+    int maxScore(int[] cardPoints, int k);
 
-        int score = 0;
-        int windowSum = 0;
-        int n = cardPoints.length, m = n - k;
-        // consider all windows of size m = n - k
-        for (int i = 0; i < n; i++) {
-            if (i >= m) {
-                score = Math.max(score, totalSum - windowSum);
-                windowSum -= cardPoints[i - m]; // keep sum of last m elements
+    class MaximumPointsYouCanObtainFromCardsRev1 implements MaximumPointsYouCanObtainFromCards {
+
+        // O(N) time | O(1) space
+        @Override
+        public int maxScore(int[] cardPoints, int k) {
+            // Keep a window of size m = n - k over the array
+            // The answer is max(answer, sumOfArray - sumOfCurrentWindow)
+            int totalSum = 0;
+            for (int cardPoint : cardPoints) {
+                totalSum += cardPoint;
             }
-            windowSum += cardPoints[i];
+
+            int ans = 0;
+            int windowSum = 0;
+            int n = cardPoints.length;
+            int m = n - k;
+            // consider all windows of size m = n - k
+            for (int i = 0; i < n; i++) {
+                if (i >= m) {
+                    ans = Math.max(ans, totalSum - windowSum);
+                    windowSum -= cardPoints[i - m]; // keep sum of last m elements
+                }
+                windowSum += cardPoints[i];
+            }
+            return Math.max(ans, totalSum - windowSum);
         }
-        return Math.max(score, totalSum - windowSum);
+    }
+
+    class MaximumPointsYouCanObtainFromCardsRev2 implements MaximumPointsYouCanObtainFromCards {
+
+        // O(N) time | O(1) space
+        @Override
+        public int maxScore(int[] cardPoints, int k) {
+            int n = cardPoints.length;
+
+            int ans = 0;
+            int leftSum = 0;
+            for (int i = 0; i < k; i++) {
+                leftSum += cardPoints[i];
+            }
+
+            ans = Math.max(ans, leftSum);
+
+            int rightSum = 0;
+            for (int i = 0; i < k; i++) {
+                // if we take card[n - i - 1], we can't then take card[k - i - 1]
+                rightSum += cardPoints[n - i - 1];
+                leftSum -= cardPoints[k - i - 1];
+                ans = Math.max(ans, leftSum + rightSum);
+            }
+            return ans;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumPointsYouCanObtainFromCardsTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumPointsYouCanObtainFromCardsTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MaximumPointsYouCanObtainFromCards.MaximumPointsYouCanObtainFromCardsRev1
+import com.github.dkoval.leetcode.challenge.MaximumPointsYouCanObtainFromCards.MaximumPointsYouCanObtainFromCardsRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MaximumPointsYouCanObtainFromCardsTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(1, 2, 3, 4, 5, 6, 1),
                 3,
@@ -43,10 +49,28 @@ internal class MaximumPointsYouCanObtainFromCardsTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the maximum score you can obtain`(cardPoints: IntArray, k: Int, expected: Int) {
-        val actual = MaximumPointsYouCanObtainFromCards().maxScore(cardPoints, k)
+    @Nested
+    inner class MaximumPointsYouCanObtainFromCardsRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum score you can obtain`(cardPoints: IntArray, k: Int, expected: Int) {
+            MaximumPointsYouCanObtainFromCardsRev1().test(cardPoints, k, expected)
+        }
+    }
+
+    @Nested
+    inner class MaximumPointsYouCanObtainFromCardsRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum score you can obtain`(cardPoints: IntArray, k: Int, expected: Int) {
+            MaximumPointsYouCanObtainFromCardsRev2().test(cardPoints, k, expected)
+        }
+    }
+
+    private fun MaximumPointsYouCanObtainFromCards.test(cardPoints: IntArray, k: Int, expected: Int) {
+        val actual = maxScore(cardPoints, k)
         assertEquals(expected, actual)
     }
 }
