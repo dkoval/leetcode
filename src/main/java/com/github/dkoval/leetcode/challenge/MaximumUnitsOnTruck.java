d307ba411e4465ea1ee853f89fca5df9106f5492
dkoval <koval.dmitriy@gmail.com>
2022-07-01 10:56:14 +0200

    Revise "Maximum Units on a Truck" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MaximumUnitsOnTruck.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MaximumUnitsOnTruck.java
@@ -3,7 +3,7 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/604/week-2-june-8th-june-14th/3778/">Maximum Units on a Truck</a>
+ * <a href="https://leetcode.com/problems/maximum-units-on-a-truck/">Maximum Units on a Truck</a>
  * <p>
  * You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes,
  * where boxTypes[i] = [numberOfBoxes_i, numberOfUnitsPerBox_i]:
@@ -15,19 +15,28 @@ import java.util.Arrays;
  * You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
  * <p>
  * Return the maximum total number of units that can be put on the truck.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= boxTypes.length <= 1000</li>
+ *  <li>1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000</li>
+ *  <li>1 <= truckSize <= 10</li>
+ * </ul>
  */
 public class MaximumUnitsOnTruck {
 
     // O(NlogN) time | O(1) space
     public int maximumUnits(int[][] boxTypes, int truckSize) {
+        // sort by numberOfUnitsPerBox in desc order
         Arrays.sort(boxTypes, (boxType1, boxType2) -> boxType2[1] - boxType1[1]);
-        int capacity = truckSize;
+
         int totalNumUnits = 0;
+        int capacity = truckSize;
         for (int[] boxType : boxTypes) {
-            if (capacity > 0) {
-                totalNumUnits += Math.min(boxType[0], capacity) * boxType[1];
-                capacity -= boxType[0];
-            } else {
+            int numBoxesCanTake = Math.min(boxType[0], capacity);
+            totalNumUnits += numBoxesCanTake * boxType[1];
+            capacity -= numBoxesCanTake;
+            if (capacity == 0) {
                 break;
             }
         }
