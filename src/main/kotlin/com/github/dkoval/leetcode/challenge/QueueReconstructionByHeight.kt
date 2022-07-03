a31b5d0f327a383aa3a49e8d5f1c4e0d8ba86cce
dkoval <koval.dmitriy@gmail.com>
2022-07-03 19:24:14 +0200

    Update description of "Queue Reconstruction by Height" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/QueueReconstructionByHeight.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/QueueReconstructionByHeight.kt
@@ -1,23 +1,31 @@
 package com.github.dkoval.leetcode.challenge
 
 /**
- * [Queue Reconstruction by Height](https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3352/)
+ * [Queue Reconstruction by Height](https://leetcode.com/problems/queue-reconstruction-by-height/)
  *
  * Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers `(h, k)`,
  * where `h` is the height of the person and `k` is the number of people in front of this person who have a height greater
  * than or equal to `h`. Write an algorithm to reconstruct the queue.
+ *
+ * Constraints:
+ *
+ * - 1 <= people.length <= 2000
+ * - 0 <= hi <= 106
+ * - 0 <= ki < people.length
+ * - It is guaranteed that the queue can be reconstructed
  */
-object QueueReconstructionByHeight  {
+object QueueReconstructionByHeight {
 
     fun reconstructQueue(people: Array<IntArray>): Array<IntArray> {
-        people.sortWith(Comparator { (h1, k1), (h2, k2) ->
+        people.sortWith { (h1, k1), (h2, k2) ->
             // if persons are of the same height, sort by k in ascending order;
             // otherwise, sort by height in descending order
             if (h1 == h2) k1 - k2 else h2 - h1
-        })
+        }
+
         val result = mutableListOf<IntArray>()
         for (person in people) {
-            result.add(person[1], person)
+            result.add(person[1], person) // shifts old value at index i by 1 position to the right
         }
         return result.toTypedArray()
     }
