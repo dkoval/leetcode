15755859545205da22165762470ed74b8255f6dd
dkoval <koval.dmitriy@gmail.com>
2022-07-03 18:33:52 +0200

    Update docs

--- src/main/java/com/github/dkoval/leetcode/challenge/MinimumMovesToEqualArrayElements2.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumMovesToEqualArrayElements2.java
@@ -3,11 +3,20 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/600/week-3-may-15th-may-21st/3748/">Minimum Moves to Equal Array Elements II</a>
+ * <a href="https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/">Minimum Moves to Equal Array Elements II</a>
  * <p>
  * Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
  * <p>
  * In one move, you can increment or decrement an element of the array by 1.
+ * <p>
+ * Test cases are designed so that the answer will fit in a 32-bit integer.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == nums.length</li>
+ *  <li>1 <= nums.length <= 10^5</li>
+ *  <li>-10^9 <= nums[i] <= 10^9</li>
+ * </ul>
  */
 public interface MinimumMovesToEqualArrayElements2 {
 
@@ -44,9 +53,11 @@ public interface MinimumMovesToEqualArrayElements2 {
             // Idea: find the central point in nums[] from which each other point is nearer, i.e. median
             // Step #1: sort the input array
             Arrays.sort(nums);
+
             // Step #2: find the middle element of sorted array
             int n = nums.length, mid = n / 2;
             int median = (n % 2 == 0) ? (nums[mid] + nums[mid - 1]) / 2 : nums[mid];
+
             // Step #3: calculate number of steps to make elements equal to the median
             int numSteps = 0;
             for (int num : nums) {
@@ -56,7 +67,7 @@ public interface MinimumMovesToEqualArrayElements2 {
         }
     }
 
-    // Resource: https://www.youtube.com/watch?v=FGgL5QxZLno&t=1s
+    // Resource: https://www.youtube.com/watch?v=FGgL5QxZLno
     class MinimumMovesToEqualArrayElements2UsingTwoPointers implements MinimumMovesToEqualArrayElements2 {
 
         // O(NlogN) time | O(1) space
