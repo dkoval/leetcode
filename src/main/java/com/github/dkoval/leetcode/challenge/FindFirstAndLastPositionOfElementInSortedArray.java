091251550f06e1287b40bb0fb15a91ab19523ad8
dkoval <koval.dmitriy@gmail.com>
2022-07-08 19:14:00 +0200

    Re-solved "Find First and Last Position of Element in Sorted Array" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,51 +1,116 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/597/week-5-april-29th-april-30th/3725/">Find First and Last Position of Element in Sorted Array</a>
+ * <a href="https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/">Find First and Last Position of Element in Sorted Array</a>
  * <p>
  * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
  * <p>
  * If target is not found in the array, return [-1, -1].
  * <p>
  * Follow up: Could you write an algorithm with O(log n) runtime complexity?
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>0 <= nums.length <= 10^5</li>
+ * <li>-10^9 <= nums[i] <= 10^9</li>
+ * <li>nums is a non-decreasing array</li>
+ * <li>-10^9 <= target <= 10^9</li>
+ * </ul>
  */
-public class FindFirstAndLastPositionOfElementInSortedArray {
+public interface FindFirstAndLastPositionOfElementInSortedArray {
 
-    public int[] searchRange(int[] nums, int target) {
-        int idx = binarySearch(nums, target);
-        if (idx == -1) {
-            return new int[]{-1, -1};
+    int[] searchRange(int[] nums, int target);
+
+    // O(N) time in the worst case if nums = [target, target, ..., target]
+    // O(1) space
+    class FindFirstAndLastPositionOfElementInSortedArrayRev1 implements FindFirstAndLastPositionOfElementInSortedArray {
+
+        public int[] searchRange(int[] nums, int target) {
+            int idx = binarySearch(nums, target);
+            if (idx == -1) {
+                return new int[]{-1, -1};
+            }
+
+            int[] ans = new int[]{idx, idx};
+
+            // find leftmost i such that nums[i] = target
+            while (ans[0] - 1 >= 0 && nums[ans[0] - 1] == nums[idx]) {
+                ans[0]--;
+            }
+
+            // find rightmost i such that nums[i] = target
+            while (ans[1] + 1 < nums.length && nums[ans[1] + 1] == nums[idx]) {
+                ans[1]++;
+            }
+
+            return ans;
         }
 
-        int[] answer = new int[2];
-        // find leftmost i such that nums[i] = target
-        int i = idx - 1;
-        while (i >= 0 && nums[i] == nums[i + 1]) {
-            i--;
+        private int binarySearch(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+            return -1;
         }
-        answer[0] = i + 1;
-
-        // find rightmost i such that nums[i] = target
-        i = idx + 1;
-        while (i < nums.length && nums[i] == nums[i - 1]) {
-            i++;
-        }
-        answer[1] = i - 1;
-        return answer;
     }
 
-    private int binarySearch(int[] nums, int target) {
-        int lo = 0, hi = nums.length - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] < target) {
-                lo = mid + 1;
-            } else if (nums[mid] > target) {
-                hi = mid - 1;
-            } else {
-                return mid;
-            }
+    // O(logN) time | O(1) space
+    class FindFirstAndLastPositionOfElementInSortedArrayRev2 implements FindFirstAndLastPositionOfElementInSortedArray {
+
+        @Override
+        public int[] searchRange(int[] nums, int target) {
+            return new int[]{firstIndexOf(nums, target), lastIndexOf(nums, target)};
         }
-        return -1;
+
+        private int firstIndexOf(int[] nums, int target) {
+            int n = nums.length;
+            int left = 0;
+            int right = n - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    if (mid == 0 || nums[mid - 1] != target) {
+                        return mid;
+                    }
+                    // not the very first occurrence of target, shift the right boundary to the left
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+
+        private int lastIndexOf(int[] nums, int target) {
+            int n = nums.length;
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    if (mid == n - 1 || nums[mid + 1] != target) {
+                        return mid;
+                    }
+                    // not the very last occurrence of target, shift the left boundary to the right
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/FindFirstAndLastPositionOfElementInSortedArrayTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/FindFirstAndLastPositionOfElementInSortedArrayTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.FindFirstAndLastPositionOfElementInSortedArray.FindFirstAndLastPositionOfElementInSortedArrayRev1
+import com.github.dkoval.leetcode.challenge.FindFirstAndLastPositionOfElementInSortedArray.FindFirstAndLastPositionOfElementInSortedArrayRev2
 import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class FindFirstAndLastPositionOfElementInSortedArrayTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(5, 7, 7, 8, 8, 10),
                 8,
@@ -28,14 +34,36 @@ class FindFirstAndLastPositionOfElementInSortedArrayTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find first and last position of element in sorted array`(
-        nums: IntArray,
-        target: Int,
-        expected: IntArray
-    ) {
-        val actual = FindFirstAndLastPositionOfElementInSortedArray().searchRange(nums, target)
+    @Nested
+    inner class FindFirstAndLastPositionOfElementInSortedArrayRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find first and last position of element in sorted array`(
+            nums: IntArray,
+            target: Int,
+            expected: IntArray
+        ) {
+            FindFirstAndLastPositionOfElementInSortedArrayRev1().test(nums, target, expected)
+        }
+    }
+
+    @Nested
+    inner class FindFirstAndLastPositionOfElementInSortedArrayRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find first and last position of element in sorted array`(
+            nums: IntArray,
+            target: Int,
+            expected: IntArray
+        ) {
+            FindFirstAndLastPositionOfElementInSortedArrayRev2().test(nums, target, expected)
+        }
+    }
+
+    private fun FindFirstAndLastPositionOfElementInSortedArray.test(nums: IntArray, target: Int, expected: IntArray) {
+        val actual = searchRange(nums, target)
         assertArrayEquals(expected, actual)
     }
 }
