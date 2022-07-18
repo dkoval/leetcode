5db16c1dfc59328e857a4485781a7448ed6ec1c8
dkoval <koval.dmitriy@gmail.com>
2022-07-18 22:45:43 +0200

    Revise "Valid Triangle Number" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ValidTriangleNumber.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ValidTriangleNumber.java
@@ -3,33 +3,52 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/610/week-3-july-15th-july-21st/3815/">Valid Triangle Number</a>
+ * <a href="https://leetcode.com/problems/valid-triangle-number/">Valid Triangle Number</a>
  * <p>
  * Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take
  * them as side lengths of a triangle.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 1000</li>
+ *  <li>0 <= nums[i] <= 1000</li>
+ * </ul>
  */
-public class ValidTriangleNumber {
+public interface ValidTriangleNumber {
+
+    int triangleNumber(int[] nums);
 
     // O(N^2) time | O(1) space
-    public int triangleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int count = 0;
+    class ValidTriangleNumberRev1 implements ValidTriangleNumber {
 
-        for (int i = n - 1; i >= 2; i--) {
-            int l = 0, r = i - 1;
-            while (l < r) {
-                if (nums[l] + nums[r] > nums[i]) {
-                    // Since nums[] is sorted in asc order, inequality x + nums[r] > nums[i]
-                    // also hold for all x in nums[l + 1 : r - 1].
-                    // The total number of (l, r) pairs that satisfy inequality nums[l] + nums[r] > nums[i] is r - l.
-                    count += r - l;
-                    r--;
-                } else {
-                    l++;
+        @Override
+        public int triangleNumber(int[] nums) {
+            // triplet (a, b, c) form a triangle IFF
+            // 1. a + b > c
+            // 2. a + c > b
+            // 3. b + c > a
+            // Assuming a <= b <= c, we only need to check whether a + b > c
+            int n = nums.length;
+            Arrays.sort(nums);
+
+            int count = 0;
+            for (int i = n - 1; i >= 2; i--) {
+                int l = 0;
+                int r = i - 1;
+                while (l < r) {
+                    if (nums[l] + nums[r] > nums[i]) {
+                        // since nums[] is sorted in asc order, inequality x + nums[r] > nums[i]
+                        // holds true for all x in nums[l : r - 1].
+                        // The total number of (l, r) pairs that satisfy inequality nums[l] + nums[r] > nums[i]
+                        // equals to (r - 1) - l + 1 = r - l
+                        count += r - l;
+                        r--;
+                    } else {
+                        l++;
+                    }
                 }
             }
+            return count;
         }
-        return count;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/ValidTriangleNumberTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ValidTriangleNumberTest.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.ValidTriangleNumber.ValidTriangleNumberRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class ValidTriangleNumberTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(2, 2, 3, 4),
                 3
@@ -21,13 +26,21 @@ internal class ValidTriangleNumberTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the number of triplets chosen from the array that can make triangles`(
-        nums: IntArray,
-        expected: Int
-    ) {
-        val actual = ValidTriangleNumber().triangleNumber(nums)
+    @Nested
+    inner class ValidTriangleNumberRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of triplets chosen from the array that can make triangles`(
+            nums: IntArray,
+            expected: Int
+        ) {
+            ValidTriangleNumberRev1().test(nums, expected)
+        }
+    }
+
+    private fun ValidTriangleNumber.test(nums: IntArray, expected: Int) {
+        val actual = triangleNumber(nums)
         assertEquals(expected, actual)
     }
 }
