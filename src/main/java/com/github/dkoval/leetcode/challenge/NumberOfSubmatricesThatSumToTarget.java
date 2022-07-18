3e962266a90c4aab9b075b43caa7199684c1bc58
dkoval <koval.dmitriy@gmail.com>
2022-07-18 14:56:04 +0200

    Re-solve "Number of Submatrices That Sum to Target" hard problem

--- src/main/java/com/github/dkoval/leetcode/challenge/NumberOfSubmatricesThatSumToTarget.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/NumberOfSubmatricesThatSumToTarget.java
@@ -4,38 +4,137 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3711/">Number of Submatrices That Sum to Target</a>
+ * <a href="https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/">Number of Submatrices That Sum to Target (Hard)</a>
  * <p>
  * Given a matrix and a target, return the number of non-empty submatrices that sum to target.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= matrix.length <= 100</li>
+ *  <li>1 <= matrix[0].length <= 100</li>
+ *  <li>-1000 <= matrix[i] <= 1000</li>
+ *  <li>-10^8 <= target <= 10^8</li>
+ * </ul>
  */
-public class NumberOfSubmatricesThatSumToTarget {
+public interface NumberOfSubmatricesThatSumToTarget {
 
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        // for each row i, calculate prefix sum, i.e. matrix[i][j] = sumOf(matrix[i][0], ..., matrix[i][j])
-        int m = matrix.length, n = matrix[0].length;
-        for (int row = 0; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                matrix[row][col] += matrix[row][col - 1];
-            }
-        }
+    int numSubmatrixSumTarget(int[][] matrix, int target);
 
-        int count = 0;
+    class NumberOfSubmatricesThatSumToTargetRev1 implements NumberOfSubmatricesThatSumToTarget {
 
-        // for each pair of columns, find the number of subarrays whose sum equals to target
-        for (int col1 = 0; col1 < n; col1++) {
-            for (int col2 = col1; col2 < n; col2++) {
-                Map<Integer, Integer> sumOccurrences = new HashMap<>();
-                sumOccurrences.put(0, 1);
-
-                int sum = 0;
-                for (int row = 0; row < m; row++) {
-                    sum += matrix[row][col2] - (col1 > 0 ? matrix[row][col1 - 1] : 0);
-                    count += sumOccurrences.getOrDefault(sum - target, 0);
-                    sumOccurrences.put(sum, sumOccurrences.getOrDefault(sum, 0) + 1);
+        @Override
+        public int numSubmatrixSumTarget(int[][] matrix, int target) {
+            // for each row i, calculate prefix sum, i.e. matrix[i][j] = sumOf(matrix[i][0], ..., matrix[i][j])
+            int m = matrix.length;
+            int n = matrix[0].length;
+            for (int row = 0; row < m; row++) {
+                for (int col = 1; col < n; col++) {
+                    matrix[row][col] += matrix[row][col - 1];
                 }
             }
-        }
 
-        return count;
+            // for each pair of columns, find the number of subarrays whose sum equals to target
+            int count = 0;
+            for (int col1 = 0; col1 < n; col1++) {
+                for (int col2 = col1; col2 < n; col2++) {
+                    // prefix sum -> count
+                    Map<Integer, Integer> counts = new HashMap<>();
+                    counts.put(0, 1);
+
+                    int sum = 0;
+                    for (int row = 0; row < m; row++) {
+                        sum += matrix[row][col2] - (col1 > 0 ? matrix[row][col1 - 1] : 0);
+                        count += counts.getOrDefault(sum - target, 0);
+                        counts.put(sum, counts.getOrDefault(sum, 0) + 1);
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    class NumberOfSubmatricesThatSumToTargetRev2 implements NumberOfSubmatricesThatSumToTarget {
+
+        @Override
+        public int numSubmatrixSumTarget(int[][] matrix, int target) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            // vertical prefix sums
+            int[][] prefixSum = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    prefixSum[i][j] += (i > 0 ? prefixSum[i - 1][j] : 0) + matrix[i][j];
+                }
+            }
+
+            // now, the problem reduces to https://leetcode.com/problems/subarray-sum-equals-k/
+            int count = 0;
+            // consider all submatrices between rows i1 and i2
+            for (int i1 = 0; i1 < m; i1++) {
+                for (int i2 = i1; i2 < m; i2++) {
+                    // prefix sum -> count
+                    Map<Integer, Integer> counts = new HashMap<>();
+                    counts.put(0, 1); // corner case, denotes an empty prefix
+
+                    int sum = 0; // current prefix sum in a 1D array
+                    for (int j = 0; j < n; j++) {
+                        sum += prefixSum[i2][j] - (i1 > 0 ? prefixSum[i1 - 1][j] : 0);
+                        int diff = sum - target; // chop off prefix(es) with sum == diff
+                        count += counts.getOrDefault(diff, 0);
+                        counts.put(sum, counts.getOrDefault(sum, 0) + 1);
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    class NumberOfSubmatricesThatSumToTargetRev3 implements NumberOfSubmatricesThatSumToTarget {
+
+        @Override
+        public int numSubmatrixSumTarget(int[][] matrix, int target) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            // 2D prefix sum, i.e.
+            // prefixSum[i][j] is the sum of all elements in a submatrix defined by
+            // (0, 0) upper-left and (i, j) bottom-right corners
+            int[][] prefixSum = new int[m][n];
+
+            // 1st pass - compute prefix sum for each row i
+            for (int i = 0; i < m; i++) {
+                prefixSum[i][0] = matrix[i][0];
+                for (int j = 1; j < n; j++) {
+                    prefixSum[i][j] += prefixSum[i][j - 1] + matrix[i][j];
+                }
+            }
+
+            // 2nd pass - combine prefix sums of (i - 1) and i-th rows
+            for (int i = 1; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    prefixSum[i][j] += prefixSum[i - 1][j];
+                }
+            }
+
+            // now, the problem reduces to https://leetcode.com/problems/subarray-sum-equals-k/
+            int count = 0;
+            // consider all submatrices between rows i1 and i2
+            for (int i1 = 0; i1 < m; i1++) {
+                for (int i2 = i1; i2 < m; i2++) {
+                    // prefix sum -> count
+                    Map<Integer, Integer> counts = new HashMap<>();
+                    counts.put(0, 1); // corner case, denotes an empty prefix
+
+                    for (int j = 0; j < n; j++) {
+                        int sum = prefixSum[i2][j] - (i1 > 0 ? prefixSum[i1 - 1][j] : 0);
+                        int diff = sum - target; // chop off prefix(es) with sum == diff
+                        count += counts.getOrDefault(diff, 0);
+                        counts.put(sum, counts.getOrDefault(sum, 0) + 1);
+                    }
+                }
+            }
+            return count;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/NumberOfSubmatricesThatSumToTargetTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/NumberOfSubmatricesThatSumToTargetTest.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.NumberOfSubmatricesThatSumToTarget.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class NumberOfSubmatricesThatSumToTargetTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     intArrayOf(0, 1, 0),
@@ -37,14 +42,50 @@ internal class NumberOfSubmatricesThatSumToTargetTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the number of non-empty submatrices that sum to target`(
-        matrix: Array<IntArray>,
-        target: Int,
-        expected: Int
-    ) {
-        val actual = NumberOfSubmatricesThatSumToTarget().numSubmatrixSumTarget(matrix, target)
+    @Nested
+    inner class NumberOfSubmatricesThatSumToTargetRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of non-empty submatrices that sum to target`(
+            matrix: Array<IntArray>,
+            target: Int,
+            expected: Int
+        ) {
+            NumberOfSubmatricesThatSumToTargetRev1().test(matrix, target, expected)
+        }
+    }
+
+    @Nested
+    inner class NumberOfSubmatricesThatSumToTargetRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of non-empty submatrices that sum to target`(
+            matrix: Array<IntArray>,
+            target: Int,
+            expected: Int
+        ) {
+            NumberOfSubmatricesThatSumToTargetRev2().test(matrix, target, expected)
+        }
+    }
+
+    @Nested
+    inner class NumberOfSubmatricesThatSumToTargetRev3Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of non-empty submatrices that sum to target`(
+            matrix: Array<IntArray>,
+            target: Int,
+            expected: Int
+        ) {
+            NumberOfSubmatricesThatSumToTargetRev3().test(matrix, target, expected)
+        }
+    }
+
+    private fun NumberOfSubmatricesThatSumToTarget.test(matrix: Array<IntArray>, target: Int, expected: Int) {
+        val actual = numSubmatrixSumTarget(matrix, target)
         assertEquals(expected, actual)
     }
 }
