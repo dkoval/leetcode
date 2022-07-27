73b2ad38a9cce8fe613a0a5595d1ab0ee0538a09
dkoval <koval.dmitriy@gmail.com>
2022-07-28 01:11:32 +0200

    Solution to "Most Profit Assigning Work" problem

---
+++ src/main/java/com/github/dkoval/leetcode/problems/MostProfitAssigningWork.java
@@ -0,0 +1,69 @@
+package com.github.dkoval.leetcode.problems;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * <a href="https://leetcode.com/problems/most-profit-assigning-work/">Most Profit Assigning Work</a>
+ * <p>
+ * You have n jobs and m workers. You are given three arrays: difficulty, profit, and worker where:
+ * <p>
+ * difficulty[i] and profit[i] are the difficulty and the profit of the ith job, and
+ * worker[j] is the ability of jth worker (i.e., the jth worker can only complete a job with difficulty at most worker[j]).
+ * Every worker can be assigned at most one job, but one job can be completed multiple times.
+ * <p>
+ * For example, if three workers attempt the same job that pays $1, then the total profit will be $3.
+ * If a worker cannot complete any job, their profit is $0.
+ * Return the maximum profit we can achieve after assigning the workers to the jobs.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == difficulty.length</li>
+ *  <li>n == profit.length</li>
+ *  <li>m == worker.length</li>
+ *  <li>1 <= n, m <= 10^4</li>
+ *  <li>1 <= difficulty[i], profit[i], worker[i] <= 10^5</li>
+ * </ul>
+ */
+public interface MostProfitAssigningWork {
+
+    int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker);
+
+    class MostProfitAssigningWorkUsingSortingByDifficulty implements MostProfitAssigningWork {
+
+        @Override
+        public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+            int n = difficulty.length;
+            Job[] jobs = new Job[n];
+            for (int i = 0; i < n; i++) {
+                jobs[i] = new Job(difficulty[i], profit[i]);
+            }
+
+            // sort both arrays by difficulty
+            Arrays.sort(jobs, Comparator.comparingInt(job -> job.difficulty));
+            Arrays.sort(worker);
+
+            int ans = 0;
+            int maxProfitSoFar = 0;
+            int i = 0;
+            for (int skill : worker) {
+                while (i < n && skill >= jobs[i].difficulty) {
+                    maxProfitSoFar = Math.max(maxProfitSoFar, jobs[i].profit);
+                    i++;
+                }
+                ans += maxProfitSoFar;
+            }
+            return ans;
+        }
+
+        private static class Job {
+            final int difficulty;
+            final int profit;
+
+            Job(int difficulty, int profit) {
+                this.difficulty = difficulty;
+                this.profit = profit;
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/MostProfitAssigningWorkTest.kt
@@ -0,0 +1,51 @@
+package com.github.dkoval.leetcode.problems
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class MostProfitAssigningWorkTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(2, 4, 6, 8, 10),
+                intArrayOf(10, 20, 30, 40, 50),
+                intArrayOf(4, 5, 6, 7),
+                100
+            ),
+            Arguments.of(
+                intArrayOf(85, 47, 57),
+                intArrayOf(24, 66, 99),
+                intArrayOf(40, 25, 25),
+                0
+            )
+        )
+    }
+
+    @Nested
+    inner class MostProfitAssigningWorkUsingSortingByDifficultyTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum profit we can achieve after assigning the workers to the jobs`(
+            difficulty: IntArray,
+            profit: IntArray,
+            worker: IntArray,
+            expected: Int
+        ) {
+
+        }
+    }
+
+    private fun MostProfitAssigningWork.test(difficulty: IntArray, profit: IntArray, worker: IntArray, expected: Int) {
+        val actual = maxProfitAssignment(difficulty, profit, worker)
+        assertEquals(expected, actual)
+    }
+}
