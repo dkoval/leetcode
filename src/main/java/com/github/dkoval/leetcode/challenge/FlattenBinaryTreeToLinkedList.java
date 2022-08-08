b2921325b7825e6796e81117fc746ba730d8d6a8
dkoval <koval.dmitriy@gmail.com>
2022-08-08 23:13:38 +0200

    Simplify solution to "Flatten Binary Tree to Linked List" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/FlattenBinaryTreeToLinkedList.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/FlattenBinaryTreeToLinkedList.java
@@ -3,7 +3,7 @@ package com.github.dkoval.leetcode.challenge;
 import com.github.dkoval.leetcode.TreeNode;
 
 /**
- * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3742/">Flatten Binary Tree to Linked List</a>
+ * <a href="https://leetcode.com/problems/flatten-binary-tree-to-linked-list/">Flatten Binary Tree to Linked List</a>
  * <p>
  * Given the root of a binary tree, flatten the tree into a "linked list":
  * <ul>
@@ -22,31 +22,69 @@ import com.github.dkoval.leetcode.TreeNode;
  * <p>
  * Follow up: Can you flatten the tree in-place (with O(1) extra space)?
  */
-public class FlattenBinaryTreeToLinkedList {
+public interface FlattenBinaryTreeToLinkedList {
+
+    void flatten(TreeNode root);
 
     // O(N) time | O(N) space
-    public void flatten(TreeNode root) {
-        if (root == null) {
-            return;
+    class FlattenBinaryTreeToLinkedListRev1 implements FlattenBinaryTreeToLinkedList {
+
+        @Override
+        public void flatten(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+
+            flatten(left);
+            flatten(right);
+
+            root.left = null;
+            root.right = left;
+            findRightMost(root).right = right;
         }
 
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-
-        root.left = null;
-        flatten(left);
-        flatten(right);
-
-        root.right = left;
-        TreeNode rightMost = findRightMost(root);
-        rightMost.right = right;
+        private TreeNode findRightMost(TreeNode root) {
+            TreeNode curr = root;
+            while (curr != null && curr.right != null) {
+                curr = curr.right;
+            }
+            return curr;
+        }
     }
 
-    private TreeNode findRightMost(TreeNode root) {
-        TreeNode curr = root;
-        while (curr != null && curr.right != null) {
-            curr = curr.right;
+    // O(N) time | O(N) space
+    class FlattenBinaryTreeToLinkedListRev2 implements FlattenBinaryTreeToLinkedList {
+
+        // Resource: https://www.youtube.com/watch?v=rKnD7rLT0lI
+        @Override
+        public void flatten(TreeNode root) {
+            doFlatten(root);
         }
-        return curr;
+
+        // Flattens the `root` tree into a list and returns its tail
+        private TreeNode doFlatten(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+
+            TreeNode leftTail = doFlatten(root.left);
+            TreeNode rightTail = doFlatten(root.right);
+
+            // connect flattened subtrees
+            if (leftTail != null) {
+                leftTail.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+
+            // return the tail of the flattened list
+            if (leftTail == null && rightTail == null) {
+                return root;
+            }
+            return (rightTail != null) ? rightTail : leftTail;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/FlattenBinaryTreeToLinkedListTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/FlattenBinaryTreeToLinkedListTest.kt
@@ -1,14 +1,56 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.FlattenBinaryTreeToLinkedList.FlattenBinaryTreeToLinkedListRev1
+import com.github.dkoval.leetcode.challenge.FlattenBinaryTreeToLinkedList.FlattenBinaryTreeToLinkedListRev2
 import com.github.dkoval.leetcode.equalsTo
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Condition
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class FlattenBinaryTreeToLinkedListTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(2).apply {
+                        left = TreeNode(3)
+                        right = TreeNode(4)
+                    }
+                    right = TreeNode(5).apply {
+                        right = TreeNode(6)
+                    }
+                },
+                TreeNode(1).apply {
+                    right = TreeNode(2).apply {
+                        right = TreeNode(3).apply {
+                            right = TreeNode(4).apply {
+                                right = TreeNode(5).apply {
+                                    right = TreeNode(6)
+                                }
+                            }
+                        }
+                    }
+                }
+            ),
+            Arguments.of(
+                null,
+                null
+            ),
+            Arguments.of(
+                TreeNode(0),
+                TreeNode(0)
+            )
+        )
+    }
 
     companion object {
         @JvmStatic
@@ -46,10 +88,28 @@ internal class FlattenBinaryTreeToLinkedListTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should flatten a binary tree into a linked list`(root: TreeNode?, expected: TreeNode?) {
-        FlattenBinaryTreeToLinkedList().flatten(root)
+    @Nested
+    inner class FlattenBinaryTreeToLinkedListRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider:: class)
+        fun `should flatten a binary tree into a linked list`(root: TreeNode?, expected: TreeNode?) {
+            FlattenBinaryTreeToLinkedListRev1().test(root, expected)
+        }
+    }
+
+    @Nested
+    inner class FlattenBinaryTreeToLinkedListRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider:: class)
+        fun `should flatten a binary tree into a linked list`(root: TreeNode?, expected: TreeNode?) {
+            FlattenBinaryTreeToLinkedListRev2().test(root, expected)
+        }
+    }
+
+    private fun FlattenBinaryTreeToLinkedList.test(root: TreeNode?, expected: TreeNode?) {
+        flatten(root)
         assertThat(root).`is`(Condition({ actual -> expected.equalsTo(actual) }, "treeNode"))
     }
 }
