54355bfa1b99b0b11a89642b1ff5672b8ec27d1f
dkoval <koval.dmitriy@gmail.com>
2022-08-18 08:39:07 +0200

    Re-solve "Reduce Array Size to The Half" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ReduceArraySizeToHalf.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ReduceArraySizeToHalf.java
@@ -1,36 +1,46 @@
 package com.github.dkoval.leetcode.challenge;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/608/week-1-july-1st-july-7th/3804/">Reduce Array Size to The Half</a>
+ * <a href="https://leetcode.com/problems/reduce-array-size-to-the-half/">Reduce Array Size to The Half</a>
  * <p>
  * Given an array arr.  You can choose a set of integers and remove all the occurrences of these integers in the array.
  * <p>
  * Return the minimum size of the set so that at least half of the integers of the array are removed.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= arr.length <= 10^5</li>
+ *  <li>arr.length is even</li>
+ *  <li>1 <= arr[i] <= 10^5</li>
+ * </ul>
  */
-public class ReduceArraySizeToHalf {
+public interface ReduceArraySizeToHalf {
 
-    // O(NlogN) time | O(N) space
-    public int minSetSize(int[] arr) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : arr) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
+    int minSetSize(int[] arr);
 
-        Integer[] frequencies = frequencyMap.values().toArray(new Integer[0]);
-        Arrays.sort(frequencies, Comparator.reverseOrder());
-        int target = arr.length / 2;
-        int numRemoved = 0;
-        int minSetSize = 0;
-        int i = 0;
-        while (numRemoved < target) {
-            numRemoved += frequencies[i++];
-            minSetSize++;
+    // O(N*logN) time | O(N) space
+    class ReduceArraySizeToHalfRev1 implements ReduceArraySizeToHalf {
+
+        @Override
+        public int minSetSize(int[] arr) {
+            Map<Integer, Integer> frequencyMap = new HashMap<>();
+            for (int x : arr) {
+                frequencyMap.put(x, frequencyMap.getOrDefault(x, 0) + 1);
+            }
+
+            List<Integer> frequencies = new ArrayList<>(frequencyMap.values());
+            frequencies.sort(Comparator.reverseOrder());
+
+            int n = arr.length;
+            int size = n;
+            int i = 0;
+            while (size > n / 2) {
+                size -= frequencies.get(i);
+                i++;
+            }
+            return i;
         }
-        return minSetSize;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/ReduceArraySizeToHalfTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ReduceArraySizeToHalfTest.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.ReduceArraySizeToHalf.ReduceArraySizeToHalfRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class ReduceArraySizeToHalfTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(3, 3, 3, 3, 5, 5, 5, 2, 2, 7),
                 2
@@ -33,13 +38,21 @@ class ReduceArraySizeToHalfTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the minimum size of the set so that at least half of the integers of the array are removed`(
-        arr: IntArray,
-        expected: Int
-    ) {
-        val actual = ReduceArraySizeToHalf().minSetSize(arr)
+    @Nested
+    inner class ReduceArraySizeToHalfRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum size of the set so that at least half of the integers of the array are removed`(
+            arr: IntArray,
+            expected: Int
+        ) {
+            ReduceArraySizeToHalfRev1().test(arr, expected)
+        }
+    }
+
+    private fun ReduceArraySizeToHalf.test(arr: IntArray, expected: Int) {
+        val actual = minSetSize(arr)
         assertEquals(expected, actual)
     }
 }
