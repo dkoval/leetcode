ba15db9df80933f6e432ad3cae7c563bcc22c027
dkoval <koval.dmitriy@gmail.com>
2022-09-01 12:03:36 +0200

    Re-solve "Count Good Nodes in Binary Tree" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/CountGoodNodesInBinaryTree.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/CountGoodNodesInBinaryTree.java
@@ -3,7 +3,7 @@ package com.github.dkoval.leetcode.challenge;
 import com.github.dkoval.leetcode.TreeNode;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/615/week-3-august-15th-august-21st/3899/">Count Good Nodes in Binary Tree</a>
+ * <a href="https://leetcode.com/problems/count-good-nodes-in-binary-tree/">Count Good Nodes in Binary Tree</a>
  * <p>
  * Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
  * <p>
@@ -15,21 +15,32 @@ import com.github.dkoval.leetcode.TreeNode;
  *  <li>Each node's value is between [-10^4, 10^4].</li>
  * </ul>
  */
-public class CountGoodNodesInBinaryTree {
+public interface CountGoodNodesInBinaryTree {
+
+    int goodNodes(TreeNode root);
 
     // O(N) time, where N is the number of nodes in a binary tree | O(H) space, where H is the height of a binary tree
-    public int goodNodes(TreeNode root) {
-        return goodNodes(root, root.val);
-    }
+    class CountGoodNodesInBinaryTreeRev1 implements CountGoodNodesInBinaryTree {
 
-    private int goodNodes(TreeNode root, int currPathMax) {
-        // DFS
-        if (root == null) {
-            return 0;
+        public int goodNodes(TreeNode root) {
+            return dfs(root, root.val);
         }
 
-        int x = (root.val >= currPathMax) ? 1 : 0;
-        currPathMax = Math.max(currPathMax, root.val);
-        return x + goodNodes(root.left, currPathMax) + goodNodes(root.right, currPathMax);
+        private int dfs(TreeNode root, int currPathMax) {
+            // DFS
+            if (root == null) {
+                return 0;
+            }
+
+            int count = 0;
+            if (root.val >= currPathMax) {
+                count++;
+            }
+
+            currPathMax = Math.max(currPathMax, root.val);
+            count += dfs(root.left, currPathMax);
+            count += dfs(root.right, currPathMax);
+            return count;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/CountGoodNodesInBinaryTreeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/CountGoodNodesInBinaryTreeTest.kt
@@ -1,16 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.CountGoodNodesInBinaryTree.CountGoodNodesInBinaryTreeRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class CountGoodNodesInBinaryTreeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(p0: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 TreeNode(3).apply {
                     left = TreeNode(1).apply {
@@ -39,10 +44,18 @@ internal class CountGoodNodesInBinaryTreeTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the number of good nodes in a binary tree`(root: TreeNode, expected: Int) {
-        val actual = CountGoodNodesInBinaryTree().goodNodes(root)
+    @Nested
+    inner class CountGoodNodesInBinaryTreeRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of good nodes in a binary tree`(root: TreeNode, expected: Int) {
+            CountGoodNodesInBinaryTreeRev1().test(root, expected)
+        }
+    }
+
+    private fun CountGoodNodesInBinaryTree.test(root: TreeNode, expected: Int) {
+        val actual = goodNodes(root)
         assertEquals(expected, actual)
     }
 }
