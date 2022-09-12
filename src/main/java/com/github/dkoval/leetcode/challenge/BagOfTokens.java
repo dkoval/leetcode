c8110add24c306373e2ed45e2c19399f0ab62789
dkoval <koval.dmitriy@gmail.com>
2022-09-12 10:25:39 +0200

    Revise "Bag of Tokens" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/BagOfTokens.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/BagOfTokens.java
@@ -3,7 +3,7 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/explore/featured/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3506/">Bag of Tokens</a>
+ * <a href="https://leetcode.com/problems/bag-of-tokens//">Bag of Tokens</a>
  * <p>
  * You have an initial power of P, an initial score of 0, and a bag of tokens where tokens[i] is the value of
  * the ith token (0-indexed).
@@ -17,29 +17,51 @@ import java.util.Arrays;
  * Each token may be played at most once and in any order. You do not have to play all the tokens.
  * <p>
  * Return the largest possible score you can achieve after playing any number of tokens.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>0 <= tokens.length <= 1000</li>
+ *  <li>0 <= tokens[i], power < 10^4</li>
+ * </ul>
  */
-public class BagOfTokens {
+public interface BagOfTokens {
 
-    public int bagOfTokensScore(int[] tokens, int P) {
-        Arrays.sort(tokens);
-        int power = P;
-        int i = 0, j = tokens.length - 1;
-        int score = 0, maxScore = 0;
-        while (i <= j) {
-            if (power >= tokens[i]) {
-                power -= tokens[i];
-                score++;
-                maxScore = Math.max(maxScore, score);
-                i++;
+    int bagOfTokensScore(int[] tokens, int power);
 
-            } else if (score > 0) {
-                power += tokens[j];
-                score--;
-                j--;
-            } else {
-                return maxScore;
+    // O(N*logN) time | O(1) space
+    class BagOfTokensGreedy implements BagOfTokens {
+
+        public int bagOfTokensScore(int[] tokens, int power) {
+            int n = tokens.length;
+
+            // sort tokens in ASC order
+            Arrays.sort(tokens);
+
+            int left = 0;
+            int right = n - 1;
+            int score = 0;
+            int maxScore = 0;
+            while (left <= right) {
+                if (power >= tokens[left]) {
+                    // when playing a token face up, we want to minimize lost units of power, therefore
+                    // we choose tokens[left]
+                    power -= tokens[left];
+                    score++;
+                    maxScore = Math.max(maxScore, score);
+                    left++;
+                } else if (score >= 1) {
+                    // when playing a token face down, we want to maximize gained units of power, therefore
+                    // we choose tokens[right]
+                    power += tokens[right];
+                    score--;
+                    maxScore = Math.max(maxScore, score);
+                    right--;
+                } else {
+                    // can't play anymore
+                    return maxScore;
+                }
             }
+            return maxScore;
         }
-        return maxScore;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/BagOfTokensTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/BagOfTokensTest.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.BagOfTokens.BagOfTokensGreedy
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class BagOfTokensTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(100),
                 50,
@@ -28,14 +33,22 @@ internal class BagOfTokensTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the largest possible score you can achieve after playing any number of tokens`(
-        tokens: IntArray,
-        P: Int,
-        expected: Int
-    ) {
-        val actual = BagOfTokens().bagOfTokensScore(tokens, P)
+    @Nested
+    inner class BagOfTokensGreedyTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the largest possible score you can achieve after playing any number of tokens`(
+            tokens: IntArray,
+            power: Int,
+            expected: Int
+        ) {
+            BagOfTokensGreedy().test(tokens, power, expected)
+        }
+    }
+
+    private fun BagOfTokens.test(tokens: IntArray, power: Int, expected: Int) {
+        val actual = bagOfTokensScore(tokens, power)
         assertEquals(expected, actual)
     }
 }
