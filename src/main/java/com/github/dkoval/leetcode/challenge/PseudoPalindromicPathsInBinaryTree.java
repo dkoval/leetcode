3eb76ae137f56ac93a6cbf9e08eb357b60f0b5c3
dkoval <koval.dmitriy@gmail.com>
2022-09-16 10:28:28 +0200

    Revise "Pseudo-Palindromic Paths in a Binary Tree" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/PseudoPalindromicPathsInBinaryTree.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/PseudoPalindromicPathsInBinaryTree.java
@@ -3,38 +3,60 @@ package com.github.dkoval.leetcode.challenge;
 import com.github.dkoval.leetcode.TreeNode;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/573/week-5-december-29th-december-31st/3585/">Pseudo-Palindromic Paths in a Binary Tree</a>
+ * <a href="https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/">Pseudo-Palindromic Paths in a Binary Tree</a>
+ * <p>
+ * Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic
+ * if at least one permutation of the node values in the path is a palindrome.
+ * <p>
+ * Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree is in the range [1, 10^5]</li>
+ *  <li>1 <= Node.val <= 9</li>
+ * </ul>
  */
-public class PseudoPalindromicPathsInBinaryTree {
-    private final int[] digits = new int[10];
-    private int count = 0;
+public interface PseudoPalindromicPathsInBinaryTree {
 
-    public int pseudoPalindromicPaths (TreeNode root) {
-        dfs(root);
-        return count;
-    }
+    int pseudoPalindromicPaths(TreeNode root);
 
-    private void dfs(TreeNode root) {
-        if (root == null) {
-            return;
+    // O(N) time | O(H) space
+    class PseudoPalindromicPathsInBinaryTreeRev1 implements PseudoPalindromicPathsInBinaryTree {
+
+        @Override
+        public int pseudoPalindromicPaths(TreeNode root) {
+            return dfs(root, new int[10]);
         }
-        digits[root.val]++;
-        if (root.left == null && root.right == null && isPalindrome()) {
-            count++;
-        } else {
-            dfs(root.left);
-            dfs(root.right);
-        }
-        digits[root.val]--;
-    }
 
-    private boolean isPalindrome() {
-        int numOddFrequencies = 0;
-        for (int i = 1; i <= 9; i++) {
-            if (digits[i] % 2 != 0) {
-                numOddFrequencies++;
+        private int dfs(TreeNode root, int[] digits) {
+            if (root == null) {
+                return 0;
             }
+
+            digits[root.val]++;
+            int count = 0;
+
+            if (root.left == null && root.right == null && isPseudoPalindromic(digits)) {
+                count++;
+            } else {
+                count += dfs(root.left, digits);
+                count += dfs(root.right, digits);
+            }
+
+            // backtrack
+            digits[root.val]--;
+            return count;
         }
-        return numOddFrequencies <= 1;
+
+        private boolean isPseudoPalindromic(int[] digits) {
+            // count digits having odd frequency
+            int odd = 0;
+            for (int i = 1; i <= 9; i++) {
+                if (digits[i] % 2 != 0) {
+                    odd++;
+                }
+            }
+            return odd <= 1;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/PseudoPalindromicPathsInBinaryTreeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/PseudoPalindromicPathsInBinaryTreeTest.kt
@@ -1,16 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.PseudoPalindromicPathsInBinaryTree.PseudoPalindromicPathsInBinaryTreeRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class PseudoPalindromicPathsInBinaryTreeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 TreeNode(2).apply {
                     left = TreeNode(3).apply {
@@ -42,13 +47,21 @@ internal class PseudoPalindromicPathsInBinaryTreeTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the number of pseudo-palindromic paths going from the root node to leaf nodes`(
-        root: TreeNode?,
-        expected: Int
-    ) {
-        val actual = PseudoPalindromicPathsInBinaryTree().pseudoPalindromicPaths(root)
+    @Nested
+    inner class PseudoPalindromicPathsInBinaryTreeRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of pseudo-palindromic paths going from the root node to leaf nodes`(
+            root: TreeNode,
+            expected: Int
+        ) {
+            PseudoPalindromicPathsInBinaryTreeRev1().test(root, expected)
+        }
+    }
+
+    private fun PseudoPalindromicPathsInBinaryTree.test(root: TreeNode, expected: Int) {
+        val actual = pseudoPalindromicPaths(root)
         assertEquals(expected, actual)
     }
 }
