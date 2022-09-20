d545a666c09ed5fc056eae7a3fd9b8b85b283963
dkoval <koval.dmitriy@gmail.com>
2022-09-20 10:13:53 +0200

    Space optimized solution to "Maximum Length of Repeated Subarray" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MaximumLengthOfRepeatedSubarray.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MaximumLengthOfRepeatedSubarray.java
@@ -1,7 +1,7 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/609/week-2-july-8th-july-14th/3807/">Maximum Length of Repeated Subarray</a>
+ * <a href="https://leetcode.com/problems/maximum-length-of-repeated-subarray/">Maximum Length of Repeated Subarray</a>
  * <p>
  * Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
  * <p>
@@ -11,25 +11,62 @@ package com.github.dkoval.leetcode.challenge;
  *  <li>0 <= nums1[i], nums2[i] <= 100</li>
  * </ul>
  */
-public class MaximumLengthOfRepeatedSubarray {
+public interface MaximumLengthOfRepeatedSubarray {
 
-    // O(N1 * N2) time | O(N1 * N2) space
-    public int findLength(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+    int findLength(int[] nums1, int[] nums2);
 
-        // dp[i][j] the maximum length of the longest common subarray of
-        // nums1[0:i - 1] and nums2[0:j - 1] ending with num1[i] and nums2[j]
-        int best = 0;
-        int[][] dp = new int[n1 + 1][n2 + 1];
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                    best = Math.max(best, dp[i][j]);
+    class MaximumLengthOfRepeatedSubarrayDPBottomUp implements MaximumLengthOfRepeatedSubarray {
+
+        // O(N1 * N2) time | O(N1 * N2) space
+        public int findLength(int[] nums1, int[] nums2) {
+            int n1 = nums1.length;
+            int n2 = nums2.length;
+
+            // dp[i][j] the maximum length of the longest common subarray of
+            // nums1[0 : i - 1] and nums2[0 : j - 1] ending with nums1[i] and nums2[j]
+            int best = 0;
+            int[][] dp = new int[n1 + 1][n2 + 1];
+            for (int i = 1; i <= n1; i++) {
+                for (int j = 1; j <= n2; j++) {
+                    if (nums1[i - 1] == nums2[j - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                        best = Math.max(best, dp[i][j]);
+                    }
                 }
             }
+            return best;
         }
-        return best;
+    }
+
+    class MaximumLengthOfRepeatedSubarrayWithOffset implements MaximumLengthOfRepeatedSubarray {
+
+        // O(N1 * N2) time | O(1) space
+        @Override
+        public int findLength(int[] nums1, int[] nums2) {
+            return Math.max(doFindLength(nums1, nums2), doFindLength(nums2, nums1));
+        }
+
+        private int doFindLength(int[] nums1, int[] nums2) {
+            int n1 = nums1.length;
+            int n2 = nums2.length;
+
+            int maxLength = 0;
+            for (int offset = 0; offset < n2; offset++) {
+                int currLength = 0;
+                for (int i = 0; i < n1; i++) {
+                    if (i + offset >= n2) {
+                        break;
+                    }
+
+                    if (nums1[i] == nums2[i + offset]) {
+                        currLength += 1;
+                        maxLength = Math.max(maxLength, currLength);
+                    } else {
+                        currLength = 0;
+                    }
+                }
+            }
+            return maxLength;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumLengthOfRepeatedSubarrayTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumLengthOfRepeatedSubarrayTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MaximumLengthOfRepeatedSubarray.MaximumLengthOfRepeatedSubarrayDPBottomUp
+import com.github.dkoval.leetcode.challenge.MaximumLengthOfRepeatedSubarray.MaximumLengthOfRepeatedSubarrayWithOffset
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MaximumLengthOfRepeatedSubarrayTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(1, 2, 3, 2, 1),
                 intArrayOf(3, 2, 1, 4, 7),
@@ -20,18 +26,45 @@ internal class MaximumLengthOfRepeatedSubarrayTest {
                 intArrayOf(0, 0, 0, 0, 0),
                 intArrayOf(0, 0, 0, 0, 0),
                 5
+            ),
+            Arguments.of(
+                intArrayOf(0, 1, 1, 1, 1),
+                intArrayOf(1, 0, 1, 0, 1),
+                2
             )
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the maximum length of a subarray that appears in both arrays`(
-        nums1: IntArray,
-        nums2: IntArray,
-        expected: Int
-    ) {
-        val actual = MaximumLengthOfRepeatedSubarray().findLength(nums1, nums2)
+    @Nested
+    inner class MaximumLengthOfRepeatedSubarrayDPBottomUpTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum length of a subarray that appears in both arrays`(
+            nums1: IntArray,
+            nums2: IntArray,
+            expected: Int
+        ) {
+            MaximumLengthOfRepeatedSubarrayDPBottomUp().test(nums1, nums2, expected)
+        }
+    }
+
+    @Nested
+    inner class MaximumLengthOfRepeatedSubarrayWithOffsetTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum length of a subarray that appears in both arrays`(
+            nums1: IntArray,
+            nums2: IntArray,
+            expected: Int
+        ) {
+            MaximumLengthOfRepeatedSubarrayWithOffset().test(nums1, nums2, expected)
+        }
+    }
+
+    private fun MaximumLengthOfRepeatedSubarray.test(nums1: IntArray, nums2: IntArray, expected: Int) {
+        val actual = findLength(nums1, nums2)
         assertEquals(expected, actual)
     }
 }
