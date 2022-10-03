ed48a26ce840003613f24e4a25bd84ded3fafde4
dkoval <koval.dmitriy@gmail.com>
2022-10-03 10:23:11 +0200

    Solution to "Minimum Time to Make Rope Colorful" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumTimeToMakeRopeColorful.java
@@ -0,0 +1,54 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/minimum-time-to-make-rope-colorful/">Minimum Time to Make Rope Colorful</a>
+ * <p>
+ * Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+ * <p>
+ * Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color,
+ * so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful.
+ * You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+ * <p>
+ * Return the minimum time Bob needs to make the rope colorful.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == colors.length == neededTime.length</li>
+ *  <li>1 <= n <= 10^5</li>
+ *  <li>1 <= neededTime[i] <= 10^4</li>
+ *  <li>colors contains only lowercase English letters.</li>
+ * </ul>
+ */
+public interface MinimumTimeToMakeRopeColorful {
+
+    int minCost(String colors, int[] neededTime);
+
+    // O(N) time | O(1) space
+    class MinimumTimeToMakeRopeColorfulRev1 implements MinimumTimeToMakeRopeColorful {
+
+        @Override
+        public int minCost(String colors, int[] neededTime) {
+            int n = colors.length();
+
+            int total = 0;
+            int i = 1;
+            while (i < n) {
+                if (colors.charAt(i) == colors.charAt(i - 1)) {
+                    // in a sequence of balloons of the same color, leave the balloon with the maximum neededTime and
+                    // remove the remaining ones; this logic will guarantee that the total time is minimized.
+                    int sum = neededTime[i - 1];
+                    int max = neededTime[i - 1];
+                    while (i < n && colors.charAt(i) == colors.charAt(i - 1)) {
+                        sum += neededTime[i];
+                        max = Math.max(max, neededTime[i]);
+                        i++;
+                    }
+                    total += sum - max;
+                } else {
+                    i++;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumTimeToMakeRopeColorfulTest.kt
@@ -0,0 +1,53 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.MinimumTimeToMakeRopeColorful.MinimumTimeToMakeRopeColorfulRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class MinimumTimeToMakeRopeColorfulTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "abaac",
+                intArrayOf(1, 2, 3, 4, 5),
+                3
+            ),
+            Arguments.of(
+                "abc",
+                intArrayOf(1, 2, 3),
+                0
+            ),
+            Arguments.of(
+                "aabaa",
+                intArrayOf(1, 2, 3, 4, 1),
+                2
+            )
+        )
+    }
+
+    @Nested
+    inner class MinimumTimeToMakeRopeColorfulRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum time to make the rope colorful`(
+            colors: String,
+            neededTime: IntArray,
+            expected: Int
+        ) {
+            MinimumTimeToMakeRopeColorfulRev1().test(colors, neededTime, expected)
+        }
+    }
+
+    private fun MinimumTimeToMakeRopeColorful.test(colors: String, neededTime: IntArray, expected: Int) {
+        val actual = minCost(colors, neededTime)
+        assertEquals(expected, actual)
+    }
+}
