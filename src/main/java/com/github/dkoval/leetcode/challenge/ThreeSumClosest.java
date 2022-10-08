c4aab4f2d63a4ce0b0c9f2cb39ca5330321265eb
dkoval <koval.dmitriy@gmail.com>
2022-10-08 11:16:00 +0200

    Revise "3Sum Closest" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ThreeSumClosest.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ThreeSumClosest.java
@@ -3,7 +3,7 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/611/week-4-july-22nd-july-28th/3828/">3Sum Closest</a>
+ * <a href="https://leetcode.com/problems/3sum-closest/">3Sum Closest</a>
  * <p>
  * Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.
  * Return the sum of the three integers. You may assume that each input would have exactly one solution.
@@ -15,30 +15,69 @@ import java.util.Arrays;
  *  <li>-10^4 <= target <= 10^4</li>
  * </ul>
  */
-public class ThreeSumClosest {
+public interface ThreeSumClosest {
+
+    int threeSumClosest(int[] nums, int target);
 
     // O(N^2) time | O(1) space
-    public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int closestSum3 = nums[0] + nums[1] + nums[n - 1];
-        for (int i = 0; i < n - 2; i++) {
-            int l = i + 1;
-            int r = n - 1;
-            while (l < r) {
-                int currSum3 = nums[i] + nums[l] + nums[r];
-                if (currSum3 < target) {
-                    // go to the next largest element
-                    l++;
-                } else {
-                    // go to the next smallest element
-                    r--;
-                }
-                if (Math.abs(currSum3 - target) < Math.abs(closestSum3 - target)) {
-                    closestSum3 = currSum3;
+    class ThreeSumClosestRev1 implements ThreeSumClosest {
+
+        @Override
+        public int threeSumClosest(int[] nums, int target) {
+            int n = nums.length;
+            Arrays.sort(nums);
+
+            int closestSum3 = nums[0] + nums[1] + nums[n - 1];
+            for (int i = 0; i < n - 2; i++) {
+                int l = i + 1;
+                int r = n - 1;
+                while (l < r) {
+                    int currSum3 = nums[i] + nums[l] + nums[r];
+                    if (currSum3 < target) {
+                        // go to the next largest element
+                        l++;
+                    } else {
+                        // go to the next smallest element
+                        r--;
+                    }
+                    if (Math.abs(currSum3 - target) < Math.abs(closestSum3 - target)) {
+                        closestSum3 = currSum3;
+                    }
                 }
             }
+            return closestSum3;
         }
-        return closestSum3;
+    }
+
+    // O(N^2) time | O(1) space
+    class ThreeSumClosestRev2 implements ThreeSumClosest {
+
+        @Override
+        public int threeSumClosest(int[] nums, int target) {
+            int n = nums.length;
+            Arrays.sort(nums);
+
+            int bestSum = 42;
+            int bestDiff = Integer.MAX_VALUE;
+            for (int i = 0; i < n - 2; i++) {
+                int left = i + 1;
+                int right = n - 1;
+                while (left < right) {
+                    int currSum = nums[left] + nums[right] + nums[i];
+                    int currDiff = Math.abs(target - currSum);
+                    if (currDiff < bestDiff) {
+                        bestSum = currSum;
+                        bestDiff = currDiff;
+                    }
+
+                    if (currSum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            return bestSum;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/ThreeSumClosestTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ThreeSumClosestTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.ThreeSumClosest.ThreeSumClosestRev1
+import com.github.dkoval.leetcode.challenge.ThreeSumClosest.ThreeSumClosestRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class ThreeSumClosestTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(-1, 2, 1, -4),
                 1,
@@ -28,14 +34,36 @@ internal class ThreeSumClosestTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find three integers in nums such that the sum is closest to target`(
-        nums: IntArray,
-        target: Int,
-        expected: Int
-    ) {
-        val actual = ThreeSumClosest().threeSumClosest(nums, target)
+    @Nested
+    inner class ThreeSumClosestRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find three integers in nums such that the sum is closest to target`(
+            nums: IntArray,
+            target: Int,
+            expected: Int
+        ) {
+            ThreeSumClosestRev1().test(nums, target, expected)
+        }
+    }
+
+    @Nested
+    inner class ThreeSumClosestRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find three integers in nums such that the sum is closest to target`(
+            nums: IntArray,
+            target: Int,
+            expected: Int
+        ) {
+            ThreeSumClosestRev2().test(nums, target, expected)
+        }
+    }
+
+    private fun ThreeSumClosest.test(nums: IntArray, target: Int, expected: Int) {
+        val actual = threeSumClosest(nums, target)
         assertEquals(expected, actual)
     }
 }
