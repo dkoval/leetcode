07ffe78abb4ae4e85633ae2949cd00d747fa1e15
dkoval <koval.dmitriy@gmail.com>
2022-10-20 23:35:14 +0200

    Re-solve to "Integer to Roman" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/IntegerToRoman.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/IntegerToRoman.java
@@ -1,10 +1,11 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3667/">Integer to Roman</a>
+ * <a href="https://leetcode.com/problems/integer-to-roman/">Integer to Roman</a>
  * <p>
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
  * <p>
@@ -26,42 +27,104 @@ import java.util.Map;
  * <p>
  * 1 <= num <= 3999
  */
-public class IntegerToRoman {
-    private static final Map<Integer, String> romans = new LinkedHashMap<>();
+public interface IntegerToRoman {
 
-    static {
-        romans.put(1000, "M");
-        romans.put(900, "CM");
-        romans.put(500, "D");
-        romans.put(400, "CD");
-        romans.put(100, "C");
-        romans.put(90, "XC");
-        romans.put(50, "L");
-        romans.put(40, "XL");
-        romans.put(10, "X");
-        romans.put(9, "IX");
-        romans.put(5, "V");
-        romans.put(4, "IV");
-        romans.put(1, "I");
-    }
+    String intToRoman(int num);
 
-    public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<Integer, String> entry : romans.entrySet()) {
-            int x = entry.getKey();
-            String roman = entry.getValue();
-            if (num >= x) {
-                int times = num / x;
-                append(sb, roman, times);
-                num %= x;
+    class IntegerToRomanRev1 implements IntegerToRoman {
+
+        private static final Map<Integer, String> lookup = new LinkedHashMap<>();
+
+        static {
+            lookup.put(1000, "M");
+            lookup.put(900, "CM");
+            lookup.put(500, "D");
+            lookup.put(400, "CD");
+            lookup.put(100, "C");
+            lookup.put(90, "XC");
+            lookup.put(50, "L");
+            lookup.put(40, "XL");
+            lookup.put(10, "X");
+            lookup.put(9, "IX");
+            lookup.put(5, "V");
+            lookup.put(4, "IV");
+            lookup.put(1, "I");
+        }
+
+        public String intToRoman(int num) {
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<Integer, String> entry : lookup.entrySet()) {
+                int x = entry.getKey();
+                String roman = entry.getValue();
+                if (num >= x) {
+                    int times = num / x;
+                    repeat(sb, roman, times);
+                    num %= x;
+                }
+            }
+            return sb.toString();
+        }
+
+        private void repeat(StringBuilder sb, String roman, int times) {
+            while (times-- > 0) {
+                sb.append(roman);
             }
         }
-        return sb.toString();
     }
 
-    private static void append(StringBuilder sb, String roman, int times) {
-        while (times-- > 0) {
-            sb.append(roman);
+    class IntegerToRomanRev2 implements IntegerToRoman {
+
+        private static final Map<Integer, Character> lookup = new HashMap<>();
+
+        static {
+            lookup.put(1000, 'M');
+            lookup.put(500, 'D');
+            lookup.put(100, 'C');
+            lookup.put(50, 'L');
+            lookup.put(10, 'X');
+            lookup.put(5, 'V');
+            lookup.put(1, 'I');
+        }
+
+        @Override
+        public String intToRoman(int num) {
+            StringBuilder sb = new StringBuilder();
+            num = process(sb, num, 1000);
+            num = process(sb, num, 100);
+            num = process(sb, num, 10);
+            process(sb, num, 1);
+            return sb.toString();
+        }
+
+        private int process(StringBuilder sb, int num, int multiple) {
+            if (num >= multiple) {
+                char symbol = lookup.get(multiple);
+                int x = num / multiple;
+                if (x < 4) {
+                    repeat(sb, symbol, x);
+                } else  {
+                    char mid = lookup.get(5 * multiple);
+                    if (x == 4) {
+                        sb.append(symbol);
+                        sb.append(mid);
+                    } else if (x < 9) {
+                        sb.append(mid);
+                        repeat(sb, symbol, x - 5);
+                    } else /* if (x == 9) */ {
+                        char next = lookup.get(10 * multiple);
+                        sb.append(symbol);
+                        sb.append(next);
+                    }
+                }
+                num %= multiple;
+            }
+            return num;
+        }
+
+        private void repeat(StringBuilder sb, char symbol, int times) {
+            while (times-- > 0) {
+                sb.append(symbol);
+            }
         }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/IntegerToRomanTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/IntegerToRomanTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.IntegerToRoman.IntegerToRomanRev1
+import com.github.dkoval.leetcode.challenge.IntegerToRoman.IntegerToRomanRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class IntegerToRomanTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(3, "III"),
             Arguments.of(4, "IV"),
             Arguments.of(9, "IX"),
@@ -18,10 +24,28 @@ internal class IntegerToRomanTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should convert an integer to to a roman numeral`(num: Int, expected: String) {
-        val actual = IntegerToRoman().intToRoman(num)
+    @Nested
+    inner class IntegerToRomanRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should convert an integer to to a roman numeral`(num: Int, expected: String) {
+            IntegerToRomanRev1().test(num, expected)
+        }
+    }
+
+    @Nested
+    inner class IntegerToRomanRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should convert an integer to to a roman numeral`(num: Int, expected: String) {
+            IntegerToRomanRev2().test(num, expected)
+        }
+    }
+
+    private fun IntegerToRoman.test(num: Int, expected: String) {
+        val actual = intToRoman(num)
         assertEquals(expected, actual)
     }
 }
