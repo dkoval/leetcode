1ecc3be075c9418b0256a46674d9f2f6699aa7bb
dkoval <koval.dmitriy@gmail.com>
2022-10-23 10:59:02 +0200

    Revise "Set Mismatch" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SetMismatch.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SetMismatch.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3658/">Set Mismatch</a>
+ * <a href="https://leetcode.com/problems/set-mismatch/">Set Mismatch</a>
  * <p>
  * You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error,
  * one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and
@@ -13,6 +13,12 @@ import java.util.Set;
  * You are given an integer array nums representing the data status of this set after the error.
  * <p>
  * Find the number that occurs twice and the number that is missing and return them in the form of an array.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= nums.length <= 10^4</li>
+ *  <li>1 <= nums[i] <= 10^4</li>
+ * </ul>
  */
 public abstract class SetMismatch {
 
@@ -22,47 +28,57 @@ public abstract class SetMismatch {
     public static class SetMismatchNaive extends SetMismatch {
 
         public int[] findErrorNums(int[] nums) {
+            int n = nums.length;
             Set<Integer> seen = new HashSet<>();
-            int[] answer = new int[2];
-            // 1 + 2 + ... + n = n * (n + 1) / 2
-            int sum = 0;
-            for (int num : nums) {
-                if (seen.contains(num)) {
-                    answer[0] = num;
+
+            // ans[0] - duplicate, ans[1] - missing
+            int[] ans = new int[2];
+            // record the sum of non-repeatable numbers
+            int sumOfUnique = 0;
+            for (int x : nums) {
+                if (seen.contains(x)) {
+                    ans[0] = x;
                 } else {
-                    sum += num;
-                    seen.add(num);
+                    sumOfUnique += x;
+                    seen.add(x);
                 }
             }
-            answer[1] = nums.length * (nums.length + 1) / 2 - sum;
-            return answer;
+
+            // 1 + 2 + ... + n = n * (n + 1) / 2
+            ans[1] = n * (n + 1) / 2 - sumOfUnique;
+            return ans;
         }
     }
 
     // O(N) time | O(1) space
     public static class SetMismatchOptimal extends SetMismatch {
 
-        // Resource: https://www.youtube.com/watch?v=u6Dt9av66yM&t=338s
+        // Resource: https://www.youtube.com/watch?v=u6Dt9av66yM
         @Override
         public int[] findErrorNums(int[] nums) {
-            int[] answer = new int[2];
+            int n = nums.length;
+            // ans[0] - duplicate, ans[1] - missing
+            int[] ans = new int[2];
+
             // 1st pass - find the duplicate number
-            for (int num : nums) {
-                int i = Math.abs(num) - 1; // map num to index
+            for (int x : nums) {
+                int i = Math.abs(x) - 1; // map x to index
                 if (nums[i] < 0) {
-                    answer[0] = i + 1;
+                    ans[0] = i + 1;
                 } else {
-                    nums[i] *= -1; // convert to negative
+                    // mark x as visited
+                    nums[i] *= -1;
                 }
             }
+
             // 2nd pass - find the missing number
-            for (int i = 0; i < nums.length; i++) {
+            for (int i = 0; i < n; i++) {
                 if (nums[i] > 0) {
-                    answer[1] = i + 1;
+                    ans[1] = i + 1;
                     break;
                 }
             }
-            return answer;
+            return ans;
         }
     }
 }
