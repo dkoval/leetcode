b10ebec9e91f8398e21a6dbfe6fd79d93d633c62
dkoval <koval.dmitriy@gmail.com>
2022-11-08 10:23:25 +0100

    Solution to "Make The String Great" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/MakeStringGreat.java
@@ -0,0 +1,61 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * <a href="https://leetcode.com/problems/make-the-string-great/">Make The String Great</a>
+ * <p>
+ * Given a string s of lower and upper case English letters.
+ * <p>
+ * A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+ * <ul>
+ *  <li>0 <= i <= s.length - 2</li>
+ *  <li>s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.</li>
+ * </ul>
+ * To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+ * <p>
+ * Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+ * <p>
+ * Notice that an empty string is also good.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= s.length <= 100</li>
+ *  <li>s contains only lower and upper case English letters.</li>
+ * </ul>
+ */
+public interface MakeStringGreat {
+
+    String makeGood(String s);
+
+    // O(N) time | O(N) space
+    class MakeStringGreatRev1 implements MakeStringGreat {
+
+        @Override
+        public String makeGood(String s) {
+            int n = s.length();
+
+            Deque<Character> stack = new ArrayDeque<>();
+            stack.push(s.charAt(0));
+            for (int i = 1; i < n; i++) {
+                char c2 = s.charAt(i);
+                if (!stack.isEmpty() &&
+                        (Character.isUpperCase(c2) && stack.peek() == Character.toLowerCase(c2) ||
+                                Character.isLowerCase(c2) && stack.peek() == Character.toUpperCase(c2))) {
+                    stack.pop();
+                } else {
+                    stack.push(c2);
+                }
+            }
+
+            char[] ans = new char[stack.size()];
+            int i = stack.size() - 1;
+            while (!stack.isEmpty()) {
+                ans[i] = stack.pop();
+                i--;
+            }
+            return String.valueOf(ans);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MakeStringGreatTest.kt
@@ -0,0 +1,39 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.MakeStringGreat.MakeStringGreatRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class MakeStringGreatTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of("leEeetcode", "leetcode"),
+            Arguments.of("abBAcC", ""),
+            Arguments.of("s", "s"),
+            Arguments.of("Pp", "")
+        )
+    }
+
+    @Nested
+    inner class MakeStringGreatRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should make the string great`(s: String, expected: String) {
+            MakeStringGreatRev1().test(s, expected)
+        }
+    }
+
+    private fun MakeStringGreat.test(s: String, expected: String) {
+        val actual = makeGood(s)
+        assertEquals(expected, actual)
+    }
+}
