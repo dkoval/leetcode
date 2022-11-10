c411a204f6fa764e4a40aa2b40e1e4f30887b79d
dkoval <koval.dmitriy@gmail.com>
2022-11-10 09:49:43 +0100

    Re-solve "Remove All Adjacent Duplicates In String" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/RemoveAllAdjacentDuplicatesInString.java
@@ -1,11 +1,11 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Stack;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/606/week-4-june-22nd-june-28th/3794/">Remove All Adjacent Duplicates In String</a>
+ * <a href="https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/">Remove All Adjacent Duplicates In String</a>
  * <p>
  * You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent
  * and equal letters and removing them.
@@ -13,6 +13,12 @@ import java.util.Stack;
  * We repeatedly make duplicate removals on s until we no longer can.
  * <p>
  * Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= s.length <= 10^5</li>
+ *  <li>s consists of lowercase English letters</li>
+ * </ul>
  */
 public interface RemoveAllAdjacentDuplicatesInString {
 
@@ -31,11 +37,12 @@ public interface RemoveAllAdjacentDuplicatesInString {
                 }
             }
 
-            StringBuilder sb = new StringBuilder();
+            char[] ans = new char[stack.size()];
+            int i = stack.size() - 1;
             while (!stack.isEmpty()) {
-                sb.append(stack.pop());
+                ans[i--] = stack.pop();
             }
-            return sb.reverse().toString();
+            return String.valueOf(ans);
         }
     }
 
@@ -43,7 +50,7 @@ public interface RemoveAllAdjacentDuplicatesInString {
 
         @Override
         public String removeDuplicates(String s) {
-            Deque<Character> dq = new LinkedList<>();
+            Deque<Character> dq = new ArrayDeque<>();
             for (int i = 0; i < s.length(); i++) {
                 if (dq.isEmpty() || dq.peekLast() != s.charAt(i)) {
                     dq.offerLast(s.charAt(i));
@@ -52,11 +59,11 @@ public interface RemoveAllAdjacentDuplicatesInString {
                 }
             }
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder ans = new StringBuilder();
             while (!dq.isEmpty()) {
-                sb.append(dq.pollFirst());
+                ans.append(dq.pollFirst());
             }
-            return sb.toString();
+            return ans.toString();
         }
     }
 }
