826d51603b74a0596cbd0ee925532346fb46b065
dkoval <koval.dmitriy@gmail.com>
2022-12-01 10:23:45 +0100

    Re-solve "Determine if String Halves Are Alike" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/DetermineIfStringHalvesAreAlike.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/DetermineIfStringHalvesAreAlike.java
@@ -1,7 +1,11 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * <a href="https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/593/week-1-april-1st-april-7th/3699/">Determine if String Halves Are Alike</a>
+ * <a href="https://leetcode.com/problems/determine-if-string-halves-are-alike/">Determine if String Halves Are Alike</a>
  * <p>
  * You are given a string s of even length. Split this string into two halves of equal lengths,
  * and let a be the first half and b be the second half.
@@ -10,25 +14,59 @@ package com.github.dkoval.leetcode.challenge;
  * Notice that s contains uppercase and lowercase letters.
  * <p>
  * Return true if a and b are alike. Otherwise, return false.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= s.length <= 1000</li>
+ *  <li>s.length is even.</li>
+ *  <li>s consists of uppercase and lowercase letters.</li>
+ * </ul>
  */
-public class DetermineIfStringHalvesAreAlike {
+public interface DetermineIfStringHalvesAreAlike {
+
+    boolean halvesAreAlike(String s);
 
     // O(N) time | O(1) space
-    public boolean halvesAreAlike(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            count += inc(s, i);
+    class DetermineIfStringHalvesAreAlikeRev1 implements DetermineIfStringHalvesAreAlike {
+
+        @Override
+        public boolean halvesAreAlike(String s) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                count += inc(s, i);
+            }
+            return (count == 0);
         }
-        return (count == 0);
+
+        private int inc(String s, int idx) {
+            int inc = isVowel(s.charAt(idx)) ? 1 : 0;
+            return (idx < s.length() / 2) ? inc : -inc;
+        }
+
+        private boolean isVowel(char c) {
+            char lc = Character.toLowerCase(c);
+            return (lc == 'a' || lc == 'e' || lc == 'i' || lc == 'o' || lc == 'u');
+        }
     }
 
-    private int inc(String s, int idx) {
-        int inc = isVowel(s.charAt(idx)) ? 1 : 0;
-        return (idx < s.length() / 2) ? inc : -inc;
-    }
+    class DetermineIfStringHalvesAreAlikeRev2 implements DetermineIfStringHalvesAreAlike {
+        private static final Set<Character> VOWELS = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-    private boolean isVowel(char c) {
-        char lc = Character.toLowerCase(c);
-        return (lc == 'a' || lc == 'e' || lc == 'i' || lc == 'o' || lc == 'u');
+        @Override
+        public boolean halvesAreAlike(String s) {
+            int n = s.length();
+            return countVowels(s, 0, n / 2 - 1) == countVowels(s, n / 2, n - 1);
+        }
+
+        private int countVowels(String s, int start, int end) {
+            int count = 0;
+            for (int i = start; i <= end; i++) {
+                char c = Character.toLowerCase(s.charAt(i));
+                if (VOWELS.contains(c)) {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/DetermineIfStringHalvesAreAlikeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/DetermineIfStringHalvesAreAlikeTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.DetermineIfStringHalvesAreAlike.DetermineIfStringHalvesAreAlikeRev1
+import com.github.dkoval.leetcode.challenge.DetermineIfStringHalvesAreAlike.DetermineIfStringHalvesAreAlikeRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class DetermineIfStringHalvesAreAlikeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of("book", true),
             Arguments.of("textbook", false),
             Arguments.of("MerryChristmas", false),
@@ -17,10 +23,28 @@ internal class DetermineIfStringHalvesAreAlikeTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should determine of string halves are alike`(s: String, expected: Boolean) {
-        val actual = DetermineIfStringHalvesAreAlike().halvesAreAlike(s)
+    @Nested
+    inner class DetermineIfStringHalvesAreAlikeRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should determine of string halves are alike`(s: String, expected: Boolean) {
+            DetermineIfStringHalvesAreAlikeRev1().test(s, expected)
+        }
+    }
+
+    @Nested
+    inner class DetermineIfStringHalvesAreAlikeRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should determine of string halves are alike`(s: String, expected: Boolean) {
+            DetermineIfStringHalvesAreAlikeRev2().test(s, expected)
+        }
+    }
+
+    private fun DetermineIfStringHalvesAreAlike.test(s: String, expected: Boolean) {
+        val actual = halvesAreAlike(s)
         assertEquals(expected, actual)
     }
 }
