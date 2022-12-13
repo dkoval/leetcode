436c420c010ba99608cd683806ea7cf4cbb2e0cb
dkoval <koval.dmitriy@gmail.com>
2022-12-13 15:57:43 +0100

    Resolve "Minimum Falling Path Sum" problem

--- src/main/java/com/github/dkoval/leetcode/problems/MinimumFallingPathSum.java
+++ src/main/java/com/github/dkoval/leetcode/problems/MinimumFallingPathSum.java
@@ -1,5 +1,7 @@
 package com.github.dkoval.leetcode.problems;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/minimum-falling-path-sum/">Minimum Falling Path Sum</a>
  * <p>
@@ -17,39 +19,86 @@ package com.github.dkoval.leetcode.problems;
  *  <li>-100 <= matrix[i][j] <= 100</li>
  * </ul>
  */
-public class MinimumFallingPathSum {
+public interface MinimumFallingPathSum {
 
-    private static final int[][] DIRECTIONS = {{-1, -1}, {-1, 0}, {-1, 1}};
+    int minFallingPathSum(int[][] matrix);
 
     // O(N^2) time | O(N^2) space
-    public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
+    class MinimumFallingPathSumDpTopDown implements MinimumFallingPathSum {
+        private static final int[][] DIRECTIONS = {{1, -1}, {1, 0}, {1, 1}};
 
-        // dp[i][j] is the minimum falling path sum
-        int[][] dp = new int[n][n];
-        for (int col = 0; col < n; col++) {
-            dp[0][col] = matrix[0][col];
-        }
+        public int minFallingPathSum(int[][] matrix) {
+            int n = matrix.length;
 
-        for (int row = 1; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                int prevMinSum = Integer.MAX_VALUE;
-                for (int[] d : DIRECTIONS) {
-                    int prevRow = row + d[0];
-                    int prevCol = col + d[1];
-                    if (prevRow < 0 || prevRow >= n || prevCol < 0 || prevCol >= n) {
-                        continue;
-                    }
-                    prevMinSum = Math.min(prevMinSum, dp[prevRow][prevCol]);
-                }
-                dp[row][col] = matrix[row][col] + prevMinSum;
+            // memo[i][j] - min sum of any falling path starting at (i, j)
+            int[][] memo = new int[n][n];
+            for (int[] row : memo) {
+                Arrays.fill(row, Integer.MAX_VALUE);
             }
+
+            int best = Integer.MAX_VALUE;
+            for (int col = 0; col < n; col++) {
+                best = Math.min(best, dfs(matrix, 0, col, memo));
+            }
+            return best;
         }
 
-        int minSum = Integer.MAX_VALUE;
-        for (int col = 0; col < n; col++) {
-            minSum = Math.min(minSum, dp[n - 1][col]);
+        private int dfs(int[][] matrix, int row, int col, int[][] memo) {
+            // already solved?
+            if (memo[row][col] != Integer.MAX_VALUE) {
+                return memo[row][col];
+            }
+
+            int n = matrix.length;
+            int sum = matrix[row][col];
+
+            // can we still increase the sum while keeping it to a minimum?
+            int delta = Integer.MAX_VALUE;
+            for (int[] d : DIRECTIONS) {
+                int nextRow = row + d[0];
+                int nextCol = col + d[1];
+                if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n) {
+                    delta = Math.min(delta, dfs(matrix, nextRow, nextCol, memo));
+                }
+            }
+            return memo[row][col] = (delta == Integer.MAX_VALUE) ? sum : sum + delta;
         }
-        return minSum;
+    }
+
+    // O(N^2) time | O(N^2) space
+    class MinimumFallingPathSumDPBottomUp implements MinimumFallingPathSum {
+        private static final int[][] DIRECTIONS = {{-1, -1}, {-1, 0}, {-1, 1}};
+
+        @Override
+        public int minFallingPathSum(int[][] matrix) {
+            int n = matrix.length;
+
+            // dp[i][j] is the minimum falling path sum
+            int[][] dp = new int[n][n];
+            for (int col = 0; col < n; col++) {
+                dp[0][col] = matrix[0][col];
+            }
+
+            for (int row = 1; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    int prevMinSum = Integer.MAX_VALUE;
+                    for (int[] d : DIRECTIONS) {
+                        int prevRow = row + d[0];
+                        int prevCol = col + d[1];
+                        if (prevRow < 0 || prevRow >= n || prevCol < 0 || prevCol >= n) {
+                            continue;
+                        }
+                        prevMinSum = Math.min(prevMinSum, dp[prevRow][prevCol]);
+                    }
+                    dp[row][col] = matrix[row][col] + prevMinSum;
+                }
+            }
+
+            int minSum = Integer.MAX_VALUE;
+            for (int col = 0; col < n; col++) {
+                minSum = Math.min(minSum, dp[n - 1][col]);
+            }
+            return minSum;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/problems/MinimumFallingPathSumTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/MinimumFallingPathSumTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.problems
 
+import com.github.dkoval.leetcode.problems.MinimumFallingPathSum.MinimumFallingPathSumDPBottomUp
+import com.github.dkoval.leetcode.problems.MinimumFallingPathSum.MinimumFallingPathSumDpTopDown
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MinimumFallingPathSumTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     intArrayOf(2, 1, 3),
@@ -34,10 +40,28 @@ internal class MinimumFallingPathSumTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the minimum sum of any falling path through matrix`(matrix: Array<IntArray>, expected: Int) {
-        val actual = MinimumFallingPathSum().minFallingPathSum(matrix)
+    @Nested
+    inner class MinimumFallingPathSumDpTopDownTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum sum of any falling path through matrix`(matrix: Array<IntArray>, expected: Int) {
+            MinimumFallingPathSumDpTopDown().test(matrix, expected)
+        }
+    }
+
+    @Nested
+    inner class MinimumFallingPathSumDPBottomUpTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum sum of any falling path through matrix`(matrix: Array<IntArray>, expected: Int) {
+            MinimumFallingPathSumDPBottomUp().test(matrix, expected)
+        }
+    }
+
+    private fun MinimumFallingPathSum.test(matrix: Array<IntArray>, expected: Int) {
+        val actual = minFallingPathSum(matrix)
         assertEquals(expected, actual)
     }
 }
