63d95ab1fb017665f87f91e222acfd5b8584906d
dkoval <koval.dmitriy@gmail.com>
2023-01-01 13:19:15 +0100

    Resolve "Word Pattern" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/WordPattern.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/WordPattern.kt
@@ -1,7 +1,7 @@
 package com.github.dkoval.leetcode.challenge
 
 /**
- * [Word Pattern](https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3451/)
+ * [Word Pattern](https://leetcode.com/problems/word-pattern/)
  *
  * Given a pattern and a string str, find if str follows the same pattern.
  *
@@ -10,14 +10,28 @@ package com.github.dkoval.leetcode.challenge
  * Notes:
  * You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by
  * a single space.
+ *
+ * Constraints:
+ *
+ * - 1 <= pattern.length <= 300
+ * - pattern contains only lower-case English letters.
+ * - 1 <= s.length <= 3000
+ * - s contains only lowercase English letters and spaces `' '`.
+ * - s does not contain any leading or trailing spaces.
+ * - All the words in s are separated by a single space.
  */
-object WordPattern {
+interface WordPattern {
+    fun wordPattern(pattern: String, str: String): Boolean
+}
 
-    fun wordPattern(pattern: String, str: String): Boolean {
+object WordPatternRev1 : WordPattern {
+
+    override fun wordPattern(pattern: String, str: String): Boolean {
         val words = str.split(" ")
         if (pattern.length != words.size) {
             return false
         }
+
         val charToWord = mutableMapOf<Char, String>()
         val wordToChar = mutableMapOf<String, Char>()
         for (i in pattern.indices) {
@@ -31,6 +45,27 @@ object WordPattern {
             }
             charToWord[c] = word
             wordToChar[word] = c
+        }
+        return true
+    }
+}
+
+object WordPatternRev2 : WordPattern {
+
+    override fun wordPattern(pattern: String, str: String): Boolean {
+        val words = str.split(" ")
+        if (pattern.length != words.size) {
+            return false
+        }
+
+        val charToWord = mutableMapOf<Char, String>()
+        val wordToChar = mutableMapOf<String, Char>()
+        for (i in pattern.indices) {
+            val c = pattern[i]
+            val word = words[i]
+            if (word != charToWord.getOrPut(c) { word } || c != wordToChar.getOrPut(word) { c }) {
+                return false
+            }
         }
         return true
     }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/WordPatternTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/WordPatternTest.kt
@@ -1,15 +1,19 @@
 package com.github.dkoval.leetcode.challenge
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class WordPatternTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 "abba",
                 "dog cat cat dog",
@@ -33,10 +37,28 @@ internal class WordPatternTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should check if a string follows the pattern`(pattern: String, str: String, expected: Boolean) {
-        val actual = WordPattern.wordPattern(pattern, str)
+    @Nested
+    inner class WordPatternRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if a string follows the pattern`(pattern: String, s: String, expected: Boolean) {
+            WordPatternRev2.test(pattern, s, expected)
+        }
+    }
+
+    @Nested
+    inner class WordPatternRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if a string follows the pattern`(pattern: String, s: String, expected: Boolean) {
+            WordPatternRev1.test(pattern, s, expected)
+        }
+    }
+
+    private fun WordPattern.test(pattern: String, s: String, expected: Boolean) {
+        val actual = wordPattern(pattern, s)
         assertEquals(expected, actual)
     }
 }
