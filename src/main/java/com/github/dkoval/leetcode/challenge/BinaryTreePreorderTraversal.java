eed60c3fd43cd3351d3448fd81e2f9cb42478fb7
dkoval <koval.dmitriy@gmail.com>
2023-01-09 09:44:57 +0100

    Recursive solution to "Binary Tree Preorder Traversal" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/BinaryTreePreorderTraversal.java
@@ -0,0 +1,43 @@
+package com.github.dkoval.leetcode.challenge;
+
+import com.github.dkoval.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-tree-preorder-traversal/">Binary Tree Preorder Traversal</a>
+ * <p>
+ * Given the root of a binary tree, return the preorder traversal of its nodes' values.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree is in the range [0, 100].</li>
+ *  <li>-100 <= Node.val <= 100</li>
+ * </ul>
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+public interface BinaryTreePreorderTraversal {
+
+    List<Integer> preorderTraversal(TreeNode root);
+
+    class BinaryTreePreorderTraversalRecursive implements BinaryTreePreorderTraversal {
+
+        @Override
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            preorder(root, ans);
+            return ans;
+        }
+
+        private void preorder(TreeNode root, List<Integer> ans) {
+            if (root == null) {
+                return;
+            }
+
+            ans.add(root.val);
+            preorder(root.left, ans);
+            preorder(root.right, ans);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/BinaryTreePreorderTraversalTest.kt
@@ -0,0 +1,52 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.BinaryTreePreorderTraversal.BinaryTreePreorderTraversalRecursive
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class BinaryTreePreorderTraversalTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(1).apply {
+                    right = TreeNode(2).apply {
+                        left = TreeNode(3)
+                    }
+                },
+                listOf(1, 2, 3)
+            ),
+            Arguments.of(
+                null,
+                emptyList<Int>()
+            ),
+            Arguments.of(
+                TreeNode(1),
+                listOf(1)
+            )
+        )
+    }
+
+    @Nested
+    inner class BinaryTreePreorderTraversalRecursiveTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the preorder traversal of its nodes' values`(root: TreeNode?, expected: List<Int>) {
+            BinaryTreePreorderTraversalRecursive().test(root, expected)
+        }
+    }
+
+    private fun BinaryTreePreorderTraversal.test(root: TreeNode?, expected: List<Int>) {
+        val actual = preorderTraversal(root)
+        assertEquals(expected, actual)
+    }
+}
