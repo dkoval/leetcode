db659776ca7faed11466f2e39d995bfceced5a9e
dkoval <koval.dmitriy@gmail.com>
2023-01-15 12:06:59 +0100

    Micro optimizations

--- src/main/java/com/github/dkoval/leetcode/challenge/LexicographicallySmallestEquivalentString.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/LexicographicallySmallestEquivalentString.java
@@ -94,12 +94,13 @@ public interface LexicographicallySmallestEquivalentString {
                 if (graph.containsKey(c1) && graph.containsKey(c2)) {
                     SortedSet<Character> group1 = graph.get(c1);
                     SortedSet<Character> group2 = graph.get(c2);
-                    SortedSet<Character> smaller = group1.size() < group2.size() ? group1 : group2;
+                    SortedSet<Character> smaller = group1.size() <= group2.size() ? group1 : group2;
+                    SortedSet<Character> larger = group1.size() > group2.size() ? group1 : group2;
 
                     // merge & update mapping
                     group1.addAll(group2);
                     for (char c : smaller) {
-                        graph.put(c, group1);
+                        graph.put(c, larger);
                     }
                 }
 
