a8b03a9bd3edc17e0c4b4a8c50f6c9757ef8ee97
dkoval <koval.dmitriy@gmail.com>
2023-01-18 20:55:51 +0100

    Revise "Maximum Sum Circular Subarray" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/MaximumSumCircularSubarray.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/MaximumSumCircularSubarray.kt
@@ -4,7 +4,7 @@ import kotlin.math.max
 import kotlin.math.min
 
 /**
- * [Maximum Sum Circular Subarray](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3330/)
+ * [Maximum Sum Circular Subarray](https://leetcode.com/problems/maximum-sum-circular-subarray/)
  *
  * Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
  * Here, a circular array means the end of the array connects to the beginning of the array.
@@ -12,24 +12,35 @@ import kotlin.math.min
  *
  * Also, a subarray may only include each element of the fixed buffer A at most once.
  * Formally, for a subarray ```C[i], C[i+1], ..., C[j]```, there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.
+ *
+ * Constraints:
+ *
+ * - n == nums.length
+ * - 1 <= n <= 3 * 10^4
+ * - -3 * 10^4 <= ```nums[i]``` <= 3 * 10^4
  */
 object MaximumSumCircularSubarray {
 
+    // Resource: https://www.youtube.com/watch?v=fxT9KjakYPM
     fun maxSubarraySumCircular(A: IntArray): Int {
-        var totalSum = 0
-        var maxSumSoFar = 0
-        var bestMaxSum = Int.MIN_VALUE
-        var minSumSoFar = 0
-        var bestMinSum = Int.MAX_VALUE
+        var total = 0
+        var currMaxSum = 0
+        var bestMaxSum = Int.MIN_VALUE // global max sum
+        var currMinSum = 0
+        var bestMinSum = Int.MAX_VALUE // global min sum
         for (x in A) {
-            totalSum += x
+            total += x
             // Kadane's algorithm to find MAX sum subarray
-            maxSumSoFar = max(maxSumSoFar + x, x)
-            bestMaxSum = max(maxSumSoFar, bestMaxSum)
+            currMaxSum = max(currMaxSum + x, x)
+            bestMaxSum = max(currMaxSum, bestMaxSum)
             // Kadane's algorithm to find MIN sum subarray
-            minSumSoFar = min(minSumSoFar + x, x)
-            bestMinSum = min(minSumSoFar, bestMinSum)
+            currMinSum = min(currMinSum + x, x)
+            bestMinSum = min(currMinSum, bestMinSum)
         }
-        return if (bestMaxSum > 0) max(bestMaxSum, totalSum - bestMinSum) else bestMaxSum
+
+        // bestMaxSum > 0 check handles the edge case where all the values in A[] are negative.
+        // For example: A = [-5, -3, -5]
+        // in this case total == bestMinSum == -13, bestMaxSum = -3
+        return if (bestMaxSum > 0) max(bestMaxSum, total - bestMinSum) else bestMaxSum
     }
 }
