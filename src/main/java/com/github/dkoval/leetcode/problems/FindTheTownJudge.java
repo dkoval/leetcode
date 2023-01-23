2273cb062feae2b77ea9d82f0e1f49bc6e70e090
dkoval <koval.dmitriy@gmail.com>
2023-01-23 10:23:30 +0100

    Update problem description

--- src/main/java/com/github/dkoval/leetcode/problems/FindTheTownJudge.java
+++ src/main/java/com/github/dkoval/leetcode/problems/FindTheTownJudge.java
@@ -1,5 +1,30 @@
 package com.github.dkoval.leetcode.problems;
 
+/**
+ * <a href="https://leetcode.com/problems/find-the-town-judge/"> Find the Town Judge</a>
+ * <p>
+ * In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+ * <p>
+ * If the town judge exists, then:
+ * <p>
+ * The town judge trusts nobody.
+ * Everybody (except for the town judge) trusts the town judge.
+ * There is exactly one person that satisfies properties 1 and 2.
+ * <p>
+ * You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi.
+ * <p>
+ * Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= n <= 1000</li>
+ *  <li>0 <= trust.length <= 10^4</li>
+ *  <li>trust[i].length == 2</li>
+ *  <li>All the pairs of trust are unique.</li>
+ *  <li>ai != bi</li>
+ *  <li>1 <= ai, bi <= n</li>
+ * </ul>
+ */
 public interface FindTheTownJudge {
 
     int findJudge(int n, int[][] trust);
--- src/main/kotlin/com/github/dkoval/leetcode/challenge/FindTheTownJudgeByComputingTrustTable.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/FindTheTownJudgeByComputingTrustTable.kt
@@ -2,19 +2,6 @@ package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.problems.FindTheTownJudge
 
-/**
- * [Find the Town Judge](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325/)
- *
- * In a town, there are N people labelled from 1 to N. There is a rumor that one of these people is secretly the town judge.
- *
- * If the town judge exists, then:
- * - The town judge trusts nobody.
- * - Everybody (except for the town judge) trusts the town judge.
- * - There is exactly one person that satisfies properties 1 and 2.
- *
- * You are given trust, an array of pairs ```trust[i] = [a, b]``` representing that the person labelled a trusts the person labelled b.
- * If the town judge exists and can be identified, return the label of the town judge. Otherwise, return -1.
- */
 object FindTheTownJudgeByComputingTrustTable : FindTheTownJudge {
 
     override fun findJudge(N: Int, trust: Array<IntArray>): Int {
