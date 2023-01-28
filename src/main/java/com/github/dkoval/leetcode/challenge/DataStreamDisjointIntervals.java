861a061582544f9c476fc3c24bebf7b28fbaf702
dkoval <koval.dmitriy@gmail.com>
2023-01-28 15:14:51 +0100

    Naive solution to "Data Stream as Disjoint Intervals" hard problem that passes all tests

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/DataStreamDisjointIntervals.java
@@ -0,0 +1,64 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * <a href="https://leetcode.com/problems/data-stream-as-disjoint-intervals/">Data Stream as Disjoint Intervals (Hard)</a>
+ * <p>
+ * Given a data stream input of non-negative integers a1, a2, ..., an, summarize the numbers seen so far as a list of disjoint intervals.
+ * <p>
+ * Implement the SummaryRanges class:
+ * <ul>
+ *  <li>SummaryRanges() Initializes the object with an empty stream.</li>
+ *  <li>void addNum(int value) Adds the integer value to the stream.</li>
+ *  <li>int[][] getIntervals() Returns a summary of the integers in the stream currently as a list of disjoint intervals [starti, endi]. The answer should be sorted by starti.</li>
+ * </ul>
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>0 <= value <= 10^4</li>
+ *  <li>At most 3 * 104 calls will be made to addNum and getIntervals.</li>
+ * </ul>
+ * <p>
+ * Follow up: What if there are lots of merges and the number of disjoint intervals is small compared to the size of the data stream?
+ */
+public interface DataStreamDisjointIntervals {
+
+    interface SummaryRanges {
+        void addNum(int value);
+
+        int[][] getIntervals();
+    }
+
+    class DataStreamDisjointIntervalsNaive implements DataStreamDisjointIntervals, SummaryRanges {
+        final SortedSet<Integer> seen = new TreeSet<>();
+
+        @Override
+        public void addNum(int value) {
+            seen.add(value);
+        }
+
+        @Override
+        public int[][] getIntervals() {
+            List<int[]> ans = new ArrayList<>();
+            for (int x : seen) {
+                if (ans.isEmpty()) {
+                    ans.add(new int[]{x, x});
+                    continue;
+                }
+
+                int[] last = ans.get(ans.size() - 1);
+                if (x > last[1] + 1) {
+                    // start new interval
+                    ans.add(new int[]{x, x});
+                } else {
+                    last[1] = x;
+                }
+            }
+            return ans.toArray(int[][]::new);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/DataStreamDisjointIntervalsTest.kt
@@ -0,0 +1,59 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.DataStreamDisjointIntervals.*
+import com.github.dkoval.leetcode.challenge.DataStreamDisjointIntervalsTest.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class DataStreamDisjointIntervalsTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                listOf(
+                    Command.Add(1),
+                    Command.GetIntervals(listOf(intArrayOf(1, 1))),
+                    Command.Add(3),
+                    Command.GetIntervals(listOf(intArrayOf(1, 1), intArrayOf(3, 3))),
+                    Command.Add(7),
+                    Command.GetIntervals(listOf(intArrayOf(1, 1), intArrayOf(3, 3), intArrayOf(7, 7))),
+                    Command.Add(2),
+                    Command.GetIntervals(listOf(intArrayOf(1, 3), intArrayOf(7, 7))),
+                    Command.Add(6),
+                    Command.GetIntervals(listOf(intArrayOf(1, 3), intArrayOf(6, 7)))
+                )
+            )
+        )
+    }
+
+    sealed class Command {
+        data class Add(val value: Int) : Command()
+        data class GetIntervals(val expected: List<IntArray>) : Command()
+    }
+
+    @Nested
+    inner class DataStreamDisjointIntervalsNaiveTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should verify implementation`(commands: List<Command>) {
+            DataStreamDisjointIntervalsNaive().test(commands)
+        }
+    }
+}
+
+private fun DataStreamDisjointIntervals.SummaryRanges.test(commands: List<Command>) {
+    commands.forEach { command ->
+        when (command) {
+            is Command.Add -> addNum(command.value)
+            is Command.GetIntervals -> assertThat(intervals).containsExactlyElementsOf(command.expected)
+        }
+    }
+}
