4fb4ae07ac04b507565aa0646245548f46c8ad3f
dkoval <koval.dmitriy@gmail.com>
2023-02-01 10:30:33 +0100

    Solution to "Greatest Common Divisor of Strings" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,51 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/greatest-common-divisor-of-strings/">Greatest Common Divisor of Strings</a>
+ * <p>
+ * For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+ * <p>
+ * Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= str1.length, str2.length <= 1000</li>
+ *  <li>str1 and str2 consist of English uppercase letters.</li>
+ * </ul>
+ */
+public interface GreatestCommonDivisorOfStrings {
+
+    String gcdOfStrings(String str1, String str2);
+
+    class GreatestCommonDivisorOfStringsRev1 implements GreatestCommonDivisorOfStrings {
+
+        @Override
+        public String gcdOfStrings(String str1, String str2) {
+            int n1 = str1.length();
+            int n2 = str2.length();
+
+            // Brute force on prefix
+            int k = Math.min(n1, n2);
+            for (int len = k; len >= 1; len--) {
+                if (k % len != 0) {
+                    continue;
+                }
+
+                String prefix = str1.substring(0, len);
+                if (repeats(str1, prefix) && repeats(str2, prefix)) {
+                    return prefix;
+                }
+            }
+            return "";
+        }
+
+        private boolean repeats(String s, String prefix) {
+            int times = s.length() / prefix.length();
+            StringBuilder sb = new StringBuilder();
+            while (times-- > 0) {
+                sb.append(prefix);
+            }
+            return sb.toString().equals(s);
+        }
+    }
+}
