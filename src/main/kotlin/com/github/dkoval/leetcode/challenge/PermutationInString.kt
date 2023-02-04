3885b1ee5d88f86ec3b4e805eb50610c01800226
dkoval <koval.dmitriy@gmail.com>
2023-02-04 13:28:05 +0100

    Update problem description

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/PermutationInString.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/PermutationInString.kt
@@ -1,10 +1,16 @@
 package com.github.dkoval.leetcode.challenge
 
 /**
- * [Permutation in String](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3333/)
+ * [Permutation in String](https://leetcode.com/problems/permutation-in-string/)
  *
- * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1.
- * In other words, one of the first string's permutations is the substring of the second string.
+ * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+ *
+ * In other words, return true if one of s1's permutations is the substring of s2
+ *
+ * Constraints:
+ *
+ * - 1 <= s1.length, s2.length <= 10^4
+ * - s1 and s2 consist of lowercase English letters.
  */
 object PermutationInString {
 
