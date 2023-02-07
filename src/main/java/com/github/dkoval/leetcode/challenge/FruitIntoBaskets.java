2382fb0ab39561a9b4f006695a5fd2e5309dfa34
dkoval <koval.dmitriy@gmail.com>
2023-02-07 12:04:28 +0100

    Solution to "Fruit Into Baskets" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/FruitIntoBaskets.java
@@ -0,0 +1,64 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/fruit-into-baskets/">Fruit Into Baskets</a>
+ * <p>
+ * You are visiting a farm that has a single row of fruit trees arranged from left to right.
+ * The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+ * <p>
+ * You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+ * <ul>
+ *  <li>You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.</li>
+ *  <li>Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.</li>
+ *  <li>Once you reach a tree with fruit that cannot fit in your baskets, you must stop.</li>
+ * </ul>
+ * <p>
+ * Given the integer array fruits, return the maximum number of fruits you can pick.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= fruits.length <= 10^5</li>
+ *  <li>0 <= fruits[i] < fruits.length</li>
+ * </ul>
+ */
+public interface FruitIntoBaskets {
+
+    int totalFruit(int[] fruits);
+
+    // O(N) time | O(1) space
+    class FruitIntoBasketsRev1 implements FruitIntoBaskets {
+
+        @Override
+        public int totalFruit(int[] fruits) {
+            int n = fruits.length;
+
+            // baskets[i] - the last index we've seen a fruit this basket can hold
+            int[] baskets = {-1, -1};
+
+            int best = 0;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                count++;
+
+                // check if we can put fruits[i] in either of the baskets
+                boolean matches = false;
+                for (int j = 0; j < 2 && !matches; j++) {
+                    if (baskets[j] == -1 || fruits[baskets[j]] == fruits[i]) {
+                        baskets[j] = i;
+                        matches = true;
+                    }
+                }
+
+                // a new type of fruit discovered, empty on of the baskets
+                if (!matches) {
+                    count = i - Math.min(baskets[0], baskets[1]);
+                    baskets[0] = Math.max(baskets[0], baskets[1]);
+                    baskets[1] = i;
+                }
+
+                best = Math.max(best, count);
+            }
+            return best;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/FruitIntoBasketsTest.kt
@@ -0,0 +1,51 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.FruitIntoBaskets.FruitIntoBasketsRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class FruitIntoBasketsTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 1),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(0, 1, 2, 2),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3, 2, 2),
+                4
+            ),
+            Arguments.of(
+                intArrayOf(1, 0, 3, 4, 3),
+                3
+            )
+        )
+    }
+
+    @Nested
+    inner class FruitIntoBasketsRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum number of fruits you can pick`(fruits: IntArray, expected: Int) {
+            FruitIntoBasketsRev1().test(fruits, expected)
+        }
+    }
+}
+
+private fun FruitIntoBaskets.test(fruits: IntArray, expected: Int) {
+    val actual = totalFruit(fruits)
+    assertEquals(expected, actual)
+}
