b0aa73e2933c774a049491ca854563574671cab4
dkoval <koval.dmitriy@gmail.com>
2023-02-08 22:10:18 +0100

    Review "Jump Game II" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/JumpGame2.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/JumpGame2.java
@@ -1,42 +1,70 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.Arrays;
+
 /**
- * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge-2021/598/week-1-may-1st-may-7th/3732/">Jump Game II</a>
+ * <a href="https://leetcode.com/problems/jump-game-ii/">Jump Game II</a>
+ * You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
  * <p>
- * Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+ * Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i],
+ * you can jump to any nums[i + j] where:
  * <p>
- * Each element in the array represents your maximum jump length at that position.
- * <p>
- * Your goal is to reach the last index in the minimum number of jumps.
- * <p>
- * You can assume that you can always reach the last index.
+ * 0 <= j <= nums[i] and
+ * i + j < n
+ * Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
  * <p>
  * Constraints:
  * <ul>
- *  <li>1 <= nums.length <= 1000</li>
- *  <li>0 <= nums[i] <= 10^5</li>
+ *  <li>1 <= nums.length <= 10^4</li>
+ *  <li>0 <= nums[i] <= 1000</li>
  * </ul>
  */
-public class JumpGame2 {
+public interface JumpGame2 {
 
-    // O(N) time | O(N) space
-    public int jump(int[] nums) {
-        int n = nums.length;
+    int jump(int[] nums);
 
-        // dp[i] - min number of jumps required to reach the last index starting from i
-        int[] dp = new int[n];
-        dp[n - 1] = 0;
+    class JumpGame2Rev1 implements JumpGame2 {
 
-        for (int i = n - 2; i >= 0; i--) {
-            int minJumps = Integer.MAX_VALUE;
-            // consider all indices reachable from i and choose the one,
-            // which requires the minimum number of jumps to get to the last index
-            int furthestIdx = Math.min(i + nums[i], n - 1);
-            for (int k = i + 1; k <= furthestIdx; k++) {
-                minJumps = Math.min(minJumps, dp[k]);
+        @Override
+        public int jump(int[] nums) {
+            int n = nums.length;
+
+            // dp[i] - min number of jumps to reach the i-th index starting from 0
+            int[] dp = new int[n];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j <= Math.min(i + nums[i], n - 1); j++) {
+                    dp[j] = Math.min(dp[j], dp[i] + 1);
+                }
             }
-            dp[i] = (minJumps == Integer.MAX_VALUE) ? Integer.MAX_VALUE : minJumps + 1;
+            return dp[n - 1];
         }
-        return dp[0];
+    }
+
+    class JumpGame2Rev2 implements JumpGame2 {
+
+        // O(N) time | O(N) space
+        @Override
+        public int jump(int[] nums) {
+            int n = nums.length;
+
+            // dp[i] - min number of jumps required to reach the last index starting from i
+            int[] dp = new int[n];
+            dp[n - 1] = 0;
+
+            for (int i = n - 2; i >= 0; i--) {
+                int minJumps = Integer.MAX_VALUE;
+                // consider all indices reachable from i and choose the one,
+                // which requires the minimum number of jumps to get to the last index
+                int furthestIdx = Math.min(i + nums[i], n - 1);
+                for (int k = i + 1; k <= furthestIdx; k++) {
+                    minJumps = Math.min(minJumps, dp[k]);
+                }
+                dp[i] = (minJumps == Integer.MAX_VALUE) ? Integer.MAX_VALUE : minJumps + 1;
+            }
+            return dp[0];
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/JumpGame2Test.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/JumpGame2Test.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.JumpGame2.JumpGame2Rev1
+import com.github.dkoval.leetcode.challenge.JumpGame2.JumpGame2Rev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class JumpGame2Test {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(2, 3, 1, 1, 4),
                 2
@@ -17,14 +23,36 @@ internal class JumpGame2Test {
             Arguments.of(
                 intArrayOf(2, 3, 0, 1, 4),
                 2
+            ),
+            Arguments.of(
+                intArrayOf(2, 1),
+                1
             )
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the minimum number of jumps required to reach the last index`(nums: IntArray, expected: Int) {
-        val actual = JumpGame2().jump(nums)
-        assertEquals(expected, actual)
+    @Nested
+    inner class JumpGame2Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum number of jumps required to reach the last index`(nums: IntArray, expected: Int) {
+            JumpGame2Rev1().test(nums, expected)
+        }
     }
+
+    @Nested
+    inner class JumpGame2Rev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum number of jumps required to reach the last index`(nums: IntArray, expected: Int) {
+            JumpGame2Rev2().test(nums, expected)
+        }
+    }
+}
+
+private fun JumpGame2.test(nums: IntArray, expected: Int) {
+    val actual = jump(nums)
+    assertEquals(expected, actual)
 }
