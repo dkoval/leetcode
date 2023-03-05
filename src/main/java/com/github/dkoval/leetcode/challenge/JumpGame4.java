16f604f18871195644e5456259f5a1acbd502cd4
dkoval <koval.dmitriy@gmail.com>
2023-03-05 12:24:18 +0100

    Resolve "Jump Game IV" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/JumpGame4.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/JumpGame4.java
@@ -3,55 +3,74 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.*;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/572/week-4-december-22nd-december-28th/3582/">Jump Game IV</a>
+ * <a href="https://leetcode.com/problems/jump-game-iv/">Jump Game IV</a>
  * <p>
  * Given an array of integers arr, you are initially positioned at the first index of the array.
  * <p>
  * In one step you can jump from index i to index:
  * <ul>
- * <li>i + 1 where: i + 1 < arr.length.</li>
- * <li>i - 1 where: i - 1 >= 0.</li>
+ *  <li>i + 1 where: i + 1 < arr.length.</li>
+ *  <li>i - 1 where: i - 1 >= 0.</li>
  * <li>j where: arr[i] == arr[j] and i != j.</li>
  * </ul>
  * Return the minimum number of steps to reach the last index of the array.
  * <p>
  * Notice that you can not jump outside of the array at any time.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= arr.length <= 5 * 10^4</li>
+ *  <li>-10^8 <= arr[i] <= 10^8</li>
+ * </ul>
  */
 public class JumpGame4 {
 
     public int minJumps(int[] arr) {
-        Map<Integer, List<Integer>> index = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            index.computeIfAbsent(arr[i], key -> new ArrayList<>()).add(i);
+        int n = arr.length;
+        if (n <= 1) {
+            return 0;
         }
+
+        // groups indices having the same arr[i] value
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            groups.computeIfAbsent(arr[i], __ -> new ArrayList<>()).add(i);
+        }
+
         // BFS
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[arr.length];
-        int numSteps = 0;
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n];
         q.offer(0);
+        int numSteps = 0;
         while (!q.isEmpty()) {
             int size = q.size();
             while (size-- > 0) {
                 int i = q.poll();
-                if (i == arr.length - 1) {
+                if (i == n - 1) {
                     return numSteps;
                 }
-                visited[i] = true;
+
                 // add adjacent indices to the queue
                 if (i - 1 >= 0 && !visited[i - 1]) {
                     q.offer(i - 1);
+                    visited[i - 1] = true;
                 }
-                if (i + 1 <= arr.length - 1 && !visited[i + 1]) {
+
+                if (i + 1 < n && !visited[i + 1]) {
                     q.offer(i + 1);
+                    visited[i + 1] = true;
                 }
-                List<Integer> next = index.get(arr[i]);
-                if (next == null) continue;
-                for (int j : next) {
-                    if (!visited[j]) {
-                        q.offer(j);
+
+                if (groups.containsKey(arr[i])) {
+                    List<Integer> indices = groups.get(arr[i]);
+                    for (int j : indices) {
+                        if (!visited[j]) {
+                            q.offer(j);
+                            visited[j] = true;
+                        }
                     }
+                    groups.remove(arr[i]);
                 }
-                index.remove(arr[i]);
             }
             numSteps++;
         }
