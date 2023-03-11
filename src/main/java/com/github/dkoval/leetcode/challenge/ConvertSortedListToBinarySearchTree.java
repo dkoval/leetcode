b40ccb016ef3a0edaa9886aa20474cf2695bee2b
dkoval <koval.dmitriy@gmail.com>
2023-03-11 13:06:16 +0100

    Re-solve "Convert Sorted List to Binary Search Tree" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ConvertSortedListToBinarySearchTree.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ConvertSortedListToBinarySearchTree.java
@@ -4,54 +4,105 @@ import com.github.dkoval.leetcode.ListNode;
 import com.github.dkoval.leetcode.TreeNode;
 
 /**
- * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge-2021/598/week-1-may-1st-may-7th/3733/">Convert Sorted List to Binary Search Tree</a>
+ * <a href="https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/">Convert Sorted List to Binary Search Tree</a>
  * <p>
  * Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
  * <p>
  * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of
  * every node never differ by more than 1.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in head is in the range [0, 2 * 10^4].</li>
+ *  <li>-10^5 <= Node.val <= 10^5</li>
+ * </ul>
  */
-public class ConvertSortedListToBinarySearchTree {
+public interface ConvertSortedListToBinarySearchTree {
 
-    public TreeNode sortedListToBST(ListNode head) {
-        return createBST(head);
+    TreeNode sortedListToBST(ListNode head);
+
+    class ConvertSortedListToBinarySearchTreeRev1 implements ConvertSortedListToBinarySearchTree {
+
+        public TreeNode sortedListToBST(ListNode head) {
+            return createBST(head);
+        }
+
+        private int length(ListNode head) {
+            int l = 0;
+            ListNode curr = head;
+            while (curr != null) {
+                l++;
+                curr = curr.next;
+            }
+            return l;
+        }
+
+        private TreeNode createBST(ListNode head) {
+            int n = length(head);
+            if (n == 0) {
+                return null;
+            }
+            if (n == 1) {
+                return new TreeNode(head.val);
+            }
+
+            ListNode prev = null, mid = head;
+            for (int i = 0; i < n / 2; i++) {
+                prev = mid;
+                mid = mid.next;
+            }
+
+            if (prev != null) {
+                prev.next = null; // hack
+            }
+
+            ListNode next = mid.next;
+            mid.next = null; // yet another hack
+
+            TreeNode root = new TreeNode(mid.val);
+            root.left = createBST(head); // before `mid`
+            root.right = createBST(next); // after `mid`
+            return root;
+        }
     }
 
-    private int length(ListNode head) {
-        int l = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            l++;
-            curr = curr.next;
-        }
-        return l;
-    }
+    class ConvertSortedListToBinarySearchTreeRev2 implements ConvertSortedListToBinarySearchTree {
 
-    private TreeNode createBST(ListNode head) {
-        int n = length(head);
-        if (n == 0) {
-            return null;
-        }
-        if (n == 1) {
-            return new TreeNode(head.val);
-        }
+        @Override
+        public TreeNode sortedListToBST(ListNode head) {
+            // base case #1: empty list
+            if (head == null) {
+                return null;
+            }
 
-        ListNode prev = null, mid = head;
-        for (int i = 0; i < n / 2; i++) {
-            prev = mid;
-            mid = mid.next;
+            // base case #2: single-node list
+            if (head.next == null) {
+                return new TreeNode(head.val);
+            }
+
+            // find the mid node
+            ListNode prev = null;
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                prev = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            // sublist before the mid node
+            if (prev != null) {
+                prev.next = null;
+            }
+
+            // sublist after the mid node
+            ListNode head2 = slow.next;
+            slow.next = null;
+
+            TreeNode root = new TreeNode(slow.val);
+            root.left = sortedListToBST(head);
+            root.right = sortedListToBST(head2);
+            return root;
         }
-
-        if (prev != null) {
-            prev.next = null; // hack
-        }
-
-        ListNode next = mid.next;
-        mid.next = null; // yet another hack
-
-        TreeNode root = new TreeNode(mid.val);
-        root.left = createBST(head); // before `mid`
-        root.right = createBST(next); // after `mid`
-        return root;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/ConvertSortedListToBinarySearchTreeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ConvertSortedListToBinarySearchTreeTest.kt
@@ -2,17 +2,23 @@ package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.ListNode
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.ConvertSortedListToBinarySearchTree.ConvertSortedListToBinarySearchTreeRev1
+import com.github.dkoval.leetcode.challenge.ConvertSortedListToBinarySearchTree.ConvertSortedListToBinarySearchTreeRev2
 import com.github.dkoval.leetcode.equalsTo
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class ConvertSortedListToBinarySearchTreeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 ListNode(-10).apply {
                     next = ListNode(-3).apply {
@@ -35,13 +41,34 @@ internal class ConvertSortedListToBinarySearchTreeTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should convert an array of integers sorted in asc order to to a height balanced BST`(
-        head: ListNode?,
-        expected: TreeNode?
-    ) {
-        val actual = ConvertSortedListToBinarySearchTree().sortedListToBST(head)
-        assertTrue(actual.equalsTo(expected))
+    @Nested
+    inner class ConvertSortedListToBinarySearchTreeRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should convert an array of integers sorted in asc order to to a height balanced BST`(
+            head: ListNode?,
+            expected: TreeNode?
+        ) {
+            ConvertSortedListToBinarySearchTreeRev1().test(head, expected)
+        }
     }
+
+    @Nested
+    inner class ConvertSortedListToBinarySearchTreeRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should convert an array of integers sorted in asc order to to a height balanced BST`(
+            head: ListNode?,
+            expected: TreeNode?
+        ) {
+            ConvertSortedListToBinarySearchTreeRev2().test(head, expected)
+        }
+    }
+}
+
+private fun ConvertSortedListToBinarySearchTree.test(head: ListNode?, expected: TreeNode?) {
+    val actual = sortedListToBST(head)
+    assertTrue(actual.equalsTo(expected))
 }
