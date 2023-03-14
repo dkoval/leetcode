18e5761d9264a0109d8b5cf3069553d5df38b0ac
dkoval <koval.dmitriy@gmail.com>
2023-03-14 09:48:05 +0100

    Review "Sum Root to Leaf Numbers" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/SumRootToLeafNumbers.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/SumRootToLeafNumbers.kt
@@ -3,24 +3,35 @@ package com.github.dkoval.leetcode.challenge
 import com.github.dkoval.leetcode.TreeNode
 
 /**
- * [Sum Root to Leaf Numbers](https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3372/)
+ * [Sum Root to Leaf Numbers](https://leetcode.com/problems/sum-root-to-leaf-numbers/)
  *
- * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
- * An example is the root-to-leaf path 1->2->3 which represents the number 123.
- * Find the total sum of all root-to-leaf numbers.
+ * Each root-to-leaf path in the tree represents a number.
+ *
+ * For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+ * Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+ *
+ * A leaf node is a node with no children.
+ *
+ * Constraints:
+ *
+ * - The number of nodes in the tree is in the range ```[1, 1000]```.
+ * - 0 <= Node.val <= 9
+ * - The depth of the tree will not exceed 10.
  */
 object SumRootToLeafNumbers {
 
-    fun sumNumbers(root: TreeNode?): Int = doSumNumbers(root, 0)
+    fun sumNumbers(root: TreeNode?): Int = dfs(root, 0)
 
-    private fun doSumNumbers(root: TreeNode?, x: Int): Int {
+    private fun dfs(root: TreeNode?, x: Int): Int {
         if (root == null) {
             return 0
         }
+
         val y = x * 10 + root.`val`
         if (root.left == null && root.right == null) {
             return y
         }
-        return doSumNumbers(root.left, y) + doSumNumbers(root.right, y)
+
+        return dfs(root.left, y) + dfs(root.right, y)
     }
 }
