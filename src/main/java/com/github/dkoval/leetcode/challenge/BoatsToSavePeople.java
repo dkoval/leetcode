41c4cebea31aab127912920bc86e1eb93549b8f1
dkoval <koval.dmitriy@gmail.com>
2023-04-03 08:40:00 +0200

    Resolve "Boats to Save People"

--- src/main/java/com/github/dkoval/leetcode/challenge/BoatsToSavePeople.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/BoatsToSavePeople.java
@@ -3,29 +3,40 @@ package com.github.dkoval.leetcode.challenge;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3602/">Boats to Save People</a>
- *
- * The i-th person has weight people[i], and each boat can carry a maximum weight of limit.
- * Each boat carries at most 2 people at the same time, provided the sum of the weight of those people is at most limit.
- * Return the minimum number of boats to carry every given person. (It is guaranteed each person can be carried by a boat.)
+ * <a href="https://leetcode.com/problems/boats-to-save-people/">Boats to Save People</a>
+ * <p>
+ * You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats
+ * where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time,
+ * provided the sum of the weight of those people is at most limit.
+ * <p>
+ * Return the minimum number of boats to carry every given person.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= people.length <= 5 * 10^4</li>
+ *  <li>1 <= people[i] <= limit <= 3 * 10^4</li>
+ * </ul>
  */
 public class BoatsToSavePeople {
 
+    // O(N * logN) time | O(1) space
     public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
         Arrays.sort(people);
-        int l = 0, h = people.length - 1;
-        int numBoats = 0;
-        while (l <= h) {
-            if (people[l] + people[h] <= limit) {
-                // heaviest and lightest persons can be paired
-                l++;
-                h--;
-            } else {
-                // heaviest person is carried alone
-                h--;
+
+        // greedy
+        int count = 0;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            // 1 <= people[i] <= limit,
+            // meaning that we can always carry the heaviest person
+            if (people[left] + people[right] <= limit) {
+                left++;
             }
-            numBoats++;
+            right--;
+            count++;
         }
-        return numBoats;
+        return count;
     }
 }
