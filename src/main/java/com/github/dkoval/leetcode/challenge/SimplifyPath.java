8ed8affc983ef589e3a81bc4fb56da0fc702a557
dkoval <koval.dmitriy@gmail.com>
2023-04-12 08:28:32 +0200

    Resolve "Simplify Path" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SimplifyPath.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SimplifyPath.java
@@ -1,10 +1,10 @@
 package com.github.dkoval.leetcode.challenge;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3629/">Simplify Path</a>
+ * <a href="https://leetcode.com/problems/simplify-path/">Simplify Path</a>
  * <p>
  * Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style
  * file system, convert it to the simplified canonical path.
@@ -15,32 +15,45 @@ import java.util.LinkedList;
  * <p>
  * The canonical path should have the following format:
  * <ul>
- * <li>The path starts with a single slash '/'.</li>
- * <li>Any two directories are separated by a single slash '/'.</li>
- * <li>The path does not end with a trailing '/'.</li>
- * <li>The path only contains the directories on the path from the root directory to the target file or directory
- * (i.e., no period '.' or double period '..')</li>
- * </ul>
+ *  <li>The path starts with a single slash '/'.</li>
+ *  <li>Any two directories are separated by a single slash '/'.</li>
+ *  <li>The path does not end with a trailing '/'.</li>
+ *  <li>The path only contains the directories on the path from the root directory to the target file or directory
+ *  (i.e., no period '.' or double period '..')</li>
+ *  </ul>
  * Return the simplified canonical path.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= path.length <= 3000</li>
+ *  <li>path consists of English letters, digits, period '.', slash '/' or '_'.</li>
+ *  <li>path is a valid absolute Unix path.</li>
+ * </ul>
  */
 public class SimplifyPath {
 
     // O(N) time | O(N) space
     public String simplifyPath(String path) {
-        String[] dirs = path.split("/");
-        Deque<String> dq = new LinkedList<>();
-        for (String dir : dirs) {
-            if ("..".equals(dir)) {
+        String[] tokens = path.split("/");
+        Deque<String> dq = new ArrayDeque<>();
+        for (String token : tokens) {
+            if (token.isEmpty() || ".".equals(token)) {
+                continue;
+            }
+
+            if ("..".equals(token)) {
                 if (!dq.isEmpty()) {
-                    dq.removeLast();
+                    dq.pollLast();
                 }
-            } else if (!dir.isEmpty() && !".".equals(dir)) {
-                dq.addLast(dir);
+            } else {
+                dq.offerLast(token);
             }
         }
+
         if (dq.isEmpty()) {
             return "/";
         }
+
         StringBuilder sb = new StringBuilder();
         for (String dir : dq) {
             sb.append("/").append(dir);
