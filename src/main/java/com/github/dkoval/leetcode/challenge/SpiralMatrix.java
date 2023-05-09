891aa16c3c2ca4682d9239fb0706e9d2c0af0d82
dkoval <koval.dmitriy@gmail.com>
2023-05-09 10:18:07 +0200

    Review "Spiral Matrix" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SpiralMatrix.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SpiralMatrix.java
@@ -4,47 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3977/">Spiral Matrix</a>
+ * <a href="https://leetcode.com/problems/spiral-matrix/description/">Spiral Matrix</a>
  * <p>
  * Given an m x n matrix, return all elements of the matrix in spiral order.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>m == matrix.length</li>
+ *  <li>n == matrix[i].length</li>
+ *  <li>1 <= m, n <= 10</li>
+ *  <li>-100 <= matrix[i][j] <= 100</li>
+ * </ul>
  */
-public class SpiralMatrix {
+public interface SpiralMatrix {
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+    List<Integer> spiralOrder(int[][] matrix);
 
-        int startRow = 0, endRow = m - 1;
-        int startCol = 0, endCol = n - 1;
+    class SpiralMatrixRev1 implements SpiralMatrix {
 
-        List<Integer> ans = new ArrayList<>(m * n);
-        while (startRow <= endRow && startCol <= endCol) {
-            // top row
-            for (int j = startCol; j <= endCol; j++) {
-                ans.add(matrix[startRow][j]);
+        @Override
+        public List<Integer> spiralOrder(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            List<Integer> ans = new ArrayList<>(m * n);
+            int startRow = 0, endRow = m - 1;
+            int startCol = 0, endCol = n - 1;
+            while (startRow <= endRow && startCol <= endCol) {
+                // top row
+                for (int j = startCol; j <= endCol; j++) {
+                    ans.add(matrix[startRow][j]);
+                }
+
+                // right column
+                for (int i = startRow + 1; i <= endRow; i++) {
+                    ans.add(matrix[i][endCol]);
+                }
+
+                // bottom row
+                for (int j = endCol - 1; j >= startCol && startRow < endRow; j--) {
+                    ans.add(matrix[endRow][j]);
+                }
+
+                // left column
+                for (int i = endRow - 1; i > startRow && startCol < endCol; i--) {
+                    ans.add(matrix[i][startCol]);
+                }
+
+                startRow++;
+                endRow--;
+
+                startCol++;
+                endCol--;
             }
-
-            // right column
-            for (int i = startRow + 1; i <= endRow; i++) {
-                ans.add(matrix[i][endCol]);
-            }
-
-            // bottom row
-            for (int j = endCol - 1; j >= startCol && startRow < endRow; j--) {
-                ans.add(matrix[endRow][j]);
-            }
-
-            // left column
-            for (int i = endRow - 1; i > startRow && startCol < endCol; i--) {
-                ans.add(matrix[i][startCol]);
-            }
-
-            startRow++;
-            endRow--;
-
-            startCol++;
-            endCol--;
+            return ans;
         }
-        return ans;
     }
 }
