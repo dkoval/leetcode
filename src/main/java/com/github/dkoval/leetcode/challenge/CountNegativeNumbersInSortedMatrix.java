42fb2652cbe1d8c7f7b0991b62f6c4b4d5b40684
dkoval <koval.dmitriy@gmail.com>
2023-06-08 10:39:52 +0200

    Naive solution to "Count Negative Numbers in a Sorted Matrix" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/CountNegativeNumbersInSortedMatrix.java
@@ -0,0 +1,37 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/">Count Negative Numbers in a Sorted Matrix</a>
+ * <p>
+ * Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>m == grid.length</li>
+ *  <li>n == grid[i].length</li>
+ *  <li>1 <= m, n <= 100</li>
+ *  <li>-100 <= grid[i][j] <= 100</li>
+ * </ul>
+ * <p>
+ * Follow up: Could you find an O(n + m) solution?
+ */
+public interface CountNegativeNumbersInSortedMatrix {
+
+    int countNegatives(int[][] grid);
+
+    // O(m * n) time | O(1) space
+    class CountNegativeNumbersInSortedMatrixRev1 implements CountNegativeNumbersInSortedMatrix {
+
+        @Override
+        public int countNegatives(int[][] grid) {
+            int n = grid[0].length;
+            int count = 0;
+            for (int[] row : grid) {
+                for (int j = n - 1; j >= 0 && row[j] < 0; j--) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/CountNegativeNumbersInSortedMatrixTest.kt
@@ -0,0 +1,51 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.CountNegativeNumbersInSortedMatrix.CountNegativeNumbersInSortedMatrixRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class CountNegativeNumbersInSortedMatrixTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(4, 3, 2, -1),
+                    intArrayOf(3, 2, 1, -1),
+                    intArrayOf(1, 1, -1, -2),
+                    intArrayOf(-1, -1, -2, -3)
+                ),
+                8
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(3, 2),
+                    intArrayOf(1, 0)
+                ),
+                0
+            )
+        )
+    }
+
+    @Nested
+    inner class CountNegativeNumbersInSortedMatrixRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of negative numbers in grid`(grid: Array<IntArray>, expected: Int) {
+            CountNegativeNumbersInSortedMatrixRev1().test(grid, expected)
+        }
+    }
+}
+
+private fun CountNegativeNumbersInSortedMatrix.test(grid: Array<IntArray>, expected: Int) {
+    val actual = countNegatives(grid)
+    assertEquals(expected, actual)
+}
