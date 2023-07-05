d82db14a72554260e69a044e068f068870d666f3
dkoval <koval.dmitriy@gmail.com>
2023-07-05 23:13:15 +0200

    Solution to "Longest Subarray of 1's After Deleting One Element" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -0,0 +1,46 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/">Longest Subarray of 1's After Deleting One Element</a>
+ * <p>
+ * Given a binary array nums, you should delete one element from it.
+ * <p>
+ * Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 10^5</li>
+ *  <li>nums[i] is either 0 or 1</li>
+ * </ul>
+ */
+public interface LongestSubarrayOf1sAfterDeletingOneElement {
+
+    int longestSubarray(int[] nums);
+
+    // O(N) time | O(1) space
+    class LongestSubarrayOf1sAfterDeletingOneElementRev1 implements LongestSubarrayOf1sAfterDeletingOneElement {
+
+        @Override
+        public int longestSubarray(int[] nums) {
+            int n = nums.length;
+
+            // Idea: sliding window containing at most one 0
+            int start = 0;
+            int end = 0;
+
+            int best = 0;
+            int lastZero = -1;
+            while (end < n) {
+                if (nums[end] == 0) {
+                    if (lastZero >= 0) {
+                        start = lastZero + 1;
+                    }
+                    lastZero = end;
+                }
+                best = Math.max(best, end - start + 1);
+                end++;
+            }
+            return Math.max(best - 1, 0);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/LongestSubarrayOf1sAfterDeletingOneElementTest.kt
@@ -0,0 +1,62 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.LongestSubarrayOf1sAfterDeletingOneElement.LongestSubarrayOf1sAfterDeletingOneElementRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class LongestSubarrayOf1sAfterDeletingOneElementTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 1, 0, 1),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(0, 1, 1, 1, 0, 1, 1, 0, 1),
+                5
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 1),
+                2
+            ),
+            Arguments.of(
+                intArrayOf(0, 0),
+                0
+            ),
+            Arguments.of(
+                intArrayOf(0),
+                1
+            ),
+            Arguments.of(
+                intArrayOf(0, 1),
+                2
+            )
+        )
+    }
+
+    @Nested
+    inner class LongestSubarrayOf1sAfterDeletingOneElementRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the size of the longest non-empty subarray containing only 1's in the resulting array`(
+            nums: IntArray,
+            expected: Int
+        ) {
+            LongestSubarrayOf1sAfterDeletingOneElementRev1().test(nums, expected)
+        }
+    }
+}
+
+private fun LongestSubarrayOf1sAfterDeletingOneElement.test(nums: IntArray, expected: Int) {
+    val actual = longestSubarray(nums)
+    assertEquals(expected, actual)
+}
