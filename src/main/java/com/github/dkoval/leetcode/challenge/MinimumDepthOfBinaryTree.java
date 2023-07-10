42c8b5a38912f416073cee98076890a2eb97e960
dkoval <koval.dmitriy@gmail.com>
2023-07-10 09:08:27 +0200

    Resolve "Minimum Depth of Binary Tree" problem using BFS

--- src/main/java/com/github/dkoval/leetcode/challenge/MinimumDepthOfBinaryTree.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumDepthOfBinaryTree.java
@@ -2,27 +2,84 @@ package com.github.dkoval.leetcode.challenge;
 
 import com.github.dkoval.leetcode.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
- * <a href="https://leetcode.com/explore/featured/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3504/">Minimum Depth of Binary Tree</a>
+ * <a href="https://leetcode.com/problems/minimum-depth-of-binary-tree/">Minimum Depth of Binary Tree</a>
  * <p>
  * Given a binary tree, find its minimum depth.
  * <p>
  * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
  * <p>
  * Note: A leaf is a node with no children.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree is in the range [0, 10^5].</li>
+ *  <li>-1000 <= Node.val <= 1000</li>
+ * </ul>
  */
-public class MinimumDepthOfBinaryTree {
+public interface MinimumDepthOfBinaryTree {
 
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
-        int minDepth = Integer.MAX_VALUE;
-        if (root.left != null) {
-            minDepth = Math.min(minDepth, minDepth(root.left));
+    int minDepth(TreeNode root);
+
+    class MinimumDepthOfBinaryTreeDFS implements MinimumDepthOfBinaryTree {
+
+        @Override
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            // leaf node
+            if (root.left == null && root.right == null) {
+                return 1;
+            }
+
+            int minDepth = Integer.MAX_VALUE;
+            if (root.left != null) {
+                minDepth = Math.min(minDepth, minDepth(root.left));
+            }
+
+            if (root.right != null) {
+                minDepth = Math.min(minDepth, minDepth(root.right));
+            }
+            return 1 + minDepth;
         }
-        if (root.right != null) {
-            minDepth = Math.min(minDepth, minDepth(root.right));
+    }
+
+    class MinimumDepthOfBinaryTreeBFS implements MinimumDepthOfBinaryTree {
+
+        @Override
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            // BFS
+            int depth = 1;
+            Queue<TreeNode> q = new ArrayDeque<>();
+            q.offer(root);
+            while (!q.isEmpty()) {
+                int size = q.size();
+                while (size-- > 0) {
+                    TreeNode curr = q.poll();
+                    if (curr.left == null && curr.right == null) {
+                        return depth;
+                    }
+
+                    if (curr.left != null) {
+                        q.offer(curr.left);
+                    }
+
+                    if (curr.right != null) {
+                        q.offer(curr.right);
+                    }
+                }
+                depth++;
+            }
+            return depth;
         }
-        return 1 + minDepth;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumDepthOfBinaryTreeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumDepthOfBinaryTreeTest.kt
@@ -1,16 +1,22 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.challenge.MinimumDepthOfBinaryTree.MinimumDepthOfBinaryTreeBFS
+import com.github.dkoval.leetcode.challenge.MinimumDepthOfBinaryTree.MinimumDepthOfBinaryTreeDFS
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MinimumDepthOfBinaryTreeTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(null, 0),
             Arguments.of(
                 TreeNode(1),
@@ -45,10 +51,28 @@ internal class MinimumDepthOfBinaryTreeTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find the minimum depth of binary tree`(root: TreeNode?, expected: Int) {
-        val actual = MinimumDepthOfBinaryTree().minDepth(root)
-        assertEquals(expected, actual)
+    @Nested
+    inner class MinimumDepthOfBinaryTreeDFSTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the minimum depth of binary tree`(root: TreeNode?, expected: Int) {
+            MinimumDepthOfBinaryTreeDFS().test(root, expected)
+        }
     }
+
+    @Nested
+    inner class MinimumDepthOfBinaryTreeBFSTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find the minimum depth of binary tree`(root: TreeNode?, expected: Int) {
+            MinimumDepthOfBinaryTreeBFS().test(root, expected)
+        }
+    }
+}
+
+private fun MinimumDepthOfBinaryTree.test(root: TreeNode?, expected: Int) {
+    val actual = minDepth(root)
+    assertEquals(expected, actual)
 }
