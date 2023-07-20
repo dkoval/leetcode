d05c0592406c312bbf09a1d296c4de710fe1fc1d
dkoval <koval.dmitriy@gmail.com>
2023-07-20 11:59:10 +0200

    Re-solve "Asteroid Collision" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/AsteroidCollision.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/AsteroidCollision.java
@@ -4,43 +4,99 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
- * <a href="https://leetcode.com/explore/featured/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3502/">Asteroid Collision</a>
- *
+ * <a href="https://leetcode.com/problems/asteroid-collision/">Asteroid Collision</a>
+ * <p>
  * We are given an array asteroids of integers representing asteroids in a row.
- *
- * For each asteroid, the absolute value represents its size, and the sign represents its direction
- * (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
- *
+ * <p>
+ * For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left).
+ * Each asteroid moves at the same speed.
+ * <p>
  * Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode.
  * If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= asteroids.length <= 10^4</li>
+ *  <li>-1000 <= asteroids[i] <= 1000</li>
+ *  <li>asteroids[i] != 0</li>
+ * </ul>
  */
-public class AsteroidCollision {
+public interface AsteroidCollision {
 
-    public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer> result = new ArrayDeque<>();
-        int i = 0;
-        while (i < asteroids.length) {
-            int curr = asteroids[i];
-            if (result.isEmpty() || curr > 0 || result.peekLast() < 0) {
-                result.addLast(curr);
-            } else {
-                // collision
-                int prev = result.peekLast();
-                if (prev <= -curr) {
-                    result.pollLast();
+    int[] asteroidCollision(int[] asteroids);
+
+    class AsteroidCollisionRev1 implements AsteroidCollision {
+
+        @Override
+        public int[] asteroidCollision(int[] asteroids) {
+            Deque<Integer> result = new ArrayDeque<>();
+            int i = 0;
+            while (i < asteroids.length) {
+                int curr = asteroids[i];
+                if (result.isEmpty() || curr > 0 || result.peekLast() < 0) {
+                    result.addLast(curr);
+                } else {
+                    // collision
+                    int prev = result.peekLast();
+                    if (prev <= -curr) {
+                        result.pollLast();
+                    }
+                    if (prev < -curr) {
+                        // let `curr` asteroid move to the left
+                        i--;
+                    }
                 }
-                if (prev < -curr) {
-                    // let `curr` asteroid move to the left
-                    i--;
+                i++;
+            }
+            int[] arr = new int[result.size()];
+            i = 0;
+            while (!result.isEmpty()) {
+                arr[i++] = result.pollFirst();
+            }
+            return arr;
+        }
+    }
+
+    class AsteroidCollisionRev2 implements AsteroidCollision {
+
+        @Override
+        public int[] asteroidCollision(int[] asteroids) {
+            int n = asteroids.length;
+
+            Deque<Integer> stack = new ArrayDeque<>();
+            for (int x : asteroids) {
+                // the current asteroid may be potentially destroyed
+                boolean destroyed = false;
+                // process a collision, if there is any
+                while (!destroyed && !stack.isEmpty() && collide(stack.peek(), x)) {
+                    int curr = Math.abs(x);
+                    int top = Math.abs(stack.peek());
+                    if (curr > top) {
+                        stack.pop();
+                    } else if (curr == top) {
+                        stack.pop();
+                        destroyed = true;
+                    } else {
+                        destroyed = true;
+                    }
+                }
+
+                if (!destroyed) {
+                    stack.push(x);
                 }
             }
-            i++;
+
+            int[] ans = new int[stack.size()];
+            for (int i = ans.length - 1; i >= 0; i--) {
+                ans[i] = stack.pop();
+            }
+            return ans;
         }
-        int[] arr = new int[result.size()];
-        i = 0;
-        while (!result.isEmpty()) {
-            arr[i++] = result.pollFirst();
+
+        private boolean collide(int x, int y) {
+            // + / -
+            // -> / <-
+            return x > 0 && y < 0;
         }
-        return arr;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/mock/AsteroidCollisionTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/mock/AsteroidCollisionTest.kt
@@ -1,16 +1,22 @@
 package com.github.dkoval.leetcode.mock
 
 import com.github.dkoval.leetcode.challenge.AsteroidCollision
+import com.github.dkoval.leetcode.challenge.AsteroidCollision.AsteroidCollisionRev1
+import com.github.dkoval.leetcode.challenge.AsteroidCollision.AsteroidCollisionRev2
 import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class AsteroidCollisionTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(5, 10, -5),
                 intArrayOf(5, 10)
@@ -30,10 +36,28 @@ internal class AsteroidCollisionTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find out the state of the asteroids after all collisions`(asteroids: IntArray, expected: IntArray) {
-        val actual = AsteroidCollision().asteroidCollision(asteroids);
-        assertArrayEquals(expected, actual)
+    @Nested
+    inner class AsteroidCollisionRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find out the state of the asteroids after all collisions`(asteroids: IntArray, expected: IntArray) {
+            AsteroidCollisionRev1().test(asteroids, expected)
+        }
     }
+
+    @Nested
+    inner class AsteroidCollisionRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find out the state of the asteroids after all collisions`(asteroids: IntArray, expected: IntArray) {
+            AsteroidCollisionRev2().test(asteroids, expected)
+        }
+    }
+}
+
+private fun AsteroidCollision.test(asteroids: IntArray, expected: IntArray) {
+    val actual = asteroidCollision(asteroids);
+    assertArrayEquals(expected, actual)
 }
