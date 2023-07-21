da3b0ed0059a6dba91c18965d9e8bfdac2f17df4
dkoval <koval.dmitriy@gmail.com>
2023-07-21 11:56:05 +0200

    Re-solve "Number of Longest Increasing Subsequence" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/NumberOfLongestIncreasingSubsequence.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/NumberOfLongestIncreasingSubsequence.java
@@ -1,47 +1,102 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/563/week-5-october-29th-october-31st/3513/">Number of Longest Increasing Subsequence</a>
+ * <a href="https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/">Number of Longest Increasing Subsequence</a>
  * <p>
  * Given an integer array nums, return the number of longest increasing subsequences.
+ * <p>
+ * Notice that the sequence has to be strictly increasing.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 2000</li>
+ *  <li>-106 <= nums[i] <= 10^6</li>
+ * </ul>
  */
-public class NumberOfLongestIncreasingSubsequence {
+public interface NumberOfLongestIncreasingSubsequence {
+
+    int findNumberOfLIS(int[] nums);
 
     // Time complexity: O(N^2)
     // Space complexity: O(N)
-    public int findNumberOfLIS(int[] nums) {
-        int n = nums.length;
+    class NumberOfLongestIncreasingSubsequenceRev1 implements NumberOfLongestIncreasingSubsequence {
 
-        // lis[i] - the length of LIS starting at index i
-        int[] lis = new int[n];
-        // count[i] - number of LISs starting at index i
-        int[] count = new int[n];
+        @Override
+        public int findNumberOfLIS(int[] nums) {
+            int n = nums.length;
 
-        int bestLength = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            // nums[i] is a LIS of length 1
-            lis[i] = 1;
-            count[i] = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] < nums[j]) {
-                    // extend count[j] subsequences starting at index j by prepending nums[i] to all of them
-                    if (lis[i] < lis[j] + 1) {
-                        lis[i] = lis[j] + 1;
-                        count[i] = count[j];
-                    } else if (lis[i] == lis[j] + 1) {
-                        count[i] += count[j];
+            // lis[i] - the length of LIS starting at index i
+            int[] lis = new int[n];
+            // count[i] - number of LISs starting at index i
+            int[] count = new int[n];
+
+            int bestLength = 1;
+            for (int i = n - 1; i >= 0; i--) {
+                // nums[i] is a LIS of length 1
+                lis[i] = 1;
+                count[i] = 1;
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[i] < nums[j]) {
+                        // extend count[j] subsequences starting at index j by prepending nums[i] to all of them
+                        if (lis[i] < lis[j] + 1) {
+                            lis[i] = lis[j] + 1;
+                            count[i] = count[j];
+                        } else if (lis[i] == lis[j] + 1) {
+                            count[i] += count[j];
+                        }
                     }
                 }
+                bestLength = Math.max(bestLength, lis[i]);
             }
-            bestLength = Math.max(bestLength, lis[i]);
-        }
 
-        int total = 0;
-        for (int i = 0; i < n; i++) {
-            if (lis[i] == bestLength) {
-                total += count[i];
+            int total = 0;
+            for (int i = 0; i < n; i++) {
+                if (lis[i] == bestLength) {
+                    total += count[i];
+                }
             }
+            return total;
         }
-        return total;
+    }
+
+    // Time complexity: O(N^2)
+    // Space complexity: O(N)
+    class NumberOfLongestIncreasingSubsequenceRev2 implements NumberOfLongestIncreasingSubsequence {
+
+        @Override
+        public int findNumberOfLIS(int[] nums) {
+            int n = nums.length;
+
+            // len[i] - the length of LIS ending at index i
+            int[] len = new int[n];
+            // count[i] - the number of LIS of length len[i] ending at index i
+            int[] count = new int[n];
+
+            int best = 0;
+            for (int i = 0; i < n; i++) {
+                len[i] = 1;
+                count[i] = 1;
+                // check if we can append nums[i] to any existing LIS to make it even longer
+                for (int j = i - 1; j >= 0; j--) {
+                    if (nums[i] > nums[j]) {
+                        if (len[i] < len[j] + 1) {
+                            len[i] = len[j] + 1;
+                            count[i] = count[j];
+                        } else if (len[i] == len[j] + 1) {
+                            count[i] += count[j];
+                        }
+                    }
+                }
+                best = Math.max(best, len[i]);
+            }
+
+            int total = 0;
+            for (int i = 0; i < n; i++) {
+                if (len[i] == best) {
+                    total += count[i];
+                }
+            }
+            return total;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/NumberOfLongestIncreasingSubsequenceTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/NumberOfLongestIncreasingSubsequenceTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.NumberOfLongestIncreasingSubsequence.NumberOfLongestIncreasingSubsequenceRev1
+import com.github.dkoval.leetcode.challenge.NumberOfLongestIncreasingSubsequence.NumberOfLongestIncreasingSubsequenceRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class NumberOfLongestIncreasingSubsequenceTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(1, 3, 5, 4, 7),
                 // The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7]
@@ -23,10 +29,28 @@ class NumberOfLongestIncreasingSubsequenceTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return return the number of longest increasing subsequences`(nums: IntArray, expected: Int) {
-        val actual = NumberOfLongestIncreasingSubsequence().findNumberOfLIS(nums)
-        assertEquals(expected, actual)
+    @Nested
+    inner class NumberOfLongestIncreasingSubsequenceRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return return the number of longest increasing subsequences`(nums: IntArray, expected: Int) {
+            NumberOfLongestIncreasingSubsequenceRev1().test(nums, expected)
+        }
     }
+
+    @Nested
+    inner class NumberOfLongestIncreasingSubsequenceRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return return the number of longest increasing subsequences`(nums: IntArray, expected: Int) {
+            NumberOfLongestIncreasingSubsequenceRev2().test(nums, expected)
+        }
+    }
+}
+
+private fun NumberOfLongestIncreasingSubsequence.test(nums: IntArray, expected: Int) {
+    val actual = findNumberOfLIS(nums)
+    assertEquals(expected, actual)
 }
