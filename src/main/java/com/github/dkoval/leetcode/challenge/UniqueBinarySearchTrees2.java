7eba00772c11117c9cc711ac31e89af5432683df
dkoval <koval.dmitriy@gmail.com>
2023-08-05 11:41:51 +0200

    Re-solve "Unique Binary Search Trees II" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/UniqueBinarySearchTrees2.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/UniqueBinarySearchTrees2.java
@@ -7,36 +7,88 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3961/">Unique Binary Search Trees II</a>
- *
+ * <a href="https://leetcode.com/problems/unique-binary-search-trees-ii/">Unique Binary Search Trees II</a>
+ * <p>
  * Given an integer n, return all the structurally unique BST's (binary search trees),
  * which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 8
  */
-public class UniqueBinarySearchTrees2 {
+public interface UniqueBinarySearchTrees2 {
 
-    public List<TreeNode> generateTrees(int n) {
-        return generateTrees(1, n);
-    }
+    List<TreeNode> generateTrees(int n);
 
-    private List<TreeNode> generateTrees(int start, int end) {
-        if (start > end) {
-            return Collections.singletonList(null);
+    class UniqueBinarySearchTrees2Rev1 implements UniqueBinarySearchTrees2 {
+
+        @Override
+        public List<TreeNode> generateTrees(int n) {
+            return generateTrees(1, n);
         }
 
-        List<TreeNode> answer = new ArrayList<>();
-        // Consider every i as the root of a BST and then repeat the process recursively for left and right sub-trees
-        for (int i = start; i <= end; i++) {
-            List<TreeNode> lefts = generateTrees(start, i - 1);
-            List<TreeNode> rights = generateTrees(i + 1, end);
-            for (TreeNode left : lefts) {
-                for (TreeNode right : rights) {
-                    TreeNode root = new TreeNode(i);
-                    root.left = left;
-                    root.right = right;
-                    answer.add(root);
+        private List<TreeNode> generateTrees(int start, int end) {
+            if (start > end) {
+                return Collections.singletonList(null);
+            }
+
+            List<TreeNode> ans = new ArrayList<>();
+            // Consider every i as the root of a BST and then repeat the process recursively for left and right subtrees
+            for (int i = start; i <= end; i++) {
+                List<TreeNode> lefts = generateTrees(start, i - 1);
+                List<TreeNode> rights = generateTrees(i + 1, end);
+
+                for (TreeNode left : lefts) {
+                    for (TreeNode right : rights) {
+                        TreeNode root = new TreeNode(i);
+                        root.left = left;
+                        root.right = right;
+                        ans.add(root);
+                    }
                 }
             }
+            return ans;
         }
-        return answer;
+    }
+
+    class UniqueBinarySearchTrees2Rev2 implements UniqueBinarySearchTrees2 {
+
+        @Override
+        public List<TreeNode> generateTrees(int n) {
+            return generate(1, n);
+        }
+
+        private List<TreeNode> generate(int start, int end) {
+            if (start > end) {
+                return Collections.emptyList();
+            }
+
+            // select the root node from [start : end] range
+            List<TreeNode> ans = new ArrayList<>();
+            for (int root = start; root <= end; root++) {
+                // recursively generate all possible left and right subtrees
+                List<TreeNode> leftTrees = generate(start, root - 1);
+                List<TreeNode> rightTrees = generate(root + 1, end);
+
+                if (leftTrees.isEmpty() && rightTrees.isEmpty()) {
+                    ans.add(new TreeNode(root));
+                } else if (leftTrees.isEmpty()) {
+                    for (TreeNode right : rightTrees) {
+                        ans.add(new TreeNode(root, null, right));
+                    }
+                } else if (rightTrees.isEmpty()) {
+                    for (TreeNode left : leftTrees) {
+                        ans.add(new TreeNode(root, left, null));
+                    }
+                } else {
+                    for (TreeNode left : leftTrees) {
+                        for (TreeNode right : rightTrees) {
+                            ans.add(new TreeNode(root, left, right));
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/UniqueBinarySearchTrees2Test.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/UniqueBinarySearchTrees2Test.kt
@@ -1,16 +1,22 @@
 package com.github.dkoval.leetcode.challenge
 
 import com.github.dkoval.leetcode.TreeNode
-import org.assertj.core.api.Assertions
+import com.github.dkoval.leetcode.challenge.UniqueBinarySearchTrees2.UniqueBinarySearchTrees2Rev1
+import com.github.dkoval.leetcode.challenge.UniqueBinarySearchTrees2.UniqueBinarySearchTrees2Rev2
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class UniqueBinarySearchTrees2Test {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 1,
                 listOf(
@@ -49,10 +55,28 @@ internal class UniqueBinarySearchTrees2Test {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return all the structurally unique BSTs`(n: Int, expected: List<TreeNode>) {
-        val actual = UniqueBinarySearchTrees2().generateTrees(n)
-        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected)
+    @Nested
+    inner class UniqueBinarySearchTrees2Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return all the structurally unique BSTs`(n: Int, expected: List<TreeNode>) {
+            UniqueBinarySearchTrees2Rev1().test(n, expected)
+        }
     }
+
+    @Nested
+    inner class UniqueBinarySearchTrees2Rev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return all the structurally unique BSTs`(n: Int, expected: List<TreeNode>) {
+            UniqueBinarySearchTrees2Rev2().test(n, expected)
+        }
+    }
+}
+
+private fun UniqueBinarySearchTrees2.test(n: Int, expected: List<TreeNode>) {
+    val actual = generateTrees(n)
+    assertThat(actual).containsExactlyInAnyOrderElementsOf(expected)
 }
