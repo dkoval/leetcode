9a4180cb974b0ced61a50d85f53d04100ba17725
dkoval <koval.dmitriy@gmail.com>
2023-09-05 22:08:20 +0200

    Update description of "Copy List with Random Pointer" problem

--- src/main/java/com/github/dkoval/leetcode/interview/linkedlist/CopyListWithRandomPointer.java
+++ src/main/java/com/github/dkoval/leetcode/interview/linkedlist/CopyListWithRandomPointer.java
@@ -4,16 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/explore/interview/card/google/60/linked-list-5/3066/">Copy List with Random Pointer</a>
+ * <a href="https://leetcode.com/problems/copy-list-with-random-pointer/">Copy List with Random Pointer</a>
  * <p>
- * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+ * A linked list of length n is given such that each node contains an additional random pointer,
+ * which could point to any node in the list, or null.
  * <p>
- * Return a deep copy of the list.
+ * Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes,
+ * where each new node has its value set to the value of its corresponding original node.
+ * Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers
+ * in the original list and copied list represent the same list state. None of the pointers in the new list should point
+ * to nodes in the original list.
  * <p>
- * The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+ * For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding
+ * two nodes x and y in the copied list, x.random --> y.
  * <p>
- * val: an integer representing Node.val
- * random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
+ * Return the head of the copied linked list.
+ * <p>
+ * The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+ * <ul>
+ *  <li>val: an integer representing Node.val</li>
+ *  <li>random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.</li>
+ * </ul>
+ * Your code will only be given the head of the original linked list.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>0 <= n <= 1000</li>
+ *  <li>-10^4 <= Node.val <= 10^4</li>
+ *  <li>Node.random is null or is pointing to some node in the linked list.</li>
+ * </ul>
  */
 public interface CopyListWithRandomPointer {
 
