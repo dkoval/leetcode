c5f9ef076f292e4d325e2f97471a8e0281888301
dkoval <koval.dmitriy@gmail.com>
2023-11-28 18:10:40 +0100

    Solution to "Number of Ways to Divide a Long Corridor" hard problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/NumberOfWaysToDivideLongCorridor.java
@@ -0,0 +1,87 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/">Number of Ways to Divide a Long Corridor (Hard)</a>
+ * <p>
+ * Along a long library corridor, there is a line of seats and decorative plants.
+ * You are given a 0-indexed string corridor of length n consisting of letters 'S' and 'P' where each 'S' represents a seat
+ * and each 'P' represents a plant.
+ * <p>
+ * One room divider has already been installed to the left of index 0, and another to the right of index n - 1.
+ * Additional room dividers can be installed. For each position between indices i - 1 and i (1 <= i <= n - 1),
+ * at most one divider can be installed.
+ * <p>
+ * Divide the corridor into non-overlapping sections, where each section has exactly two seats with any number of plants.
+ * There may be multiple ways to perform the division. Two ways are different if there is a position with a room divider
+ * installed in the first way but not in the second way.
+ * <p>
+ * Return the number of ways to divide the corridor. Since the answer may be very large, return it modulo 10^9 + 7.
+ * If there is no way, return 0.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == corridor.length</li>
+ *  <li>1 <= n <= 10^5</li>
+ *  <li>corridor[i] is either 'S' or 'P'.</li>
+ * </ul>
+ */
+public interface NumberOfWaysToDivideLongCorridor {
+
+    int MOD = 1_000_000_007;
+
+    int numberOfWays(String corridor);
+
+    class NumberOfWaysToDivideLongCorridorRev1 implements NumberOfWaysToDivideLongCorridor {
+
+        @Override
+        public int numberOfWays(String corridor) {
+            int n = corridor.length();
+
+            // S...S P...P S...S P...P S...S ...
+            // <--->       <--->       <---> segment starts and ends with 'S'
+            // Given there are k plants between 2 consecutive segments, we can place (k + 1) dividers.
+            // Therefore, ans = (k1 + 1) * (k2 + 1) * (k3 + 1) * ...
+            int i = 0;
+
+            // skip leading P's
+            while (i < n && corridor.charAt(i) == 'P') {
+                i++;
+            }
+
+            if (i == n) {
+                return 0;
+            }
+
+            long ans = 1L;
+            boolean segmentStarted = true;
+            while (i < n) {
+                // corridor[i] == 'S', move to the next symbol (two 'S' are needed to form a segment)
+                i++;
+                if (segmentStarted) {
+                    // find the position where the current segment ends
+                    while (i < n && corridor.charAt(i) != 'S') {
+                        i++;
+                    }
+
+                    if (i < n) {
+                        segmentStarted = false;
+                    }
+                } else {
+                    // find the position where the next segment starts
+                    int dividers = 0;
+                    while (i < n && corridor.charAt(i) != 'S') {
+                        i++;
+                        dividers++;
+                    }
+
+                    if (i < n) {
+                        segmentStarted = true;
+                        ans *= dividers + 1;
+                        ans %= MOD;
+                    }
+                }
+            }
+            return segmentStarted ? 0 : (int) ans;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/NumberOfWaysToDivideLongCorridorTest.kt
@@ -0,0 +1,54 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.NumberOfWaysToDivideLongCorridor.NumberOfWaysToDivideLongCorridorRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class NumberOfWaysToDivideLongCorridorTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "SSPPSPS",
+                3
+            ),
+            Arguments.of(
+                "PPSPSP",
+                1
+            ),
+            Arguments.of(
+                "S"
+            ),
+            Arguments.of(
+                "SSSPPPSPPSPSSSSSSPPPSPP",
+                3
+            ),
+            Arguments.of(
+                "PPPPPSPPSPPSPPPSPPPPSPPPPSPPPPSPPSPPPSPSPPPSPSPPPSPSPPPSPSPPPPSPPPPSPPPSPPSPPPPSPSPPPPSPSPPPPSPSPPPSPPSPPPPSPSPSS",
+                919999993
+            )
+        )
+    }
+
+    @Nested
+    inner class NumberOfWaysToDivideLongCorridorRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of ways to divide the corridor`(corridor: String, expected: Int) {
+            NumberOfWaysToDivideLongCorridorRev1().test(corridor, expected)
+        }
+    }
+}
+
+private fun NumberOfWaysToDivideLongCorridor.test(corridor: String, expected: Int) {
+    val actual = numberOfWays(corridor)
+    assertEquals(expected, actual)
+}
