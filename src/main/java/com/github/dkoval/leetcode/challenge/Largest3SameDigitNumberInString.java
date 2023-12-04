a9a449465cf5120f650ec8c125fa82c506934fb4
dkoval <koval.dmitriy@gmail.com>
2023-12-04 21:07:37 +0100

    Solution to "Largest 3-Same-Digit Number in String" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/Largest3SameDigitNumberInString.java
@@ -0,0 +1,50 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/largest-3-same-digit-number-in-string/">Largest 3-Same-Digit Number in String</a>
+ * <p>
+ * You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+ * <ul>
+ *  <li>It is a substring of num with length 3.</li>
+ *  <li>It consists of only one unique digit.</li>
+ * </ul>
+ * Return the maximum good integer as a string or an empty string "" if no such integer exists.
+ * <p>
+ * Note:
+ * <ul>
+ *  <li>A substring is a contiguous sequence of characters within a string.</li>
+ *  <li>There may be leading zeroes in num or a good integer.</li>
+ * </ul>
+ */
+public interface Largest3SameDigitNumberInString {
+
+    String largestGoodInteger(String num);
+
+    // O(N) time | O(1) space
+    class Largest3SameDigitNumberInStringRev1 implements Largest3SameDigitNumberInString {
+
+        @Override
+        public String largestGoodInteger(String num) {
+            int n = num.length();
+
+            int left = 0;
+            int right = 0;
+            int best = -1;
+            while (right < n) {
+                while (right < n && num.charAt(right) == num.charAt(left)) {
+                    right++;
+                }
+
+                if (right - left >= 3) {
+                    best = Math.max(best, num.charAt(left) - '0');
+                }
+                left = right;
+            }
+
+            if (best != -1) {
+                return String.valueOf(best).repeat(3);
+            }
+            return "";
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/Largest3SameDigitNumberInStringTest.kt
@@ -0,0 +1,50 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.Largest3SameDigitNumberInString.Largest3SameDigitNumberInStringRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class Largest3SameDigitNumberInStringTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "6777133339",
+                "777"
+            ),
+            Arguments.of(
+                "2300019",
+                "000"
+            ),
+            Arguments.of(
+                "42352338",
+                ""
+            )
+        )
+    }
+
+    @Nested
+    inner class Largest3SameDigitNumberInStringRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum good integer as a string or an empty string if no such integer exists`(
+            num: String,
+            expected: Int
+        ) {
+            Largest3SameDigitNumberInStringRev1().test(num, expected)
+        }
+    }
+}
+
+private fun Largest3SameDigitNumberInString.test(num: String, expected: Int) {
+    val actual = largestGoodInteger(num)
+    assertEquals(expected, actual)
+}
