3d42433370dee2d37822549eb8a09278fa7e059a
dkoval <koval.dmitriy@gmail.com>
2024-01-03 20:18:40 +0100

    Solution to "Number of Laser Beams in a Bank" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/NumberOfLaserBeamsInBank.java
@@ -0,0 +1,58 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/number-of-laser-beams-in-a-bank/">Number of Laser Beams in a Bank</a>
+ * <p>
+ * Anti-theft security devices are activated inside a bank.
+ * You are given a 0-indexed binary string array bank representing the floor plan of the bank,
+ * which is an m x n 2D matrix. bank[i] represents the ith row, consisting of '0's and '1's.
+ * '0' means the cell is empty, while'1' means the cell has a security device.
+ * <p>
+ * There is one laser beam between any two security devices if both conditions are met:
+ * <ul>
+ *  <li>The two devices are located on two different rows: r1 and r2, where r1 < r2.</li>
+ *  <li>For each row i where r1 < i < r2, there are no security devices in the ith row.</li>
+ * </ul>
+ * Laser beams are independent, i.e., one beam does not interfere nor join with another.
+ * <p>
+ * Return the total number of laser beams in the bank.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>m == bank.length</li>
+ *  <li>n == bank[i].length</li>
+ *  <li>1 <= m, n <= 500</li>
+ *  <li>bank[i][j] is either '0' or '1'</li>
+ * </ul>
+ */
+public interface NumberOfLaserBeamsInBank {
+
+    int numberOfBeams(String[] bank);
+
+    // O(M * N) time | O(1) space
+    class NumberOfLaserBeamsInBankRev1 implements NumberOfLaserBeamsInBank {
+
+        @Override
+        public int numberOfBeams(String[] bank) {
+            int n = bank[0].length();
+
+            int total = 0;
+            int prev = 0;
+            for (String row : bank) {
+                // the number of security devices on the current row
+                int curr = 0;
+                for (int i = 0; i < n; i++) {
+                    if (row.charAt(i) == '1') {
+                        curr++;
+                    }
+                }
+
+                if (curr != 0) {
+                    total += prev * curr;
+                    prev = curr;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/NumberOfLaserBeamsInBankTest.kt
@@ -0,0 +1,52 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.NumberOfLaserBeamsInBank.NumberOfLaserBeamsInBankRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class NumberOfLaserBeamsInBankTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    "011001",
+                    "000000",
+                    "010100",
+                    "001000"
+                ),
+                8
+            ),
+            Arguments.of(
+                arrayOf(
+                    "000",
+                    "111",
+                    "000"
+                ),
+                0
+            )
+        )
+    }
+
+    @Nested
+    inner class NumberOfLaserBeamsInBankRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the total number of laser beams in the bank`(bank: Array<String>, expected: Int) {
+            NumberOfLaserBeamsInBankRev1().test(bank, expected)
+        }
+    }
+}
+
+private fun NumberOfLaserBeamsInBank.test(bank: Array<String>, expected: Int) {
+    val actual = numberOfBeams(bank)
+    assertEquals(expected, actual)
+}
