c672e90c80fbaa96c80ff74cd9301d6dc1a18fcd
dkoval <koval.dmitriy@gmail.com>
2024-02-01 20:25:35 +0100

    Solution to "Divide Array Into Arrays With Max Difference" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/DivideArrayIntoArraysWithMaxDifference.java
@@ -0,0 +1,58 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/">Divide Array Into Arrays With Max Difference</a>
+ * <p>
+ * You are given an integer array nums of size n and a positive integer k.
+ * <p>
+ * Divide the array into one or more arrays of size 3 satisfying the following conditions:
+ * <ul>
+ *  <li>Each element of nums should be in exactly one array.</li>
+ *  <li>The difference between any two elements in one array is less than or equal to k.</li>
+ * </ul>
+ * Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == nums.length</li>
+ *  <li>1 <= n <= 10^5</li>
+ *  <li>n is a multiple of 3</li>
+ *  <li>1 <= nums[i] <= 10^5</li>
+ *  <li>1 <= k <= 10^5</li>
+ * </ul>
+ */
+public interface DivideArrayIntoArraysWithMaxDifference {
+
+    int[][] divideArray(int[] nums, int k);
+
+    class DivideArrayIntoArraysWithMaxDifferenceRev1 implements DivideArrayIntoArraysWithMaxDifference {
+
+        @Override
+        public int[][] divideArray(int[] nums, int k) {
+            int n = nums.length;
+
+            if (n % 3 != 0) {
+                return new int[0][0];
+            }
+
+            Arrays.sort(nums);
+
+            int numGroups = n / 3;
+            int[][] ans = new int[numGroups][3];
+            for (int g = 0; g < numGroups; g++) {
+                ans[g][0] = nums[g * 3];
+                for (int i = g * 3 + 1; i < (g + 1) * 3; i++) {
+                    for (int j = g * 3; j < i; j++) {
+                        if (nums[i] - nums[j] > k) {
+                            return new int[0][0];
+                        }
+                    }
+                    ans[g][i - g * 3] = nums[i];
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/DivideArrayIntoArraysWithMaxDifferenceTest.kt
@@ -0,0 +1,58 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.DivideArrayIntoArraysWithMaxDifference.DivideArrayIntoArraysWithMaxDifferenceRev1
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class DivideArrayIntoArraysWithMaxDifferenceTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 3, 4, 8, 7, 9, 3, 5, 1),
+                2,
+                arrayOf(
+                    intArrayOf(1, 1, 3),
+                    intArrayOf(3, 4, 5),
+                    intArrayOf(7, 8, 9)
+                )
+            ),
+            Arguments.of(
+                intArrayOf(1, 3, 3, 2, 7, 3),
+                3,
+                emptyArray<IntArray>()
+            ),
+            Arguments.of(
+                intArrayOf(15, 13, 12, 13, 12, 14, 12, 2, 3, 13, 12, 14, 14, 13, 5, 12, 12, 2, 13, 2, 2),
+                2,
+                emptyArray<IntArray>()
+            )
+        )
+    }
+
+    @Nested
+    inner class DivideArrayIntoArraysWithMaxDifferenceRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return a 2D array containing all the arrays if it is impossible to satisfy the conditions`(
+            nums: IntArray,
+            k: Int,
+            expected: Array<IntArray>
+        ) {
+            DivideArrayIntoArraysWithMaxDifferenceRev1().test(nums, k, expected)
+        }
+    }
+}
+
+private fun DivideArrayIntoArraysWithMaxDifference.test(nums: IntArray, k: Int, expected: Array<IntArray>) {
+    val actual = divideArray(nums, k)
+    assertArrayEquals(expected, actual)
+}
