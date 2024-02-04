9af6c4b01ddf22b7cf04c45f966f5bac463485fb
dkoval <koval.dmitriy@gmail.com>
2024-02-04 12:54:47 +0100

    Resolve "Minimum Window Substring" hard problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MinimumWindowSubstring.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumWindowSubstring.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/615/week-3-august-15th-august-21st/3891/">Minimum Window Substring (Hard)</a>
+ * <a href="https://leetcode.com/problems/minimum-window-substring/">Minimum Window Substring (Hard)</a>
  * <p>
  * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that
  * every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
@@ -23,73 +23,66 @@ import java.util.Map;
  * <p>
  * Follow up: Could you find an algorithm that runs in O(m + n) time?
  */
-public class MinimumWindowSubstring {
+public interface MinimumWindowSubstring {
 
-    public String minWindow(String s, String t) {
-        // frequencies of characters in string t
-        Map<Character, Integer> tCounts = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            tCounts.put(t.charAt(i), tCounts.getOrDefault(t.charAt(i), 0) + 1);
-        }
+    String minWindow(String s, String t);
 
-        // boundaries of the sliding window
-        int left = 0; // inclusive
-        int right = 0; // exclusive
+    // O(M + N) time | O(ALPHA) = O(1) space
+    class MinimumWindowSubstringRev1 implements MinimumWindowSubstring {
 
-        // frequencies of characters in the sliding window s[left:right)
-        Map<Character, Integer> windowCounts = new HashMap<>();
-
-        // answer is s[start:end) substring
-        int start = -1; // inclusive
-        int end = -1; // exclusive
-        int minLength = Integer.MAX_VALUE;
-
-        while (right <= s.length()) {
-            // If the sliding window s[left:right) contains all characters of t,
-            // shrink it by advancing the left pointer;
-            // otherwise expand the window by advancing the right pointer.
-            if (isValidWindow(windowCounts, tCounts)) {
-                // check if we found a better solution
-                if (right - left < minLength) {
-                    start = left;
-                    end = right;
-                    minLength = right - left;
-                }
-
-                // remove character s[left] from the sliding window
-                char c = s.charAt(left);
-                windowCounts.put(c, windowCounts.getOrDefault(c, 0) - 1);
-                left++;
-            } else {
-                if (right == s.length()) {
-                    break;
-                }
-
-                // add character s[right] to the sliding window
-                char c = s.charAt(right);
-                windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
-                right++;
+        @Override
+        public String minWindow(String s, String t) {
+            if (t.length() > s.length()) {
+                return "";
             }
-        }
 
-        return (minLength == Integer.MAX_VALUE) ? "" : s.substring(start, end);
-    }
-
-    /**
-     * Checks if the sliding window s[left: right) contains all the characters of string t.
-     *
-     * @param windowCounts the frequencies of characters of the sliding window s[left:right).
-     * @param tCounts      the frequencies of characters of string t.
-     * @return true if the sliding window s[left:right) contains all the characters of string t.
-     */
-    private boolean isValidWindow(Map<Character, Integer> windowCounts, Map<Character, Integer> tCounts) {
-        for (Map.Entry<Character, Integer> entry : tCounts.entrySet()) {
-            char c = entry.getKey();
-            int count = entry.getValue();
-            if (!windowCounts.containsKey(c) || windowCounts.get(c) < count) {
-                return false;
+            // frequencies of characters in string t
+            Map<Character, Integer> tCounts = new HashMap<>();
+            for (int i = 0; i < t.length(); i++) {
+                char c = t.charAt(i);
+                tCounts.put(c, tCounts.getOrDefault(c, 0) + 1);
             }
+
+            // frequencies of characters in a sliding window s[left : right]
+            Map<Character, Integer> windowCounts = new HashMap<>();
+
+            int bestStart = -1;
+            int bestLength = s.length() + 1;
+
+            // boundaries of a sliding window (both inclusive)
+            int left = 0;
+            for (int right = 0; right < s.length(); right++) {
+                char last = s.charAt(right);
+                windowCounts.put(last, windowCounts.getOrDefault(last, 0) + 1);
+
+                // try to shrink a valid sliding window from the left
+                if (valid(windowCounts, tCounts)) {
+                    while (left <= right) {
+                        char first = s.charAt(left);
+                        if (windowCounts.get(first) <= tCounts.getOrDefault(first, 0)) {
+                            break;
+                        }
+                        windowCounts.put(first, windowCounts.get(first) - 1);
+                        left++;
+                    }
+
+                    if (right - left + 1 < bestLength) {
+                        bestStart = left;
+                        bestLength = right - left + 1;
+                    }
+                }
+            }
+            return (bestStart == -1) ? "" : s.substring(bestStart, bestStart + bestLength);
         }
-        return true;
+
+        private boolean valid(Map<Character, Integer> windowCounts, Map<Character, Integer> tCounts) {
+            // valid IFF: for all c in tCounts, windowCounts[c] > tCounts
+            for (char c : tCounts.keySet()) {
+                if (windowCounts.getOrDefault(c, 0) < tCounts.get(c)) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumWindowSubstringTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumWindowSubstringTest.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MinimumWindowSubstring.MinimumWindowSubstringRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MinimumWindowSubstringTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 "ADOBECODEBANC",
                 "ABC",
@@ -24,18 +29,36 @@ internal class MinimumWindowSubstringTest {
                 "a",
                 "aa",
                 ""
+            ),
+            Arguments.of(
+                "aa",
+                "aa",
+                "aa"
+            ),
+            Arguments.of(
+                "bdab",
+                "ab",
+                "ab"
             )
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the minimum window substring of s such that every character in t (including duplicates) is included in the window`(
-        s: String,
-        t: String,
-        expected: String
-    ) {
-        val actual = MinimumWindowSubstring().minWindow(s, t)
-        assertEquals(expected, actual)
+    @Nested
+    inner class MinimumWindowSubstringRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum window substring of s such that every character in t (including duplicates) is included in the window`(
+            s: String,
+            t: String,
+            expected: String
+        ) {
+            MinimumWindowSubstringRev1().test(s, t, expected)
+        }
     }
+}
+
+private fun MinimumWindowSubstring.test(s: String, t: String, expected: String) {
+    val actual = minWindow(s, t)
+    assertEquals(expected, actual)
 }
