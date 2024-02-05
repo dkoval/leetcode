a77f23f9f5debe5ab6a49a10e9bb02555f18a9ac
dkoval <koval.dmitriy@gmail.com>
2024-02-05 21:20:25 +0100

    Resolve "First Unique Character in a String" problem

--- src/main/java/com/github/dkoval/leetcode/problems/FirstUniqueCharacterInString.java
+++ src/main/java/com/github/dkoval/leetcode/problems/FirstUniqueCharacterInString.java
@@ -1,5 +1,8 @@
 package com.github.dkoval.leetcode.problems;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/problems/first-unique-character-in-a-string/">First Unique Character in a String</a>
  * <p>
@@ -11,26 +14,62 @@ package com.github.dkoval.leetcode.problems;
  *  <li>s consists of only lowercase English letters</li>
  * </ul>
  */
-public class FirstUniqueCharacterInString {
+public interface FirstUniqueCharacterInString {
 
-    private static final int ALPHABET_SIZE = 26;
+    int firstUniqChar(String s);
 
     // O(N) time | O(1) space
-    public int firstUniqChar(String s) {
-        int n = s.length();
-        int[] counts = new int[ALPHABET_SIZE];
+    class FirstUniqueCharacterInStringRev1 implements FirstUniqueCharacterInString {
 
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            counts[c - 'a']++;
+        public int firstUniqChar(String s) {
+            int n = s.length();
+            int[] counts = new int[26];
+
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                counts[c - 'a']++;
+            }
+
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                if (counts[c - 'a'] == 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    // O(N) time | O(1) space
+    class FirstUniqueCharacterInStringRev2 implements FirstUniqueCharacterInString {
+
+        @Override
+        public int firstUniqChar(String s) {
+            int n = s.length();
+
+            Map<Character,  CharInfo> seen = new LinkedHashMap<>();
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                final int index = i;
+                seen.computeIfAbsent(c, __ -> new CharInfo(index, 0)).count++;
+            }
+
+            for (CharInfo info : seen.values()) {
+                if (info.count == 1) {
+                    return info.firstIndex;
+                }
+            }
+            return -1;
         }
 
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (counts[c - 'a'] == 1) {
-                return i;
+        private static class CharInfo {
+            final int firstIndex;
+            int count;
+
+            CharInfo(int firstIndex, int count) {
+                this.firstIndex = firstIndex;
+                this.count = count;
             }
         }
-        return -1;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/problems/FirstUniqueCharacterInStringTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/FirstUniqueCharacterInStringTest.kt
@@ -1,25 +1,49 @@
 package com.github.dkoval.leetcode.problems
 
+import com.github.dkoval.leetcode.problems.FirstUniqueCharacterInString.FirstUniqueCharacterInStringRev1
+import com.github.dkoval.leetcode.problems.FirstUniqueCharacterInString.FirstUniqueCharacterInStringRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class FirstUniqueCharacterInStringTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of("leetcode", 0),
             Arguments.of("loveleetcode", 2),
             Arguments.of("aabb", -1)
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should  find the first non-repeating character in it and return its index`(s: String, expected: Int) {
-        val actual = FirstUniqueCharacterInString().firstUniqChar(s)
-        assertEquals(expected, actual)
+    @Nested
+    inner class FirstUniqueCharacterInStringRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should  find the first non-repeating character in it and return its index`(s: String, expected: Int) {
+            FirstUniqueCharacterInStringRev1().test(s, expected)
+        }
     }
+
+    @Nested
+    inner class FirstUniqueCharacterInStringRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should  find the first non-repeating character in it and return its index`(s: String, expected: Int) {
+            FirstUniqueCharacterInStringRev2().test(s, expected)
+        }
+    }
+}
+
+private fun FirstUniqueCharacterInString.test(s: String, expected: Int) {
+    val actual = firstUniqChar(s)
+    assertEquals(expected, actual)
 }
