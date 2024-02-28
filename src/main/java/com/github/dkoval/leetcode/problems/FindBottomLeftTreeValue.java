9a8cd7341799ddf34c790f1b993c16600ae4235f
dkoval <koval.dmitriy@gmail.com>
2024-02-28 09:33:04 +0100

    Resolve "Find Bottom Left Tree Value" problem

--- src/main/java/com/github/dkoval/leetcode/problems/FindBottomLeftTreeValue.java
+++ src/main/java/com/github/dkoval/leetcode/problems/FindBottomLeftTreeValue.java
@@ -3,6 +3,7 @@ package com.github.dkoval.leetcode.problems;
 import com.github.dkoval.leetcode.TreeNode;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Queue;
 
 /**
@@ -16,32 +17,62 @@ import java.util.Queue;
  *  <li>-2^31 <= Node.val <= 2^31 - 1</li>
  * </ul>
  */
-public class FindBottomLeftTreeValue {
+public interface FindBottomLeftTreeValue {
 
-    public int findBottomLeftValue(TreeNode root) {
-        int depth = depth(root);
-        // level order traversal
-        Queue<TreeNode> q = new ArrayDeque<>();
-        q.offer(root);
-        while (depth-- > 1) {
-            int size = q.size();
-            while (size-- > 0) {
-                TreeNode curr = q.poll();
-                if (curr.left != null) {
-                    q.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    q.offer(curr.right);
+    int findBottomLeftValue(TreeNode root);
+
+    class FindBottomLeftTreeValueRev1 implements FindBottomLeftTreeValue {
+
+        @Override
+        public int findBottomLeftValue(TreeNode root) {
+            int depth = depth(root);
+            // level order traversal
+            Queue<TreeNode> q = new ArrayDeque<>();
+            q.offer(root);
+            while (depth-- > 1) {
+                int size = q.size();
+                while (size-- > 0) {
+                    TreeNode curr = q.poll();
+                    if (curr.left != null) {
+                        q.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        q.offer(curr.right);
+                    }
                 }
             }
+            return q.poll().val;
         }
-        return q.poll().val;
+
+        private int depth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return 1 + Math.max(depth(root.left), depth(root.right));
+        }
     }
 
-    private int depth(TreeNode root) {
-        if (root == null) {
-            return 0;
+    class FindBottomLeftTreeValueRev2 implements FindBottomLeftTreeValue {
+
+        @Override
+        public int findBottomLeftValue(TreeNode root) {
+            TreeNode leftMost = null;
+            Deque<TreeNode> q = new ArrayDeque<>();
+            q.offer(root);
+            while (!q.isEmpty()) {
+                leftMost = q.peekFirst();
+                int size = q.size();
+                while (size-- > 0) {
+                    TreeNode curr = q.poll();
+                    if (curr.left != null) {
+                        q.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        q.offer(curr.right);
+                    }
+                }
+            }
+            return leftMost.val;
         }
-        return 1 + Math.max(depth(root.left), depth(root.right));
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/problems/FindBottomLeftTreeValueTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/FindBottomLeftTreeValueTest.kt
@@ -1,16 +1,22 @@
 package com.github.dkoval.leetcode.problems
 
 import com.github.dkoval.leetcode.TreeNode
+import com.github.dkoval.leetcode.problems.FindBottomLeftTreeValue.FindBottomLeftTreeValueRev1
+import com.github.dkoval.leetcode.problems.FindBottomLeftTreeValue.FindBottomLeftTreeValueRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class FindBottomLeftTreeValueTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 TreeNode(2).apply {
                     left = TreeNode(1)
@@ -39,10 +45,28 @@ internal class FindBottomLeftTreeValueTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the leftmost value in the last row of the tree`(root: TreeNode, expected: Int) {
-        val actual = FindBottomLeftTreeValue().findBottomLeftValue(root)
-        assertEquals(expected, actual)
+    @Nested
+    inner class FindBottomLeftTreeValueRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the leftmost value in the last row of the tree`(root: TreeNode, expected: Int) {
+            FindBottomLeftTreeValueRev1().test(root, expected)
+        }
     }
+
+    @Nested
+    inner class FindBottomLeftTreeValueRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the leftmost value in the last row of the tree`(root: TreeNode, expected: Int) {
+            FindBottomLeftTreeValueRev2().test(root, expected)
+        }
+    }
+}
+
+private fun FindBottomLeftTreeValue.test(root: TreeNode, expected: Int) {
+    val actual = findBottomLeftValue(root)
+    assertEquals(expected, actual)
 }
