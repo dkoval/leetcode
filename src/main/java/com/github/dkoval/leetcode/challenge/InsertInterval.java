c8825bf97fcab79718483199ff06b27a0890c9d5
dkoval <koval.dmitriy@gmail.com>
2024-03-17 15:01:41 +0100

    Update problem description

--- src/main/java/com/github/dkoval/leetcode/challenge/InsertInterval.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/InsertInterval.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 /**
  * <a href="https://leetcode.com/problems/insert-interval/">Insert Interval</a>
+ * You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi]
+ * represent the start and the end of the ith interval and intervals is sorted in ascending order by starti.
+ * You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
  * <p>
- * You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent
- * the start and the end of the ith curr and intervals is sorted in ascending order by starti.
- * You are also given an curr newInterval = [start, end] that represents the start and end of another curr.
- * <p>
- * Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals
- * still does not have any overlapping intervals (merge overlapping intervals if necessary).
+ * Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still
+ * does not have any overlapping intervals (merge overlapping intervals if necessary).
  * <p>
  * Return intervals after the insertion.
+ * <p>
+ * Note that you don't need to modify intervals in-place. You can make a new array and return it.
  * <p>
  * Constraints:
  * <ul>
