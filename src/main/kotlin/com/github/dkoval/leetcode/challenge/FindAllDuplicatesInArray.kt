7106d0390e94750a21401953cf5c05b1212e9cd3
dkoval <koval.dmitriy@gmail.com>
2024-03-25 20:56:02 +0100

    Resolve "Find All Duplicates in an Array" problem

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/FindAllDuplicatesInArray.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/FindAllDuplicatesInArray.kt
@@ -3,36 +3,43 @@ package com.github.dkoval.leetcode.challenge
 import kotlin.math.abs
 
 /**
- * [Find All Duplicates in an Array](https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3414/)
+ * [Find All Duplicates in an Array](https://leetcode.com/problems/find-all-duplicates-in-an-array/)
  *
- * Given an array of integers, ```1 ≤ a[i] ≤ n``` (n = size of array), some elements appear twice and others appear once.
- * Find all the elements that appear twice in this array.
+ * Given an integer array `nums` of length `n` where all the integers of `nums` are in the range `[1, n]`
+ * and each integer appears once or twice, return an array of all the integers that appears twice.
  *
- * Could you do it without extra space and in O(n) runtime?
- *
+ * You must write an algorithm that runs in `O(n)` time and uses only constant extra space.
  *
  * Constraints:
- * - n == nums.length
- * - 1 <= n <= 105
- * - 1 <= nums[i] <= n
- * - Each element in nums appears once or twice.
+ * - ```n == nums.length```
+ * - ```1 <= n <= 10^5```
+ * - ```1 <= nums[i] <= n```
+ * - Each element in `nums` appears once or twice.
  */
-object FindAllDuplicatesInArray {
+interface FindAllDuplicatesInArray {
+    fun findDuplicates(nums: IntArray): List<Int>
+}
+
+object FindAllDuplicatesInArrayRev1 : FindAllDuplicatesInArray {
 
     // O(N) time | O(1) space
     // Resource: https://www.youtube.com/watch?v=8ci8WfQ6cns
-    fun findDuplicates(nums: IntArray): List<Int> {
-        val result = mutableListOf<Int>()
-        for (num in nums) {
-            val i = abs(num)
-            if (nums[i - 1] > 0) {
-                // encountered i for the 1st time, mark
-                nums[i - 1] *= -1
+    override fun findDuplicates(nums: IntArray): List<Int> {
+        // integers of nums are in [1, n] range, whereas array indices are in [0, n - 1] range,
+        // meaning that nums[i] <=> index bijection can be built, i.e.
+        // index = nums[i] - 1 <=> nums[i] = index + 1.
+        // Then use negative marking to denote visited indices.
+        val ans = mutableListOf<Int>()
+        for (x in nums) {
+            val index = abs(x) - 1
+            if (nums[index] > 0) {
+                // encountered index for the 1st time, mark
+                nums[index] *= -1
             } else {
-                // encountered i for the 1st time, mark
-                result.add(i)
+                // encountered index for the 2nd time, add to the result
+                ans.add(index + 1)
             }
         }
-        return result
+        return ans
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/FindAllDuplicatesInArrayTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/FindAllDuplicatesInArrayTest.kt
@@ -1,15 +1,19 @@
 package com.github.dkoval.leetcode.challenge
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class FindAllDuplicatesInArrayTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(4, 3, 2, 7, 8, 2, 3, 1),
                 listOf(2, 3)
@@ -25,10 +29,18 @@ internal class FindAllDuplicatesInArrayTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should find all the elements that appear twice in an array`(nums: IntArray, expected: List<Int>) {
-        val actual = FindAllDuplicatesInArray.findDuplicates(nums)
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected)
+    @Nested
+    inner class FindAllDuplicatesInArrayRev1Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should find all the elements that appear twice in an array`(nums: IntArray, expected: List<Int>) {
+            FindAllDuplicatesInArrayRev1.test(nums, expected)
+        }
     }
+}
+
+private fun FindAllDuplicatesInArray.test(nums: IntArray, expected: List<Int>) {
+    val actual = findDuplicates(nums)
+    assertThat(actual).containsExactlyInAnyOrderElementsOf(expected)
 }
