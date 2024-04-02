78fba5998e7207c1093525102ea08453f6fbd104
dkoval <koval.dmitriy@gmail.com>
2024-04-02 20:51:18 +0200

    Resolve "Isomorphic Stings" problem. Rev1 solution is more elegant though.

--- src/main/java/com/github/dkoval/leetcode/challenge/IsomorphicStrings.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/IsomorphicStrings.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/609/week-2-july-8th-july-14th/3811/">Isomorphic Strings</a>
+ * <a href="https://leetcode.com/problems/isomorphic-strings/">Isomorphic Strings</a>
  * <p>
  * Given two strings s and t, determine if they are isomorphic.
  * <p>
@@ -12,25 +12,79 @@ import java.util.Map;
  * <p>
  * All occurrences of a character must be replaced with another character while preserving the order of characters.
  * No two characters may map to the same character, but a character may map to itself.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= s.length <= 5 * 10^4</li>
+ *  <li>t.length == s.length</li>
+ *  <li>s and t consist of any valid ascii character</li>
+ * </ul>
  */
-public class IsomorphicStrings {
+public interface IsomorphicStrings {
+
+    boolean isIsomorphic(String s, String t);
 
     // O(N) time | O(N) space
-    public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> mapping = new HashMap<>();
-        Map<Character, Character> reverseMapping = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char cs = s.charAt(i);
-            char ct = t.charAt(i);
-            if (mapping.containsKey(cs) && mapping.get(cs) != ct) {
-                return false;
+    class IsomorphicStringsRev1 implements IsomorphicStrings {
+
+        @Override
+        public boolean isIsomorphic(String s, String t) {
+            Map<Character, Character> mapping = new HashMap<>();
+            Map<Character, Character> reverseMapping = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                char cs = s.charAt(i);
+                char ct = t.charAt(i);
+                if (mapping.containsKey(cs) && mapping.get(cs) != ct) {
+                    return false;
+                }
+                if (reverseMapping.containsKey(ct) && reverseMapping.get(ct) != cs) {
+                    return false;
+                }
+                mapping.put(cs, ct);
+                reverseMapping.put(ct, cs);
             }
-            if (reverseMapping.containsKey(ct) && reverseMapping.get(ct) != cs) {
-                return false;
-            }
-            mapping.put(cs, ct);
-            reverseMapping.put(ct, cs);
+            return true;
         }
-        return true;
+    }
+
+    // O(N) time | O(N) space
+    class IsomorphicStringsRev2 implements IsomorphicStrings {
+
+        @Override
+        public boolean isIsomorphic(String s, String t) {
+            // t.length == s.length
+            int n = t.length();
+
+            Map<Character, Integer> counts1 = new HashMap<>();
+            Map<Character, Integer> counts2 = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                char c1 = s.charAt(i);
+                char c2 = t.charAt(i);
+
+                counts1.put(c1, counts1.getOrDefault(c1, 0) + 1);
+                counts2.put(c2, counts2.getOrDefault(c2, 0) + 1);
+            }
+
+            if (counts1.size() != counts2.size()) {
+                return false;
+            }
+
+            // controls that no two characters may map to the same character
+            Map<Character, Character> lookup = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                char c1 = s.charAt(i);
+                char c2 = t.charAt(i);
+
+                if (!lookup.containsKey(c1)) {
+                    lookup.put(c1, c2);
+                    continue;
+                }
+
+                if (lookup.get(c1) != c2) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/IsomorphicStringsTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/IsomorphicStringsTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.IsomorphicStrings.IsomorphicStringsRev1
+import com.github.dkoval.leetcode.challenge.IsomorphicStrings.IsomorphicStringsRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class IsomorphicStringsTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 "egg",
                 "add",
@@ -33,10 +39,28 @@ internal class IsomorphicStringsTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should determine of two strings are isomorphic`(s: String, t: String, expected: Boolean) {
-        val actual = IsomorphicStrings().isIsomorphic(s, t)
-        assertEquals(expected, actual)
+    @Nested
+    inner class IsomorphicStringsRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should determine of two strings are isomorphic`(s: String, t: String, expected: Boolean) {
+            IsomorphicStringsRev1().test(s, t, expected)
+        }
     }
+
+    @Nested
+    inner class IsomorphicStringsRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should determine of two strings are isomorphic`(s: String, t: String, expected: Boolean) {
+            IsomorphicStringsRev2().test(s, t, expected)
+        }
+    }
+}
+
+private fun IsomorphicStrings.test(s: String, t: String, expected: Boolean) {
+    val actual = isIsomorphic(s, t)
+    assertEquals(expected, actual)
 }
