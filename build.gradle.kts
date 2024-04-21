d54bc77fa4c08379e7fe89d64fcf73d81a3427db
dkoval <koval.dmitriy@gmail.com>
2024-04-21 11:32:01 +0200

    Address Gradle warnings after upgrading to 8.7
    See: https://docs.gradle.org/8.7/userguide/upgrading_version_8.html#test_suites

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.16.1")
 }
 
