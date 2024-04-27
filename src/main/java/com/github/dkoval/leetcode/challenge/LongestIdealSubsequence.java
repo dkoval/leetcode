cbca6b3d06e69f8a00b2d312e1086e0c8cb168a8
dkoval <koval.dmitriy@gmail.com>
2024-04-27 17:23:48 +0200

    DP bottom-up solution to "Longest Ideal Subsequence" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/LongestIdealSubsequence.java
@@ -0,0 +1,63 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-ideal-subsequence/">Longest Ideal Subsequence</a>
+ * <p>
+ * You are given a string s consisting of lowercase letters and an integer k. We call a string t ideal if the following
+ * conditions are satisfied:
+ * <p>
+ * t is a subsequence of the string s.
+ * The absolute difference in the alphabet order of every two adjacent letters in t is less than or equal to k.
+ * Return the length of the longest ideal string.
+ * <p>
+ * A subsequence is a string that can be derived from another string by deleting some or no characters without changing
+ * the order of the remaining characters.
+ * <p>
+ * Note that the alphabet order is not cyclic. For example, the absolute difference in the alphabet order of 'a' and 'z'
+ * is 25, not 1.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= s.length <= 10^5</li>
+ *  <li>0 <= k <= 25</li>
+ *  <li>s consists of lowercase English letters</li>
+ * </ul>
+ */
+public interface LongestIdealSubsequence {
+
+    int longestIdealString(String s, int k);
+
+    class LongestIdealSubsequenceDPBottomUpRev1 implements LongestIdealSubsequence {
+
+        @Override
+        public int longestIdealString(String s, int k) {
+            int n = s.length();
+
+            // Given an index i of the input string s,
+            // dp[c] represents the length of the longest ideal subsequence that ends a c,
+            // for any character c that occurs before s[i], where c is in ['a'..'z']
+            Map<Character, Integer> dp = new HashMap<>();
+
+            int best = 0;
+            for (int i = 0; i < n; i++) {
+                int currLength = 1;
+                // try to append s[i] to any previous subsequence
+                for (Map.Entry<Character, Integer> entry : dp.entrySet()) {
+                    char c = entry.getKey();
+                    int prevLength = entry.getValue();
+
+                    int diff = Math.abs(s.charAt(i) - c);
+                    if (diff <= k) {
+                        currLength = Math.max(currLength, prevLength + 1);
+                    }
+                }
+                best = Math.max(best, currLength);
+                dp.put(s.charAt(i), currLength);
+            }
+            return best;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/LongestIdealSubsequenceTest.kt
@@ -0,0 +1,50 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.LongestIdealSubsequence.LongestIdealSubsequenceDPBottomUpRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class LongestIdealSubsequenceTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "acfgbd",
+                2,
+                4
+            ),
+            Arguments.of(
+                "abcd",
+                3,
+                4
+            ),
+            Arguments.of(
+                "lkpkxcigcs",
+                6,
+                7
+            )
+        )
+    }
+
+    @Nested
+    inner class LongestIdealSubsequenceDPBottomUpRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the length of the longest ideal string`(s: String, k: Int, expected: Int) {
+            LongestIdealSubsequenceDPBottomUpRev1().test(s, k, expected)
+        }
+    }
+}
+
+private fun LongestIdealSubsequence.test(s: String, k: Int, expected: Int) {
+    val actual = longestIdealString(s, k)
+    assertEquals(expected, actual)
+}
