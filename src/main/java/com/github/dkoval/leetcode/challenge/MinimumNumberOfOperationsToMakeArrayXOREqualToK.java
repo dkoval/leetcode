78ad0c18e103d4c808c8cf68a238fab958eabd18
dkoval <koval.dmitriy@gmail.com>
2024-04-29 23:10:10 +0200

    Solution to "Minimum Number of Operations to Make Array XOR Equal to K" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumNumberOfOperationsToMakeArrayXOREqualToK.java
@@ -0,0 +1,56 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a hreg="https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/">Minimum Number of Operations to Make Array XOR Equal to K</a>
+ * <p>
+ * You are given a 0-indexed integer array nums and a positive integer k.
+ * <p>
+ * You can apply the following operation on the array any number of times:
+ * <p>
+ * Choose any element of the array and flip a bit in its binary representation. Flipping a bit means changing a 0 to 1 or vice versa.
+ * Return the minimum number of operations required to make the bitwise XOR of all elements of the final array equal to k.
+ * <p>
+ * Note that you can flip leading zero bits in the binary representation of elements.
+ * For example, for the number (101)2 you can flip the fourth bit and obtain (1101)2.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 10^5</li>
+ *  <li>0 <= nums[i] <= 10^6</li>
+ *  <li>0 <= k <= 10^6</li>
+ * </ul>
+ */
+public interface MinimumNumberOfOperationsToMakeArrayXOREqualToK {
+
+    int minOperations(int[] nums, int k);
+
+    class MinimumNumberOfOperationsToMakeArrayXOREqualToKRev1 implements MinimumNumberOfOperationsToMakeArrayXOREqualToK {
+
+        @Override
+        public int minOperations(int[] nums, int k) {
+            // nums = [2, 1, 3, 4], k = 1
+            // XOR(nums):
+            // 010
+            // 001
+            // 011
+            // 100
+            // ---
+            // x = 100 -> k = 001
+            // To get the answer, we need to count bits that differ in x and k, which can be done with XOR:
+            // 0 ^ 0 = 0, 1 ^ 1 = 0, 1 ^ 0 = 0 ^ 1 = 1
+            int x = 0;
+            for (int num : nums) {
+                x ^= num;
+            }
+
+            int count = 0;
+            for (int i = 0; i < 32; i++) {
+                // XOR i-th bits of x and k
+                int xi = (x >> i) & 1;
+                int ki = (k >> i) & 1;
+                count += xi ^ ki;
+            }
+            return count;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumNumberOfOperationsToMakeArrayXOREqualToKTest.kt
@@ -0,0 +1,49 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.MinimumNumberOfOperationsToMakeArrayXOREqualToK.MinimumNumberOfOperationsToMakeArrayXOREqualToKRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class MinimumNumberOfOperationsToMakeArrayXOREqualToKTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(2, 1, 3, 4),
+                1,
+                2
+            ),
+            Arguments.of(
+                intArrayOf(2, 0, 2, 0),
+                0,
+                0
+            )
+        )
+    }
+
+    @Nested
+    inner class MinimumNumberOfOperationsToMakeArrayXOREqualToKRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum number of operations required to make the bitwise XOR of all elements of the final array equal to k`(
+            nums: IntArray,
+            k: Int,
+            expected: Int
+        ) {
+            MinimumNumberOfOperationsToMakeArrayXOREqualToKRev1().test(nums, k, expected)
+        }
+    }
+}
+
+private fun MinimumNumberOfOperationsToMakeArrayXOREqualToK.test(nums: IntArray, k: Int, expected: Int) {
+    val actual = minOperations(nums, k)
+    assertEquals(expected, actual)
+}
