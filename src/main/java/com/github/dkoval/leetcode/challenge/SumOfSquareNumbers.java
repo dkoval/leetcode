b268b3bad42f14b150f68e4c1069977f8872d459
dkoval <koval.dmitriy@gmail.com>
2024-06-17 23:52:07 +0200

    Resolve "Sum of Square Numbers" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/SumOfSquareNumbers.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/SumOfSquareNumbers.java
@@ -1,30 +1,54 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/616/week-4-august-22nd-august-28th/3918/">Sum of Square Numbers</a>
+ * <a href="https://leetcode.com/problems/sum-of-square-numbers/">Sum of Square Numbers</a>
  * <p>
- * Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+ * Given a non-negative integer c, decide whether there're two integers a and b such that a^2 + b^2 = c.
  * <p>
  * Constraints:
  * <p>
- * 0 <= c <= 231 - 1
+ * 0 <= c <= 2^31 - 1
  */
-public class SumOfSquareNumbers {
+public interface SumOfSquareNumbers {
+
+    boolean judgeSquareSum(int c);
 
     // O(sqrt(c)) time | O(1) space
-    public boolean judgeSquareSum(int c) {
-        // binary search for a solution in [0 : sqrt(c)] range
-        int l = 0, r = (int) Math.sqrt(c);
-        while (l <= r) {
-            int sum = l * l + r * r;
-            if (sum < c) {
-                l++;
-            } else if (sum > c) {
-                r--;
-            } else {
-                return true;
+    class SumOfSquareNumbersRev1 implements SumOfSquareNumbers {
+
+        @Override
+        public boolean judgeSquareSum(int c) {
+            // binary search for a solution in [0 : sqrt(c)] range
+            long a = 0;
+            long b = (int) Math.sqrt(c);
+            while (a <= b) {
+                long sum = a * a + b * b;
+                if (sum < c) {
+                    a++;
+                } else if (sum > c) {
+                    b--;
+                } else {
+                    return true;
+                }
             }
+            return false;
         }
-        return false;
+    }
+
+    class SumOfSquareNumbersRev2 implements SumOfSquareNumbers {
+
+        @Override
+        public boolean judgeSquareSum(int c) {
+            // a^2 + b^2 = c
+            // the largest a^2 (or b^2) can be is c:
+            // a^2 + 0 = c <=> 0 + b^2 = c
+            for (int a = 0; a <= Math.sqrt(c); a++) {
+                int b = (int) Math.sqrt(c - a * a);
+                if (a * a + b * b == c) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/SumOfSquareNumbersTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/SumOfSquareNumbersTest.kt
@@ -1,20 +1,25 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.SumOfSquareNumbers.SumOfSquareNumbersRev1
+import com.github.dkoval.leetcode.challenge.SumOfSquareNumbers.SumOfSquareNumbersRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class SumOfSquareNumbersTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             // 1^2 + 2^2 = 5
             Arguments.of(5, true),
             // 2^2 + 2^2 = 4
             Arguments.of(4, true),
-            // no solution
             Arguments.of(3, false),
             // 1^2 + 1^2 = 2
             Arguments.of(2, true),
@@ -22,15 +27,34 @@ internal class SumOfSquareNumbersTest {
             Arguments.of(1, true),
             // 0^2 + 0^2 = 0
             Arguments.of(0, true),
-            // no solution
-            Arguments.of(Int.MAX_VALUE, false)
+            Arguments.of(Int.MAX_VALUE, false),
+            Arguments.of(2147483600, true),
+            Arguments.of(2147483646, false)
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should decide whether there are two integers a and b such that a^2 + b^2 = c`(c: Int, expected: Boolean) {
-        val actual = SumOfSquareNumbers().judgeSquareSum(c)
-        assertEquals(expected, actual)
+    @Nested
+    inner class SumOfSquareNumbersRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should decide whether there are two integers a and b such that a^2 + b^2 = c`(c: Int, expected: Boolean) {
+            SumOfSquareNumbersRev1().test(c, expected)
+        }
     }
+
+    @Nested
+    inner class SumOfSquareNumbersRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should decide whether there are two integers a and b such that a^2 + b^2 = c`(c: Int, expected: Boolean) {
+            SumOfSquareNumbersRev2().test(c, expected)
+        }
+    }
+}
+
+private fun SumOfSquareNumbers.test(c: Int, expected: Boolean) {
+    val actual = judgeSquareSum(c)
+    assertEquals(expected, actual)
 }
