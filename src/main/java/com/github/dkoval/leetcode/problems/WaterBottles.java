fdeff0413e2559ce6ff9731c7dd0a1387d6ab796
dkoval <koval.dmitriy@gmail.com>
2024-07-07 10:41:32 +0200

    Resolve "Water Bottles" problem

--- src/main/java/com/github/dkoval/leetcode/problems/WaterBottles.java
+++ src/main/java/com/github/dkoval/leetcode/problems/WaterBottles.java
@@ -16,18 +16,42 @@ package com.github.dkoval.leetcode.problems;
  *  <li>2 <= numExchange <= 100</li>
  * </ul>
  */
-public class WaterBottles {
+public interface WaterBottles {
 
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int count = numBottles;
-        int numEmptyBottles = numBottles;
-        while (numEmptyBottles >= numExchange) {
-            int numRefilledBottles = numEmptyBottles / numExchange;
-            count += numRefilledBottles;
-            // prepare for the next iteration
-            numEmptyBottles %= numExchange;
-            numEmptyBottles += numRefilledBottles;
+    int numWaterBottles(int numBottles, int numExchange);
+
+    class WaterBottlesRev1 implements WaterBottles {
+
+        @Override
+        public int numWaterBottles(int numBottles, int numExchange) {
+            int count = numBottles;
+            int numEmptyBottles = numBottles;
+            while (numEmptyBottles >= numExchange) {
+                int numRefilledBottles = numEmptyBottles / numExchange;
+                count += numRefilledBottles;
+                // prepare for the next iteration
+                numEmptyBottles %= numExchange;
+                numEmptyBottles += numRefilledBottles;
+            }
+            return count;
         }
-        return count;
+    }
+
+    class WaterBottlesRev2 implements WaterBottles {
+
+        @Override
+        public int numWaterBottles(int numBottles, int numExchange) {
+            int ans = 0;
+            int numEmpty = 0;
+            while (numBottles > 0) {
+                // drink all full water bottles
+                ans += numBottles;
+                numEmpty += numBottles;
+                // exchange empty water bottles
+                numBottles = numEmpty / numExchange;
+                numEmpty %= numExchange;
+            }
+            return ans;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/problems/WaterBottlesTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/WaterBottlesTest.kt
@@ -1,28 +1,56 @@
 package com.github.dkoval.leetcode.problems
 
+import com.github.dkoval.leetcode.problems.WaterBottles.WaterBottlesRev1
+import com.github.dkoval.leetcode.problems.WaterBottles.WaterBottlesRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class WaterBottlesTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(9, 3, 13),
             Arguments.of(15, 4, 19)
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the maximum number of water bottles you can drink`(
-        numBottles: Int,
-        numExchange: Int,
-        expected: Int
-    ) {
-        val actual = WaterBottles().numWaterBottles(numBottles, numExchange)
-        assertEquals(expected, actual)
+    @Nested
+    inner class WaterBottlesRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum number of water bottles you can drink`(
+            numBottles: Int,
+            numExchange: Int,
+            expected: Int
+        ) {
+            WaterBottlesRev1().test(numBottles, numExchange, expected)
+        }
     }
+
+    @Nested
+    inner class WaterBottlesRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum number of water bottles you can drink`(
+            numBottles: Int,
+            numExchange: Int,
+            expected: Int
+        ) {
+            WaterBottlesRev2().test(numBottles, numExchange, expected)
+        }
+    }
+}
+
+private fun WaterBottles.test(numBottles: Int, numExchange: Int, expected: Int) {
+    val actual = numWaterBottles(numBottles, numExchange)
+    assertEquals(expected, actual)
 }
