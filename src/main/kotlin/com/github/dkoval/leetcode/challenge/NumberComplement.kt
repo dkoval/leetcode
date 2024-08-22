b7e1cdf11446b9681499c1b4fe1055a1a9a7321f
dkoval <koval.dmitriy@gmail.com>
2024-08-22 11:05:03 +0200

    Update "Number Complement" problem description

--- src/main/kotlin/com/github/dkoval/leetcode/challenge/NumberComplement.kt
+++ src/main/kotlin/com/github/dkoval/leetcode/challenge/NumberComplement.kt
@@ -1,9 +1,14 @@
 package com.github.dkoval.leetcode.challenge
 
 /**
- * [Number Complement](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319/)
+ * [Number Complement](https://leetcode.com/problems/number-complement/)
  *
- * Given a positive integer num, output its complement number. The complement strategy is to flip the bits of its binary representation.
+ * The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's
+ * in its binary representation.
+ *
+ * For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+ *
+ * Given an integer num, return its complement.
  *
  * Constraints:
  * 1 <= num < 2^31
