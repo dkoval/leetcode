bbb6a74f45aa28fb0dad883c1b5efe30de3c63e0
dkoval <koval.dmitriy@gmail.com>
2024-09-08 19:25:55 +0200

    Move Toolchains resolver plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
     jacoco
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "leetcode"
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
-}
