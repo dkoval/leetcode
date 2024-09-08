80acd017c799349bcb121bafced2861ee6ab181b
dkoval <koval.dmitriy@gmail.com>
2024-09-08 19:07:11 +0200

    Bump JUnit 5 to v5.10.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
     testImplementation("org.assertj:assertj-core:3.16.1")
 }
 
