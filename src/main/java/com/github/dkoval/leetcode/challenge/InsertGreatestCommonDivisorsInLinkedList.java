5d2440fc45f57118d35bd91ee28dc3f165dc4ea0
dkoval <koval.dmitriy@gmail.com>
2024-09-10 21:01:19 +0200

    Solution to "Insert Greatest Common Divisors in Linked List" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/InsertGreatestCommonDivisorsInLinkedList.java
@@ -0,0 +1,52 @@
+package com.github.dkoval.leetcode.challenge;
+
+import com.github.dkoval.leetcode.ListNode;
+
+/**
+ * <a href="https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/">Insert Greatest Common Divisors in Linked List</a>
+ * <p>
+ * Given the head of a linked list head, in which each node contains an integer value.
+ * <p>
+ * Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+ * <p>
+ * Return the linked list after insertion.
+ * <p>
+ * The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the list is in the range [1, 5000]</li>
+ *  <li>1 <= Node.val <= 1000</li>
+ * </ul>
+ */
+public interface InsertGreatestCommonDivisorsInLinkedList {
+
+    ListNode insertGreatestCommonDivisors(ListNode head);
+
+    // O(N) time | O(1) space
+    class InsertGreatestCommonDivisorsInLinkedListRev1 implements InsertGreatestCommonDivisorsInLinkedList {
+
+        @Override
+        public ListNode insertGreatestCommonDivisors(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode prev = head;
+            ListNode curr = head.next;
+            while (curr != null) {
+                prev.next = new ListNode(gcd(prev.val, curr.val), curr);
+                prev = curr;
+                curr = curr.next;
+            }
+            return head;
+        }
+
+        private int gcd(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/InsertGreatestCommonDivisorsInLinkedListTest.kt
@@ -0,0 +1,44 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.ListNode
+import com.github.dkoval.leetcode.challenge.InsertGreatestCommonDivisorsInLinkedList.InsertGreatestCommonDivisorsInLinkedListRev1
+import com.github.dkoval.leetcode.equalsTo
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class InsertGreatestCommonDivisorsInLinkedListTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                ListNode.headOf(18, 6, 10, 3),
+                ListNode.headOf(18, 6, 6, 2, 10, 1, 3)
+            ),
+            Arguments.of(
+                ListNode.headOf(7),
+                ListNode.headOf(7)
+            )
+        )
+    }
+
+    @Nested
+    inner class InsertGreatestCommonDivisorsInLinkedListRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the linked list after insertion`(head: ListNode, expected: ListNode) {
+            InsertGreatestCommonDivisorsInLinkedListRev1().test(head, expected)
+        }
+    }
+}
+
+fun InsertGreatestCommonDivisorsInLinkedList.test(head: ListNode, expected: ListNode) {
+    assertTrue { insertGreatestCommonDivisors(head).equalsTo(expected) }
+}
