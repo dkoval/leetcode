9cd778ffa5084a03a797450f89efb5ba48a573db
dkoval <koval.dmitriy@gmail.com>
2024-11-18 11:20:04 +0100

    Solution to "Defuse the Bomb" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/DefuseBomb.java
@@ -0,0 +1,57 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/defuse-the-bomb/">Defuse the Bomb</a>
+ * <p>
+ * You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
+ * <p>
+ * To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+ * <ul>
+ *  <li>If k > 0, replace the ith number with the sum of the next k numbers.</li>
+ *  <li>If k < 0, replace the ith number with the sum of the previous k numbers.</li>
+ *  <li>If k == 0, replace the ith number with 0.</li>
+ * </ul>
+ * As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+ * <p>
+ * Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == code.length</li>
+ *  <li>1 <= n <= 100</li>
+ *  <li>1 <= code[i] <= 100</li>
+ *  <li>-(n - 1) <= k <= n - 1</li>
+ * </ul>
+ */
+public interface DefuseBomb {
+
+    int[] decrypt(int[] code, int k);
+
+    class DefuseBombRev1 implements DefuseBomb {
+
+        private static int mod(int x, int n) {
+            x %= n;
+            return (x < 0) ? x + n : x;
+        }
+
+        @Override
+        public int[] decrypt(int[] code, int k) {
+            int n = code.length;
+
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                int curr = i;
+                int step = (k > 0) ? 1 : -1;
+                int repeat = Math.abs(k);
+                while (repeat > 0) {
+                    curr += step;
+                    sum += code[mod(curr, n)];
+                    repeat--;
+                }
+                ans[i] = sum;
+            }
+            return ans;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/DefuseBombTest.kt
@@ -0,0 +1,50 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.DefuseBomb.DefuseBombRev1
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class DefuseBombTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(5, 7, 1, 4),
+                3,
+                intArrayOf(12, 10, 16, 13)
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3, 4),
+                0,
+                intArrayOf(0, 0, 0, 0)
+            ),
+            Arguments.of(
+                intArrayOf(2, 4, 9, 3),
+                -2,
+                intArrayOf(12, 5, 6, 13)
+            )
+        )
+    }
+
+    @Nested
+    inner class DefuseBombRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the decrypted code to defuse the bomb`(code: IntArray, k: Int, expected: IntArray) {
+            DefuseBombRev1().test(code, k, expected)
+        }
+    }
+}
+
+private fun DefuseBomb.test(code: IntArray, k: Int, expected: IntArray) {
+    val actual = decrypt(code, k)
+    assertArrayEquals(expected, actual)
+}
