d6c9cda19bae4ace8bebb64b6916fa47fd4e1c02
dkoval <koval.dmitriy@gmail.com>
2025-01-18 20:32:06 +0100

    Solution to "Minimum Cost to Make at Least One Valid Path in a Grid" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumCostToMakeAtLeastOneValidPathInGrid.java
@@ -0,0 +1,103 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * <a href="https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/">Minimum Cost to Make at Least One Valid Path in a Grid (Hard)</a>
+ * <p>
+ * Given an m x n grid. Each cell of the grid has a sign pointing to the next cell you should visit if you are currently in this cell.
+ * The sign of grid[i][j] can be:
+ * <ul>
+ *  <li>1 which means go to the cell to the right. (i.e go from grid[i][j] to grid[i][j + 1])</li>
+ *  <li>2 which means go to the cell to the left. (i.e go from grid[i][j] to grid[i][j - 1])</li>
+ *  <li>3 which means go to the lower cell. (i.e go from grid[i][j] to grid[i + 1][j])</li>
+ *  <li>4 which means go to the upper cell. (i.e go from grid[i][j] to grid[i - 1][j])</li>
+ * </ul>
+ * Notice that there could be some signs on the cells of the grid that point outside the grid.
+ * <p>
+ * You will initially start at the upper left cell (0, 0). A valid path in the grid is a path that starts from
+ * the upper left cell (0, 0) and ends at the bottom-right cell (m - 1, n - 1) following the signs on the grid.
+ * The valid path does not have to be the shortest.
+ * <p>
+ * You can modify the sign on a cell with cost = 1. You can modify the sign on a cell one time only.
+ * <p>
+ * Return the minimum cost to make the grid have at least one valid path.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>m == grid.length</li>
+ *  <li>n == grid[i].length</li>
+ *  <li>1 <= m, n <= 100</li>
+ *  <li>1 <= grid[i][j] <= 4</li>
+ * </ul>
+ */
+public interface MinimumCostToMakeAtLeastOneValidPathInGrid {
+
+    int minCost(int[][] grid);
+
+    class MinimumCostToMakeAtLeastOneValidPathInGridRev1 implements MinimumCostToMakeAtLeastOneValidPathInGrid {
+
+        @Override
+        public int minCost(int[][] grid) {
+            final var m = grid.length;
+            final var n = grid[0].length;
+
+            // Dijkstra's algorithm
+            final var q = new PriorityQueue<Cell>(Comparator.comparingInt(it -> it.cost));
+
+            final var costs = new int[m][n];
+            for (var items : costs) {
+                Arrays.fill(items, Integer.MAX_VALUE);
+            }
+
+            q.offer(new Cell(0, 0, 0));
+            while (!q.isEmpty()) {
+                var curr = q.poll();
+                costs[curr.row][curr.col] = curr.cost;
+
+                if (curr.row == m - 1 && curr.col == n - 1) {
+                    return curr.cost;
+                }
+
+                for (Direction d : Direction.values()) {
+                    final var nextRow = curr.row + d.drow;
+                    final var nextCol = curr.col + d.dcol;
+
+                    // out of bounds?
+                    if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n) {
+                        continue;
+                    }
+
+                    final var delta = (d.sign == grid[curr.row][curr.col]) ? 0 : 1;
+                    if (curr.cost + delta < costs[nextRow][nextCol]) {
+                        q.offer(new Cell(nextRow, nextCol, curr.cost + delta));
+                        costs[nextRow][nextCol] = curr.cost + delta;
+                    }
+                }
+            }
+            return Integer.MAX_VALUE;
+        }
+
+        private enum Direction {
+            RIGHT(0, 1, 1),
+            LEFT(0, -1, 2),
+            DOWN(1, 0, 3),
+            UP(-1, 0, 4);
+
+            final int drow;
+            final int dcol;
+            final int sign;
+
+            Direction(int drow, int dcol, int sign) {
+                this.drow = drow;
+                this.dcol = dcol;
+                this.sign = sign;
+            }
+        }
+
+        private record Cell(int row, int col, int cost) {
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MinimumCostToMakeAtLeastOneValidPathInGridTest.kt
@@ -0,0 +1,62 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.MinimumCostToMakeAtLeastOneValidPathInGrid.MinimumCostToMakeAtLeastOneValidPathInGridRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class MinimumCostToMakeAtLeastOneValidPathInGridTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 1, 1, 1),
+                    intArrayOf(2, 2, 2, 2),
+                    intArrayOf(1, 1, 1, 1),
+                    intArrayOf(2, 2, 2, 2)
+                ),
+                3
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 1, 3),
+                    intArrayOf(3, 2, 2),
+                    intArrayOf(1, 1, 4)
+                ),
+                0
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 2),
+                    intArrayOf(4, 3)
+                ),
+                1
+            )
+        )
+    }
+
+    @Nested
+    inner class MinimumCostToMakeAtLeastOneValidPathInGridRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the minimum cost to make the grid have at least one valid path`(
+            grid: Array<IntArray>,
+            expected: Int
+        ) {
+            MinimumCostToMakeAtLeastOneValidPathInGridRev1().test(grid, expected)
+        }
+    }
+}
+
+private fun MinimumCostToMakeAtLeastOneValidPathInGrid.test(grid: Array<IntArray>, expected: Int) {
+    val actual = minCost(grid)
+    assertEquals(expected, actual)
+}
