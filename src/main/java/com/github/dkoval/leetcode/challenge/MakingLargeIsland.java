0bb54c9176717c5cda1f83376fa98d39fae360f5
dkoval <koval.dmitriy@gmail.com>
2025-01-31 23:21:58 +0100

    Resolve "Making A Large Island" hard problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MakingLargeIsland.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MakingLargeIsland.java
@@ -2,86 +2,105 @@ package com.github.dkoval.leetcode.challenge;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3835/">Making A Large Island (Hard)</a>
+ * <a href="https://leetcode.com/problems/making-a-large-island/">Making A Large Island (Hard)</a>
  * <p>
  * You are given an n x n binary matrix grid. You are allowed to change at most one 0 to be 1.
  * <p>
  * Return the size of the largest island in grid after applying this operation.
  * <p>
- * An island is a 4-directionally connected group of 1s
+ * An island is a 4-directionally connected group of 1s.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>n == grid.length</li>
+ *  <li>n == grid[i].length</li>
+ *  <li>1 <= n <= 500</li>
+ *  <li>grid[i][j] is either 0 or 1.</li>
+ * </ul>
  */
-public class MakingLargeIsland {
+public interface MakingLargeIsland {
 
-    private static final int STARTING_LABEL = 1;
-    private static final int[][] DIRECTIONS = {{0, -1}, {0, 1}, {-1, 0}, {1,0}};
+    int largestIsland(int[][] grid);
 
-    // O(N^2) time | O(N^2) space
-    // Resource: https://www.youtube.com/watch?v=ggEq5JrnmH0
-    public int largestIsland(int[][] grid) {
-        int n = grid.length;
+    // O(N^2) time | O(1) space
+    class MakingLargeIslandRev1 implements MakingLargeIsland {
 
-        int[][] labels = new int[n][n];
-        // stores label -> area mapping for an island
-        Map<Integer, Integer> areas = new HashMap<>();
-        // label = 0 denotes cells that do not belong to any island, therefore area is set to 0
-        areas.put(0, 0);
+        // up, down, left, right
+        private static final int[][] DIRS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        int currLabel = STARTING_LABEL;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (grid[row][col] == 1) {
-                    int area = dfs(grid, row, col, labels, currLabel);
-                    areas.put(currLabel++, area);
+        @Override
+        public int largestIsland(int[][] grid) {
+            final var m = grid.length;
+            final var n = grid[0].length;
+
+            // pass #1: detect islands
+            var maxArea = 0;
+            // map island to its area
+            var islandId = 2; // 0 - water, 1 - unvisited land
+            final var areas = new HashMap<Integer, Integer>();
+            for (var row = 0; row < m; row++) {
+                for (var col = 0; col < n; col++) {
+                    if (grid[row][col] == 1) {
+                        var area = traverse(grid, row, col, islandId);
+                        maxArea = Math.max(maxArea, area);
+                        areas.put(islandId++, area);
+                    }
                 }
             }
-        }
 
-        int maxArea = areas.getOrDefault(STARTING_LABEL, 0);
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (grid[row][col] == 0) {
-                    // `used` set avoids adding islands with the same label
-                    Set<Integer> used = new HashSet<>();
-                    for (int[] d : DIRECTIONS) {
-                        int nextRow = row + d[0];
-                        int nextCol = col + d[1];
-                        if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n) {
-                            used.add(labels[nextRow][nextCol]);
-                        }
+            // pass #2: connect islands to form a larger one
+            for (var row = 0; row < m; row++) {
+                for (var col = 0; col < n; col++) {
+                    if (grid[row][col] != 0) {
+                        continue;
                     }
 
-                    int area = 1;
-                    for (int label : used) {
-                        area += areas.get(label);
+                    // 1. flip (row, col) to 1
+                    // 2. check adjacent cells to see if a larger island can be formed
+                    var area = 1;
+                    var seen = new HashSet<Integer>();
+                    for (var d : DIRS) {
+                        var nextRow = row + d[0];
+                        var nextCol = col + d[1];
+
+                        if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n) {
+                            continue;
+                        }
+
+                        if (grid[nextRow][nextCol] > 1 && !seen.contains(grid[nextRow][nextCol])) {
+                            area += areas.get(grid[nextRow][nextCol]);
+                            seen.add(grid[nextRow][nextCol]);
+                        }
                     }
                     maxArea = Math.max(maxArea, area);
                 }
             }
-        }
-        return maxArea;
-    }
-
-    // Assigns a label to an island starting at grid[row][col] and its returns area
-    private int dfs(int[][] grid, int row, int col, int[][] labels, int label) {
-        int n = grid.length;
-
-        // consider unlabeled 1s only
-        if (row < 0 || row >= n || col < 0 || col >= n || grid[row][col] == 0 || labels[row][col] != 0) {
-            return 0;
+            return maxArea;
         }
 
-        // assign a label
-        labels[row][col] = label;
+        private int traverse(int[][] grid, int row, int col, int islandId) {
+            // DFS
+            final var m = grid.length;
+            final var n = grid[0].length;
 
-        // compute the area by exploring a 4-connected 1s
-        int area = 1;
-        for (int[] d : DIRECTIONS) {
-            area += dfs(grid, row + d[0], col + d[1], labels, label);
+            // mark (row, col) as visited
+            grid[row][col] = islandId;
+            var area = 1;
+            for (var d : DIRS) {
+                var nextRow = row + d[0];
+                var nextCol = col + d[1];
+
+                if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n) {
+                    continue;
+                }
+
+                if (grid[nextRow][nextCol] == 1) {
+                    area += traverse(grid, nextRow, nextCol, islandId);
+                }
+            }
+            return area;
         }
-        return area;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MakingLargeIslandTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MakingLargeIslandTest.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MakingLargeIsland.MakingLargeIslandRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MakingLargeIslandTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     intArrayOf(1, 0),
@@ -53,13 +58,21 @@ internal class MakingLargeIslandTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should return the size of the largest island in grid after changing ot most one 0 to 1`(
-        grid: Array<IntArray>,
-        expected: Int
-    ) {
-        val actual = MakingLargeIsland().largestIsland(grid)
-        assertEquals(expected, actual)
+    @Nested
+    inner class MakingLargeIslandRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the size of the largest island in grid after changing ot most one 0 to 1`(
+            grid: Array<IntArray>,
+            expected: Int
+        ) {
+            MakingLargeIslandRev1().test(grid, expected)
+        }
     }
+}
+
+private fun MakingLargeIsland.test(grid: Array<IntArray>, expected: Int) {
+    val actual = largestIsland(grid)
+    assertEquals(expected, actual)
 }
