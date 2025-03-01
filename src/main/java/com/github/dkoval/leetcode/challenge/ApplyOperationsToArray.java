f1364a79b5867bf0296431e9ded36254f9afa13c
dkoval <koval.dmitriy@gmail.com>
2025-03-01 16:28:18 +0100

    Solution to "Apply Operations to the Array" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/ApplyOperationsToArray.java
@@ -0,0 +1,60 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/apply-operations-to-the-array/">Apply Operations to the Array</a>
+ * <p>
+ * You are given a 0-indexed array nums of size n consisting of non-negative integers.
+ * <p>
+ * You need to apply n - 1 operations to this array where, in the ith operation (0-indexed), you will apply the following on the ith element of nums:
+ * <p>
+ * If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
+ * After performing all the operations, shift all the 0's to the end of the array.
+ * <p>
+ * For example, the array [1,0,2,0,0,1] after shifting all its 0's to the end, is [1,2,1,0,0,0].
+ * <p>
+ * Return the resulting array.
+ * <p>
+ * Note that the operations are applied sequentially, not all at once.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= nums.length <= 2000</li>
+ *  <li>0 <= nums[i] <= 1000</li>
+ * </ul>
+ */
+public interface ApplyOperationsToArray {
+
+    int[] applyOperations(int[] nums);
+
+    class ApplyOperationsToArrayRev1 implements ApplyOperationsToArray {
+
+        @Override
+        public int[] applyOperations(int[] nums) {
+            final var n = nums.length;
+
+            for (var i = 0; i < n - 1; i++) {
+                if (nums[i] == nums[i + 1]) {
+                    nums[i] *= 2;
+                    nums[i + 1] = 0;
+                }
+            }
+
+            var zeroIndex = 0;
+            while (zeroIndex < n && nums[zeroIndex] != 0) {
+                zeroIndex++;
+            }
+
+            var i = zeroIndex + 1;
+            while (i < n) {
+                if (nums[i] != 0) {
+                    nums[zeroIndex] = nums[i];
+                    nums[i] = 0;
+                    zeroIndex++;
+                }
+                i++;
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ApplyOperationsToArrayTest.kt
@@ -0,0 +1,46 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.ApplyOperationsToArray.ApplyOperationsToArrayRev1
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class ApplyOperationsToArrayTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments?>? = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 2, 1, 1, 0),
+                intArrayOf(1, 4, 2, 0, 0, 0)
+            ),
+            Arguments.of(
+                intArrayOf(0, 1),
+                intArrayOf(1, 0)
+            )
+        )
+    }
+
+    @Nested
+    inner class ApplyOperationsToArrayRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the final array after performing all operations`(
+            nums: IntArray,
+            expected: IntArray
+        ) {
+            ApplyOperationsToArrayRev1().test(nums, expected)
+        }
+    }
+}
+
+private fun ApplyOperationsToArray.test(nums: IntArray, expected: IntArray) {
+    val actual = applyOperations(nums)
+    assertArrayEquals(expected, actual)
+}
