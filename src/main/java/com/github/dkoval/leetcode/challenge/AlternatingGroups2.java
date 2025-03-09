6aee0b42dd1c4b5ea7a8d34d75d933ca8c10edee
dkoval <koval.dmitriy@gmail.com>
2025-03-09 15:55:53 +0100

    Solution to " Alternating Groups II" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/AlternatingGroups2.java
@@ -0,0 +1,59 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/alternating-groups-ii/">Alternating Groups II</a>
+ * <p>
+ * There is a circle of red and blue tiles. You are given an array of integers colors and an integer k.
+ * The color of tile i is represented by colors[i]:
+ * <ul>
+ *  <li>colors[i] == 0 means that tile i is red.</li>
+ *  <li>colors[i] == 1 means that tile i is blue.</li>
+ * </ul>
+ * An alternating group is every k contiguous tiles in the circle with alternating colors
+ * (each tile in the group except the first and last one has a different color from its left and right tiles).
+ * <p>
+ * Return the number of alternating groups.
+ * <p>
+ * Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>3 <= colors.length <= 10^5</li>
+ *  <li>0 <= colors[i] <= 1</li>
+ *  <li>3 <= k <= colors.length</li>
+ * </ul>
+ */
+public interface AlternatingGroups2 {
+
+    int numberOfAlternatingGroups(int[] colors, int k);
+
+    class AlternatingGroups2Rev1 implements AlternatingGroups2 {
+
+        @Override
+        public int numberOfAlternatingGroups(int[] colors, int k) {
+            final var n = colors.length;
+
+            // Idea: sliding window.
+            // Trivia fact: if the starting index of a group is (n - 1) we want to
+            // take (k - 1) firsts elements for the group to be of size k.
+            var count = 0;
+            var left = 0;
+            for (var right = 1; right < n + k - 1; right++) {
+                if (colors[right % n] == colors[(right - 1) % n]) {
+                    // elements aren't alternating, hence reset the window
+                    left = right;
+                }
+
+                // is window too big?
+                if (right - left + 1 > k) {
+                    left++;
+                }
+
+                if (right - left + 1 == k) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/AlternatingGroups2Test.kt
@@ -0,0 +1,51 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.AlternatingGroups2.AlternatingGroups2Rev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class AlternatingGroups2Test {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(0, 1, 0, 1, 0),
+                3,
+                3
+            ),
+            Arguments.of(
+                intArrayOf(0, 1, 0, 0, 1, 0, 1),
+                6,
+                2
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 0, 1),
+                4,
+                0
+            )
+        )
+    }
+
+    @Nested
+    inner class AlternatingGroups2Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the number of alternating groups`(colors: IntArray, k: Int, expected: Int) {
+            AlternatingGroups2Rev1().test(colors, k, expected)
+
+        }
+    }
+}
+
+private fun AlternatingGroups2.test(colors: IntArray, k: Int, expected: Int) {
+    val actual = numberOfAlternatingGroups(colors, k)
+    assertEquals(expected, actual)
+}
