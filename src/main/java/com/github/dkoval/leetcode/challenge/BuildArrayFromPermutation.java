a269de648b20981b865d1b3b9da45a25f5e8de61
dkoval <koval.dmitriy@gmail.com>
2025-05-06 19:10:05 +0200

    Solution to "Build Array from Permutation" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/BuildArrayFromPermutation.java
@@ -0,0 +1,36 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/build-array-from-permutation/">Build Array from Permutation</a>
+ * <p>
+ * Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
+ * for each 0 <= i < nums.length and return it.
+ * <p>
+ * A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 1000</li>
+ *  <li>0 <= nums[i] < nums.length</li>
+ *  <li>The elements in nums are distinct</li>
+ * <ul>
+ * Follow-up: Can you solve it without using an extra space (i.e., O(1) memory)?
+ */
+public interface BuildArrayFromPermutation {
+
+    int[] buildArray(int[] nums);
+
+    class BuildArrayFromPermutationRev1 implements BuildArrayFromPermutation {
+
+        @Override
+        public int[] buildArray(int[] nums) {
+            final var n = nums.length;
+
+            final var ans = new int[n];
+            for (var i = 0; i < n; i++) {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/BuildArrayFromPermutationTest.kt
@@ -0,0 +1,43 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.BuildArrayFromPermutation.BuildArrayFromPermutationRev1
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class BuildArrayFromPermutationTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(0, 2, 1, 5, 3, 4),
+                intArrayOf(0, 1, 2, 4, 5, 3)
+            ),
+            Arguments.of(
+                intArrayOf(5, 0, 1, 2, 3, 4),
+                intArrayOf(4, 5, 0, 1, 2, 3)
+            )
+        )
+    }
+
+    @Nested
+    inner class BuildArrayFromPermutationTest {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should build array from permutation`(nums: IntArray, expected: IntArray) {
+            BuildArrayFromPermutationRev1().test(nums, expected)
+        }
+    }
+}
+
+private fun BuildArrayFromPermutation.test(nums: IntArray, expected: IntArray) {
+    val actual = buildArray(nums)
+    assertArrayEquals(expected, actual)
+}
