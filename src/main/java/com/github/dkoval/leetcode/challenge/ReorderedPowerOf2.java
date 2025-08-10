864ddd9a8912b46799b611d6c55be8354a0442f1
dkoval <koval.dmitriy@gmail.com>
2025-08-10 23:05:07 +0200

    Resolve "Reordered Power of 2" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ReorderedPowerOf2.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ReorderedPowerOf2.java
@@ -5,31 +5,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/590/week-3-march-15th-march-21st/3679/">Reordered Power of 2</a>
+ * <a href="https://leetcode.com/problems/reordered-power-of-2/">Reordered Power of 2</a>
  * <p>
- * Starting with a positive integer N, we reorder the digits in any order (including the original order) such
- * that the leading digit is not zero.
+ * You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
  * <p>
- * Return true if and only if we can do this in a way such that the resulting number is a power of 2.
+ * Return true if and only if we can do this so that the resulting number is a power of two.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 10^9
  */
-public class ReorderedPowerOf2 {
-    private final static Set<String> pow2 = new HashSet<>();
+public interface ReorderedPowerOf2 {
 
-    static {
-        for (int i = 0; i < 32; i++) {
-            String x = normalize(1 << i); // x = 2^i
-            pow2.add(x);
+    boolean reorderedPowerOf2(int n);
+
+    class ReorderedPowerOf2Rev1 implements ReorderedPowerOf2 {
+
+        private final static Set<String> pow2 = new HashSet<>();
+
+        static {
+            for (int i = 0; i < 32; i++) {
+                String x = normalize(1 << i); // x = 2^i
+                pow2.add(x);
+            }
         }
-    }
 
-    private static String normalize(int x) {
-        char[] chars = String.valueOf(x).toCharArray();
-        Arrays.sort(chars);
-        return String.valueOf(chars);
-    }
+        private static String normalize(int x) {
+            char[] chars = String.valueOf(x).toCharArray();
+            Arrays.sort(chars);
+            return String.valueOf(chars);
+        }
 
-    public boolean reorderedPowerOf2(int N) {
-        String x = normalize(N);
-        return pow2.contains(x);
+        @Override
+        public boolean reorderedPowerOf2(int n) {
+            String x = normalize(n);
+            return pow2.contains(x);
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/ReorderedPowerOf2Test.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ReorderedPowerOf2Test.kt
@@ -1,15 +1,20 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.ReorderedPowerOf2.ReorderedPowerOf2Rev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class ReorderedPowerOf2Test {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(1, true),
             Arguments.of(10, false),
             Arguments.of(16, true),
@@ -18,10 +23,18 @@ internal class ReorderedPowerOf2Test {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should check if N is reordered power of 2`(N: Int, expected: Boolean) {
-        val actual = ReorderedPowerOf2().reorderedPowerOf2(N)
-        assertEquals(expected, actual)
+    @Nested
+    inner class ReorderedPowerOf2Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should check if N is reordered power of 2`(n: Int, expected: Boolean) {
+            ReorderedPowerOf2Rev1().test(n, expected)
+        }
     }
+}
+
+private fun ReorderedPowerOf2.test(n: Int, expected: Boolean) {
+    val actual = reorderedPowerOf2(n)
+    assertEquals(expected, actual)
 }
