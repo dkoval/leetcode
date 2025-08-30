0b19278cba1d6bbef19b10b6fc90965976aa4331
dkoval <koval.dmitriy@gmail.com>
2025-08-31 00:09:31 +0200

    Resolve "Valid Sudoku" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/ValidSudoku.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/ValidSudoku.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/615/week-3-august-15th-august-21st/3904/">Valid Sudoku</a>
+ * <a href="https://leetcode.com/problems/valid-sudoku/">Valid Sudoku</a>
  * <p>
  * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
  * <ul>
@@ -18,44 +18,105 @@ import java.util.Set;
  *  <li>A Sudoku board (partially filled) could be valid but is not necessarily solvable.</li>
  *  <li>Only the filled cells need to be validated according to the mentioned rules.</li>
  * </ul>
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>board.length == 9</li>
+ *  <li>board[i].length == 9</li>
+ *  <li>board[i][j] is a digit 1-9 or '.'.</li>
+ * </ul>
  */
-public class ValidSudoku {
+public interface ValidSudoku {
 
-    private static final int N = 9;
-    private static final int M = 3;
+    boolean isValidSudoku(char[][] board);
 
-    // O(N^2) time | O(1) space
-    public boolean isValidSudoku(char[][] board) {
-        // check 9 rows, 9 columns and 9 squares 3x3
-        for (int i = 0; i < N; i++) {
-            boolean validRow = isValid(board, i, 0, i, N - 1);
-            boolean validCol = isValid(board, 0, i, N - 1, i);
+    class ValidSudokuRev1 implements ValidSudoku {
 
-            int fatRow = i / M;
-            int fatCol = i % M;
-            boolean validSquare = isValid(board, fatRow * M, fatCol * M, fatRow * M + M - 1 , fatCol * M + M - 1);
+        private static final int N = 9;
+        private static final int M = 3;
 
-            if (!validRow || !validCol || !validSquare) {
-                return false;
-            }
-        }
-        return true;
-    }
+        // O(N^2) time | O(1) space
+        @Override
+        public boolean isValidSudoku(char[][] board) {
+            // check 9 rows, 9 columns and 9 squares 3x3
+            for (int i = 0; i < N; i++) {
+                boolean validRow = isValid(board, i, 0, i, N - 1);
+                boolean validCol = isValid(board, 0, i, N - 1, i);
 
-    private boolean isValid(char[][] board, int startRow, int startCol, int endRow, int endCol) {
-        Set<Character> seen = new HashSet<>();
-        for (int row = startRow; row <= endRow; row++) {
-            for (int col = startCol; col <= endCol; col++) {
-                char c = board[row][col];
-                if (c == '.') {
-                    continue;
-                }
-                if (seen.contains(c)) {
+                int fatRow = i / M;
+                int fatCol = i % M;
+                boolean validSquare = isValid(board, fatRow * M, fatCol * M, fatRow * M + M - 1, fatCol * M + M - 1);
+
+                if (!validRow || !validCol || !validSquare) {
                     return false;
                 }
-                seen.add(c);
             }
+            return true;
         }
-        return true;
+
+        private boolean isValid(char[][] board, int startRow, int startCol, int endRow, int endCol) {
+            Set<Character> seen = new HashSet<>();
+            for (int row = startRow; row <= endRow; row++) {
+                for (int col = startCol; col <= endCol; col++) {
+                    char c = board[row][col];
+                    if (c == '.') {
+                        continue;
+                    }
+                    if (seen.contains(c)) {
+                        return false;
+                    }
+                    seen.add(c);
+                }
+            }
+            return true;
+        }
+    }
+
+    class ValidSudokuRev2 implements ValidSudoku {
+
+        @Override
+        public boolean isValidSudoku(char[][] board) {
+            // validate each row
+            for (var row = 0; row < 9; row++) {
+                if (!isValid(board, row, 0, row, 8)) {
+                    return false;
+                }
+            }
+
+            // validate each col
+            for (var col = 0; col < 9; col++) {
+                if (!isValid(board, 0, col, 8, col)) {
+                    return false;
+                }
+            }
+
+            // validate each of 3x3 boxes
+            for (var bigRow = 0; bigRow < 3; bigRow++) {
+                for (var bigCol = 0; bigCol < 3; bigCol++) {
+                    if (!isValid(board, bigRow * 3, bigCol * 3, bigRow * 3 + 2, bigCol * 3 + 2)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private boolean isValid(char[][] board, int startRow, int startCol, int endRow, int endCol) {
+            final var seen = new boolean[9];
+            for (var row = startRow; row <= endRow; row++) {
+                for (var col = startCol; col <= endCol; col++) {
+                    if (board[row][col] == '.') {
+                        continue;
+                    }
+
+                    final var x = board[row][col] - '0';
+                    if (seen[x - 1]) {
+                        return false;
+                    }
+                    seen[x - 1] = true;
+                }
+            }
+            return true;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/ValidSudokuTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/ValidSudokuTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.ValidSudoku.ValidSudokuRev1
+import com.github.dkoval.leetcode.challenge.ValidSudoku.ValidSudokuRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class ValidSudokuTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
@@ -41,10 +47,28 @@ internal class ValidSudokuTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should determine if a 9 x 9 sudoku board is valid`(board: Array<CharArray>, expected: Boolean) {
-        val actual = ValidSudoku().isValidSudoku(board)
-        assertEquals(expected, actual)
+    @Nested
+    inner class ValidSudokuRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should determine if a 9 x 9 sudoku board is valid`(board: Array<CharArray>, expected: Boolean) {
+            ValidSudokuRev1().test(board, expected)
+        }
     }
+
+    @Nested
+    inner class ValidSudokuRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should determine if a 9 x 9 sudoku board is valid`(board: Array<CharArray>, expected: Boolean) {
+            ValidSudokuRev2().test(board, expected)
+        }
+    }
+}
+
+private fun ValidSudoku.test(board: Array<CharArray>, expected: Boolean) {
+    val actual = isValidSudoku(board)
+    assertEquals(expected, actual)
 }
