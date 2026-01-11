0c86b54adf3b15ac3cf84c48f51a805b09781e34
dkoval <koval.dmitriy@gmail.com>
2026-01-11 16:20:18 +0100

    Resolve "Maximal Rectangle" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MaximalRectangle.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MaximalRectangle.java
@@ -11,61 +11,103 @@ import java.util.Stack;
  * <ul>
  *  <li>rows == matrix.length</li>
  *  <li>cols == matrix[i].length</li>
- *  <li>0 <= row, cols <= 200</li>
+ *  <li>1 <= row, cols <= 200</li>
  *  <li>matrix[i][j] is '0' or '1'</li>
  * </ul>
  */
-public class MaximalRectangle {
+public interface MaximalRectangle {
+
+    int maximalRectangle(char[][] matrix);
 
     // O(M * N) time | O(N) space
-    public int maximalRectangle(char[][] matrix) {
-        int m = matrix.length;
-        if (m == 0) {
-            return 0;
-        }
-        int n = matrix[0].length;
-        int maxArea = 0;
+    class MaximalRectangleRev1 implements MaximalRectangle {
 
-        // Given row i and column j, hist[j] is the height of histogram at index j at level.
-        // For example:
-        // 1   1     <- row 0, level 0
-        // 1   1 1 1 <- row 1, level 1
-        // 1 1 1 1 1 <- row 2, level 2
-        int[] hist = new int[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                hist[j] = matrix[i][j] == '1' ? hist[j] + 1 : 0;
+        @Override
+        public int maximalRectangle(char[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            int maxArea = 0;
+
+            // Given row i and column j, hist[j] is the height of histogram at index j at level.
+            // For example:
+            // 1   1     <- row 0, level 0
+            // 1   1 1 1 <- row 1, level 1
+            // 1 1 1 1 1 <- row 2, level 2
+            int[] hist = new int[n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    hist[j] = matrix[i][j] == '1' ? hist[j] + 1 : 0;
+                }
+                maxArea = Math.max(maxArea, areaOfLargestRectangleInHistogram(hist));
             }
-            maxArea = Math.max(maxArea, areaOfLargestRectangleInHistogram(hist));
+            return maxArea;
         }
-        return maxArea;
-    }
 
-    // Resource: https://www.youtube.com/watch?v=ZmnqCZp9bBs
-    // O(N) time | O(N) space
-    private int areaOfLargestRectangleInHistogram(int[] hist) {
-        int n = hist.length;
-        int maxArea = 0;
+        // Resource: https://www.youtube.com/watch?v=ZmnqCZp9bBs
+        // O(N) time | O(N) space
+        private int areaOfLargestRectangleInHistogram(int[] hist) {
+            int n = hist.length;
+            int maxArea = 0;
 
-        // mono stack: stores indices of hist[] such that for any pair of indices i < j, h[i] <= h[j]
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+            // mono stack: stores indices of hist[] such that for any pair of indices i < j, h[i] <= h[j]
+            Stack<Integer> stack = new Stack<>();
+            stack.push(0);
 
-        for (int i = 1; i < n; i++) {
-            while (!stack.isEmpty() && hist[i] < hist[stack.peek()]) {
+            for (int i = 1; i < n; i++) {
+                while (!stack.isEmpty() && hist[i] < hist[stack.peek()]) {
+                    int height = hist[stack.pop()];
+                    int area = stack.isEmpty() ? height * i : height * (i - stack.peek() - 1);
+                    maxArea = Math.max(maxArea, area);
+                }
+                stack.push(i);
+            }
+
+            // at this stage, i == n
+            while (!stack.isEmpty()) {
                 int height = hist[stack.pop()];
-                int area = stack.isEmpty() ? height * i : height * (i - stack.peek() - 1);
+                int area = stack.isEmpty() ? height * n : height * (n - stack.peek() - 1);
                 maxArea = Math.max(maxArea, area);
             }
-            stack.push(i);
+            return maxArea;
+        }
+    }
+
+    class MaximalRectangleRev2 implements MaximalRectangle {
+
+        @Override
+        public int maximalRectangle(char[][] matrix) {
+            final var m = matrix.length;
+            final var n = matrix[0].length;
+
+            // pre-compute the prefix sum for each column
+            final var prefix = new int[m + 1][n];
+            for (var row = 0; row < m; row++) {
+                for (var col = 0; col < n; col++) {
+                    prefix[row + 1][col] = prefix[row][col] + (matrix[row][col] - '0');
+                }
+            }
+
+            var best = 0;
+            for (var topRow = 0; topRow < m; topRow++) {
+                for (var bottomRow = 0; bottomRow < m; bottomRow++) {
+                    final var height = bottomRow - topRow + 1;
+                    var width = 0;
+                    for (var col = 0; col < n; col++) {
+                        if (columnWithAllOnes(prefix, col, topRow, bottomRow)) {
+                            width++;
+                        } else {
+                            width = 0;
+                        }
+                        best = Math.max(best, height * width);
+                    }
+                }
+            }
+            return best;
         }
 
-        // at this stage, i == n
-        while (!stack.isEmpty()) {
-            int height = hist[stack.pop()];
-            int area = stack.isEmpty() ? height * n : height * (n - stack.peek() - 1);
-            maxArea = Math.max(maxArea, area);
+        private boolean columnWithAllOnes(int[][] prefix, int col, int topRow, int bottomRow) {
+            return prefix[bottomRow + 1][col] - prefix[topRow][col] == bottomRow - topRow + 1;
         }
-        return maxArea;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximalRectangleTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximalRectangleTest.kt
@@ -1,15 +1,21 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.MaximalRectangle.MaximalRectangleRev1
+import com.github.dkoval.leetcode.challenge.MaximalRectangle.MaximalRectangleRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class MaximalRectangleTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 arrayOf(
                     charArrayOf('1', '0', '1', '0', '0'),
@@ -18,10 +24,6 @@ internal class MaximalRectangleTest {
                     charArrayOf('1', '0', '0', '1', '0')
                 ),
                 6
-            ),
-            Arguments.of(
-                arrayOf<CharArray>(),
-                0
             ),
             Arguments.of(
                 arrayOf(
@@ -45,10 +47,34 @@ internal class MaximalRectangleTest {
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `find the largest rectangle containing only 1's and return its area`(matrix: Array<CharArray>, expected: Int) {
-        val actual = MaximalRectangle().maximalRectangle(matrix)
-        assertEquals(expected, actual)
+    @Nested
+    inner class MaximalRectangleRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `find the largest rectangle containing only 1's and return its area`(
+            matrix: Array<CharArray>,
+            expected: Int
+        ) {
+            MaximalRectangleRev1().test(matrix, expected)
+        }
     }
+
+    @Nested
+    inner class MaximalRectangleRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `find the largest rectangle containing only 1's and return its area`(
+            matrix: Array<CharArray>,
+            expected: Int
+        ) {
+            MaximalRectangleRev2().test(matrix, expected)
+        }
+    }
+}
+
+private fun MaximalRectangle.test(matrix: Array<CharArray>, expected: Int) {
+    val actual = maximalRectangle(matrix)
+    assertEquals(expected, actual)
 }
