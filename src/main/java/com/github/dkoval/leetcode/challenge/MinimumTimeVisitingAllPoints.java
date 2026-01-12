cb0c41c0213a5f6aa2de769bf39ea065ee4c1814
dkoval <koval.dmitriy@gmail.com>
2026-01-12 20:41:14 +0100

    Resolve "Minimum Time Visiting All Points" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/MinimumTimeVisitingAllPoints.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/MinimumTimeVisitingAllPoints.java
@@ -36,8 +36,9 @@ public interface MinimumTimeVisitingAllPoints {
 
         @Override
         public int minTimeToVisitAllPoints(int[][] points) {
-            int n = points.length;
-            int total = 0;
+            final var n = points.length;
+
+            var total = 0;
             for (int i = 1; i < n; i++) {
                 int dx = Math.abs(points[i][0] - points[i - 1][0]);
                 int dy = Math.abs(points[i][1] - points[i - 1][1]);
