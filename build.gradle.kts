31869b76744be7a5144f2c8c1ec2bcb60016845d
dkoval <koval.dmitriy@gmail.com>
2026-01-27 23:46:44 +0100

    Add JUnit Platform Launcher as a test runtime dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.14.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
