78ef565cc0ab59f030c68f0c31a5d9464f339203
dkoval <koval.dmitriy@gmail.com>
2026-01-27 23:30:23 +0100

    Remove unused test runtime dependency from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.14.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.14.2")
     testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
