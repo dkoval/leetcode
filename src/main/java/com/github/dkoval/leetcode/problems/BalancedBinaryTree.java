48a42c6b3e75e13a526c674e79691aae6d70df9c
dkoval <koval.dmitriy@gmail.com>
2026-02-08 19:33:51 +0100

    Resolve "Balanced Binary Tree" problem

--- src/main/java/com/github/dkoval/leetcode/problems/BalancedBinaryTree.java
+++ src/main/java/com/github/dkoval/leetcode/problems/BalancedBinaryTree.java
@@ -10,12 +10,18 @@ import com.github.dkoval.leetcode.TreeNode;
  * For this problem, a height-balanced binary tree is defined as:
  * <p>
  * a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>The number of nodes in the tree is in the range [0, 5000]</li>
+ *  <li>-10^4 <= Node.val <= 10^4</li>
+ * </ul>
  */
-public abstract class BalancedBinaryTreeJava {
+public interface BalancedBinaryTree {
 
-    public abstract boolean isBalanced(TreeNode root);
+    boolean isBalanced(TreeNode root);
 
-    public static class BalancedBinaryTreeNaiveJava extends BalancedBinaryTreeJava {
+    class BalancedBinaryTreeRev1 implements BalancedBinaryTree {
 
         // Resource: https://leetcode.com/problems/balanced-binary-tree/solution/
         // Time complexity: O(N^2).
@@ -26,22 +32,21 @@ public abstract class BalancedBinaryTreeJava {
                 return true;
             }
 
-            if (Math.abs(height(root.left) - height(root.right)) > 1) {
-                return false;
-            }
-
-            return isBalanced(root.left) && isBalanced(root.right);
+            return Math.abs(height(root.left) - height(root.right)) <= 1
+                    && isBalanced(root.left)
+                    && isBalanced(root.right);
         }
 
         private int height(TreeNode root) {
             if (root == null) {
                 return -1;
             }
+
             return 1 + Math.max(height(root.left), height(root.right));
         }
     }
 
-    public static class BalancedBinaryTreeRecursiveJava extends BalancedBinaryTreeJava {
+    class BalancedBinaryTreeRecursive implements BalancedBinaryTree {
 
         // Time complexity: O(N). For every subtree, we compute its height in constant time as well as compare the height of its children.
         // Space complexity: O(N). The recursion stack may go up to O(N) if the tree is unbalanced.
@@ -50,16 +55,9 @@ public abstract class BalancedBinaryTreeJava {
             return checkIfBalancedAndComputeHeight(root).isBalanced;
         }
 
-        private static class Answer {
+        private record Answer(boolean isBalanced, int height) {
+
             static final Answer NOT_BALANCED = new Answer(false, -1);
-
-            final boolean isBalanced;
-            final int height;
-
-            private Answer(boolean isBalanced, int height) {
-                this.isBalanced = isBalanced;
-                this.height = height;
-            }
         }
 
         private Answer checkIfBalancedAndComputeHeight(TreeNode root) {
--- src/test/kotlin/com/github/dkoval/leetcode/problems/BalancedBinaryTreeTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/problems/BalancedBinaryTreeTest.kt
@@ -1,23 +1,28 @@
 package com.github.dkoval.leetcode.problems
 
 import com.github.dkoval.leetcode.TreeNode
-import com.github.dkoval.leetcode.problems.BalancedBinaryTreeJava.BalancedBinaryTreeNaiveJava
-import com.github.dkoval.leetcode.problems.BalancedBinaryTreeJava.BalancedBinaryTreeRecursiveJava
+import com.github.dkoval.leetcode.problems.BalancedBinaryTree.BalancedBinaryTreeRecursive
+import com.github.dkoval.leetcode.problems.BalancedBinaryTree.BalancedBinaryTreeRev1
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
 import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
 import java.util.stream.Stream
 
 internal class BalancedBinaryTreeTest {
 
     class InputArgumentsProvider : ArgumentsProvider {
 
-        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
-            Arguments.of(
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            arguments(
                 TreeNode(3).apply {
                     left = TreeNode(9)
                     right = TreeNode(20).apply {
@@ -27,7 +32,7 @@ internal class BalancedBinaryTreeTest {
                 },
                 true
             ),
-            Arguments.of(
+            arguments(
                 TreeNode(1).apply {
                     left = TreeNode(2).apply {
                         left = TreeNode(3).apply {
@@ -44,12 +49,12 @@ internal class BalancedBinaryTreeTest {
     }
 
     @Nested
-    inner class BalancedBinaryTreeNaiveJavaTest {
+    inner class BalancedBinaryTreeRev1Test {
 
         @ParameterizedTest
         @ArgumentsSource(InputArgumentsProvider::class)
         fun `should check whether binary tree is balanced`(root: TreeNode?, expected: Boolean) {
-            BalancedBinaryTreeNaiveJava().test(root, expected)
+            BalancedBinaryTreeRev1().test(root, expected)
         }
     }
 
@@ -59,12 +64,12 @@ internal class BalancedBinaryTreeTest {
         @ParameterizedTest
         @ArgumentsSource(InputArgumentsProvider::class)
         fun `should check whether binary tree is balanced`(root: TreeNode?, expected: Boolean) {
-            BalancedBinaryTreeRecursiveJava().test(root, expected)
+            BalancedBinaryTreeRecursive().test(root, expected)
         }
     }
+}
 
-    private fun BalancedBinaryTreeJava.test(root: TreeNode?, expected: Boolean) {
-        val actual = isBalanced(root);
-        assertEquals(expected, actual)
-    }
+private fun BalancedBinaryTree.test(root: TreeNode?, expected: Boolean) {
+    val actual = isBalanced(root);
+    assertEquals(expected, actual)
 }
