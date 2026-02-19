d78468b9d3bcaa1a61b4aedfa3a0ec8e7b1cee76
dkoval <koval.dmitriy@gmail.com>
2026-02-19 22:17:23 +0100

    Resolve "Count Binary Substrings" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/CountBinarySubstrings.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/CountBinarySubstrings.java
@@ -1,42 +1,83 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/596/week-4-april-22nd-april-28th/3718/">Count Binary Substrings</a>
+ * <a href="https://leetcode.com/problems/count-binary-substrings/">Count Binary Substrings</a>
  * <p>
- * Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's,
+ * Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's,
  * and all the 0's and all the 1's in these substrings are grouped consecutively.
  * <p>
  * Substrings that occur multiple times are counted the number of times they occur.
  * <p>
- * Note:
+ * Constraints:
  * <ul>
- *  <li>s.length will be between 1 and 50,000.</li>
- *  <li>s will only consist of "0" or "1" characters.</li>
+ *  <li>1 <= s.length <= 105</li>
+ *  <li>s[i] is either '0' or '1'.</li>
  * </ul>
  */
-public class CountBinarySubstrings {
+public interface CountBinarySubstrings {
+
+    int countBinarySubstrings(String s);
 
     // O(N) time | O(1) space
-    public int countBinarySubstrings(String s) {
-        // Let group be a contiguous sequence of 0's (00...0) or 1's (11...1).
-        // Given two groups G1 = 000... and G2 = 11..., the number of substrings formed from concatenated G1 + G2 string
-        // that have the same number of 0's and 1's is N = min(G1.length, G2.length).
-        // Applying the above formula to all (G[i], G[i + 1]) pairs gives us the final count.
-        int prevGroupLength = -1;
-        int currGroupLength = 1;
-        int count = 0;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) != s.charAt(i - 1)) {
-                if (prevGroupLength != -1) {
-                    count += Math.min(prevGroupLength, currGroupLength);
+    class CountBinarySubstringsRev1 implements CountBinarySubstrings {
+
+        @Override
+        public int countBinarySubstrings(String s) {
+            // Let group be a contiguous sequence of 0's (00...0) or 1's (11...1).
+            // Given two groups G1 = 000... and G2 = 11..., the number of substrings formed from concatenated G1 + G2 string
+            // that have the same number of 0's and 1's is N = min(G1.length, G2.length).
+            // Applying the above formula to all (G[i], G[i + 1]) pairs gives us the final count.
+            int prevGroupLength = -1;
+            int currGroupLength = 1;
+            int count = 0;
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) != s.charAt(i - 1)) {
+                    if (prevGroupLength != -1) {
+                        count += Math.min(prevGroupLength, currGroupLength);
+                    }
+                    prevGroupLength = currGroupLength;
+                    currGroupLength = 1;
+                } else {
+                    currGroupLength++;
                 }
-                prevGroupLength = currGroupLength;
-                currGroupLength = 1;
-            } else {
-                currGroupLength++;
             }
+            count += (prevGroupLength > 0) ? Math.min(prevGroupLength, currGroupLength) : 0;
+            return count;
         }
-        count += (prevGroupLength > 0) ? Math.min(prevGroupLength, currGroupLength) : 0;
-        return count;
+    }
+
+    // O(N) time | O(1) space
+    class CountBinarySubstringsRev2 implements CountBinarySubstrings {
+
+        @Override
+        public int countBinarySubstrings(String s) {
+            final var n = s.length();
+
+
+            var total = 0;
+            var counts = new int[2];
+            var current = '#';
+            for (var i = 0; i < n; i++) {
+                if (current == '#' || s.charAt(i) == current) {
+                    if (current == '#') {
+                        current = s.charAt(i);
+                    }
+                    counts[s.charAt(i) - '0']++;
+                    continue;
+                }
+
+                // at this stage, at least 2 groups of 0's and 1's exist
+                if (counts[0] > 0 && counts[1] > 0) {
+                    total += Math.min(counts[0], counts[1]);
+                }
+
+                current = s.charAt(i);
+                counts[s.charAt(i) - '0'] = 1;
+            }
+
+            // handle the last 2 groups of 0's and 1's
+            total += Math.min(counts[0], counts[1]);
+            return total;
+        }
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/CountBinarySubstringsTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/CountBinarySubstringsTest.kt
@@ -1,33 +1,65 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.CountBinarySubstrings.CountBinarySubstringsRev1
+import com.github.dkoval.leetcode.challenge.CountBinarySubstrings.CountBinarySubstringsRev2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
 
 internal class CountBinarySubstringsTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
-            Arguments.of(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            arguments(
                 "00110011",
                 6
             ),
-            Arguments.of(
+            arguments(
                 "10101",
                 4
             )
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should count the number of non-empty substrings that have the same number of 0's and 1's`(
-        s: String,
-        expected: Int
-    ) {
-        val actual = CountBinarySubstrings().countBinarySubstrings(s)
-        assertEquals(expected, actual)
+    @Nested
+    inner class InputArgumentsProviderRev1 : ArgumentsProvider {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should count the number of non-empty substrings that have the same number of 0's and 1's`(
+            s: String,
+            expected: Int
+        ) {
+            CountBinarySubstringsRev1().test(s, expected)
+        }
     }
+
+    @Nested
+    inner class InputArgumentsProviderRev2 : ArgumentsProvider {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should count the number of non-empty substrings that have the same number of 0's and 1's`(
+            s: String,
+            expected: Int
+        ) {
+            CountBinarySubstringsRev2().test(s, expected)
+        }
+    }
+}
+
+private fun CountBinarySubstrings.test(s: String, expected: Int) {
+    val actual = countBinarySubstrings(s)
+    assertEquals(expected, actual)
 }
