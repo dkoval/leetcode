c9e094349f82f514608cfb8172d0275edf173df9
dkoval <koval.dmitriy@gmail.com>
2026-05-10 21:07:48 +0200

    Solution to "Maximum Number of Jumps to Reach the Last Index" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/MaximumNumberOfJumpsToReachLastIndex.java
@@ -0,0 +1,59 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/">Maximum Number of Jumps to Reach the Last Index</a>
+ * <p>
+ * You are given a 0-indexed array nums of n integers and an integer target.
+ * <p>
+ * You are initially positioned at index 0. In one step, you can jump from index i to any index j such that:
+ * <p>
+ * 0 <= i < j < n
+ * -target <= nums[j] - nums[i] <= target
+ * Return the maximum number of jumps you can make to reach index n - 1.
+ * <p>
+ * If there is no way to reach index n - 1, return -1.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= nums.length == n <= 1000</li>
+ *  <li>-10^9 <= nums[i] <= 10^9</li>
+ *  <li>0 <= target <= 2 * 10^9</li>
+ * </ul>
+ */
+public interface MaximumNumberOfJumpsToReachLastIndex {
+
+    int maximumJumps(int[] nums, int target);
+
+    class MaximumNumberOfJumpsToReachLastIndexRev1 implements MaximumNumberOfJumpsToReachLastIndex {
+
+        @Override
+        public int maximumJumps(int[] nums, int target) {
+            final var n = nums.length;
+            final var count = calc(nums, target, 0, new Integer[n]);
+            return (count >= 0) ? count : -1;
+        }
+
+        // the max number of steps you can make to reach (n - 1) from index
+        private int calc(int[] nums, int target, int index, Integer[] dp) {
+            final var n = nums.length;
+
+            // base case
+            if (index == n - 1) {
+                return 0;
+            }
+
+            // already solved?
+            if (dp[index] != null) {
+                return dp[index];
+            }
+
+            var count = 0;
+            for (var j = index + 1; j < n; j++) {
+                if (Math.abs(nums[j] - nums[index]) <= target) {
+                    count = Math.max(count, 1 + calc(nums, target, j, dp));
+                }
+            }
+            return dp[index] = (count > 0) ? count : Integer.MIN_VALUE;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/MaximumNumberOfJumpsToReachLastIndexTest.kt
@@ -0,0 +1,62 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.MaximumNumberOfJumpsToReachLastIndex.MaximumNumberOfJumpsToReachLastIndexRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
+
+internal class MaximumNumberOfJumpsToReachLastIndexTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 3, 6, 4, 1, 2),
+                0,
+                -1
+            ),
+            Arguments.of(
+                intArrayOf(1, 3, 6, 4, 1, 2),
+                3,
+                5
+            ),
+            Arguments.of(
+                intArrayOf(1, 3, 6, 4, 1, 2),
+                0,
+                -1
+            )
+        )
+    }
+
+    @Nested
+    inner class MaximumNumberOfJumpsToReachLastIndexRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the maximum number of jumps to reach the last index`(
+            nums: IntArray,
+            start: Int,
+            expected: Int
+        ) {
+            MaximumNumberOfJumpsToReachLastIndexRev1().test(nums, start, expected)
+        }
+    }
+}
+
+private fun MaximumNumberOfJumpsToReachLastIndex.test(
+    nums: IntArray,
+    start: Int,
+    expected: Int
+) {
+    val actual = maximumJumps(nums, start)
+    assertEquals(expected, actual)
+}
