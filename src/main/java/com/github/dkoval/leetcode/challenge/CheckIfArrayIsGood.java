802108ec63a8fcf367511ca2b8cef525b0ebf186
dkoval <koval.dmitriy@gmail.com>
2026-05-14 22:26:55 +0200

    Solution to "Check if Array is Good" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/CheckIfArrayIsGood.java
@@ -0,0 +1,46 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/check-if-array-is-good/">Check if Array is Good</a>
+ * <p>
+ * You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+ * <p>
+ * base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once,
+ * plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+ * <p>
+ * Return true if the given array is good, otherwise return false.
+ * <p>
+ * Note: A permutation of integers represents an arrangement of these numbers.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 100</li>
+ *  <li>1 <= num[i] <= 200</li>
+ * </ul>
+ */
+public interface CheckIfArrayIsGood {
+
+    boolean isGood(int[] nums);
+
+    class CheckIfArrayIsGoodRev1 implements CheckIfArrayIsGood {
+
+        @Override
+        public boolean isGood(int[] nums) {
+            final var n = nums.length;
+
+            if (n < 2) {
+                return false;
+            }
+
+            Arrays.sort(nums);
+            for (var i = 0; i < n - 2; i++) {
+                if (nums[i + 1] != nums[i] + 1) {
+                    return false;
+                }
+            }
+            return (nums[n - 1] == nums[n - 2]) && (nums[n - 1] == n - 1);
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/CheckIfArrayIsGoodTest.kt
@@ -0,0 +1,67 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.CheckIfArrayIsGood.CheckIfArrayIsGoodRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
+
+internal class CheckIfArrayIsGoodTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(2, 1, 3),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(1, 3, 3, 2),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(1, 1),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(3, 4, 4, 1, 2, 1),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(5, 7, 3, 1, 5, 2, 6, 4),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(1),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(9, 9),
+                false
+            )
+        )
+    }
+
+    @Nested
+    inner class CheckIfArrayIsGoodRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return true if the array is good or false otherwise`(nums: IntArray, expected: Boolean) {
+            CheckIfArrayIsGoodRev1().test(nums, expected)
+        }
+    }
+}
+
+private fun CheckIfArrayIsGood.test(nums: IntArray, expected: Boolean) {
+    val actual = isGood(nums)
+    assertEquals(expected, actual)
+}
