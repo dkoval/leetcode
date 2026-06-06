3952c6faa5a4b294293a2b2e21c50a5613c4b079
dkoval <koval.dmitriy@gmail.com>
2026-06-07 00:05:53 +0200

    Add tests for "Left and Right Sum Differences" problem

---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/LeftAndRightSumDifferencesTest.kt
@@ -0,0 +1,58 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.LeftAndRightSumDifferences.LeftAndRightSumDifferencesRev1
+import com.github.dkoval.leetcode.challenge.LeftAndRightSumDifferences.LeftAndRightSumDifferencesRev2
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
+
+internal class LeftAndRightSumDifferencesTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(10, 4, 8, 3),
+                intArrayOf(15, 1, 11, 22)
+            ),
+            Arguments.of(
+                intArrayOf(1),
+                intArrayOf(0)
+            )
+        )
+    }
+
+    @Nested
+    inner class LeftAndRightSumDifferencesRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return left and right sum differences`(nums: IntArray, expected: IntArray) {
+            LeftAndRightSumDifferencesRev1().test(nums, expected)
+        }
+    }
+
+    @Nested
+    inner class LeftAndRightSumDifferencesRev2Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return left and right sum differences`(nums: IntArray, expected: IntArray) {
+            LeftAndRightSumDifferencesRev2().test(nums, expected)
+        }
+    }
+}
+
+private fun LeftAndRightSumDifferences.test(nums: IntArray, expected: IntArray) {
+    val actual = leftRightDifference(nums)
+    assertArrayEquals(expected, actual)
+}
