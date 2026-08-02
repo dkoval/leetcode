d0d06147d2893e42f21c277cf5a151942a07c391
dkoval <koval.dmitriy@gmail.com>
2026-08-02 21:03:29 +0200

    Revise "Stone Game" problem

--- src/main/java/com/github/dkoval/leetcode/challenge/StoneGame.java
+++ src/main/java/com/github/dkoval/leetcode/challenge/StoneGame.java
@@ -1,50 +1,69 @@
 package com.github.dkoval.leetcode.challenge;
 
 /**
- * <a href="https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/">Stone Game</a>
+ * <a href="https://https://leetcode.com/problems/stone-game/">Stone Game</a>
  * <p>
- * Alex and Lee play a game with piles of stones.  There are an even number of piles arranged in a row,
+ * Alex and Lee play a game with piles of stones. There are an even number of piles arranged in a row,
  * and each pile has a positive integer number of stones piles[i].
  * <p>
- * The objective of the game is to end with the most stones.  The total number of stones is odd, so there are no ties.
+ * The objective of the game is to end with the most stones. The total number of stones is odd, so there are no ties.
  * <p>
  * Alex and Lee take turns, with Alex starting first.  Each turn, a player takes the entire pile of stones from either
  * the beginning or the end of the row. This continues until there are no more piles left,
  * at which point the person with the most stones wins.
  * <p>
  * Assuming Alex and Lee play optimally, return True if and only if Alex wins the game.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>2 <= piles.length <= 500</li>
+ *  <li>piles.length is even.</li>
+ *  <li>1 <= piles[i] <= 500</li>
+ *  <li>sum(piles[i]) is odd.</li>
+ * </ul>
  */
-public class StoneGame {
+public interface StoneGame {
+
+    boolean stoneGame(int[] piles);
 
     // O(N^2) time | O(N^2) space
-    public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        Integer[][] memo = new Integer[n][n];
-        // score1 + score2 = sum(piles)
-        // score1 - score2 = delta
-        // + --------------------------
-        // = 2 * score1 = sum(piles) + delta = score1 + score2 + delta
-        // score1 = score2 + delta, therefore for the 1st player to win, delta has to be > 0
-        return scoreDelta(piles, 0, n - 1, memo) > 0;
-    }
+    class StoneGameRev1 implements StoneGame {
 
-    // left index is in [0, N-1], right index is in [left, N - 1], which results in O(N^2) inputs.
-    // For each input, O(1) work is being done, therefore the overall time complexity is O(N^2).
-    private int scoreDelta(int[] piles, int left, int right, Integer[][] memo) {
-        if (left == right) {
-            return piles[left];
+        @Override
+        public boolean stoneGame(int[] piles) {
+            final var n = piles.length;
+
+            // Idea: DP top-down
+            // score1 + score2 = sum(piles)
+            // score1 - score2 = delta
+            // + --------------------------
+            // = 2 * score1 = sum(piles) + delta = score1 + score2 + delta
+            // score1 = score2 + delta, therefore for the 1st player to win, delta has to be > 0
+            return scoreDelta(piles, 0, n - 1, new Integer[n][n]) > 0;
         }
 
-        if (memo[left][right] != null) {
-            return memo[left][right];
+        // left index is in [0, N-1], right index is in [left, N - 1], which results in O(N^2) inputs.
+        // For each input, O(1) work is being done, therefore the overall time complexity is O(N^2).
+        private int scoreDelta(int[] piles, int left, int right, Integer[][] dp) {
+            if (left == right) {
+                return piles[left];
+            }
+
+            // already solved?
+            if (dp[left][right] != null) {
+                return dp[left][right];
+            }
+
+            // take the best score while considering left and right piles
+            int delta = Integer.MIN_VALUE;
+
+            // option 1: player takes piles[left]
+            delta = Math.max(delta, piles[left] - scoreDelta(piles, left + 1, right, dp));
+            // option 2: player takes piles[right]
+            delta = Math.max(delta, piles[right] - scoreDelta(piles, left, right - 1, dp));
+
+            // cache and return the result
+            return dp[left][right] = delta;
         }
-
-        // take the best score while considering left and right piles
-        int delta = Integer.MIN_VALUE;
-        delta = Math.max(delta, piles[left] - scoreDelta(piles, left + 1, right, memo));
-        delta = Math.max(delta, piles[right] - scoreDelta(piles, left, right - 1, memo));
-
-        memo[left][right] = delta;
-        return delta;
     }
 }
--- src/test/kotlin/com/github/dkoval/leetcode/challenge/StoneGameTest.kt
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/StoneGameTest.kt
@@ -1,26 +1,102 @@
 package com.github.dkoval.leetcode.challenge
 
+import com.github.dkoval.leetcode.challenge.StoneGame.StoneGameRev1
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
 
 internal class StoneGameTest {
 
-    companion object {
-        @JvmStatic
-        fun input(): List<Arguments> = listOf(
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
             Arguments.of(
                 intArrayOf(5, 3, 4, 5),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(3, 7, 2, 3),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(
+                    7,
+                    7,
+                    12,
+                    16,
+                    41,
+                    48,
+                    41,
+                    48,
+                    11,
+                    9,
+                    34,
+                    2,
+                    44,
+                    30,
+                    27,
+                    12,
+                    11,
+                    39,
+                    31,
+                    8,
+                    23,
+                    11,
+                    47,
+                    25,
+                    15,
+                    23,
+                    4,
+                    17,
+                    11,
+                    50,
+                    16,
+                    50,
+                    38,
+                    34,
+                    48,
+                    27,
+                    16,
+                    24,
+                    22,
+                    48,
+                    50,
+                    10,
+                    26,
+                    27,
+                    9,
+                    43,
+                    13,
+                    42,
+                    46,
+                    24
+                ),
                 true
             )
         )
     }
 
-    @ParameterizedTest
-    @MethodSource("input")
-    fun `should True if and only if the 1st player wins the game`(piles: IntArray, expected: Boolean) {
-        val actual = StoneGame().stoneGame(piles)
-        assertEquals(expected, actual)
+    @Nested
+    inner class StoneGameRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should True if and only if the 1st player wins the game`(piles: IntArray, expected: Boolean) {
+            StoneGameRev1().test(piles, expected)
+        }
     }
+}
+
+private fun StoneGame.test(piles: IntArray, expected: Boolean) {
+    val actual = stoneGame(piles)
+    assertEquals(expected, actual)
 }
