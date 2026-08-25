36d66d10e4d57fc6827f4398a100f91ff627663f
dkoval <koval.dmitriy@gmail.com>
2026-08-25 22:54:19 +0200

    Solution to "Smallest Missing Multiple of K" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/SmallestMissingMultipleOfK.java
@@ -0,0 +1,44 @@
+package com.github.dkoval.leetcode.challenge;
+
+import java.util.HashSet;
+
+/**
+ * <a href="https://leetcode.com/problems/smallest-missing-multiple-of-k/">Smallest Missing Multiple of K</a>
+ * <p>
+ * Given an integer array nums and an integer k, return the smallest positive multiple of k that is missing from nums.
+ * <p>
+ * A multiple of k is any positive integer divisible by k.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 100</li>
+ *  <li>1 <= nums[i] <= 100</li>
+ *  <li>1 <= k <= 100</li>
+ * </ul>
+ */
+public interface SmallestMissingMultipleOfK {
+
+    int missingMultiple(int[] nums, int k);
+
+    class SmallestMissingMultipleOfKRev1 implements SmallestMissingMultipleOfK {
+
+        @Override
+        public int missingMultiple(int[] nums, int k) {
+            var uniq = new HashSet<Integer>();
+            var max = Integer.MIN_VALUE;
+            for (var x : nums) {
+                uniq.add(x);
+                max = Math.max(max, x);
+            }
+
+            var missing = k;
+            while (missing <= max) {
+                if (!uniq.contains(missing)) {
+                    return missing;
+                }
+                missing += k;
+            }
+            return missing;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/SmallestMissingMultipleOfKTest.kt
@@ -0,0 +1,52 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.SmallestMissingMultipleOfK.SmallestMissingMultipleOfKRev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
+
+internal class SmallestMissingMultipleOfKTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(8, 2, 3, 4, 6),
+                2,
+                10
+            ),
+            Arguments.of(
+                intArrayOf(1, 4, 7, 10, 15),
+                5,
+                5
+            ),
+        )
+    }
+
+    @Nested
+    inner class SmallestMissingMultipleOfKRev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the smallest positive multiple of k that is missing from nums`(
+            nums: IntArray,
+            k: Int,
+            expected: Int
+        ) {
+            SmallestMissingMultipleOfKRev1().test(nums, k, expected)
+        }
+    }
+}
+
+private fun SmallestMissingMultipleOfK.test(nums: IntArray, k: Int, expected: Int) {
+    val actual = missingMultiple(nums, k)
+    assertEquals(expected, actual)
+}
