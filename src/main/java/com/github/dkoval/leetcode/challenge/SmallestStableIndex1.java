30813b4660a626b38e349146aca72bc3977d9d66
dkoval <koval.dmitriy@gmail.com>
2026-09-04 23:22:16 +0200

    Solution to "Smallest Stable Index I" problem

---
+++ src/main/java/com/github/dkoval/leetcode/challenge/SmallestStableIndex1.java
@@ -0,0 +1,52 @@
+package com.github.dkoval.leetcode.challenge;
+
+/**
+ * <a href="https://leetcode.com/problems/smallest-stable-index-i/">Smallest Stable Index I</a>
+ * <p>
+ * You are given an integer array nums of length n and an integer k.
+ * <p>
+ * For each index i, define its instability score as max(nums[0..i]) - min(nums[i..n - 1]).
+ * <p>
+ * In other words:
+ * <p>
+ * max(nums[0..i]) is the largest value among the elements from index 0 to index i.
+ * min(nums[i..n - 1]) is the smallest value among the elements from index i to index n - 1.
+ * An index i is called stable if its instability score is less than or equal to k.
+ * <p>
+ * Return the smallest stable index. If no such index exists, return -1.
+ * <p>
+ * Constraints:
+ * <ul>
+ *  <li>1 <= nums.length <= 100</li>
+ *  <li>0 <= nums[i] <= 10^9</li>
+ *  <li>0 <= k <= 10^9</li>
+ * </ul>
+ */
+public interface SmallestStableIndex1 {
+
+    int firstStableIndex(int[] nums, int k);
+
+    class SmallestStableIndex1Rev1 implements SmallestStableIndex1 {
+
+        @Override
+        public int firstStableIndex(int[] nums, int k) {
+            final var n = nums.length;
+
+            final var max = new int[n];
+            final var min = new int[n];
+            max[0] = nums[0];
+            min[n - 1] = nums[n - 1];
+            for (var i = 1; i < n; i++) {
+                max[i] = Math.max(nums[i], max[i - 1]);
+                min[n - i - 1] = Math.min(nums[n - 1 - i], min[n - i]);
+            }
+
+            for (var i = 0; i < n; i++) {
+                if (max[i] - min[i] <= k) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/dkoval/leetcode/challenge/SmallestStableIndex1Test.kt
@@ -0,0 +1,58 @@
+package com.github.dkoval.leetcode.challenge
+
+import com.github.dkoval.leetcode.challenge.SmallestStableIndex1.SmallestStableIndex1Rev1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
+import java.util.stream.Stream
+
+internal class SmallestStableIndex1Test {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+
+        override fun provideArguments(
+            parameters: ParameterDeclarations,
+            context: ExtensionContext
+        ): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(5, 0, 1, 4),
+                3,
+                3
+            ),
+            Arguments.of(
+                intArrayOf(3, 2, 1),
+                1,
+                -1
+            ),
+            Arguments.of(
+                intArrayOf(0),
+                0,
+                0
+            )
+        )
+    }
+
+    @Nested
+    inner class SmallestStableIndex1Rev1Test {
+
+        @ParameterizedTest
+        @ArgumentsSource(InputArgumentsProvider::class)
+        fun `should return the smallest stable index of the given array nums`(
+            nums: IntArray,
+            k: Int,
+            expected: Int
+        ) {
+            SmallestStableIndex1Rev1().test(nums, k, expected)
+        }
+    }
+}
+
+private fun SmallestStableIndex1.test(nums: IntArray, k: Int, expected: Int) {
+    val actual = firstStableIndex(nums, k)
+    assertEquals(expected, actual)
+}
